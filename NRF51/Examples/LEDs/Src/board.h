f78e0ca9deb886acc1071b7822b1d45941a89fc7
CIT\Manuel.Caballero <vszener@gmail.com>
2017-04-13 10:21:31 +0100

    Board header file added. It declares the components of the electronic board.

---
+++ NRF51/Examples/LEDs/Src/board.h
@@ -0,0 +1,7 @@
+
+
+/* LEDs */
+#define LED1    21     /*!<   LED1: P0.21   */
+#define LED2    22     /*!<   LED2: P0.22   */
+#define LED3    23     /*!<   LED3: P0.23   */
+#define LED4    24     /*!<   LED4: P0.24   */
