efc5c23bda50b8942a89b80be7a3fc6311efe809
CIT\Manuel.Caballero <vszener@gmail.com>
2017-05-05 15:58:15 +0100

    Variables header added.

---
+++ NRF51/Examples/Timer/Src/variables.h
@@ -0,0 +1,27 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        5/May/2017
+ * @version     5/May/2017   The ORIGIN
+ * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
+ *              ( SDK 1.1.0 ).
+ * @warning     NaN
+ */
+
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+#include "board.h"
+
+/* CONSTANTS */
+#define ON  1
+#define OFF 0
+
+
+/* VARIABLES */
+uint32_t changeLEDsSTATE;       /*!<   changeLEDsSTATE = ON:     [todo].
+                                       changeLEDsSTATE = OFF:    [todo]       */
