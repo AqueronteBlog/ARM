86e9b4725db0cbd25d21f9b5c5d9d78c6610a350
CIT\Manuel.Caballero <vszener@gmail.com>
2017-05-05 15:58:02 +0100

    Interrupts header added.

---
+++ NRF51/Examples/Timer/Src/interrupts.h
@@ -0,0 +1,23 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        5/May/2017
+ * @version     5/May/2017   The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ */
+
+#include "variables.h"
+
+
+/* INTERRUPT PRTOTYPES */
+void TIMER0_IRQHandler ( void );
+
+
+/* VARIABLES */
+extern uint32_t changeLEDsSTATE;
