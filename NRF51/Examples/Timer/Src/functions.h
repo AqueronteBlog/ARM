8f53f139c60102a9dfa58554f1a5f765c71b292b
CIT\Manuel.Caballero <vszener@gmail.com>
2017-05-05 15:57:46 +0100

    Function header.

---
+++ NRF51/Examples/Timer/Src/functions.h
@@ -0,0 +1,20 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        5/May/2017
+ * @version     5/May/2017   The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ */
+
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+#include "variables.h"
+
+/* FUNCTION PROTOTYPES */
+void conf_GPIO      ( void );
+void conf_Timer0    ( void );
