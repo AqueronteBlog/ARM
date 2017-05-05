df467fcd42dd41ff9f19f2a8e9a92341db1b80c2
CIT\Manuel.Caballero <vszener@gmail.com>
2017-05-05 15:44:50 +0100

    Timer project example created.

---
+++ NRF51/Examples/Timer/Src/main.c
@@ -0,0 +1,24 @@
+/*
+**
+**                           Main.c
+**
+**
+**********************************************************************/
+/*
+   Last committed:     $Revision: 00 $
+   Last changed by:    $Author: $
+   Last changed date:  $Date:  $
+   ID:                 $Id:  $
+
+**********************************************************************/
+
+#include "nrf.h"
+#include "ble.h"
+
+int main( void )
+{
+   while( 1 )
+   {
+
+   }
+}
