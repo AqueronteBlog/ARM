049374ddca16245ceeab82ad3d5bd149e4891704
AqueronteBlog <aqueronteblog@gmail.com>
2017-05-23 13:15:05 +0100

    New global variable added to work with Timer2 like a counter.

--- NRF51/Examples/Timer/Src/variables.h
+++ NRF51/Examples/Timer/Src/variables.h
@@ -7,7 +7,8 @@
  *
  * @author      Manuel Caballero
  * @date        5/May/2017
- * @version     5/May/2017   The ORIGIN
+ * @version     23/May/2017   my375msDelay variable added.
+ *              5/May/2017    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     NaN
@@ -24,4 +25,7 @@
 
 /* VARIABLES */
 uint32_t changeLEDsSTATE;       /*!<   changeLEDsSTATE = ON:     [todo].
-                                       changeLEDsSTATE = OFF:    [todo]       */
+                                       changeLEDsSTATE = OFF:    [todo]         */
+
+uint32_t my375msDelay;          /*!<   counter for the Timer2 to generate a
+                                       delay of 0.375s                         */
