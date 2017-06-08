36578fe7f14b7cd74eb6327460a91de2354880df
AqueronteBlog <aqueronteblog@gmail.com>
2017-06-08 16:30:23 +0100

    NRF51 RTC Example. Interrupt header

---
+++ NRF51/Examples/RTC/Src/interrupts.h
@@ -0,0 +1,25 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        8/June/2017
+ * @version     8/June/20175/May/2017    The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ */
+
+#include "variables.h"
+
+
+/* INTERRUPT PRTOTYPES */
+void RTC0_IRQHandler ( void );
+void RTC1_IRQHandler ( void );
+
+
+/* VARIABLES */
+extern uint32_t changeLEDsSTATE;
+extern uint32_t my375msDelay;
