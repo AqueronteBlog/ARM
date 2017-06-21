91398db1ab5288e1fe2ba0cafb29b8911287ec74
AqueronteBlog <aqueronteblog@gmail.com>
2017-06-21 13:21:56 +0100

    NRF51 TEMP Example. Variables to be used on this example.

--- NRF51/Examples/Temp/Src/variables.h
+++ NRF51/Examples/Temp/Src/variables.h
@@ -7,8 +7,8 @@
  *
  * @author      Manuel Caballero
  * @date        2/June/2017
- * @version     7/June/2017    Variables to handle the UART added.
- *              2/June/2017    The ORIGIN
+ * @version     21/June/2017    Variables to handle both the UART and the TEMP added.
+ *              2/June/2017     The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     NaN
@@ -22,15 +22,8 @@
 #define ON              1
 #define OFF             0
 
-#define YES             1
-#define NO              0
-
-
-#define TX_BUFF_SIZE    32
 
 
 /* VARIABLES */
-//uint8_t myMessage[ TX_BUFF_SIZE ];      /*!<   Message to be transmitted through the UART   */
-//uint8_t *myPtr;                         /*!<   Pointer to point out myMessage   */
-uint32_t myTEMP;                         /*!<   It will store the temperature data from the internal temperature sensor     */
-uint8_t  TX_inProgress;                  /*!<   It indicates if a transmission is in progress                               */
+uint32_t myTEMP;                         /*!<   It will store the temperature data from the internal temperature sensor             */
+uint8_t  dataToBeTX;                     /*!<   A counter. It indicates how many data it will be transmitted through the UART       */
