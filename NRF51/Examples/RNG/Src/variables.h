b90d368ee39623474e50e70ccf63ed898a91efdf
AqueronteBlog <aqueronteblog@gmail.com>
2017-06-22 11:08:19 +0100

    NRF51 RNG Example. Variables adapted to this example

--- NRF51/Examples/RNG/Src/variables.h
+++ NRF51/Examples/RNG/Src/variables.h
@@ -6,8 +6,8 @@
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        21/June/2017
- * @version     21/June/2017    The ORIGIN
+ * @date        22/June/2017
+ * @version     22/June/2017    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     NaN
@@ -18,8 +18,8 @@
 #include "board.h"
 
 /* CONSTANTS */
-#define ON              1
-#define OFF             0
+#define TX_ENABLE       1
+#define TX_DISABLE      0
 
 #define YES             1
 #define NO              0
@@ -27,6 +27,6 @@
 
 
 /* VARIABLES */
-uint8_t myRX_buff;                      /*!<   It collects the data from the UART   */
-uint8_t myRandomNumber;                 /*!<   It gets the random number   */
-uint8_t TX_inProgress;                  /*!<   It indicates if a transmission is in progress   */
+uint8_t myRX_buff;                      /*!<   It collects the data from the UART         */
+uint8_t myRandomNumber;                 /*!<   It gets the random number                  */
+
