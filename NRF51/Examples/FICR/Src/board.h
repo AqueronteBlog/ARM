9d4fd4a0f2b51991e06f42e9b8fa868db770d66e
AqueronteBlog <aqueronteblog@gmail.com>
2017-06-26 16:47:57 +0100

    NRF51 FICR Example. UARTpins definition added

--- NRF51/Examples/FICR/Src/board.h
+++ NRF51/Examples/FICR/Src/board.h
@@ -7,21 +7,27 @@
  *
  * @author      Manuel Caballero
  * @date        12/April/2017
- * @version     16/June/2017    Buttons definition added.
+ * @version     26/June/2017    Uart definition added.
+ *              16/June/2017    Buttons definition added.
  *              12/April/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN
  */
 
 /* LEDs */
-#define LED1    21     /*!<   LED1: P0.21   */
-#define LED2    22     /*!<   LED2: P0.22   */
-#define LED3    23     /*!<   LED3: P0.23   */
-#define LED4    24     /*!<   LED4: P0.24   */
+#define LED1        21     /*!<   LED1: P0.21   */
+#define LED2        22     /*!<   LED2: P0.22   */
+#define LED3        23     /*!<   LED3: P0.23   */
+#define LED4        24     /*!<   LED4: P0.24   */
 
 
 /* Buttons */
-#define BTN1    17     /*!<   Button 1: P0.17   */
-#define BTN2    18     /*!<   Button 2: P0.18   */
-#define BTN3    19     /*!<   Button 3: P0.19   */
-#define BTN4    20     /*!<   Button 4: P0.20   */
+#define BTN1        17     /*!<   Button 1: P0.17   */
+#define BTN2        18     /*!<   Button 2: P0.18   */
+#define BTN3        19     /*!<   Button 3: P0.19   */
+#define BTN4        20     /*!<   Button 4: P0.20   */
+
+
+/* UART0 */
+#define UART0_TX    9      /*!<   UART TX: P0.21   */
+#define UART0_RX    11     /*!<   UART RX: P0.22   */
