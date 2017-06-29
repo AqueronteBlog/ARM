f54065fed444401d61865d048fdeafe5d9333d8e
AqueronteBlog <aqueronteblog@gmail.com>
2017-06-29 10:11:56 +0100

    NRF51 SWI Example. Main code updated with the right resources to use

--- NRF51/Examples/SWI/Src/main.c
+++ NRF51/Examples/SWI/Src/main.c
@@ -1,21 +1,20 @@
 /**
  * @brief       main.c
- * @details     It changes the state of the LED1 and LED4 using the RTCs.
+ * @details     [TODO] XXX
  *
- *                  - LED1. RTC0: Tick, Interrupt ON.           Blinks every 125ms.
- *                  - LED4. RTC1: Channel 0, Interrupt ON.      Blinks every 2s.
  *
  *              This firmware is just an example about how to use the RTCs on the nrf51422.
  *
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        8/June/2017
- * @version     8/June/2017    The ORIGIN
+ * @date        29/June/2017
+ * @version     29/June/2017    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice
  *              is not used in this example anyway because of Bluetooth was not used.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
 #include "nrf.h"
@@ -28,11 +27,10 @@ int main( void )
 {
     conf_GPIO   ();
     conf_LFCLK  ();
+    conf_SWI    ();
     conf_RTC0   ();
-    conf_RTC1   ();
 
     NRF_RTC0->TASKS_START = 1;    // Start RTC0
-    NRF_RTC1->TASKS_START = 1;    // Start RTC1
 
     while( 1 )
     {
