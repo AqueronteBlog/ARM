a1260feaac9e4716efb7ace3e5841f7de30c97ef
AqueronteBlog <aqueronteblog@gmail.com>
2017-06-29 10:11:27 +0100

    NRF51 SWI Example. Variables for this example updated

--- NRF51/Examples/SWI/Src/variables.h
+++ NRF51/Examples/SWI/Src/variables.h
@@ -6,11 +6,12 @@
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        8/June/2017
- * @version     8/June/2017    The ORIGIN
+ * @date        29/June/2017
+ * @version     29/June/2017    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     NaN
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
 #include "nrf.h"
@@ -18,10 +19,11 @@
 #include "board.h"
 
 /* CONSTANTS */
-#define ON  1
-#define OFF 0
+#define LED1_ON  0x01
+#define LED2_ON  ( LED1_ON << 1 )
+#define LED3_ON  ( LED2_ON << 1 )
+#define LED4_ON  ( LED3_ON << 1 )
 
 
 /* VARIABLES */
-uint32_t changeLEDsSTATE;       /*!<   changeLEDsSTATE = ON:    Turn LED on
-                                       changeLEDsSTATE = OFF:   Turn LED off   */
+uint32_t chooseLEDON;       /*!<   it turns on the right LED   */
