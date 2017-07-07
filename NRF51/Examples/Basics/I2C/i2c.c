614b106468038d96a2b0f62b1965bb38b6b9ea47
AqueronteBlog <aqueronteblog@gmail.com>
2017-07-07 12:42:02 +0100

    NRF51 Basics. Right TWI0 object. Now I2C write works as expected. It would be better if we could pass the object by reference

--- NRF51/Examples/Basics/I2C/i2c.c
+++ NRF51/Examples/Basics/I2C/i2c.c
@@ -32,7 +32,7 @@
  * @author      Manuel Caballero
  * @date        6/July/2017
  * @version     6/July/2017   The ORIGIN
- * @pre         NaN
+ * @pre         I2C communication is by polling mode.
  * @warning     This function takes for granted that TWI0 is used.
  */
 uint32_t    i2c_write   ( uint8_t* i2c_buff, uint32_t i2c_data_length )
@@ -40,17 +40,20 @@ uint32_t    i2c_write   ( uint8_t* i2c_buff, uint32_t i2c_data_length )
     uint32_t    i           =   0;
     uint32_t    i2c_timeout =   0;
 
-
+// Start transmission
     for ( i = 0; i < i2c_data_length; i++ )
     {
-        NRF_TWI1->TXD              =   0x7E;
+        NRF_TWI0->TXD              =   *i2c_buff;
         NRF_TWI0->TASKS_STARTTX    =   1;
 
         i2c_timeout                =   232323;
-        while( ( NRF_TWI0->EVENTS_TXDSENT == 0 ) && ( --i2c_timeout ) );
-        NRF_TWI0->EVENTS_TXDSENT   =   0;
+        while( ( NRF_TWI0->EVENTS_TXDSENT == 0 ) && ( --i2c_timeout ) );        // Wait until the data is transmitted or timeout
+        NRF_TWI0->EVENTS_TXDSENT   =   0;                                       // reset flag
+
+        i2c_buff++;                                                             // Prepare next data to be transmitted
     }
 
+// Check if everything went fine
     if ( i2c_timeout < 1 )
         return I2C_FAILURE;
     else
