994decaddea7a316b204f4b9f568e772adeab6ab
AqueronteBlog <aqueronteblog@gmail.com>
2017-07-11 15:32:48 +0100

    NRF51 ADC Example. Variables for this example improved

--- NRF51/Examples/ADC/Src/variables.h
+++ NRF51/Examples/ADC/Src/variables.h
@@ -6,12 +6,12 @@
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        2/June/2017
- * @version     21/June/2017    Variables to handle both the UART and the TEMP added.
- *              2/June/2017     The ORIGIN
+ * @date        11/July/2017
+ * @version     11/July/2017     The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     NaN
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
 #include "nrf.h"
@@ -25,5 +25,4 @@
 
 
 /* VARIABLES */
-uint32_t myTEMP;                         /*!<   It will store the temperature data from the internal temperature sensor             */
-uint8_t  dataToBeTX;                     /*!<   A counter. It indicates how many data it will be transmitted through the UART       */
+uint8_t myADCvalue;                      /*!<   It will store the data from the ADC     */
