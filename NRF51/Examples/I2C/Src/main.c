1362f33095892151a3f436221bb42a190211acc7
AqueronteBlog <aqueronteblog@gmail.com>
2017-07-11 12:24:29 +0100

    NRF51 I2C Example. The code is completed, tested and working properly.

--- NRF51/Examples/I2C/Src/main.c
+++ NRF51/Examples/I2C/Src/main.c
@@ -1,7 +1,7 @@
 /**
  * @brief       main.c
  * @details     This project reads the humidity and the temperature from the external
- *              sensor: HTU21D.
+ *              sensor: HTU21D, and transmits the raw data through the UART.
  *
  *              This firmware is just an example about how to use the I2C on the nrf51422.
  *
@@ -9,7 +9,8 @@
  *
  * @author      Manuel Caballero
  * @date        30/June/2017
- * @version     10/July/2017    New features for the external sensor device added.
+ * @version     11/July/2017    UART is ready to transmit raw data.
+ *              10/July/2017    New features for the external sensor device added.
  *              30/June/2017    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
@@ -27,9 +28,13 @@
 
 int main( void )
 {
-    float    myTEMP =   0;
-    float    myRH   =   0;
-    uint32_t aux    =   0;
+    uint32_t ii             =       0;
+    // float    myTEMP         =       0;
+    // float    myRH           =       0;
+    uint8_t  myRawTemp[]    =       { 0, 0, 0 };
+    uint8_t  myRawRH[]      =       { 0, 0, 0 };
+    uint32_t aux            =       0;
+
 
     conf_GPIO   ();
     conf_UART   ();
@@ -38,10 +43,9 @@ int main( void )
 
 
     mySTATE                  =   0;                 // Reset counter
-    NRF_TWI0->ADDRESS        =   HTU21D_ADDR;       // HTU21D device address
 
     HTU21D_SoftReset ( NRF_TWI0, HTU21D_ADDR );
-    HTU21D_Init      ( NRF_TWI0, HTU21D_ADDR, HTU21D_MODE_NO_HOLD_MASTER, USER_REGISTER_RESOLUTION_12RH_14TEMP, USER_REGISTER_HEATER_DISABLED );
+    HTU21D_Init      ( NRF_TWI0, HTU21D_ADDR, HTU21D_MODE_NO_HOLD_MASTER, USER_REGISTER_RESOLUTION_11RH_11TEMP, USER_REGISTER_HEATER_DISABLED );
 
     NRF_TIMER0->TASKS_START  =   1;                 // Start Timer0
 
@@ -49,7 +53,7 @@ int main( void )
     while( 1 )
     {
         //NRF_POWER->SYSTEMOFF = 1;
-        NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
+        NRF_POWER->TASKS_LOWPWR = 1;                // Sub power mode: Low power.
 
         // Enter System ON sleep mode
 		__WFE();
@@ -58,22 +62,45 @@ int main( void )
 		__WFE();
 
 		switch ( mySTATE ){
+        default:
         case 1:
             aux = HTU21D_TriggerTemperature ( NRF_TWI0, HTU21D_ADDR );
             break;
 
         case 2:
-            aux = HTU21D_ReadTemperature ( NRF_TWI0, HTU21D_ADDR, &myTEMP );
-            aux = HTU21D_TriggerHumidity ( NRF_TWI0, HTU21D_ADDR );
+            // aux = HTU21D_ReadTemperature    ( NRF_TWI0, HTU21D_ADDR, &myTEMP );
+            aux = HTU21D_ReadRawTemperature ( NRF_TWI0, HTU21D_ADDR, &myRawTemp[0] );
+            aux = HTU21D_TriggerHumidity    ( NRF_TWI0, HTU21D_ADDR );
             break;
 
         case 3:
-            HTU21D_ReadHumidity ( NRF_TWI0, HTU21D_ADDR, &myRH );
-            __NOP();
-            mySTATE =   0;
-            break;
+            // aux = HTU21D_ReadHumidity       ( NRF_TWI0, HTU21D_ADDR, &myRH );
+            aux = HTU21D_ReadRawTemperature ( NRF_TWI0, HTU21D_ADDR, &myRawRH[0] );
 
-        default:
+            // Store Temperature & Humidity into the UART TX buffer
+            for ( ii = 0; ii < 3; ii++ )                            // Temperature + Checksum: 3 first bytes
+                TX_buff[ii]  =  myRawTemp[ii];
+
+            for ( ii = 3; ii < 6; ii++ )                            // Humidity + Checksum: The other 3 bytes
+                TX_buff[ii]  =  myRawRH[ii - 3];
+
+
+            // Start transmitting through the UART
+            NRF_GPIO->OUTCLR             =   ( 1UL << LED1 );       // Turn the LED1 on
+
+            myPtr                        =   &TX_buff[0];
+            TX_inProgress                =   YES;
+            NRF_UART0->TASKS_STARTTX     =   1;
+            NRF_UART0->TXD               =   *myPtr++;
+
+            // Wait until the message is transmitted
+            while ( TX_inProgress == YES ){
+                __WFE();
+                // Make sure any pending events are cleared
+                __SEV();
+                __WFE();
+            }
+
             mySTATE =   0;
             break;
 		}
