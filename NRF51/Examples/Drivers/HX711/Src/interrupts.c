d55398f67b56589cff43325246f40615773e1f06
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-14 14:04:33 +0100

    NRF51 HX711 Example. The project was just started ( MCP4725 was used as a template )

---
+++ NRF51/Examples/Drivers/HX711/Src/interrupts.c
@@ -0,0 +1,39 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        14/September/2017
+ * @version     14/September/2017    The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "interrupts.h"
+
+/**
+ * @brief       void TIMER0_IRQHandler ()
+ * @details     There is a new interrupt every 0.5s, increment the myState variable
+ *              so in the main can process that information.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        2/August/2017
+ * @version     2/August/2017   The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ */
+void TIMER0_IRQHandler()
+{
+    if ( ( NRF_TIMER0->EVENTS_COMPARE[0] != 0 ) && ( ( NRF_TIMER0->INTENSET & TIMER_INTENSET_COMPARE0_Msk ) != 0 ) )
+    {
+        mySTATE++;
+
+        NRF_TIMER0->EVENTS_COMPARE[0] = 0;                  // Clear ( flag ) compare register 0 event
+    }
+}
