5cf68b9feea3ea91b8e404e5ba1e861eb6164174
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-18 14:16:43 +0100

    NRF51 ADS1231 Example. Variables file was updated to this example.

--- NRF51/Examples/Drivers/ADS1231/Src/variables.h
+++ NRF51/Examples/Drivers/ADS1231/Src/variables.h
@@ -6,8 +6,8 @@
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        14/September/2017
- * @version     14/September/2017     The ORIGIN
+ * @date        18/September/2017
+ * @version     18/September/2017     The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     NaN
