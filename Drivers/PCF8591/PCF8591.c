ff1b893935d2ee95557a1bd143d67c30ca7b571f
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-22 11:35:40 +0100

    DRIVERS. PCF8591 for NRF51, ADC part is ready to be tested.

--- Drivers/PCF8591/PCF8591.c
+++ Drivers/PCF8591/PCF8591.c
@@ -1,379 +1,200 @@
 /**
- * @brief       MCP4725.c
- * @details     12-Bit Digital-to-Analog Converter with EEPROM Memory.
+ * @brief       PCF8591.c
+ * @details     8-bit A/D and D/A converter.
  *              Functions file.
  *
  *
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017    The ORIGIN
+ * @date        22/September/2017
+ * @version     22/September/2017    The ORIGIN
  * @pre         NaN.
  * @warning     NaN
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
- #include "MCP4725.h"
+#include "PCF8591.h"
 
 
 /**
- * @brief       MCP4725_Reset   ( NRF_TWI_Type* )
+ * @brief       PCF8591_SetADC ( NRF_TWI_Type* , PCF8591_address_t , PCF8591_analog_input_programming_t , PCF8591_auto_increment_status_t , PCF8591_channel_number_t )
  *
- * @details     It performs an internal reset similar to a
- *              power-on-reset ( POR ).
+ * @details     It configures the ADC.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
+ * @param[in]    myinstance:        Peripheral's Instance.
+ * @param[in]    myPCF8591Addr:     I2C Device address.
+ * @param[in]    myAnalogInputs:    The analog input programming.
+ * @param[in]    myAutoIncrement:   Auto-increment flag enabled/disabled.
+ * @param[in]    myADCchannel:      ADC Channel number.
  *
  * @param[out]   NaN.
  *
  *
- * @return       Status of MCP4725_Reset.
+ * @return       Status of PCF8591_SetADC.
  *
  *
  * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
+ * @date        22/September/2017
+ * @version     22/September/2017   The ORIGIN
  * @pre         NaN
- * @warning     The user MUST respect the time it takes this instruction to be
- *              performed ( max. 50ms ).
+ * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_Reset   ( NRF_TWI_Type* myinstance )
+PCF8591_status_t  PCF8591_SetADC ( NRF_TWI_Type* myinstance, PCF8591_address_t myPCF8591Addr, PCF8591_analog_input_programming_t myAnalogInputs, PCF8591_auto_increment_status_t myAutoIncrement, PCF8591_channel_number_t myADCchannel )
 {
-    uint8_t     cmd                 =    MCP4725_GENERAL_CALL_RESET;
+    uint8_t     cmd                 =    0;
     uint32_t    aux                 =    0;
 
 
-    aux = i2c_write ( myinstance, MCP4725_GENERAL_CALL, &cmd, 1, I2C_STOP_BIT );
+    // ANALOG INPUT PROGRAMMING
+    switch ( myAnalogInputs )
+    {
+    default:
+    case PCF8591_FOUR_SINGLE_ENDED_INPUTS:
+        // cmd  &=  0xCF;
+        break;
+
+    case PCF8591_THREE_DIFFERENTIAL_INPUTS:
+        cmd  |=  0x10;
+        break;
+
+    case PCF8591_SINGLE_ENDED_AND_DIFFERENTIAL_MIXED:
+        cmd  |=  0x20;
+        break;
+
+    case PCF8591_TWO_DIFFERENTIAL_INPUTS:
+        cmd  |=  0x30;
+        break;
+    }
+
+    _ANALOG_INPUT_PROGRAMMING    =   myAnalogInputs;
+
+
+    // AUTO-INCREMENT FLAG
+    if ( myAutoIncrement == 1 )
+        cmd  |=  0x04;
+
+
+    _AUTO_INCREMENT_STATUS   =   myAutoIncrement;
+
+
+
+    // A/D CHANNEL NUMBER
+    switch ( myADCchannel )
+    {
+    default:
+    case PCF8591_CHANNEL_0:
+        // cmd  &=  0xFC;
+        break;
+
+    case PCF8591_CHANNEL_1:
+        cmd  |=  0x01;
+        break;
+
+    case PCF8591_CHANNEL_2:
+        cmd  |=  0x02;
+        break;
+
+    case PCF8591_CHANNEL_3:
+        cmd  |=  0x03;
+        break;
+    }
+
+
+    _CHANNEL_NUMBER   =   myADCchannel;
+
+
+
+    // Update Control Byte
+    aux = i2c_write ( myinstance, myPCF8591Addr, &cmd, 1, I2C_STOP_BIT );
 
 
 
     if ( aux == I2C_SUCCESS )
-       return   MCP4725_SUCCESS;
+        return   PCF8591_SUCCESS;
     else
-       return   MCP4725_FAILURE;
+        return   PCF8591_FAILURE;
 }
 
 
 
 /**
- * @brief       MCP4725_WakeUp   ( NRF_TWI_Type* )
+ * @brief       PCF8591_ReadADC ( NRF_TWI_Type* myinstance, PCF8591_address_t myPCF8591Addr, PCF8591_vector_data_t* myADC_Data )
  *
- * @details     The power-down bits of the DAC register are set to a normal operation.
+ * @details     It gets the ADC result from the device.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
+ * @param[in]    myinstance:        Peripheral's Instance.
+ * @param[in]    myPCF8591Addr:     I2C Device address.
  *
- * @param[out]   NaN.
+ * @param[out]   myADC_Data:        ADC result into the chosen channel.
  *
  *
- * @return       Status of MCP4725_WakeUp.
+ * @return       Status of PCF8591_ReadADC.
  *
  *
  * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
+ * @date        22/September/2017
+ * @version     22/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_WakeUp   ( NRF_TWI_Type* myinstance )
+PCF8591_status_t  PCF8591_ReadADC ( NRF_TWI_Type* myinstance, PCF8591_address_t myPCF8591Addr, PCF8591_vector_data_t* myADC_Data )
 {
-    uint8_t     cmd                 =    MCP4725_GENERAL_CALL_WAKE_UP;
+    uint8_t     cmd[]               =    { 0, 0, 0, 0 };
     uint32_t    aux                 =    0;
+    uint32_t    myNumberReadings    =    0;
 
 
-    aux = i2c_write ( myinstance, MCP4725_GENERAL_CALL, &cmd, 1, I2C_STOP_BIT );
+    // Previously converted byte
+    aux = i2c_read ( myinstance, myPCF8591Addr, &cmd[0], 1 );
 
 
-
-    if ( aux == I2C_SUCCESS )
-       return   MCP4725_SUCCESS;
+    if ( _AUTO_INCREMENT_STATUS == PCF8591_AUTO_INCREMENT_ENABLED )
+        myNumberReadings    =   4;
     else
-       return   MCP4725_FAILURE;
-}
+        myNumberReadings    =   1;
 
 
-/**
- * @brief       MCP4725_PowerMode ( NRF_TWI_Type* , MCP4725_address_t , MCP4725_write_command_type_t , MCP4725_operation_mode_t )
- *
- * @details     It configures the power mode of the device.
- *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
- * @param[in]    myWriteCMD:            It writes the command into the DAC or EEPROM/DAC.
- * @param[in]    myPowerMode:           Normal Mode or one of the Power-Down available modes.
- *
- * @param[out]   NaN.
- *
- *
- * @return       Status of MCP4725_PowerMode.
- *
- *
- * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-MCP4725_status_t  MCP4725_PowerMode ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, MCP4725_operation_mode_t myPowerMode )
-{
-    uint8_t     cmd[]             =    { 0, 0, 0, 0, 0, 0 };
-    uint32_t    aux               =    0;
-    uint32_t    dataTX            =    3;
-
-
-    // Read the device to mask the default value
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
+    aux = i2c_read ( myinstance, myPCF8591Addr, &cmd[0], myNumberReadings );
 
 
 
-    // Choose the power mode
-    switch ( myPowerMode ){
-        default:
-        case NORMAL_MODE:
-                cmd[0]   =  0;
-                break;
+    switch ( _CHANNEL_NUMBER )
+    {
+    default:
+    case PCF8591_CHANNEL_0:
+        myADC_Data->ADC_Channel_0    =   cmd[ 0 ];
+        myADC_Data->ADC_Channel_1    =   cmd[ 1 ];
+        myADC_Data->ADC_Channel_2    =   cmd[ 2 ];
+        myADC_Data->ADC_Channel_3    =   cmd[ 3 ];
+        break;
 
-        case POWER_DOWN_1KOHM_RESISTIVE_LOAD_MODE:
-                cmd[0]   =  0x01;
-                break;
+    case PCF8591_CHANNEL_1:
+        myADC_Data->ADC_Channel_1    =   cmd[ 0 ];
+        myADC_Data->ADC_Channel_2    =   cmd[ 1 ];
+        myADC_Data->ADC_Channel_3    =   cmd[ 2 ];
+        myADC_Data->ADC_Channel_0    =   cmd[ 3 ];
+        break;
 
-        case POWER_DOWN_100KOHM_RESISTIVE_LOAD_MODE:
-                cmd[0]   =  0x02;
-                break;
+    case PCF8591_CHANNEL_2:
+        myADC_Data->ADC_Channel_2    =   cmd[ 0 ];
+        myADC_Data->ADC_Channel_3    =   cmd[ 1 ];
+        myADC_Data->ADC_Channel_0    =   cmd[ 2 ];
+        myADC_Data->ADC_Channel_1    =   cmd[ 3 ];
+        break;
 
-        case POWER_DOWN_500KOHM_RESISTIVE_LOAD_MODE:
-                cmd[0]   =  0x03;
-                break;
-    }
-
-
-    // Prepare the data according to the write mode
-    switch ( myWriteCMD ){
-        default:
-        case FAST_MODE:
-                cmd[0] <<=  4;
-                cmd[0]  |=  ( ( cmd[1] & 0xF0 ) >> 4 );
-                cmd[1]   =  ( ( cmd[1] & 0x0F ) << 4 );
-                cmd[1]  |=  ( ( cmd[2] & 0xF0 ) >> 4 );
-
-                dataTX   =   2;
-                break;
-
-        case WRITE_DAC_REGISTER_MODE:
-                cmd[0] <<=  1;
-                cmd[0]  |=  0x40;
-                break;
-
-        case WRITE_DAC_AND_EEPROM_REGISTER_MODE:
-                cmd[0] <<=  1;
-                cmd[0]  |=  0x60;
-                break;
+    case PCF8591_CHANNEL_3:
+        myADC_Data->ADC_Channel_3    =   cmd[ 0 ];
+        myADC_Data->ADC_Channel_0    =   cmd[ 1 ];
+        myADC_Data->ADC_Channel_1    =   cmd[ 2 ];
+        myADC_Data->ADC_Channel_2    =   cmd[ 3 ];
+        break;
     }
 
 
 
-    aux = i2c_write ( myinstance, ADDR, &cmd[0], dataTX, I2C_STOP_BIT );
-
-
-
     if ( aux == I2C_SUCCESS )
-       return   MCP4725_SUCCESS;
+        return   PCF8591_SUCCESS;
     else
-       return   MCP4725_FAILURE;
-}
-
-
-
-/**
- * @brief       MCP4725_SetNewValue ( NRF_TWI_Type* , MCP4725_address_t , MCP4725_write_command_type_t , Vector_new_dac_value_t )
- *
- * @details     It sends a new output value.
- *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
- * @param[in]    myWriteCMD:            It writes the command into the DAC or EEPROM/DAC.
- * @param[in]    myDACNewValue:         New output value.
- *
- * @param[out]   NaN.
- *
- *
- * @return       Status of MCP4725_SetNewValue.
- *
- *
- * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-MCP4725_status_t  MCP4725_SetNewValue   ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, Vector_new_dac_value_t myDACNewValue )
-{
-    uint8_t     cmd[]             =    { 0, 0, 0 };
-    uint32_t    aux               =    0;
-    uint32_t    dataTX            =    3;
-
-
-    // 12-Bit of resolution ONLY!
-    if ( myDACNewValue.DAC_New_Value > 4095 )
-        return  MCP4725_FAILURE;
-
-
-    // Prepare the data according to the write mode
-    cmd[1]  |=  ( ( myDACNewValue.DAC_New_Value & 0xFF0 ) >> 4 );
-    cmd[2]  |=  ( ( myDACNewValue.DAC_New_Value & 0x00F ) << 4 );
-
-    switch ( myWriteCMD ){
-        default:
-        case FAST_MODE:
-                cmd[0]  |=  ( ( myDACNewValue.DAC_New_Value & 0xF00 ) >> 8 );
-                cmd[1]   =  ( myDACNewValue.DAC_New_Value & 0x0FF );
-
-                dataTX   =   2;
-                break;
-
-        case WRITE_DAC_REGISTER_MODE:
-                cmd[0]  |=  0x40;
-                break;
-
-        case WRITE_DAC_AND_EEPROM_REGISTER_MODE:
-                cmd[0]  |=  0x60;
-                break;
-    }
-
-
-
-    aux = i2c_write ( myinstance, ADDR, &cmd[0], dataTX, I2C_STOP_BIT );
-
-
-
-    if ( aux == I2C_SUCCESS )
-       return   MCP4725_SUCCESS;
-    else
-       return   MCP4725_FAILURE;
-}
-
-
-
-/**
- * @brief       MCP4725_EEPROM_Status ( NRF_TWI_Type* , MCP4725_address_t , MCP4725_eeprom_status_t )
- *
- * @details     It gets the eeprom status.
- *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
- * @param[in]    myEEPROM_Status:       EEPROM status.
- *
- * @param[out]   NaN.
- *
- *
- * @return       Status of MCP4725_EEPROM_Status.
- *
- *
- * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-MCP4725_status_t  MCP4725_EEPROM_Status ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_eeprom_status_t* myEEPROM_Status )
-{
-    uint8_t     cmd[]             =    { 0, 0, 0, 0, 0 };
-    uint32_t    aux               =    0;
-
-    // Read command
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
-
-    // Update EEPROM status
-    *myEEPROM_Status =   ( ( cmd[0] & 0x80 ) >> 7 );
-
-
-
-    if ( aux == I2C_SUCCESS )
-       return   MCP4725_SUCCESS;
-    else
-       return   MCP4725_FAILURE;
-}
-
-
-
-/**
- * @brief       MCP4725_GetEEPROM_Data ( NRF_TWI_Type* , MCP4725_address_t , Vector_data_t* )
- *
- * @details     It gets the eeprom value.
- *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
- * @param[in]    myEEPROMData:          EEPROM value.
- *
- * @param[out]   NaN.
- *
- *
- * @return       Status of MCP4725_GetEEPROM_Data.
- *
- *
- * @author      Manuel Caballero
- * @date        8/September/2017
- * @version     8/September/2017   The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-MCP4725_status_t  MCP4725_GetEEPROM_Data    ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myEEPROMData )
-{
-    uint8_t     cmd[]             =    { 0, 0, 0, 0, 0 };
-    uint32_t    aux               =    0;
-
-    // Read command
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
-
-    // Read EEPROM value
-    myEEPROMData->EEPROM_Data =   ( ( cmd[3] & 0x0F ) << 8 ) | ( cmd[4] );
-
-
-
-    if ( aux == I2C_SUCCESS )
-       return   MCP4725_SUCCESS;
-    else
-       return   MCP4725_FAILURE;
-}
-
-
-
-/**
- * @brief       MCP4725_GetDAC_Data ( NRF_TWI_Type* , MCP4725_address_t , Vector_data_t* )
- *
- * @details     It gets the DAC value.
- *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
- * @param[in]    myDACData:             DAC value.
- *
- * @param[out]   NaN.
- *
- *
- * @return       Status of MCP4725_GetDAC_Data.
- *
- *
- * @author      Manuel Caballero
- * @date        8/September/2017
- * @version     8/September/2017   The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-MCP4725_status_t  MCP4725_GetDAC_Data    ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myDACData )
-{
-    uint8_t     cmd[]             =    { 0, 0, 0, 0, 0 };
-    uint32_t    aux               =    0;
-
-    // Read command
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
-
-    // Read DAC value
-    myDACData->DAC_Data =   ( ( cmd[1] & 0xF0 ) << 4 ) | ( ( ( ( cmd[1] & 0x0F ) >> 4 ) | ( cmd[2] & 0xF0 ) >> 4 ) );
-
-
-
-    if ( aux == I2C_SUCCESS )
-       return   MCP4725_SUCCESS;
-    else
-       return   MCP4725_FAILURE;
+        return   PCF8591_FAILURE;
 }
--- Drivers/PCF8591/PCF8591.h
+++ Drivers/PCF8591/PCF8591.h
@@ -1,14 +1,14 @@
 /**
- * @brief       MCP4725.h
- * @details     12-Bit Digital-to-Analog Converter with EEPROM Memory.
+ * @brief       PCF8591.h
+ * @details     8-bit A/D and D/A converter.
  *              Header file.
  *
  *
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017    The ORIGIN
+ * @date        22/September/2017
+ * @version     22/September/2017    The ORIGIN
  * @pre         NaN.
  * @warning     NaN
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
@@ -21,87 +21,102 @@
 
 
 /**
-  * @brief   DEFAULT ADDRESSES ( NOTE1: The A2 and A1 are programmed to '00' (default), if not requested by customer.
-  *                              NOTE2: On my board, the A2 and A1 are programmed to '01'. )
+  * @brief   DEFAULT ADDRESSES
   */
-typedef enum{
-    MCP4725_ADDRESS_LOW     =   0x62,                   /*!<   A0 pin ties to GND                                            */
-    MCP4725_ADDRESS_HIGH    =   0x63                    /*!<   A0 pin ties to VDD                                            */
-} MCP4725_address_t;
+typedef enum
+{
+    PCF8591_ADDRESS_0     =   0x48,                   /*!<   A2 A1 A0: 000                                            */
+    PCF8591_ADDRESS_1     =   0x49,                   /*!<   A2 A1 A0: 001                                            */
+    PCF8591_ADDRESS_2     =   0x4A,                   /*!<   A2 A1 A0: 010                                            */
+    PCF8591_ADDRESS_3     =   0x4B,                   /*!<   A2 A1 A0: 011                                            */
+    PCF8591_ADDRESS_4     =   0x4C,                   /*!<   A2 A1 A0: 100                                            */
+    PCF8591_ADDRESS_5     =   0x4D,                   /*!<   A2 A1 A0: 101                                            */
+    PCF8591_ADDRESS_6     =   0x4E,                   /*!<   A2 A1 A0: 110                                            */
+    PCF8591_ADDRESS_7     =   0x4F                    /*!<   A2 A1 A0: 111                                            */
+} PCF8591_address_t;
 
 
+// CONTROL BYTE
 /**
-  * @brief   COMMANDS
+  * @brief   ANALOG OUTPUT ENABLE FLAG
   */
-#define MCP4725_GENERAL_CALL             0x00              /*!<   The MCP4725 device acknowledges the general call address                             */
-
-/* General Call Commands */
-/**
-  * @brief   GENERAL CALL COMMANDS
-  */
-#define MCP4725_GENERAL_CALL_RESET       0x06              /*!<  Perform an internal reset similar to a power-on-reset (POR).                          */
-#define MCP4725_GENERAL_CALL_WAKE_UP     0x09              /*!<  The power-down bits of the DAC register are set to a normal operation.                */
-
-
-
-
-
-/* Commands Registers */
-/**
-  * @brief   WRITE COMMAND TYPE
-  */
-typedef enum{
-    FAST_MODE                              =   0,           /*!<  This command is used to change the DAC register. EEPROM is not affected.              */
-    WRITE_DAC_REGISTER_MODE                =   1,           /*!<  Load configuration bits and data code to the DAC Register.                            */
-    WRITE_DAC_AND_EEPROM_REGISTER_MODE     =   2            /*!<  Load configuration bits and data code to the DAC Register and also write the EEPROM.  */
-} MCP4725_write_command_type_t;
+typedef enum
+{
+    PCF8591_DAC_ENABLED        =   1,                                /*!<  PCF8591 DAC is enabled.                                */
+    PCF8591_DAC_DISABLE        =   0                                 /*!<  PCF8591 DAC is disabled                                */
+} PCF8591_dac_status_t;
 
 
 
 /**
-  * @brief   POWER-DOWN MODE
+  * @brief   ANALOG INPUT PROGRAMMING
   */
-typedef enum{
-    NORMAL_MODE                                 =   0,      /*!<  Normal Mode.                                                                          */
-    POWER_DOWN_1KOHM_RESISTIVE_LOAD_MODE        =   1,      /*!<  Power-Down Mode. 1 k‎Ω resistor to ground.                                             */
-    POWER_DOWN_100KOHM_RESISTIVE_LOAD_MODE      =   2,      /*!<  Power-Down Mode. 100 k‎Ω resistor to ground.                                           */
-    POWER_DOWN_500KOHM_RESISTIVE_LOAD_MODE      =   3       /*!<  Power-Down Mode. 500 k‎Ω resistor to ground.                                           */
-} MCP4725_operation_mode_t;
+typedef enum
+{
+    PCF8591_FOUR_SINGLE_ENDED_INPUTS             =   0,              /*!<  PCF8591 Scale in kg.                                   */
+    PCF8591_THREE_DIFFERENTIAL_INPUTS            =   1,              /*!<  PCF8591 Scale in  g.                                   */
+    PCF8591_SINGLE_ENDED_AND_DIFFERENTIAL_MIXED  =   2,              /*!<  PCF8591 Scale in mg.                                   */
+    PCF8591_TWO_DIFFERENTIAL_INPUTS              =   3               /*!<  PCF8591 Scale in ug.                                   */
+} PCF8591_analog_input_programming_t;
 
 
 
 /**
-  * @brief   READY/#BUSY BIT
+  * @brief   AUTO-INCREMENT FLAG
   */
-typedef enum{
-    EEPROM_BUSY                                 =   0,      /*!<  EEPROM write is not completed.                                                        */
-    EEPROM_READY                                =   1       /*!<  EEPROM write is complete.                                                             */
-} MCP4725_eeprom_status_t;
+typedef enum
+{
+    PCF8591_AUTO_INCREMENT_ENABLED               =   1,              /*!<  PCF8591 Auto-increment is enabled.                     */
+    PCF8591_AUTO_INCREMENT_DISABLED              =   0               /*!<  PCF8591 Auto-increment is disabled                     */
+} PCF8591_auto_increment_status_t;
 
+
+
+/**
+  * @brief   A/D CHANNEL NUMBER
+  */
+typedef enum
+{
+    PCF8591_CHANNEL_0                            =   0,              /*!<  PCF8591 Channel 0 Active                               */
+    PCF8591_CHANNEL_1                            =   1,              /*!<  PCF8591 Channel 1 Active                               */
+    PCF8591_CHANNEL_2                            =   2,              /*!<  PCF8591 Channel 2 Active                               */
+    PCF8591_CHANNEL_3                            =   3               /*!<  PCF8591 Channel 3 Active                               */
+} PCF8591_channel_number_t;
 
 
 
 #ifndef VECTOR_STRUCT_H
 #define VECTOR_STRUCT_H
-typedef struct{
-    uint16_t EEPROM_Data;
-    uint16_t DAC_Data;
-} Vector_data_t;
+typedef struct
+{
+    uint8_t ADC_Channel_0;
+    uint8_t ADC_Channel_1;
+    uint8_t ADC_Channel_2;
+    uint8_t ADC_Channel_3;
+} PCF8591_vector_data_t;
 
-typedef struct{
+typedef struct
+{
     uint32_t DAC_New_Value;
-} Vector_new_dac_value_t;
+} PCF8591_vector_new_dac_value_t;
 #endif
 
+
+
+
+uint8_t _ANALOG_INPUT_PROGRAMMING;
+uint8_t _AUTO_INCREMENT_STATUS;
+uint8_t _CHANNEL_NUMBER;
 
 
 /**
   * @brief   INTERNAL CONSTANTS
   */
-typedef enum{
-    MCP4725_SUCCESS     =       0,
-    MCP4725_FAILURE     =       1
-} MCP4725_status_t;
+typedef enum
+{
+    PCF8591_SUCCESS     =       0,
+    PCF8591_FAILURE     =       1
+} PCF8591_status_t;
 
 
 
@@ -109,10 +124,6 @@ typedef enum{
 /**
   * @brief   FUNCTION PROTOTYPES
   */
-MCP4725_status_t  MCP4725_Reset                 ( NRF_TWI_Type* myinstance );
-MCP4725_status_t  MCP4725_WakeUp                ( NRF_TWI_Type* myinstance );
-MCP4725_status_t  MCP4725_PowerMode             ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, MCP4725_operation_mode_t myPowerMode );
-MCP4725_status_t  MCP4725_SetNewValue           ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, Vector_new_dac_value_t myDACNewValue );
-MCP4725_status_t  MCP4725_EEPROM_Status         ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_eeprom_status_t* myEEPROM_Status );
-MCP4725_status_t  MCP4725_GetEEPROM_Data        ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myEEPROMData );
-MCP4725_status_t  MCP4725_GetDAC_Data           ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myDACData );
+PCF8591_status_t  PCF8591_SetADC                ( NRF_TWI_Type* myinstance, PCF8591_address_t myPCF8591Addr, PCF8591_analog_input_programming_t myAnalogInputs, PCF8591_auto_increment_status_t myAutoIncrement, PCF8591_channel_number_t myADCchannel );
+PCF8591_status_t  PCF8591_ReadADC               ( NRF_TWI_Type* myinstance, PCF8591_address_t myPCF8591Addr, PCF8591_vector_data_t* myADC_Data );
+
