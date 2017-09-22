6301591083b8bccd562035f6d25ee6575956080d
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-22 11:36:37 +0100

    NRF51 PCF8591 Example. Minor changes, last compilation.

---
+++ NRF51/Examples/Drivers/PCF8591/obj/Debug/Src/main.d
@@ -0,0 +1,13 @@
+obj\debug\src\main.o: Src\main.c Inc/nrf.h Inc/nrf51.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cm0.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cminstr.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cmfunc.h \
+ Inc/system_nrf51.h Inc/nrf51_bitfields.h Inc/nrf51_deprecated.h \
+ Inc/compiler_abstraction.h softdevice\Include/ble.h \
+ softdevice\Include/ble_ranges.h softdevice\Include/ble_types.h \
+ softdevice\Include/ble_gap.h softdevice\Include/nrf_svc.h \
+ softdevice\Include/ble_l2cap.h softdevice\Include/ble_err.h \
+ softdevice\Include/nrf_error.h softdevice\Include/ble_gatt.h \
+ softdevice\Include/ble_gattc.h softdevice\Include/ble_gatts.h \
+ Src\variables.h Inc/nrf51_bitfields.h Src\board.h Src\functions.h \
+ ..\..\..\..\Drivers\PCF8591/PCF8591.h ..\..\Basics\I2C/i2c.h
---
+++ NRF51/Examples/Drivers/PCF8591/obj/Debug/~#/~#/~#/~#/Drivers/PCF8591/pcf8591.d
@@ -0,0 +1,9 @@
+obj\debug\~\#\~\#\~\#\~\#\drivers\pcf8591\pcf8591.o: \
+ ..\..\..\..\Drivers\PCF8591\PCF8591.c \
+ ..\..\..\..\Drivers\PCF8591\PCF8591.h ..\..\Basics\I2C/i2c.h Inc/nrf.h \
+ Inc/nrf51.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cm0.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cminstr.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cmfunc.h \
+ Inc/system_nrf51.h Inc/nrf51_bitfields.h Inc/nrf51_deprecated.h \
+ Inc/compiler_abstraction.h Inc/nrf51_bitfields.h
