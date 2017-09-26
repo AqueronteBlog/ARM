442026c212c7d1c3c189b619df08cca7f5a4ff18
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-26 20:14:43 +0100

    BASICS. SPI library was started, both function and header files were created ( SPI library is going to be based on I2C library )

---
+++ NRF51/Examples/Basics/SPI/spi.c
@@ -0,0 +1,181 @@
+/**
+ * @brief       spi.c
+ * @details     NRF51 SPI function libraries.
+ *              Function file.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        26/September/2017
+ * @version     26/September/2017    The ORIGIN
+ * @pre         NaN
+ * @warning     This file is ONLY for NRF51 device.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "i2c.h"
+
+
+/**
+ * @brief       uint32_t  i2c_write   ( uint8_t* i2c_buff, uint32_t i2c_data_length, uint32_t i2c_generate_stop )
+ * @details     Send data through I2C bus.
+ *
+ * @param[in]    *i2c_buff:             Data to be transmitted.
+ * @param[in]    i2c_data_length:       Amount of data to be transmitted.
+ * @param[in]    i2c_generate_stop:     Amount of data to be transmitted.
+ *
+ * @param[out]   Status of i2c_write:   I2C_STOP_BIT:       Generate a STOP bit.
+ *                                      I2C_NO_STOP_BIT:    DO NOT generate a STOP bit
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        26/September/2017
+ * @version     26/September/2017   The ORIGIN
+ * @pre         I2C communication is by polling mode.
+ * @warning     This function takes for granted that TWI0 is used.
+ */
+uint32_t    i2c_write   ( NRF_TWI_Type* myinstance, uint32_t ADDR, uint8_t* i2c_buff, uint32_t i2c_data_length, uint32_t i2c_generate_stop )
+{
+    uint32_t    i                   =   0;
+    uint32_t    i2c_timeout1        =   0;
+    uint32_t    i2c_timeout2        =   0;
+    uint32_t    i2c_default_addr    =   0;
+
+
+    // Save the default i2c address and set the one for this device
+    i2c_default_addr     =   myinstance->ADDRESS;
+    myinstance->ADDRESS  =   ADDR;
+
+
+// Start transmission
+    // Reset flag and start event
+    myinstance->EVENTS_TXDSENT   =   0;
+    //NRF_TWI0->EVENTS_TXDSENT   =   0;
+    myinstance->TASKS_STARTTX    =   1;
+
+    // Start transmitting data
+    for ( i = 0; i < i2c_data_length; i++ )
+    {
+        myinstance->TXD              =   *i2c_buff;
+
+        i2c_timeout1               =   232323;
+        while( ( myinstance->EVENTS_TXDSENT == 0 ) && ( --i2c_timeout1 ) );       // Wait until the data is transmitted or timeout1
+        myinstance->EVENTS_TXDSENT   =   0;                                       // reset flag
+
+        i2c_buff++;
+    }
+
+    // Generate a STOP bit if it is required
+    if ( i2c_generate_stop == I2C_STOP_BIT )
+    {
+        myinstance->EVENTS_STOPPED   =   0;
+        myinstance->TASKS_STOP       =   1;
+        i2c_timeout2               =   232323;
+        while( ( myinstance->EVENTS_STOPPED == 0 ) && ( --i2c_timeout2 ) );
+        myinstance->EVENTS_STOPPED   =   0;
+    }
+
+
+    // Restore the default I2C address
+    myinstance->ADDRESS  =   i2c_default_addr;
+
+
+    // Check if everything went fine
+    if ( ( i2c_timeout1 < 1 ) || ( i2c_timeout2 < 1 ) )
+        return I2C_FAILURE;
+    else
+        return I2C_SUCCESS;
+}
+
+
+
+/**
+ * @brief       uint32_t  i2c_read   ( uint8_t* i2c_buff, uint32_t i2c_data_length )
+ * @details     Read data through I2C bus.
+ *
+ * @param[in]    *i2c_buff:         Data to be transmitted.
+ * @param[in]    i2c_data_length:   Amount of data to be transmitted.
+ *
+ * @param[out]   Status of i2c_read.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        26/September/2017
+ * @version     26/September/2017   The ORIGIN
+ * @pre         I2C communication is by polling mode.
+ * @warning     This function takes for granted that TWI0 is used.
+ */
+uint32_t    i2c_read   ( NRF_TWI_Type* myinstance, uint32_t ADDR, uint8_t* i2c_buff, uint32_t i2c_data_length )
+{
+    uint32_t    i                   =   0;
+    uint32_t    i2c_timeout1        =   0;
+    uint32_t    i2c_timeout2        =   0;
+    uint32_t    i2c_default_addr    =   0;
+
+
+    // Save the default i2c address and set the one for this device
+    i2c_default_addr     =   myinstance->ADDRESS;
+    myinstance->ADDRESS  =   ADDR;
+
+
+// Start reading
+    // Clear the shortcuts
+    myinstance->SHORTS           =   ( TWI_SHORTS_BB_STOP_Disabled   << TWI_SHORTS_BB_STOP_Pos    );
+    myinstance->SHORTS           =   ( TWI_SHORTS_BB_SUSPEND_Enabled << TWI_SHORTS_BB_SUSPEND_Pos );
+
+
+    // Reset flag and start reading
+    myinstance->EVENTS_RXDREADY  =   0;
+    myinstance->TASKS_STARTRX    =   1;
+
+
+    // Read data from i2c bus
+    for ( i = 0; i < i2c_data_length; i++ )
+    {
+    // Enable the right shortcut ( NRF51 Reference Manual 28.5 Master read sequence. Figure 65 )
+        if ( i == ( i2c_data_length - 1 ) )
+            myinstance->SHORTS           =   ( TWI_SHORTS_BB_STOP_Enabled << TWI_SHORTS_BB_STOP_Pos );
+        else
+            myinstance->SHORTS           =   ( TWI_SHORTS_BB_SUSPEND_Enabled << TWI_SHORTS_BB_SUSPEND_Pos );
+
+    // It releases the bus
+        myinstance->TASKS_RESUME     =   1;                                     // NOTE: This is important to be here otherwise the STOP event
+                                                                                //       will be clocked one byte later.
+
+    // Wait until the data arrives or timeout
+        i2c_timeout1                 =   232323;
+        while( ( myinstance->EVENTS_RXDREADY == 0 ) && ( --i2c_timeout1 ) );    // Wait until the data is read or timeout1
+        myinstance->EVENTS_RXDREADY  =   0;                                     // reset flag
+
+    // Read data and prepare the next one
+        *i2c_buff                  =   myinstance->RXD;
+        i2c_buff++;
+    }
+
+
+    // Wait until the STOP event is produced or timeout2
+    i2c_timeout2                 =   232323;
+    while( ( myinstance->EVENTS_STOPPED == 0 ) && ( --i2c_timeout2 ) );
+    myinstance->EVENTS_STOPPED   =   0;
+
+
+    // Reset shortcuts
+    myinstance->SHORTS           =   ( TWI_SHORTS_BB_SUSPEND_Disabled << TWI_SHORTS_BB_SUSPEND_Pos );
+    myinstance->SHORTS           =   ( TWI_SHORTS_BB_STOP_Disabled << TWI_SHORTS_BB_STOP_Pos );
+
+
+    // Restore the default I2C address
+    myinstance->ADDRESS  =   i2c_default_addr;
+
+
+    // Check if everything went fine
+    if ( ( i2c_timeout1 < 1 ) || ( i2c_timeout2 < 1 ) )
+        return I2C_FAILURE;
+    else
+        return I2C_SUCCESS;
+}
---
+++ NRF51/Examples/Basics/SPI/spi.h
@@ -0,0 +1,35 @@
+/**
+ * @brief       spi.h
+ * @details     NRF51 SPI function libraries.
+ *              Header file.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        26/September/2017
+ * @version     26/September/2017    The ORIGIN
+ * @pre         NaN
+ * @warning     This file is ONLY for NRF51 device.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+
+
+/**
+  * @brief   INTERNAL CONSTANTS
+  */
+#define I2C_SUCCESS      0x00
+#define I2C_FAILURE      0x01
+
+#define I2C_STOP_BIT     0x00
+#define I2C_NO_STOP_BIT  0x01
+
+
+/**
+  * @brief   FUNCTION PROTOTYPES
+  */
+uint32_t    i2c_write   ( NRF_TWI_Type* myinstance, uint32_t ADDR, uint8_t* i2c_buff, uint32_t length, uint32_t i2c_generate_stop );
+uint32_t    i2c_read    ( NRF_TWI_Type* myinstance, uint32_t ADDR, uint8_t* i2c_buff, uint32_t length );
