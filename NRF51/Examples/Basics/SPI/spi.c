5f86b8056509ce99b86f0cde178e9a951416ca8f
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-27 21:12:36 +0100

    NRF51 Basics. SPI is ready to be tested.

--- NRF51/Examples/Basics/SPI/spi.c
+++ NRF51/Examples/Basics/SPI/spi.c
@@ -18,7 +18,7 @@
 
 
 /**
- * @brief       spi_transfer   ( NRF_TWI_Type* , uint8_t* , uint32_t , uint8_t* , uint32_t )
+ * @brief       spi_transfer   ( NRF_SPI_Type* , uint8_t* , uint32_t , uint8_t* , uint32_t )
  *
  * @details     Send/Receive data through the SPI bus.
  *
@@ -39,18 +39,39 @@
  * @pre         SPI communication is by polling mode.
  * @warning     NaN.
  */
-spi_status_t    spi_transfer ( NRF_TWI_Type* myinstance, uint8_t* spi_tx_buff, uint32_t spi_tx_length, uint8_t* spi_rx_buff, uint32_t spi_rx_length )
+spi_status_t    spi_transfer ( NRF_SPI_Type* myinstance, uint8_t* spi_tx_buff, uint32_t spi_tx_length, uint8_t* spi_rx_buff, uint32_t spi_rx_length )
 {
+    uint32_t     i                   =   0;
+    uint32_t     spi_timeout1        =   0;
+    uint32_t     spi_timeout2        =   0;
+
     spi_status_t mySPI_status,
 
 
 
-// Start transmission
+// SPI TX
+    for ( i = 0; i < spi_tx_length; i++ ){
+        myinstance->TXD        =   *spi_tx_buff++;                                // Send data
 
+        i2c_timeout1                =   232323;
+        while( ( myinstance->EVENTS_READY == 0 ) && ( --spi_timeout1 ) );         // Wait until the data is transmitted or timeout1
+        *spi_rx_buff                =   myinstance->RXD;                          // Dummy value
+        myinstance->EVENTS_READY    =   0;                                        // reset flag
+    }
+
+// SPI RX
+    for ( i = 0; i < spi_rx_length; i++ ){
+        myinstance->TXD        =   0x00;                                          // Dummy value
+
+        i2c_timeout2                =   232323;
+        while( ( myinstance->EVENTS_READY == 0 ) && ( --spi_timeout2 ) );         // Wait until the data is transmitted or timeout2
+        *spi_rx_buff++             =   myinstance->RXD;                           // Get data
+        myinstance->EVENTS_READY    =   0;                                        // reset flag
+    }
 
 
     // Check if everything went fine
-    if ( ( i2c_timeout1 < 1 ) || ( i2c_timeout2 < 1 ) )
+    if ( ( spi_timeout1 < 1 ) || ( spi_timeout2 < 1 ) )
         return SPI_FAILURE;
     else
         return SPI_SUCCESS;
