891ebe3b9df3fcc11a95c7f8035e9b04969c31e6
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-28 19:33:53 +0100

    NRF51 MAX7219 Example. Firmware to test is the compilation was successful

--- NRF51/Examples/Drivers/MAX7219/Src/main.c
+++ NRF51/Examples/Drivers/MAX7219/Src/main.c
@@ -1,15 +1,12 @@
 /**
  * @brief       main.c
- * @details     This firmware shows how to work with two peripherals: SPI ( Master )
- *              and SPIS ( Slave ). Every one second, the Master sends one byte to the
- *              Slave. It depends on the data transmitted by the Master, the Slave will
- *              answer with a different byte.
+ * @details     [todo].
  *
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        27/June/2017
- * @version     27/June/2017        The ORIGIN
+ * @date        28/September/2017
+ * @version     28/September/2017        The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice
@@ -25,6 +22,7 @@
 
 int main( void )
 {
+    /*
     conf_GPIO   ();
     conf_SPI0   ();
     conf_SPIS1  ();
@@ -32,7 +30,7 @@ int main( void )
 
     NRF_TIMER0->TASKS_START = 1;            // Start Timer0
 
-
+*/
     while( 1 )
     {
         //NRF_POWER->SYSTEMOFF = 1;
