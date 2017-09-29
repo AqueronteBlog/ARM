5b186a2cb0988be9d47d8ec8f57445339c3bcd5f
AqueronteBlog <aqueronteblog@gmail.com>
2017-09-29 17:00:24 +0100

    NRF51 MAX7219 Example. Minor changes, last compilation

--- NRF51/Examples/Drivers/MAX7219/obj/Debug/Src/main.d
+++ NRF51/Examples/Drivers/MAX7219/obj/Debug/Src/main.d
@@ -9,4 +9,5 @@ obj\debug\src\main.o: Src\main.c Inc/nrf.h Inc/nrf51.h \
  softdevice\Include/ble_l2cap.h softdevice\Include/ble_err.h \
  softdevice\Include/nrf_error.h softdevice\Include/ble_gatt.h \
  softdevice\Include/ble_gattc.h softdevice\Include/ble_gatts.h \
- Src\variables.h Inc/nrf51_bitfields.h Src\board.h Src\functions.h
+ Src\variables.h Inc/nrf51_bitfields.h Src\board.h Src\functions.h \
+ ..\..\..\..\Drivers\MAX7219/MAX7219.h ..\..\Basics\SPI/spi.h
