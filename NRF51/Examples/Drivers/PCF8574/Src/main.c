335af2986cc850207b7bb93796893a057b09611f
AqueronteBlog <aqueronteblog@gmail.com>
2017-10-11 14:03:47 +0100

    NRF51 PCF8574 Example. First test for the PCF8574 library

--- NRF51/Examples/Drivers/PCF8574/Src/main.c
+++ NRF51/Examples/Drivers/PCF8574/Src/main.c
@@ -8,8 +8,8 @@
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        22/September/2017
- * @version     22/September/2017    The ORIGIN
+ * @date        11/October/2017
+ * @version     11/October/2017    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice
@@ -21,40 +21,21 @@
 #include "ble.h"
 #include "variables.h"
 #include "functions.h"
-#include "PCF8591.h"
+#include "PCF8574.h"
 
 
 int main( void )
 {
-    uint8_t                 myNewDAC_value   =   128;
-
-    PCF8591_status_t        aux;
-    PCF8591_vector_data_t   myADC_Data;
+    PCF8574_status_t        aux;
+    PCF8574_vector_data_t   myData;
 
     conf_GPIO   ();
     conf_TWI0   ();
-    //conf_TIMER0 ();
-
-    //aux = PCF8591_SetADC  ( NRF_TWI0, PCF8591_ADDRESS_0, PCF8591_FOUR_SINGLE_ENDED_INPUTS, PCF8591_AUTO_INCREMENT_ENABLED, PCF8591_CHANNEL_0 );
-    aux = PCF8591_SetDAC  ( NRF_TWI0, PCF8591_ADDRESS_0, PCF8591_DAC_ENABLED );
-    while(1)
-    {
-        aux = PCF8591_SetADC  ( NRF_TWI0, PCF8591_ADDRESS_0, PCF8591_FOUR_SINGLE_ENDED_INPUTS, PCF8591_AUTO_INCREMENT_ENABLED, PCF8591_CHANNEL_0 );
-        aux = PCF8591_ReadADC ( NRF_TWI0, PCF8591_ADDRESS_0, &myADC_Data );
-
-        aux = PCF8591_NewDACValue ( NRF_TWI0, PCF8591_ADDRESS_0, myNewDAC_value );
-
-        if ( myNewDAC_value < 246 )
-            myNewDAC_value  +=  10;
-        else
-            myNewDAC_value   =   0;
-    }
+    conf_TIMER0 ();
 
 
-    /*
 
     NRF_TIMER0->TASKS_START  =   1;                 // Start Timer0
-
 
     while( 1 )
     {
@@ -68,33 +49,22 @@ int main( void )
     	__WFE();
 
 
-    	switch ( mySTATE ){
-        default:
-        case 1:
-        // Vout ~ 0V
-            NRF_GPIO->OUTCLR             =   ( 1UL << LED1 );       // Turn the LED1 on
-            myNewDACData.DAC_New_Value   =   0;
-            aux = MCP4725_SetNewValue ( NRF_TWI0, MCP4725_ADDRESS_LOW, FAST_MODE, myNewDACData );
-            break;
+    	if ( mySTATE )
+        {
+            myData.data   =   ( PCF8574_P0_OUTPUT_LOW | PCF8574_P1_OUTPUT_HIGH | PCF8574_P2_OUTPUT_LOW | PCF8574_P3_OUTPUT_HIGH |
+                                PCF8574_P4_OUTPUT_LOW | PCF8574_P5_OUTPUT_HIGH | PCF8574_P6_OUTPUT_LOW | PCF8574_P7_OUTPUT_HIGH );
 
-        case 2:
-        // Vout = ~ ( Vref * 0.5 )
-            myNewDACData.DAC_New_Value   =   2048;
-            aux = MCP4725_SetNewValue ( NRF_TWI0, MCP4725_ADDRESS_LOW, WRITE_DAC_AND_EEPROM_REGISTER_MODE, myNewDACData );
-            break;
+            aux           =   PCF8574_SetPins ( NRF_TWI0, PCF8574_ADDRESS_0, myData );
+        }
+    	else
+        {
+    	     myData.data  =   ( PCF8574_P0_OUTPUT_HIGH | PCF8574_P1_OUTPUT_LOW | PCF8574_P2_OUTPUT_HIGH | PCF8574_P3_OUTPUT_LOW |
+                                PCF8574_P4_OUTPUT_HIGH | PCF8574_P5_OUTPUT_LOW | PCF8574_P6_OUTPUT_HIGH | PCF8574_P7_OUTPUT_LOW );
 
-        case 3:
-        // Vout ~ Vref
-            myNewDACData.DAC_New_Value   =   4095;
-            aux = MCP4725_SetNewValue ( NRF_TWI0, MCP4725_ADDRESS_LOW, WRITE_DAC_REGISTER_MODE, myNewDACData );
-
-            mySTATE =   0;
-            NRF_GPIO->OUTSET             =   ( 1UL << LED1 );       // Turn the LED1 off
-            break;
-    	}
+            aux           =   PCF8574_SetPins ( NRF_TWI0, PCF8574_ADDRESS_0, myData );
+        }
 
         //__NOP();
 
     }
-    */
 }
