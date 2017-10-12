8b35124bdb2ad7ea3f972a62fee1db1381fad972
AqueronteBlog <aqueronteblog@gmail.com>
2017-10-12 13:53:15 +0100

    DRIVERS. HCM5885L The functions need to be completed

--- Drivers/HMC5883L/HMC5883L.c
+++ Drivers/HMC5883L/HMC5883L.c
@@ -1,35 +1,35 @@
 /**
- * @brief       PCF8574.h
- * @details     Remote 8-bit I/O expander for I2C-bus with interrupt.
+ * @brief       HMC5883L.h
+ * @details     3-Axis Digital Compass IC.
  *              Functions file.
  *
  *
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        11/October/2017
- * @version     11/October/2017    The ORIGIN
+ * @date        12/October/2017
+ * @version     12/October/2017    The ORIGIN
  * @pre         NaN.
  * @warning     NaN
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
-#include "PCF8574.h"
+#include "HMC5883L.h"
 
 
 /**
- * @brief       PCF8574_SetPins ( NRF_TWI_Type* , PCF8574_address_t , PCF8574_vector_data_t )
+ * @brief       HMC5883L_SetPins ( NRF_TWI_Type* , HMC5883L_address_t , HMC5883L_vector_data_t )
  *
  * @details     It configures the port of the device.
  *
  * @param[in]    myinstance:        Peripheral's Instance.
- * @param[in]    myPCF8574Addr:     I2C Device address.
+ * @param[in]    myHMC5883LAddr:     I2C Device address.
  * @param[in]    myConfDATA:        Data to set up the device.
  *
  * @param[out]   NaN.
  *
  *
- * @return       Status of PCF8574_SetPins.
+ * @return       Status of HMC5883L_SetPins.
  *
  *
  * @author      Manuel Caballero
@@ -38,36 +38,36 @@
  * @pre         NaN
  * @warning     NaN.
  */
-PCF8574_status_t  PCF8574_SetPins   ( NRF_TWI_Type* myinstance, PCF8574_address_t myPCF8574Addr, PCF8574_vector_data_t  myConfDATA )
+HMC5883L_status_t  HMC5883L_SetPins   ( NRF_TWI_Type* myinstance, HMC5883L_address_t myHMC5883LAddr, HMC5883L_vector_data_t  myConfDATA )
 {
     uint32_t     aux     =    0;
 
 
     // Configure the ports of the device
-    aux = i2c_write ( myinstance, myPCF8574Addr, &myConfDATA.data, 1, I2C_STOP_BIT );
+    aux = i2c_write ( myinstance, myHMC5883LAddr, &myConfDATA, 1, I2C_STOP_BIT );
 
 
 
     if ( aux == I2C_SUCCESS )
-        return   PCF8574_SUCCESS;
+        return   HMC5883L_SUCCESS;
     else
-        return   PCF8574_FAILURE;
+        return   HMC5883L_FAILURE;
 }
 
 
 
 /**
- * @brief       PCF8574_ReadPins ( NRF_TWI_Type* , PCF8574_address_t , PCF8574_vector_data_t*  )
+ * @brief       HMC5883L_ReadPins ( NRF_TWI_Type* , HMC5883L_address_t , HMC5883L_vector_data_t*  )
  *
  * @details     It gets the data from the device ( port status ).
  *
  * @param[in]    myinstance:        Peripheral's Instance.
- * @param[in]    myPCF8574Addr:     I2C Device address.
+ * @param[in]    myHMC5883LAddr:     I2C Device address.
  *
  * @param[out]   myReadDATA:        ADC result into the chosen channel.
  *
  *
- * @return       Status of PCF8574_ReadPins.
+ * @return       Status of HMC5883L_ReadPins.
  *
  *
  * @author      Manuel Caballero
@@ -76,19 +76,19 @@ PCF8574_status_t  PCF8574_SetPins   ( NRF_TWI_Type* myinstance, PCF8574_address_
  * @pre         NaN
  * @warning     NaN.
  */
-PCF8574_status_t  PCF8574_ReadPins ( NRF_TWI_Type* myinstance, PCF8574_address_t myPCF8574Addr, PCF8574_vector_data_t* myReadDATA )
+HMC5883L_status_t  HMC5883L_ReadPins ( NRF_TWI_Type* myinstance, HMC5883L_address_t myHMC5883LAddr, HMC5883L_vector_data_t* myReadDATA )
 {
     uint32_t     aux  =    0;
 
 
     // Read the port
-    aux = i2c_read ( myinstance, myPCF8574Addr, &myReadDATA->data, 1 );
+    aux = i2c_read ( myinstance, myHMC5883LAddr, &myReadDATA, 1 );
 
 
 
 
     if ( aux == I2C_SUCCESS )
-        return   PCF8574_SUCCESS;
+        return   HMC5883L_SUCCESS;
     else
-        return   PCF8574_FAILURE;
+        return   HMC5883L_FAILURE;
 }
