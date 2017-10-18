ecf85b741f16556253816fcfae6e712f0619f1c4
AqueronteBlog <aqueronteblog@gmail.com>
2017-10-18 11:11:41 +0100

    DRIVERS. MCP4725 for NRF51 was updated to the last I2C version

--- Drivers/MCP4725/MCP4725.c
+++ Drivers/MCP4725/MCP4725.c
@@ -18,12 +18,47 @@
 
 
 /**
- * @brief       MCP4725_Reset   ( NRF_TWI_Type* )
+ * @brief       MCP4725_Init ( I2C_parameters_t )
+ *
+ * @details     It configures the I2C peripheral.
+ *
+ * @param[in]    myI2Cparameters:       I2C parameters.
+ *
+ * @param[out]   NaN.
+ *
+ *
+ * @return       Status of MCP4725_Init.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        18/October/2017
+ * @version     18/October/2017   The ORIGIN
+ * @pre         NaN
+ * @warning     NaN.
+ */
+MCP4725_status_t  MCP4725_Init ( I2C_parameters_t myI2Cparameters )
+{
+    i2c_status_t aux;
+
+    aux  =   i2c_init ( myI2Cparameters );
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   MCP4725_SUCCESS;
+    else
+        return   MCP4725_FAILURE;
+}
+
+
+
+/**
+ * @brief       MCP4725_Reset   ( I2C_parameters_t )
  *
  * @details     It performs an internal reset similar to a
  *              power-on-reset ( POR ).
  *
- * @param[in]    myinstance:            Peripheral's Instance.
+ * @param[in]    myI2Cparameters:       I2C parameters.
  *
  * @param[out]   NaN.
  *
@@ -33,18 +68,23 @@
  *
  * @author      Manuel Caballero
  * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
+ * @version     18/October/2017    Adapted to the new I2C driver.
+ *              7/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     The user MUST respect the time it takes this instruction to be
  *              performed ( max. 50ms ).
  */
-MCP4725_status_t  MCP4725_Reset   ( NRF_TWI_Type* myinstance )
+MCP4725_status_t  MCP4725_Reset   ( I2C_parameters_t myI2Cparameters )
 {
     uint8_t     cmd                 =    MCP4725_GENERAL_CALL_RESET;
     uint32_t    aux                 =    0;
 
 
-    aux = i2c_write ( myinstance, MCP4725_GENERAL_CALL, &cmd, 1, I2C_STOP_BIT );
+
+    myI2Cparameters.ADDR     =   MCP4725_GENERAL_CALL;
+
+    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_STOP_BIT );
+
 
 
 
@@ -57,11 +97,11 @@ MCP4725_status_t  MCP4725_Reset   ( NRF_TWI_Type* myinstance )
 
 
 /**
- * @brief       MCP4725_WakeUp   ( NRF_TWI_Type* )
+ * @brief       MCP4725_WakeUp   ( I2C_parameters_t )
  *
  * @details     The power-down bits of the DAC register are set to a normal operation.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
+ * @param[in]    myI2Cparameters:  I2C parameters.
  *
  * @param[out]   NaN.
  *
@@ -71,17 +111,20 @@ MCP4725_status_t  MCP4725_Reset   ( NRF_TWI_Type* myinstance )
  *
  * @author      Manuel Caballero
  * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
+ * @version     18/October/2017    Adapted to the new I2C driver.
+ *              7/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_WakeUp   ( NRF_TWI_Type* myinstance )
+MCP4725_status_t  MCP4725_WakeUp   ( I2C_parameters_t myI2Cparameters )
 {
     uint8_t     cmd                 =    MCP4725_GENERAL_CALL_WAKE_UP;
     uint32_t    aux                 =    0;
 
 
-    aux = i2c_write ( myinstance, MCP4725_GENERAL_CALL, &cmd, 1, I2C_STOP_BIT );
+    myI2Cparameters.ADDR     =   MCP4725_GENERAL_CALL;
+
+    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_STOP_BIT );
 
 
 
@@ -93,12 +136,11 @@ MCP4725_status_t  MCP4725_WakeUp   ( NRF_TWI_Type* myinstance )
 
 
 /**
- * @brief       MCP4725_PowerMode ( NRF_TWI_Type* , MCP4725_address_t , MCP4725_write_command_type_t , MCP4725_operation_mode_t )
+ * @brief       MCP4725_PowerMode ( I2C_parameters_t , MCP4725_write_command_type_t , MCP4725_operation_mode_t )
  *
  * @details     It configures the power mode of the device.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
+ * @param[in]    myI2Cparameters:       I2C parameters.
  * @param[in]    myWriteCMD:            It writes the command into the DAC or EEPROM/DAC.
  * @param[in]    myPowerMode:           Normal Mode or one of the Power-Down available modes.
  *
@@ -110,11 +152,12 @@ MCP4725_status_t  MCP4725_WakeUp   ( NRF_TWI_Type* myinstance )
  *
  * @author      Manuel Caballero
  * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
+ * @version     18/October/2017    Adapted to the new I2C driver.
+ *              7/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_PowerMode ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, MCP4725_operation_mode_t myPowerMode )
+MCP4725_status_t  MCP4725_PowerMode ( I2C_parameters_t myI2Cparameters, MCP4725_write_command_type_t myWriteCMD, MCP4725_operation_mode_t myPowerMode )
 {
     uint8_t     cmd[]             =    { 0, 0, 0, 0, 0, 0 };
     uint32_t    aux               =    0;
@@ -122,7 +165,7 @@ MCP4725_status_t  MCP4725_PowerMode ( NRF_TWI_Type* myinstance, MCP4725_address_
 
 
     // Read the device to mask the default value
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
+    aux = i2c_read ( myI2Cparameters, &cmd[0], 5 );
 
 
 
@@ -172,7 +215,7 @@ MCP4725_status_t  MCP4725_PowerMode ( NRF_TWI_Type* myinstance, MCP4725_address_
 
 
 
-    aux = i2c_write ( myinstance, ADDR, &cmd[0], dataTX, I2C_STOP_BIT );
+    aux = i2c_write ( myI2Cparameters, &cmd[0], dataTX, I2C_STOP_BIT );
 
 
 
@@ -185,12 +228,11 @@ MCP4725_status_t  MCP4725_PowerMode ( NRF_TWI_Type* myinstance, MCP4725_address_
 
 
 /**
- * @brief       MCP4725_SetNewValue ( NRF_TWI_Type* , MCP4725_address_t , MCP4725_write_command_type_t , Vector_new_dac_value_t )
+ * @brief       MCP4725_SetNewValue ( I2C_parameters_t , MCP4725_write_command_type_t , Vector_new_dac_value_t )
  *
  * @details     It sends a new output value.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
+ * @param[in]    myI2Cparameters:       I2C parameters.
  * @param[in]    myWriteCMD:            It writes the command into the DAC or EEPROM/DAC.
  * @param[in]    myDACNewValue:         New output value.
  *
@@ -202,11 +244,12 @@ MCP4725_status_t  MCP4725_PowerMode ( NRF_TWI_Type* myinstance, MCP4725_address_
  *
  * @author      Manuel Caballero
  * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
+ * @version     18/October/2017    Adapted to the new I2C driver.
+ *              7/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_SetNewValue   ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, Vector_new_dac_value_t myDACNewValue )
+MCP4725_status_t  MCP4725_SetNewValue   ( I2C_parameters_t myI2Cparameters, MCP4725_write_command_type_t myWriteCMD, Vector_new_dac_value_t myDACNewValue )
 {
     uint8_t     cmd[]             =    { 0, 0, 0 };
     uint32_t    aux               =    0;
@@ -242,7 +285,7 @@ MCP4725_status_t  MCP4725_SetNewValue   ( NRF_TWI_Type* myinstance, MCP4725_addr
 
 
 
-    aux = i2c_write ( myinstance, ADDR, &cmd[0], dataTX, I2C_STOP_BIT );
+    aux = i2c_write ( myI2Cparameters, &cmd[0], dataTX, I2C_STOP_BIT );
 
 
 
@@ -255,12 +298,11 @@ MCP4725_status_t  MCP4725_SetNewValue   ( NRF_TWI_Type* myinstance, MCP4725_addr
 
 
 /**
- * @brief       MCP4725_EEPROM_Status ( NRF_TWI_Type* , MCP4725_address_t , MCP4725_eeprom_status_t )
+ * @brief       MCP4725_EEPROM_Status ( I2C_parameters_t myI2Cparameters , MCP4725_eeprom_status_t )
  *
  * @details     It gets the eeprom status.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
+ * @param[in]    myI2Cparameters:       I2C parameters.
  * @param[in]    myEEPROM_Status:       EEPROM status.
  *
  * @param[out]   NaN.
@@ -271,17 +313,18 @@ MCP4725_status_t  MCP4725_SetNewValue   ( NRF_TWI_Type* myinstance, MCP4725_addr
  *
  * @author      Manuel Caballero
  * @date        7/September/2017
- * @version     7/September/2017   The ORIGIN
+ * @version     18/October/2017    Adapted to the new I2C driver.
+ *              7/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_EEPROM_Status ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_eeprom_status_t* myEEPROM_Status )
+MCP4725_status_t  MCP4725_EEPROM_Status ( I2C_parameters_t myI2Cparameters, MCP4725_eeprom_status_t* myEEPROM_Status )
 {
     uint8_t     cmd[]             =    { 0, 0, 0, 0, 0 };
     uint32_t    aux               =    0;
 
     // Read command
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
+    aux = i2c_read ( myI2Cparameters, &cmd[0], 5 );
 
     // Update EEPROM status
     *myEEPROM_Status =   ( ( cmd[0] & 0x80 ) >> 7 );
@@ -297,12 +340,11 @@ MCP4725_status_t  MCP4725_EEPROM_Status ( NRF_TWI_Type* myinstance, MCP4725_addr
 
 
 /**
- * @brief       MCP4725_GetEEPROM_Data ( NRF_TWI_Type* , MCP4725_address_t , Vector_data_t* )
+ * @brief       MCP4725_GetEEPROM_Data ( I2C_parameters_t , Vector_data_t* )
  *
  * @details     It gets the eeprom value.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
+ * @param[in]    myI2Cparameters:       I2C parameters.
  * @param[in]    myEEPROMData:          EEPROM value.
  *
  * @param[out]   NaN.
@@ -313,17 +355,18 @@ MCP4725_status_t  MCP4725_EEPROM_Status ( NRF_TWI_Type* myinstance, MCP4725_addr
  *
  * @author      Manuel Caballero
  * @date        8/September/2017
- * @version     8/September/2017   The ORIGIN
+ * @version     18/October/2017    Adapted to the new I2C driver.
+ *              8/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_GetEEPROM_Data    ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myEEPROMData )
+MCP4725_status_t  MCP4725_GetEEPROM_Data    ( I2C_parameters_t myI2Cparameters, Vector_data_t* myEEPROMData )
 {
     uint8_t     cmd[]             =    { 0, 0, 0, 0, 0 };
     uint32_t    aux               =    0;
 
     // Read command
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
+    aux = i2c_read ( myI2Cparameters, &cmd[0], 5 );
 
     // Read EEPROM value
     myEEPROMData->EEPROM_Data =   ( ( cmd[3] & 0x0F ) << 8 ) | ( cmd[4] );
@@ -339,12 +382,11 @@ MCP4725_status_t  MCP4725_GetEEPROM_Data    ( NRF_TWI_Type* myinstance, MCP4725_
 
 
 /**
- * @brief       MCP4725_GetDAC_Data ( NRF_TWI_Type* , MCP4725_address_t , Vector_data_t* )
+ * @brief       MCP4725_GetDAC_Data ( I2C_parameters_t , Vector_data_t* )
  *
  * @details     It gets the DAC value.
  *
- * @param[in]    myinstance:            Peripheral's Instance.
- * @param[in]    ADDR:                  I2C Device's address.
+ * @param[in]    myI2Cparameters:       I2C parameters.
  * @param[in]    myDACData:             DAC value.
  *
  * @param[out]   NaN.
@@ -355,17 +397,18 @@ MCP4725_status_t  MCP4725_GetEEPROM_Data    ( NRF_TWI_Type* myinstance, MCP4725_
  *
  * @author      Manuel Caballero
  * @date        8/September/2017
- * @version     8/September/2017   The ORIGIN
+ * @version     18/October/2017    Adapted to the new I2C driver.
+ *              8/September/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
-MCP4725_status_t  MCP4725_GetDAC_Data    ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myDACData )
+MCP4725_status_t  MCP4725_GetDAC_Data    ( I2C_parameters_t myI2Cparameters, Vector_data_t* myDACData )
 {
     uint8_t     cmd[]             =    { 0, 0, 0, 0, 0 };
     uint32_t    aux               =    0;
 
     // Read command
-    aux = i2c_read ( myinstance, ADDR, &cmd[0], 5 );
+    aux = i2c_read ( myI2Cparameters, &cmd[0], 5 );
 
     // Read DAC value
     myDACData->DAC_Data =   ( ( cmd[1] & 0xF0 ) << 4 ) | ( ( ( ( cmd[1] & 0x0F ) >> 4 ) | ( cmd[2] & 0xF0 ) >> 4 ) );
--- Drivers/MCP4725/MCP4725.h
+++ Drivers/MCP4725/MCP4725.h
@@ -109,10 +109,11 @@ typedef enum{
 /**
   * @brief   FUNCTION PROTOTYPES
   */
-MCP4725_status_t  MCP4725_Reset                 ( NRF_TWI_Type* myinstance );
-MCP4725_status_t  MCP4725_WakeUp                ( NRF_TWI_Type* myinstance );
-MCP4725_status_t  MCP4725_PowerMode             ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, MCP4725_operation_mode_t myPowerMode );
-MCP4725_status_t  MCP4725_SetNewValue           ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_write_command_type_t myWriteCMD, Vector_new_dac_value_t myDACNewValue );
-MCP4725_status_t  MCP4725_EEPROM_Status         ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, MCP4725_eeprom_status_t* myEEPROM_Status );
-MCP4725_status_t  MCP4725_GetEEPROM_Data        ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myEEPROMData );
-MCP4725_status_t  MCP4725_GetDAC_Data           ( NRF_TWI_Type* myinstance, MCP4725_address_t ADDR, Vector_data_t* myDACData );
+MCP4725_status_t  MCP4725_Init                  ( I2C_parameters_t myI2Cparameters );
+MCP4725_status_t  MCP4725_Reset                 ( I2C_parameters_t myI2Cparameters );
+MCP4725_status_t  MCP4725_WakeUp                ( I2C_parameters_t myI2Cparameters );
+MCP4725_status_t  MCP4725_PowerMode             ( I2C_parameters_t myI2Cparameters, MCP4725_write_command_type_t myWriteCMD, MCP4725_operation_mode_t myPowerMode );
+MCP4725_status_t  MCP4725_SetNewValue           ( I2C_parameters_t myI2Cparameters, MCP4725_write_command_type_t myWriteCMD, Vector_new_dac_value_t myDACNewValue );
+MCP4725_status_t  MCP4725_EEPROM_Status         ( I2C_parameters_t myI2Cparameters, MCP4725_eeprom_status_t* myEEPROM_Status );
+MCP4725_status_t  MCP4725_GetEEPROM_Data        ( I2C_parameters_t myI2Cparameters, Vector_data_t* myEEPROMData );
+MCP4725_status_t  MCP4725_GetDAC_Data           ( I2C_parameters_t myI2Cparameters, Vector_data_t* myDACData );
