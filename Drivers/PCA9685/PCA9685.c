39414152af83d8268849a18cc9c97267552b203a
AqueronteBlog <aqueronteblog@gmail.com>
2017-11-01 20:09:41 +0000

    DRIVERS. PCA9685 for NRF51, SoftReset function was completed and ready to be tested.

--- Drivers/PCA9685/PCA9685.c
+++ Drivers/PCA9685/PCA9685.c
@@ -31,8 +31,8 @@
  *
  *
  * @author      Manuel Caballero
- * @date        18/October/2017
- * @version     18/October/2017   The ORIGIN
+ * @date        31/October/2017
+ * @version     31/October/2017   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
@@ -53,72 +53,35 @@ PCA9685_status_t  PCA9685_Init ( I2C_parameters_t myI2Cparameters )
 
 
 /**
- * @brief       PCA9685_SetPins ( I2C_parameters_t , PCA9685_vector_data_t )
+ * @brief       PCA9685_SoftReset ( I2C_parameters_t )
  *
- * @details     It configures the port of the device.
+ * @details     It performs a software reset.
  *
  * @param[in]    myI2Cparameters:   I2C parameters.
- * @param[in]    myConfDATA:        Data to set up the device.
  *
  * @param[out]   NaN.
  *
  *
- * @return       Status of PCA9685_SetPins.
+ * @return       Status of PCA9685_SoftReset.
  *
  *
  * @author      Manuel Caballero
- * @date        11/October/2017
- * @version     18/October/2017     Adapted to the new I2C driver.
- *              11/October/2017     The ORIGIN
- * @pre         NaN
+ * @date        1/November/2017
+ * @version     1/November/2017     The ORIGIN
+ * @pre         The device will be ready to be addressed again within
+ *              the specified bus free time ( t_BUF ).
  * @warning     NaN.
  */
-PCA9685_status_t  PCA9685_SetPins   ( I2C_parameters_t myI2Cparameters, PCA9685_vector_data_t  myConfDATA )
+PCA9685_status_t  PCA9685_SoftReset   ( I2C_parameters_t myI2Cparameters )
 {
-    uint32_t     aux     =    0;
+    uint8_t      cmd   =   SWRST;
+    i2c_status_t aux;
 
 
-    // Configure the ports of the device
-    aux = i2c_write ( myI2Cparameters, &myConfDATA.data, 1, I2C_STOP_BIT );
+    myI2Cparameters.ADDR    =   GENERAL_CALL_ADDRESS;
 
 
-
-    if ( aux == I2C_SUCCESS )
-        return   PCA9685_SUCCESS;
-    else
-        return   PCA9685_FAILURE;
-}
-
-
-
-/**
- * @brief       PCA9685_ReadPins ( I2C_parameters_t , PCA9685_vector_data_t*  )
- *
- * @details     It gets the data from the device ( port status ).
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- *
- * @param[out]   myReadDATA:        ADC result into the chosen channel.
- *
- *
- * @return       Status of PCA9685_ReadPins.
- *
- *
- * @author      Manuel Caballero
- * @date        11/October/2017
- * @version     18/October/2017     Adapted to the new I2C driver.
- *              11/October/2017     The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-PCA9685_status_t  PCA9685_ReadPins ( I2C_parameters_t myI2Cparameters, PCA9685_vector_data_t* myReadDATA )
-{
-    uint32_t     aux  =    0;
-
-
-    // Read the port
-    aux = i2c_read ( myI2Cparameters, &myReadDATA->data, 1 );
-
+    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_STOP_BIT );
 
 
 
