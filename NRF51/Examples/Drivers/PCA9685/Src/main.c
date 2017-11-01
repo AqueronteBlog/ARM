32b6d47e825499339938f1cdd376d41cb05db103
AqueronteBlog <aqueronteblog@gmail.com>
2017-11-01 20:08:29 +0000

    NRF51 PCA9685 Example. The main was adapted to check the library. PCA9685_SoftReset was compiled without errors.

--- NRF51/Examples/Drivers/PCA9685/Src/main.c
+++ NRF51/Examples/Drivers/PCA9685/Src/main.c
@@ -8,8 +8,8 @@
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        22/September/2017
- * @version     22/September/2017    The ORIGIN
+ * @date        31/October/2017
+ * @version     31/October/2017    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice
@@ -22,15 +22,15 @@
 #include "ble.h"
 #include "variables.h"
 #include "functions.h"
-#include "SI7006.h"
+#include "PCA9685.h"
 
 
 int main( void )
 {
-    I2C_parameters_t           mySI7006_I2C_parameters;
+    I2C_parameters_t           myPCA9685_I2C_parameters;
 
-    SI7006_status_t            aux;
-    SI7006_vector_data_t       mySI7006_Data;
+    PCA9685_status_t           aux;
+    //PCA9685_vector_data_t      myPCA9685_Data;
 
 
     conf_GPIO   ();
@@ -39,35 +39,37 @@ int main( void )
 
 
     // I2C definition
-    mySI7006_I2C_parameters.TWIinstance =    NRF_TWI0;
-    mySI7006_I2C_parameters.SDA         =    TWI0_SDA;
-    mySI7006_I2C_parameters.SCL         =    TWI0_SCL;
-    mySI7006_I2C_parameters.ADDR        =    SI7006_ADDRESS;
-    mySI7006_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
-    mySI7006_I2C_parameters.SDAport     =    NRF_GPIO;
-    mySI7006_I2C_parameters.SCLport     =    NRF_GPIO;
+    myPCA9685_I2C_parameters.TWIinstance =    NRF_TWI0;
+    myPCA9685_I2C_parameters.SDA         =    TWI0_SDA;
+    myPCA9685_I2C_parameters.SCL         =    TWI0_SCL;
+    myPCA9685_I2C_parameters.ADDR        =    PCA9685_ADDRESS_0;
+    myPCA9685_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+    myPCA9685_I2C_parameters.SDAport     =    NRF_GPIO;
+    myPCA9685_I2C_parameters.SCLport     =    NRF_GPIO;
 
     // Configure I2C peripheral
-    aux  =   SI7006_Init ( mySI7006_I2C_parameters );
+    aux  =   PCA9685_Init ( myPCA9685_I2C_parameters );
 
 
     // Reset the device
-    aux  =   SI7006_SoftReset ( mySI7006_I2C_parameters );
-    nrf_delay_ms ( 15 );
+    aux  =   PCA9685_SoftReset ( myPCA9685_I2C_parameters );
+    nrf_delay_us ( 5 );
+
+/*
 
     // Configure the device
-    aux  =   SI7006_Conf  ( mySI7006_I2C_parameters, SI7006_RESOLUTION_RH_12_TEMP_14, SI7006_HTRE_DISABLED );
+    aux  =   PCA9685_Conf  ( myPCA9685_I2C_parameters, PCA9685_RESOLUTION_RH_12_TEMP_14, PCA9685_HTRE_DISABLED );
 
     // Get the Electronic Serial Number
-    aux  =   SI7006_GetElectronicSerialNumber ( mySI7006_I2C_parameters, &mySI7006_Data );
+    aux  =   PCA9685_GetElectronicSerialNumber ( myPCA9685_I2C_parameters, &myPCA9685_Data );
 
     // Get the Firmware revision
-    aux  =   SI7006_GetFirmwareRevision       ( mySI7006_I2C_parameters, &mySI7006_Data );
+    aux  =   PCA9685_GetFirmwareRevision       ( myPCA9685_I2C_parameters, &myPCA9685_Data );
 
 
     NRF_TIMER0->TASKS_START  =   1;                 // Start Timer0
 
-
+*/
     while( 1 )
     {
         //NRF_POWER->SYSTEMOFF = 1;
@@ -79,27 +81,27 @@ int main( void )
     	__SEV();
     	__WFE();
 
-
+/*
     	switch ( mySTATE ){
         default:
         case 1:
         // Trigger a new Humidity and Temperature measurement
             NRF_GPIO->OUTCLR             =   ( 1UL << LED1 );       // Turn the LED1 on
 
-            aux = SI7006_TriggerHumidity ( mySI7006_I2C_parameters, SI7006_MEASURE_RELATIVE_HUMIDITY_NO_HOLD_MASTER_MODE );
+            aux = PCA9685_TriggerHumidity ( myPCA9685_I2C_parameters, PCA9685_MEASURE_RELATIVE_HUMIDITY_NO_HOLD_MASTER_MODE );
             break;
 
         case 2:
         // Get the result: Humidity and Temperature
 
-            aux = SI7006_ReadHumidity           ( mySI7006_I2C_parameters, &mySI7006_Data );
-            aux = SI7006_ReadTemperatureFromRH  ( mySI7006_I2C_parameters, &mySI7006_Data );
+            aux = PCA9685_ReadHumidity           ( myPCA9685_I2C_parameters, &myPCA9685_Data );
+            aux = PCA9685_ReadTemperatureFromRH  ( myPCA9685_I2C_parameters, &myPCA9685_Data );
 
             mySTATE =   0;
             NRF_GPIO->OUTSET             =   ( 1UL << LED1 );       // Turn the LED1 off
             break;
     	}
-
+*/
         //__NOP();
     }
 }
