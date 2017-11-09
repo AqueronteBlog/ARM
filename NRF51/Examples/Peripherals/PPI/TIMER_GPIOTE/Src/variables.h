767909706e9e7b39271d0aa829eea3b3c7f5920c
AqueronteBlog <aqueronteblog@gmail.com>
2017-11-09 10:33:49 +0000

    NRF51 PPI Example. PPI Timer and GPTIOE. The variables were adapted to this example.

--- NRF51/Examples/Peripherals/PPI/TIMER_GPIOTE/Src/variables.h
+++ NRF51/Examples/Peripherals/PPI/TIMER_GPIOTE/Src/variables.h
@@ -6,8 +6,8 @@
  * @return      NA
  *
  * @author      Manuel Caballero
- * @date        22/September/2017
- * @version     22/September/2017     The ORIGIN
+ * @date        9/November/2017
+ * @version     9/November/2017     The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     NaN
@@ -27,5 +27,5 @@
 
 
 /* VARIABLES */
-uint32_t mySTATE;                       /*!<   It indicates the next action to be performed                                       */
+
 
