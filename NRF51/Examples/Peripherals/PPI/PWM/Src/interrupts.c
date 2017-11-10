905102efe6c382d39f353435314938cfd7f00db8
AqueronteBlog <aqueronteblog@gmail.com>
2017-11-10 11:40:20 +0000

    NRF51 PPI Example. PWM. Minor changes, last compilation without anny errors

---
+++ NRF51/Examples/Peripherals/PPI/PWM/Src/interrupts.c
@@ -0,0 +1,15 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        9/November/2017
+ * @version     9/November/2017    The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "interrupts.h"
---
+++ NRF51/Examples/Peripherals/PPI/PWM/Src/interrupts.h
@@ -0,0 +1,25 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        9/November/2017
+ * @version     9/November/2017    The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "variables.h"
+
+
+/* INTERRUPT PRTOTYPES */
+
+
+
+
+/* VARIABLES */
+
