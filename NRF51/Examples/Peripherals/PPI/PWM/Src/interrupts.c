65922521c9c0928ce99f4ec1acb95d1aa5a03745
AqueronteBlog <aqueronteblog@gmail.com>
2017-11-10 17:24:26 +0000

    NRF51 PPI Example. PWM. This example does NOT use any interrupts

--- NRF51/Examples/Peripherals/PPI/PWM/Src/interrupts.c
+++ NRF51/Examples/Peripherals/PPI/PWM/Src/interrupts.c
@@ -13,3 +13,31 @@
  */
 
 #include "interrupts.h"
+
+
+/**
+ * @brief       void TIMER0_IRQHandler ()
+ * @details     Timer interruption. [todo].
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        10/November/2017
+ * @version     10/November/2017   The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ */
+/*
+void TIMER0_IRQHandler()
+{
+    if ( ( NRF_TIMER0->EVENTS_COMPARE[0] != 0 ) && ( ( NRF_TIMER0->INTENSET & TIMER_INTENSET_COMPARE0_Msk ) != 0 ) )
+    {
+        NRF_TIMER0->CC[0]       +=   500;
+        NRF_TIMER0->EVENTS_COMPARE[0] = 0;                      // Clear ( flag ) compare register 0 event
+    }
+    if ( ( NRF_TIMER0->EVENTS_COMPARE[1] != 0 ) && ( ( NRF_TIMER0->INTENSET & TIMER_INTENSET_COMPARE1_Msk ) != 0 ) )
+    {
+    }
+}
+*/
