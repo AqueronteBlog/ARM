93fc0797717eef1e4cfa4e20ebaf7a3146f6409d
AqueronteBlog <aqueronteblog@gmail.com>
2017-11-12 21:11:39 +0000

    NRF51 PPI Example. RTC with GPIOTE. No interrupts are used in this example. PPI handles everything

---
+++ NRF51/Examples/Peripherals/PPI/RTC_GPTIOE/Src/interrupts.c
@@ -0,0 +1,15 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        12/November/2017
+ * @version     12/November/2017    The ORIGIN
+ * @pre         NaN
+ * @warning     NaN
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "interrupts.h"
