d845f7d58fa4c9d7c902de07c0e55fcd6b0c02ef
Manuel Caballero <aqueronteblog@gmail.com>
2017-12-20 12:06:01 +0000

    NRF51 DS3231 Example. UART pins were added

--- NRF51/Examples/Drivers/DS3231/Src/board.h
+++ NRF51/Examples/Drivers/DS3231/Src/board.h
@@ -21,6 +21,9 @@
 #define LED3        23     /*!<   LED3: P0.23   */
 #define LED4        24     /*!<   LED4: P0.24   */
 
+/* UART0 */
+#define UART0_TX    9      /*!<   UART TX: P0.9    */
+#define UART0_RX    11     /*!<   UART RX: P0.11   */
 
 /* TWI0 */
 #define TWI0_SCL    7      /*!<   TWI0 SCL: P0.7    */
