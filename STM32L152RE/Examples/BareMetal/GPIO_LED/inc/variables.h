6f71ce978c640f404d2b08a8f3e810b500b10b6a
Manuel Caballero <aqueronteblog@gmail.com>
2017-12-28 21:48:22 +0100

    STM32L152RE GPIO LED Example. Variables for this example were updated

--- STM32L152RE/Examples/BareMetal/GPIO_LED/inc/variables.h
+++ STM32L152RE/Examples/BareMetal/GPIO_LED/inc/variables.h
@@ -11,10 +11,16 @@
  * @pre         NaN
  * @warning     NaN
  */
+#include "stm32l1xx.h"
 
 #ifndef VARIABLES_H_
 #define VARIABLES_H_
 
 
+/**
+      * @brief   GENERAL VARIABLES
+      */
+uint32_t myDelay;              				/*!<  Delay                        */
+uint32_t mySystemCoreClock;					/*!<  System CLK in MHz  		   */
 
 #endif /* VARIABLES_H_ */
