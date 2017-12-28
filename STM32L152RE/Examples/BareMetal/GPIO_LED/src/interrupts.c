7777ce9b1364e4416fcb89572f1b6ae63bd04a52
Manuel Caballero <aqueronteblog@gmail.com>
2017-12-28 21:49:05 +0100

    STM32L152RE GPIO LED Example. All the interrupts are into interrupts.c file.

--- STM32L152RE/Examples/BareMetal/GPIO_LED/src/interrupts.c
+++ STM32L152RE/Examples/BareMetal/GPIO_LED/src/interrupts.c
@@ -10,5 +10,23 @@
  * @pre         NaN
  * @warning     NaN
  */
+#include "interrupts.h"
 
-
+/**
+ * @brief       void SysTick_Handler ()
+ * @details     It increments myDelay every 1ms.
+ *
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        28/December/2017
+ * @version     28/December/2017   The ORIGIN
+ * @pre         NaN.
+ * @warning     NaN
+ */
+void SysTick_Handler(void)
+{
+	myDelay++;
+}
