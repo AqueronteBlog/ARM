afea365779e9914008e07364e786493b124d0413
Manuel Caballero <aqueronteblog@gmail.com>
2018-01-21 21:15:36 +0000

    DRIVERS. DS1624, function file has just started to be completed, trigger and stop a new temperature conversion are ready to be tested

--- Drivers/DS1624/DS1624.c
+++ Drivers/DS1624/DS1624.c
@@ -31,8 +31,8 @@
  *
  *
  * @author      Manuel Caballero
- * @date        1/December/2017
- * @version     1/December/2017   The ORIGIN
+ * @date        18/January/2018
+ * @version     18/January/2018   The ORIGIN
  * @pre         NaN
  * @warning     NaN.
  */
@@ -53,66 +53,73 @@ DS1624_status_t  DS1624_Init ( I2C_parameters_t myI2Cparameters )
 
 
 /**
- * @brief       DS1624_ReadTemperature ( I2C_parameters_t , DS1624_vector_data_t* )
+ * @brief       DS1624_StartConvertTemperature ( I2C_parameters_t )
  *
- * @details     It gets the temperature.
+ * @details     It triggers a new temperature conversion.
  *
  * @param[in]    myI2Cparameters:   I2C parameters.
  *
- * @param[out]   myTemperature:     Temperature data.
+ * @param[out]   NaN.
  *
  *
- * @return       Status of DS1624_ReadTemperature.
+ * @return       Status of DS1624_StartConvertTemperature.
  *
  *
  * @author      Manuel Caballero
- * @date        11/December/2017
- * @version     11/December/2017     The ORIGIN
- * @pre         The temperature registers are updated after each user-initiated conversion and on every 64-second conversion.
+ * @date        21/January/2018
+ * @version     21/January/2018     The ORIGIN
+ * @pre         Temperature Conversion Time ( t_TC ) 200ms maximum.
  * @warning     NaN.
  */
-DS1624_status_t  DS1624_ReadTemperature   ( I2C_parameters_t myI2Cparameters, DS1624_vector_data_t*  myTemperature )
+DS1624_status_t  DS1624_StartConvertTemperature ( I2C_parameters_t myI2Cparameters )
 {
-//    uint8_t      cmd[]       =   { DS1624_MSB_TEMPERATURE, 0 };
-    uint32_t     aux         =   0;
-//
-//
-//    // It gets the temperature
-//    aux = i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
-//    aux = i2c_read  ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ) );
-//
-//
-//    // Parse the data
-//    // 1. Check if the Temperature is positive or negative
-//    if ( ( cmd[0] & 0b10000000 ) == 0b00000000 )
-//        myTemperature->Temperature   =   cmd[0];                           // Positive value
-//    else
-//        myTemperature->Temperature   =   -1.0 * ( ( ~cmd[0] ) + 1 );       // Negative value
-//
-//
-//    // 2. Decimal part. 0.25°C resolution
-//    switch( cmd[1] )
-//    {
-//        // x.00°C
-//    default:
-//    case 0b00000000:
-//        break;
-//
-//        // x.25°C
-//    case 0b01000000:
-//        myTemperature->Temperature  +=    0.25;
-//        break;
-//
-//        // x.50°C
-//    case 0b10000000:
-//        myTemperature->Temperature  +=    0.50;
-//        break;
-//
-//        // x.75°C
-//    case 0b11000000:
-//        myTemperature->Temperature  +=    0.75;
-//        break;
-//    }
+    uint8_t      cmd       =   DS1624_START_CONVERT_T;
+    i2c_status_t aux       =   0;
+
+
+    // It triggers a new temperature conversion
+    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   DS1624_SUCCESS;
+    else
+        return   DS1624_FAILURE;
+}
+
+
+/**
+ * @brief       DS1624_StopConvertTemperature ( I2C_parameters_t )
+ *
+ * @details     It halts the temperature conversion.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   NaN.
+ *
+ *
+ * @return       Status of DS1624_StopConvertTemperature.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        21/January/2018
+ * @version     21/January/2018     The ORIGIN
+ * @pre         In continuous conversion mode, a StopConvertT command halt
+ *              a continuous conversion. To restart, the StartConvertT
+ *              command must be issued. In one-shot mode, a StartConvertT command
+ *              must be issued for every temperature reading desired.
+ * @warning     NaN.
+ */
+DS1624_status_t  DS1624_StopConvertTemperature ( I2C_parameters_t myI2Cparameters )
+{
+    uint8_t      cmd       =   DS1624_STOP_CONVERT_T;
+    i2c_status_t aux       =   0;
+
+
+    // It sends the command
+    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_STOP_BIT );
 
 
 
