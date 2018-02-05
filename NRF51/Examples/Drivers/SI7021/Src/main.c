9a94b117c0e0b719b6487eabb9a530f320d5c25c
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-05 12:16:29 +0000

    NRF51 SI7021 Example. This project was completed and tested, it works as expected.

--- NRF51/Examples/Drivers/SI7021/Src/main.c
+++ NRF51/Examples/Drivers/SI7021/Src/main.c
@@ -1,15 +1,16 @@
 /**
  * @brief       main.c
- * @details     [TODO]
+ * @details     This example shows how to work with the external sensor SI7021, I2C Temperature and
+ *              Humidity sensor.
  *
- *              The voltage will change every 0.5 seconds by the timer, the rest of the time, the
- *              microcontroller is in low power.
+ *              A new humidity and temperature measurement will be triggered every 0.5 seconds by the timer,
+ *              the rest of the time, the microcontroller is in low power.
  *
- * @return      NA
+ * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        22/September/2017
- * @version     22/September/2017    The ORIGIN
+ * @date        5/February/2018
+ * @version     5/February/2018    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice
@@ -22,15 +23,15 @@
 #include "ble.h"
 #include "variables.h"
 #include "functions.h"
-#include "SI7006.h"
+#include "SI7021.h"
 
 
 int main( void )
 {
-    I2C_parameters_t           mySI7006_I2C_parameters;
+    I2C_parameters_t           mySI7021_I2C_parameters;
 
-    SI7006_status_t            aux;
-    SI7006_vector_data_t       mySI7006_Data;
+    SI7021_status_t            aux;
+    SI7021_vector_data_t       mySI7021_Data;
 
 
     conf_GPIO   ();
@@ -39,30 +40,30 @@ int main( void )
 
 
     // I2C definition
-    mySI7006_I2C_parameters.TWIinstance =    NRF_TWI0;
-    mySI7006_I2C_parameters.SDA         =    TWI0_SDA;
-    mySI7006_I2C_parameters.SCL         =    TWI0_SCL;
-    mySI7006_I2C_parameters.ADDR        =    SI7006_ADDRESS;
-    mySI7006_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
-    mySI7006_I2C_parameters.SDAport     =    NRF_GPIO;
-    mySI7006_I2C_parameters.SCLport     =    NRF_GPIO;
+    mySI7021_I2C_parameters.TWIinstance =    NRF_TWI0;
+    mySI7021_I2C_parameters.SDA         =    TWI0_SDA;
+    mySI7021_I2C_parameters.SCL         =    TWI0_SCL;
+    mySI7021_I2C_parameters.ADDR        =    SI7021_ADDRESS;
+    mySI7021_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+    mySI7021_I2C_parameters.SDAport     =    NRF_GPIO;
+    mySI7021_I2C_parameters.SCLport     =    NRF_GPIO;
 
     // Configure I2C peripheral
-    aux  =   SI7006_Init ( mySI7006_I2C_parameters );
+    aux  =   SI7021_Init ( mySI7021_I2C_parameters );
 
 
     // Reset the device
-    aux  =   SI7006_SoftReset ( mySI7006_I2C_parameters );
+    aux  =   SI7021_SoftReset ( mySI7021_I2C_parameters );
     nrf_delay_ms ( 15 );
 
     // Configure the device
-    aux  =   SI7006_Conf  ( mySI7006_I2C_parameters, SI7006_RESOLUTION_RH_12_TEMP_14, SI7006_HTRE_DISABLED );
+    aux  =   SI7021_Conf  ( mySI7021_I2C_parameters, SI7021_RESOLUTION_RH_12_TEMP_14, SI7021_HTRE_DISABLED );
 
     // Get the Electronic Serial Number
-    aux  =   SI7006_GetElectronicSerialNumber ( mySI7006_I2C_parameters, &mySI7006_Data );
+    aux  =   SI7021_GetElectronicSerialNumber ( mySI7021_I2C_parameters, &mySI7021_Data );
 
     // Get the Firmware revision
-    aux  =   SI7006_GetFirmwareRevision       ( mySI7006_I2C_parameters, &mySI7006_Data );
+    aux  =   SI7021_GetFirmwareRevision       ( mySI7021_I2C_parameters, &mySI7021_Data );
 
 
     NRF_TIMER0->TASKS_START  =   1;                 // Start Timer0
@@ -86,14 +87,14 @@ int main( void )
         // Trigger a new Humidity and Temperature measurement
             NRF_GPIO->OUTCLR             =   ( 1UL << LED1 );       // Turn the LED1 on
 
-            aux = SI7006_TriggerHumidity ( mySI7006_I2C_parameters, SI7006_MEASURE_RELATIVE_HUMIDITY_NO_HOLD_MASTER_MODE );
+            aux = SI7021_TriggerHumidity ( mySI7021_I2C_parameters, SI7021_MEASURE_RELATIVE_HUMIDITY_NO_HOLD_MASTER_MODE );
             break;
 
         case 2:
         // Get the result: Humidity and Temperature
 
-            aux = SI7006_ReadHumidity           ( mySI7006_I2C_parameters, &mySI7006_Data );
-            aux = SI7006_ReadTemperatureFromRH  ( mySI7006_I2C_parameters, &mySI7006_Data );
+            aux = SI7021_ReadHumidity           ( mySI7021_I2C_parameters, &mySI7021_Data );
+            aux = SI7021_ReadTemperatureFromRH  ( mySI7021_I2C_parameters, &mySI7021_Data );
 
             mySTATE =   0;
             NRF_GPIO->OUTSET             =   ( 1UL << LED1 );       // Turn the LED1 off
