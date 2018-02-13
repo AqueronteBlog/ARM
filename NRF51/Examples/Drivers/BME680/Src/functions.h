86b72cbed99d66488069b95124531c29f33e6ba8
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-13 12:38:49 +0000

    NRF51 BME680 Example. The project was just started, it needs to be developed.

---
+++ NRF51/Examples/Drivers/BME680/Src/functions.h
@@ -0,0 +1,23 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        18/January/2018
+ * @version     18/January/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+#include "variables.h"
+
+/* FUNCTION PROTOTYPES */
+void conf_GPIO      ( void );
+void conf_TIMER0    ( void );
+void conf_UART      ( void );
+
