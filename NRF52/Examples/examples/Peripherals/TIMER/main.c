914651de1b6521db30146878c33203e78c2221df
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-14 20:29:05 +0000

    NRF52 TIMERs Example. The project was completed and tested, it works as expected.

--- NRF52/Examples/examples/Peripherals/TIMER/main.c
+++ NRF52/Examples/examples/Peripherals/TIMER/main.c
@@ -1,8 +1,15 @@
 /**
  * @brief       main.c
- * @details     [TODO].
+ * @details     It changes the state of the LEDs using the Timers.
  *
- *              The microcontroller remains in low power the rest of the time.
+ *                  - LED1. Timer0: 32-bit, Channel 0, Interrupt ON.    Blinks every 0.125s.
+ *                  - LED2. Timer1: 16-bit, Channel 0, Interrupt OFF.   Blinks every 0.25s.
+ *                  - LED3. Timer2:  8-bit, Channel 0, Interrupt ON.    Blinks every 0.375s.
+ *                  - LED4. Timer0: 32-bit, Channel 1, Interrupt ON.    Blinks every 0.5s.
+ *
+ *              This firmware is just an example about how to use the timers on the NRF52832. On
+ *              polling mode or by interrupts with different channels.
+ *
  *
  * @return      N/A
  *
@@ -33,22 +40,29 @@ int main(void)
 {
   conf_GPIO   ();
   conf_TIMER0 ();
-  
+  conf_TIMER1 ();
+  conf_TIMER2 ();
 
-  NRF_TIMER0->TASKS_START  =   1;               // Start Timer0
-
+  NRF_TIMER0->TASKS_START = 1;    // Start Timer0
+  NRF_TIMER2->TASKS_START = 1;    // Start Timer2
 
   while( 1 )
   {
-    //NRF_POWER->SYSTEMOFF = 1;
-    NRF_POWER->TASKS_LOWPWR = 1;                // Sub power mode: Low power.
+      NRF_P0->OUTSET        =   ( 1 << LED2 );
+      delay();
+      NRF_P0->OUTCLR        =   ( 1 << LED2 );
+      delay();
 
-    // Enter System ON sleep mode
-    __WFE();
-    // Make sure any pending events are cleared
-    __SEV();
-    __WFE();
-  }
+      /*
+      //NRF_POWER->SYSTEMOFF = 1;
+      NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
+      // Enter System ON sleep mode
+      __WFE();
+      // Make sure any pending events are cleared
+      __SEV();
+      __WFE();
+      */
+    }
 }
 
 
