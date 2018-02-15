16dd10d93bd2351abaf12df9b5cc7e1a58567d95
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-15 21:03:17 +0000

    NRF52 RTC Example. The main program is ready to be tested.

--- NRF52/Examples/examples/Peripherals/RTC/main.c
+++ NRF52/Examples/examples/Peripherals/RTC/main.c
@@ -1,21 +1,18 @@
 /**
  * @brief       main.c
- * @details     It changes the state of the LEDs using the Timers.
+ * @details     It changes the state of the LED1 and LED4 using the RTCs.
  *
- *                  - LED1. Timer0: 32-bit, Channel 0, Interrupt ON.    Blinks every 0.125s.
- *                  - LED2. Timer1: 16-bit, Channel 0, Interrupt OFF.   Blinks every 0.25s.
- *                  - LED3. Timer2:  8-bit, Channel 0, Interrupt ON.    Blinks every 0.375s.
- *                  - LED4. Timer0: 32-bit, Channel 1, Interrupt ON.    Blinks every 0.5s.
+ *                  - LED1. RTC0: Tick, Interrupt ON.           Blinks every 125ms.
+ *                  - LED4. RTC1: Channel 0, Interrupt ON.      Blinks every 2s.
  *
- *              This firmware is just an example about how to use the timers on the NRF52832. On
- *              polling mode or by interrupts with different channels.
+ *              This firmware is just an example about how to use the RTCs on the NRF51832.
  *
  *
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        14/February/2018
- * @version     14/February/2018    The ORIGIN
+ * @date        15/February/2018
+ * @version     15/February/2018    The ORIGIN
  * @pre         This firmware was tested on the nrf52-DK with Segger Embedded Studio v3.34
  *              ( SDK 14.2.0 ).
  * @warning     The softdevice (s132) is taken into account, Bluetooth was not used although.
@@ -39,29 +36,23 @@
 int main(void)
 {
   conf_GPIO   ();
-  conf_TIMER0 ();
-  conf_TIMER1 ();
-  conf_TIMER2 ();
+  conf_LFCLK  ();
+  conf_RTC0   ();
+  conf_RTC2   ();
 
-  NRF_TIMER0->TASKS_START = 1;    // Start Timer0
-  NRF_TIMER2->TASKS_START = 1;    // Start Timer2
+  NRF_RTC0->TASKS_START = 1;    // Start RTC0
+  NRF_RTC2->TASKS_START = 1;    // Start RTC1
 
   while( 1 )
   {
-      NRF_P0->OUTSET        =   ( 1 << LED2 );
-      delay();
-      NRF_P0->OUTCLR        =   ( 1 << LED2 );
-      delay();
-
-      /*
-      //NRF_POWER->SYSTEMOFF = 1;
+      // NRF_POWER->SYSTEMOFF    = 1;
       NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
+
       // Enter System ON sleep mode
       __WFE();
       // Make sure any pending events are cleared
       __SEV();
       __WFE();
-      */
     }
 }
 
