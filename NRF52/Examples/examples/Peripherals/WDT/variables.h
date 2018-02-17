9054484c506d27e0e77c804417b900f03d2ddbc1
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-17 20:23:15 +0000

    NRF52 WDT Example. Files Board and Variables were added.

---
+++ NRF52/Examples/examples/Peripherals/WDT/variables.h
@@ -0,0 +1,45 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        17/February/2018
+ * @version     17/February/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "board.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* CONSTANTS */
+#define ON  1
+#define OFF 0
+
+
+/* VARIABLES */
+/* VARIABLES */
+static uint32_t changeLEDsSTATE;       /*!<   changeLEDsSTATE = ON:     [todo].
+                                              changeLEDsSTATE = OFF:    [todo]         */
+
+static uint32_t my375msDelay;          /*!<   counter for the Timer2 to generate a
+                                              delay of 0.375s                         */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
