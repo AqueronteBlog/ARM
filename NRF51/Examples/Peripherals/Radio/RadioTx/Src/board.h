6601f7a9b007fc3a686325687e1bef5c40878154
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-19 20:29:22 +0000

    NRF51 RADIO TX Example. Files like Variables and Board were just added.

---
+++ NRF51/Examples/Peripherals/Radio/RadioTx/Src/board.h
@@ -0,0 +1,28 @@
+/**
+ * @brief       board.h
+ * @details     It characterizes the board ( for this example ): nrf51-DK.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        19/February/2018
+ * @version     19/February/2018    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+/* LEDs */
+#define LED1        21     /*!<   LED1: P0.21   */
+#define LED2        22     /*!<   LED2: P0.22   */
+#define LED3        23     /*!<   LED3: P0.23   */
+#define LED4        24     /*!<   LED4: P0.24   */
+
+/* UART0 */
+#define UART0_TX    9      /*!<   UART TX: P0.9    */
+#define UART0_RX    11     /*!<   UART RX: P0.11   */
+
+/* TWI0 */
+#define TWI0_SCL    7      /*!<   TWI0 SCL: P0.7    */
+#define TWI0_SDA    30     /*!<   TWI0 SDA: P0.30   */
