198d93a111d5d238bb67130cb6848ac220ad1857
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-20 21:24:49 +0000

    NRF52 RADIO_RX Example. Interrupt files were added for this example

---
+++ NRF52/Examples/examples/Peripherals/RADIO_RX/interrupts.c
@@ -0,0 +1,16 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        17/February/2018
+ * @version     17/February/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
+
---
+++ NRF52/Examples/examples/Peripherals/RADIO_RX/interrupts.h
@@ -0,0 +1,36 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        17/February/2018
+ * @version     17/February/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "variables.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* INTERRUPT PRTOTYPES */
+void WDT_IRQHandler ( void );
+
+
+/* VARIABLES */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
