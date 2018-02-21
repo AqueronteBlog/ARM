63f78742294c1003abee7db3d9897b3ce1d67929
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-21 13:48:10 +0000

    NRF52 RADIO_TX Example. The example works but I would like to use Matching address instead of CRC

--- NRF52/Examples/examples/Peripherals/RADIO_TX/main.c
+++ NRF52/Examples/examples/Peripherals/RADIO_TX/main.c
@@ -23,37 +23,43 @@
 #include "nordic_common.h"
 #include "nrf.h"
 #include "variables.h"
+#include "nrf_delay.h"
+
 
 /**@brief Function for application main entry.
  */
 int main(void) {
-  uint8_t myPacket[16] = {0};
-  uint8_t myLEDS_state = 0;
+  uint8_t myPacket[20] = "TontoElQueLoLea!";
+  uint8_t myChangePackage = 0;
 
   conf_CLK    ();
   conf_GPIO   ();
   conf_RADIO  ( &myPacket[0] );
 
   while (1) {
-    NRF_RADIO->TASKS_RXEN = 1;
-    while (NRF_RADIO->EVENTS_DISABLED == 0);
-    NRF_RADIO->EVENTS_DISABLED = 0;
+    NRF_P0->OUTCLR     =   ( 1 << LED1 );
 
 
+    NRF_RADIO->TASKS_TXEN       =    1;
+    while ( ( NRF_RADIO->EVENTS_DISABLED ) == 0 );    // [TODO]       This is DANGEROUS, if something goes wrong, the uC will get stuck here!!!.
+                                                      // [WORKAROUND] Insert a counter.
+    NRF_RADIO->EVENTS_DISABLED  =    0;
 
-    if (NRF_RADIO->EVENTS_CRCOK == 1) {
-      if ( myLEDS_state == 0 )
+    NRF_P0->OUTSET     =   ( 1 << LED1 );
+
+    nrf_delay_ms ( 1000 );
+    
+    if ( myChangePackage == 0 )
       {
-        NRF_P0->OUTCLR  = ( ( 1 << LED1 ) | ( 1 << LED2 ) | ( 1 << LED3 ) | ( 1 << LED4 ) );
-        myLEDS_state    = 1;
+        myPacket[15]   =   '?';
+        myChangePackage    = 1;
       }
       else{
-        NRF_P0->OUTSET  = ( ( 1 << LED1 ) | ( 1 << LED2 ) | ( 1 << LED3 ) | ( 1 << LED4 ) );
-        myLEDS_state    = 0;
+        myPacket[15]   =   '!';
+        myChangePackage    = 0;
       }
 
-      NRF_RADIO->EVENTS_CRCOK = 0;
-    }
+
 
     /*
       //NRF_POWER->SYSTEMOFF = 1;
