362d8a189312b2b02de2031af7c0f5e6f02fc5c2
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-22 16:05:18 +0000

    NRF51 AS3933 Example. Board and Variables files were added

---
+++ NRF51/Examples/Drivers/AS3933/Src/board.h
@@ -0,0 +1,34 @@
+/**
+ * @brief       board.h
+ * @details     It characterizes the board ( for this example ): nrf51-DK.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        22/February/2018
+ * @version     22/February/2018      The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+/* LEDs */
+#define LED1        21     /*!<   LED1: P0.21   */
+#define LED2        22     /*!<   LED2: P0.22   */
+#define LED3        23     /*!<   LED3: P0.23   */
+#define LED4        24     /*!<   LED4: P0.24   */
+
+/* UART0 */
+#define UART0_TX    9      /*!<   UART TX: P0.9    */
+#define UART0_RX    11     /*!<   UART RX: P0.11   */
+
+/* TWI0 */
+#define TWI0_SCL    7      /*!<   TWI0 SCL: P0.7    */
+#define TWI0_SDA    30     /*!<   TWI0 SDA: P0.30   */
+
+/* SPI0 */
+#define SPI0_SCK    29     /*!<   SPI0 SCLK: P0.29   */
+#define SPI0_MOSI   25     /*!<   SPI0 MOSI: P0.25   */
+#define SPI0_MISO   28     /*!<   SPI0 MISO: P0.28   */
+#define SPI0_CS     5      /*!<   SPI0 CS:   P0.5    */
