ff17766dc230d434e2382818933993bf3968eee5
Manuel Caballero <aqueronteblog@gmail.com>
2018-02-23 16:30:17 +0000

    NRF51 AS3933 Example. Minor changes, last compilation without any errors

---
+++ NRF51/Examples/Drivers/AS3933/obj/Debug/~#/~#/~#/~#/Drivers/AS3933/as3933.d
@@ -0,0 +1,8 @@
+obj\debug\~\#\~\#\~\#\~\#\drivers\as3933\as3933.o: \
+ ..\..\..\..\Drivers\AS3933\AS3933.c ..\..\..\..\Drivers\AS3933\AS3933.h \
+ ..\..\Basics\SPI/spi.h Inc/nrf.h Inc/nrf51.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cm0.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cminstr.h \
+ c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cmfunc.h \
+ Inc/system_nrf51.h Inc/nrf51_bitfields.h Inc/nrf51_deprecated.h \
+ Inc/compiler_abstraction.h Inc/nrf51_bitfields.h
