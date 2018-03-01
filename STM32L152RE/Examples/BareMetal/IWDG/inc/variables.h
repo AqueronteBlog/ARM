762926865c996d01041e6dad560f996a7b7b843d
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-01 20:20:30 +0000

    STM32L152RE IWDG Example. Variable and Board files were added.

---
+++ STM32L152RE/Examples/BareMetal/IWDG/inc/variables.h
@@ -0,0 +1,34 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        1/March/2018
+ * @version     1/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "stm32l1xx.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+      * @brief   GENERAL VARIABLES
+      */
+uint32_t myDelay;              				/*!<  Delay                        */
+uint32_t mySystemCoreClock;					/*!<  System CLK in MHz  		   */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
