9abcca03654df7b7e1c7611f5e9ea774491b77dc
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-07 19:29:21 +0000

    NRF52 GPIOTE Example.. Interrupts files were added

---
+++ NRF52/Examples/examples/Peripherals/GPIOTE/interrupts.c
@@ -0,0 +1,16 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        7/March/2018
+ * @version     7/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
+
---
+++ NRF52/Examples/examples/Peripherals/GPIOTE/interrupts.h
@@ -0,0 +1,35 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        7/March/2018
+ * @version     7/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "variables.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* INTERRUPT PRTOTYPES */
+
+
+/* VARIABLES */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
