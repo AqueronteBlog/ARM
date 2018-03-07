b8a4ea40f730cb6cc2859cfc6192b514817ad88e
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-07 19:29:04 +0000

    NRF52 GPIOTE Example.. Function files were added

---
+++ NRF52/Examples/examples/Peripherals/GPIOTE/functions.c
@@ -0,0 +1,48 @@
+/**
+ * @brief       functions.c
+ * @details     Functions.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        7/March/2018
+ * @version     7/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "functions.h"
+
+
+
+/**
+ * @brief       void conf_GPIO  ( void )
+ * @details     It configures GPIO to work with the LEDs.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        12/February/2018
+ * @version     13/February/2018   All the LEDs OFF at the beginning.
+ *              12/February/2018   The ORIGIN   
+ * @pre         N/A
+ * @warning     N/A
+ */
+void conf_GPIO  ( void )
+{
+    uint32_t i = 0;
+
+    for ( i = LED1; i < ( LED4 + 1 ); i++ )
+    {
+      NRF_P0->PIN_CNF[i]    =   ( GPIO_PIN_CNF_DIR_Output         <<  GPIO_PIN_CNF_DIR_Pos    ) |
+                                ( GPIO_PIN_CNF_INPUT_Disconnect   <<  GPIO_PIN_CNF_INPUT_Pos  ) |
+                                ( GPIO_PIN_CNF_PULL_Disabled      <<  GPIO_PIN_CNF_PULL_Pos   ) |
+                                ( GPIO_PIN_CNF_DRIVE_S0S1         <<  GPIO_PIN_CNF_DRIVE_Pos  ) |
+                                ( GPIO_PIN_CNF_SENSE_Disabled     <<  GPIO_PIN_CNF_SENSE_Pos  );
+
+      NRF_P0->OUTSET        =   ( 1 << i );
+    }
+}
+
---
+++ NRF52/Examples/examples/Peripherals/GPIOTE/functions.h
@@ -0,0 +1,34 @@
+/**
+ * @brief       functions.h
+ * @details     Functions header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        7/March/2018
+ * @version     7/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "variables.h"
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* FUNCTION PROTOTYPES */
+void conf_GPIO   ( void );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTION_H_ */
