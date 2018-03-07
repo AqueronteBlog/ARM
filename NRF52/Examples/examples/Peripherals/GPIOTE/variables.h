5765fc863fbb1a8c5c260a7a3b0f87a400e0aee9
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-07 19:28:42 +0000

    NRF52 GPIOTE Example.. Variables and Board files were added.

---
+++ NRF52/Examples/examples/Peripherals/GPIOTE/variables.h
@@ -0,0 +1,39 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        7/March/2018
+ * @version     7/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "board.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* CONSTANTS */
+#define ON  1
+#define OFF 0
+
+
+/* VARIABLES */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
