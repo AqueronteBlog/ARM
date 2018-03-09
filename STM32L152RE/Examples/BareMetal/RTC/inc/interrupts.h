085d6c685b1822554718b70fba6e13aa7e9c819e
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-09 16:45:07 +0000

    STM32L152RE IWDG Example. Interrupts files were added

---
+++ STM32L152RE/Examples/BareMetal/RTC/inc/interrupts.h
@@ -0,0 +1,34 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        9/March/2018
+ * @version     9/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "stm32l1xx.h"
+#include "variables.h"
+#include "board.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PROTOTYPES
+
+// VARIABLES
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
