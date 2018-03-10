72bec574fc5701c056fcdd5bdc80503f07ea35cf
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-10 21:03:52 +0000

    NRF52 SPI Example. Main file was just added.

---
+++ NRF52/Examples/examples/Peripherals/SPI/main.c
@@ -0,0 +1,55 @@
+/**
+ * @brief       main.c
+ * @details     [todo].
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        10/March/2018
+ * @version     10/March/2018    The ORIGIN
+ * @pre         This firmware was tested on the nrf52-DK with Segger Embedded Studio v3.34a
+ *              ( SDK 14.2.0 ).
+ * @warning     The softdevice (s132) is taken into account, Bluetooth was not used although.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "nordic_common.h"
+#include "nrf.h"
+#include "variables.h"
+#include "functions.h"
+#include "interrupts.h"
+
+
+
+/**@brief Function for application main entry.
+ */
+int main(void)
+{
+  conf_GPIO  ();
+  conf_WDT   ();
+  
+
+  NRF_WDT->TASKS_START = 1;               // Start watchdog ( WDT )
+
+  while( 1 )
+  {
+      //NRF_POWER->SYSTEMOFF = 1;
+      NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
+
+      // Enter System ON sleep mode
+      __WFE();
+      // Make sure any pending events are cleared
+      __SEV();
+      __WFE();
+    }
+}
+
+
+/**
+ * @}
+ */
