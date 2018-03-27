30143aaadeff0afd6d395ed194dcf52a7a3034e9
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-27 20:02:19 +0100

    NRF52 PWM Example. Board file was added

---
+++ NRF52/Examples/examples/Peripherals/PWM/board.h
@@ -0,0 +1,35 @@
+/**
+ * @brief       board.h
+ * @details     It characterizes the board ( for this example ): nrf52-DK.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        27/March/2018
+ * @version     27/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* LEDs */
+typedef enum{
+  LED1  = 17,               /*!<   LED1: P0.17   */
+  LED2  = 18,               /*!<   LED2: P0.18   */
+  LED3  = 19,               /*!<   LED3: P0.19   */
+  LED4  = 20                /*!<   LED4: P0.20   */
+} NRF52DK_board_LEDS_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H_ */
