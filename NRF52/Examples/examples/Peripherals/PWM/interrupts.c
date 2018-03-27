ac9082652b4ef871c38b61e399cbaedd7e426824
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-27 20:03:00 +0100

    NRF52 PWM Example. Interrupts files were added.

---
+++ NRF52/Examples/examples/Peripherals/PWM/interrupts.c
@@ -0,0 +1,16 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        27/March/2018
+ * @version     27/March/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
+
