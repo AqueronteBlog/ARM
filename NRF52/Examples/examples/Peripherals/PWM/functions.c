aacc3b223f02cc6b8dc7180230501e0a0b6b3f24
Manuel Caballero <aqueronteblog@gmail.com>
2018-03-29 10:17:38 +0100

    NRF52 PWM Example. PWM0 frequency was updated to 4Hz. All the channels at 50% duty cycle

--- NRF52/Examples/examples/Peripherals/PWM/functions.c
+++ NRF52/Examples/examples/Peripherals/PWM/functions.c
@@ -52,26 +52,26 @@ void conf_GPIO  ( void )
  * @details     It configures the PWM0 peripheral for 4 channels.
  *
  *              PWM frequency:  
- *                  * f_PWM = 100Hz ( COUNTER = T_PWM / T_PWM_CLK = ( 100 / 2MHz )^( -1 ) = 20000 ) 
+ *                  * f_PWM = 4Hz ( COUNTER = T_PWM / T_PWM_CLK = ( 4 / 125kHz )^( -1 ) = 31250 ) 
  *
  *              Channel0:
  *                  * LED1.
- *                  * Duty cycle: 50% ( COUNTER * 0.5 = 10000 ).
+ *                  * Duty cycle: 50% ( COUNTER * 0.5 = 15625 ).
  *                  * Polarity: LOW.
  *
  *              Channel1:
  *                  * LED2.
- *                  * Duty cycle: 50% ( COUNTER * 0.5 = 10000 ).
+ *                  * Duty cycle: 50% ( COUNTER * 0.5 = 15625 ).
  *                  * Polarity: HIGH.
  *
  *              Channel2:
  *                  * LED3.
- *                  * Duty cycle: 50% ( COUNTER * 0.5 = 10000 ).
+ *                  * Duty cycle: 50% ( COUNTER * 0.5 = 15625 ).
  *                  * Polarity: LOW.
  *
  *              Channel3:
  *                  * LED4.
- *                  * Duty cycle: 50% ( COUNTER * 0.5 = 10000 ).
+ *                  * Duty cycle: 50% ( COUNTER * 0.5 = 15625 ).
  *                  * Polarity: HIGH.
  *
  *
@@ -79,7 +79,8 @@ void conf_GPIO  ( void )
  *
  * @author      Manuel Caballero
  * @date        27/March/2018
- * @version     28/March/2018   PWM0 function was completed, the comments were updated too.
+ * @version     29/March/2018   PWM0 frequency was updated to 4Hz.
+ *              28/March/2018   PWM0 function was completed, the comments were updated too.
  *              27/March/2018   The ORIGIN
  * @pre         N/A
  * @warning     N/A.
@@ -87,7 +88,7 @@ void conf_GPIO  ( void )
 void conf_PWM0  ( void )
 {
   /* Duty cycle for 4 channels  */
-  uint16_t pwm_seq[]     =   { 10000, 10000 | 0x8000, 10000, 10000 | 0x8000 };
+  uint16_t pwm_seq[]     =   { 15625, 15625 | 0x8000, 15625, 15625 | 0x8000 };
 
   
   /* Connect 4 channels  */
@@ -110,11 +111,11 @@ void conf_PWM0  ( void )
   /* PWM0 UP mode */
   NRF_PWM0->MODE        =   ( PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos );
   
-  /* f_PWM_CLK = 2Mhz */
-  NRF_PWM0->PRESCALER   =   ( PWM_PRESCALER_PRESCALER_DIV_8 << PWM_PRESCALER_PRESCALER_Pos );
+  /* f_PWM_CLK = 125khz */
+  NRF_PWM0->PRESCALER   =   ( PWM_PRESCALER_PRESCALER_DIV_128 << PWM_PRESCALER_PRESCALER_Pos );
   
-  /* f_PWM0 = ( 2MHz / 20000 ) = 100Hz ( 10ms ) */
-  NRF_PWM0->COUNTERTOP  =   ( 20000 << PWM_COUNTERTOP_COUNTERTOP_Pos );                 
+  /* f_PWM0 = ( 125kHz / 31250 ) = 4Hz ( 250ms ) */
+  NRF_PWM0->COUNTERTOP  =   ( 31250 << PWM_COUNTERTOP_COUNTERTOP_Pos );                 
   
   /* LOPP disabled */
   NRF_PWM0->LOOP        =   ( PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos );
