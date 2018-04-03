04198fdc5cf10493cec5e2eb779aec1952854f0b
Manuel Caballero <aqueronteblog@gmail.com>
2018-04-03 19:58:07 +0100

    NRF52 SWI Example. Main file was added

---
+++ NRF52/Examples/examples/Peripherals/SWI/main.c
@@ -0,0 +1,60 @@
+/**
+ * @brief       main.c
+ * @details     [todo].
+ *
+ *                  - LED1. RTC0: Tick, Interrupt ON.           Blinks every 125ms.
+ *                  - LED4. RTC1: Channel 0, Interrupt ON.      Blinks every 2s.
+ *
+ *              This firmware is just an example about how to use the RTCs on the NRF51832.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        3/April/2018
+ * @version     3/April/2018    The ORIGIN
+ * @pre         This firmware was tested on the nrf52-DK with Segger Embedded Studio v3.34a
+ *              ( SDK 14.2.0 ).
+ * @warning     The softdevice (s132) is taken into account, Bluetooth was not used although.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "nordic_common.h"
+#include "nrf.h"
+#include "variables.h"
+#include "functions.h"
+#include "interrupts.h"
+
+
+
+/**@brief Function for application main entry.
+ */
+int main(void)
+{
+  conf_GPIO   ();
+  conf_LFCLK  ();
+  conf_RTC0   ();
+
+  NRF_RTC0->TASKS_START = 1;    // Start RTC0
+
+  while( 1 )
+  {
+      // NRF_POWER->SYSTEMOFF    = 1;
+      NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
+
+      // Enter System ON sleep mode
+      __WFE();
+      // Make sure any pending events are cleared
+      __SEV();
+      __WFE();
+    }
+}
+
+
+/**
+ * @}
+ */
