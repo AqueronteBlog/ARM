ed45210a0497b3bddb1a6034fe490d58faea1436
Manuel Caballero <aqueronteblog@gmail.com>
2018-04-03 20:37:51 +0100

    NRF52 SWI Example. The project was completed and tested, it works as expected.

--- NRF52/Examples/examples/Peripherals/SWI/main.c
+++ NRF52/Examples/examples/Peripherals/SWI/main.c
@@ -1,11 +1,16 @@
 /**
  * @brief       main.c
- * @details     [todo].
+ * @details     This example shows how the software interrupts work. Every software interrupt
+ *              will be linked to one LED.
  *
- *                  - LED1. RTC0: Tick, Interrupt ON.           Blinks every 125ms.
- *                  - LED4. RTC1: Channel 0, Interrupt ON.      Blinks every 2s.
+ *              Every 125ms, the state of the LEDs will change depending on the following:
  *
- *              This firmware is just an example about how to use the RTCs on the NRF51832.
+ *                  - SWI5: Turn on the LED1.
+ *                  - SWI1: Turn on the LED2.
+ *                  - SWI3: Turn on the LED3.
+ *                  - SWI4: Turn on the LED4.
+ *
+ *              This firmware is just an example about how to use the SWI on the NRF52832.
  *
  *
  * @return      N/A
@@ -30,6 +35,14 @@
 #include "interrupts.h"
 
 
+/**@brief CONSTANTS.
+ */
+
+/**@brief VARIABLES.
+ */
+uint32_t chooseLEDON   =   LED1_ON;    /*< Variable to choose which LED to turn on   >*/
+
+
 
 /**@brief Function for application main entry.
  */
@@ -38,20 +51,21 @@ int main(void)
   conf_GPIO   ();
   conf_LFCLK  ();
   conf_RTC0   ();
+  conf_SWI    ();
 
   NRF_RTC0->TASKS_START = 1;    // Start RTC0
+  
+  // NRF_POWER->SYSTEMOFF    = 1;
+  NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
 
   while( 1 )
   {
-      // NRF_POWER->SYSTEMOFF    = 1;
-      NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
-
       // Enter System ON sleep mode
       __WFE();
       // Make sure any pending events are cleared
       __SEV();
       __WFE();
-    }
+   }
 }
 
 
