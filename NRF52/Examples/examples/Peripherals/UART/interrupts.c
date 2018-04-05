00a3b35d3f1c10484702e736a379442513258814
Manuel Caballero <aqueronteblog@gmail.com>
2018-04-05 20:09:48 +0100

    NRF52 UART Example. Interrupt files were added

---
+++ NRF52/Examples/examples/Peripherals/UART/interrupts.c
@@ -0,0 +1,16 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        5/April/2018
+ * @version     5/April/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
+
---
+++ NRF52/Examples/examples/Peripherals/UART/interrupts.h
@@ -0,0 +1,37 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        5/April/2018
+ * @version     5/April/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "board.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* INTERRUPT PRTOTYPES */
+
+
+/* VARIABLES */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
