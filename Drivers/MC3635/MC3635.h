21f9ce560950cfb60691310d6df13e373a6264de
Manuel Caballero <aqueronteblog@gmail.com>
2018-04-06 20:13:15 +0100

    DRIVERS. MC3635. Some comments were updated

--- Drivers/MC3635/MC3635.h
+++ Drivers/MC3635/MC3635.h
@@ -1,19 +1,18 @@
 /**
- * @brief       ADXL345.h
- * @details     3-Axis, ±2 g/±4 g/±8 g/±16 g Digital Accelerometer.
+ * @brief       MC3635.h
+ * @details     3-Axis Accelerometer.
  *              Header file.
  *
  *
- * @return      NA
+ * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        11/August/2017
- * @version     11/August/2017    The ORIGIN
- * @pre         Although the ADXL345 can be handled by SPI or I2C, this library is only
- *              adapted to be used by I2C.
- * @pre         FIFO functions are NOT implemented.
- * @warning     NaN
- * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ * @date        6/April/2018
+ * @version     6/April/2018   The ORIGIN
+ * @pre         Although the MC3635 can be handled by SPI or I2C, this library is only
+ *              adapted to be used by I2C..
+ * @warning     N/A
+ * @pre         This code belongs to Nimbus Centre ( http://www.nimbus.cit.ie ).
  */
 
 
@@ -21,49 +20,57 @@
 #include "stdbool.h"
 #include "i2c.h"
 
+#ifndef MC3635_H_
+#define MC3635_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 
 /**
   * @brief   DEFAULT ADDRESSES
   */
 typedef enum{
-    ADXL345_ALT_ADDRESS_LOW     =   0x53,
-    ADXL345_ALT_ADDRESS_HIGH    =   0x1D
-} ADXL345_address_t;
+    MC3635_ALT_ADDRESS_LOW     =   0x53,
+    MC3635_ALT_ADDRESS_HIGH    =   0x1D
+} MC3635_address_t;
 
 
 /**
   * @brief   REGISTERS MAP
   */
-#define ADXL345_DEVID                           0x00        /*!<   Device ID                                                    */
-#define ADXL345_THRESH_TAP                      0x1D        /*!<   Tap threshold                                                */
-#define ADXL345_OFSX                            0x1E        /*!<   X-axis offset                                                */
-#define ADXL345_OFSY                            0x1F        /*!<   Y-axis offset                                                */
-#define ADXL345_OFSZ                            0x20        /*!<   Z-axis offset                                                */
-#define ADXL345_DUR                             0x21        /*!<   Tap duration                                                 */
-#define ADXL345_LATENT                          0x22        /*!<   Tap latency                                                  */
-#define ADXL345_WINDOW                          0x23        /*!<   Tap window                                                   */
-#define ADXL345_THRESH_ACT                      0x24        /*!<   Activity threshold                                           */
-#define ADXL345_THRESH_INACT                    0x25        /*!<   Inactivity threshold                                         */
-#define ADXL345_TIME_INACT                      0x26        /*!<   Inactivity time                                              */
-#define ADXL345_ACT_INACT_CTL                   0x27        /*!<   Axis enable control for activity and inactivity detection    */
-#define ADXL345_THRESH_FF                       0x28        /*!<   Free-fall threshold                                          */
-#define ADXL345_TIME_FF                         0x29        /*!<   Free-fall time                                               */
-#define ADXL345_TAP_AXES                        0x2A        /*!<   Axis control for single tap/double tap                       */
-#define ADXL345_ACT_TAP_STATUS                  0x2B        /*!<   Source of single tap/double tap                              */
-#define ADXL345_BW_RATE                         0x2C        /*!<   Data rate and power mode control                             */
-#define ADXL345_POWER_CTL                       0x2D        /*!<   Power-saving features control                                */
-#define ADXL345_INT_ENABLE                      0x2E        /*!<   Interrupt enable control                                     */
-#define ADXL345_INT_MAP                         0x2F        /*!<   Interrupt mapping control                                    */
-#define ADXL345_INT_SOURCE                      0x30        /*!<   Source of interrupts                                         */
-#define ADXL345_DATA_FORMAT                     0x31        /*!<   Data format control                                          */
-#define ADXL345_DATAX0                          0x32        /*!<   X-Axis Data 0                                                */
-#define ADXL345_DATAX1                          0x33        /*!<   X-Axis Data 1                                                */
-#define ADXL345_DATAY0                          0x34        /*!<   Y-Axis Data 0                                                */
-#define ADXL345_DATAY1                          0x35        /*!<   Y-Axis Data 1                                                */
-#define ADXL345_DATAZ0                          0x36        /*!<   Z-Axis Data 0                                                */
-#define ADXL345_DATAZ1                          0x37        /*!<   Z-Axis Data 1                                                */
-#define ADXL345_FIFO_CTL                        0x38        /*!<   FIFO control                                                 */
-#define ADXL345_FIFO_STATUS                     0x39        /*!<   FIFO status                                                  */
+#define MC3635_DEVID                           0x00        /*!<   Device ID                                                    */
+#define MC3635_THRESH_TAP                      0x1D        /*!<   Tap threshold                                                */
+#define MC3635_OFSX                            0x1E        /*!<   X-axis offset                                                */
+#define MC3635_OFSY                            0x1F        /*!<   Y-axis offset                                                */
+#define MC3635_OFSZ                            0x20        /*!<   Z-axis offset                                                */
+#define MC3635_DUR                             0x21        /*!<   Tap duration                                                 */
+#define MC3635_LATENT                          0x22        /*!<   Tap latency                                                  */
+#define MC3635_WINDOW                          0x23        /*!<   Tap window                                                   */
+#define MC3635_THRESH_ACT                      0x24        /*!<   Activity threshold                                           */
+#define MC3635_THRESH_INACT                    0x25        /*!<   Inactivity threshold                                         */
+#define MC3635_TIME_INACT                      0x26        /*!<   Inactivity time                                              */
+#define MC3635_ACT_INACT_CTL                   0x27        /*!<   Axis enable control for activity and inactivity detection    */
+#define MC3635_THRESH_FF                       0x28        /*!<   Free-fall threshold                                          */
+#define MC3635_TIME_FF                         0x29        /*!<   Free-fall time                                               */
+#define MC3635_TAP_AXES                        0x2A        /*!<   Axis control for single tap/double tap                       */
+#define MC3635_ACT_TAP_STATUS                  0x2B        /*!<   Source of single tap/double tap                              */
+#define MC3635_BW_RATE                         0x2C        /*!<   Data rate and power mode control                             */
+#define MC3635_POWER_CTL                       0x2D        /*!<   Power-saving features control                                */
+#define MC3635_INT_ENABLE                      0x2E        /*!<   Interrupt enable control                                     */
+#define MC3635_INT_MAP                         0x2F        /*!<   Interrupt mapping control                                    */
+#define MC3635_INT_SOURCE                      0x30        /*!<   Source of interrupts                                         */
+#define MC3635_DATA_FORMAT                     0x31        /*!<   Data format control                                          */
+#define MC3635_DATAX0                          0x32        /*!<   X-Axis Data 0                                                */
+#define MC3635_DATAX1                          0x33        /*!<   X-Axis Data 1                                                */
+#define MC3635_DATAY0                          0x34        /*!<   Y-Axis Data 0                                                */
+#define MC3635_DATAY1                          0x35        /*!<   Y-Axis Data 1                                                */
+#define MC3635_DATAZ0                          0x36        /*!<   Z-Axis Data 0                                                */
+#define MC3635_DATAZ1                          0x37        /*!<   Z-Axis Data 1                                                */
+#define MC3635_FIFO_CTL                        0x38        /*!<   FIFO control                                                 */
+#define MC3635_FIFO_STATUS                     0x39        /*!<   FIFO status                                                  */
 
 
 /* Commands Registers */
@@ -80,7 +87,7 @@ typedef enum{
 typedef enum{
     ACTIVITY_DC_Enabled     =   1,                          /*!<  Activity DC-Coupled Enabled.                                   */
     ACTIVITY_AC_Enabled     =   2                           /*!<  Activity AC-Coupled Enabled.                                   */
-} ADXL345_act_inact_ctl_activity_dc_coupled_t;
+} MC3635_act_inact_ctl_activity_dc_coupled_t;
 
 
 /* Bit 6 - 4 : ACT_X ( D6 ), ACT_Y ( D5 ) y ACT_Z enable ( D4 ). */
@@ -93,14 +100,14 @@ typedef enum{
     ACT_XY_Enabled    =   6,                                /*!<  XY-axis Enabled.                                               */
     ACT_XYZ_Enabled   =   7,                                /*!<  XYZ-axis Enabled.                                              */
     ACT_XYZ_Disabled  =   8                                 /*!<  XYZ-axis Disabled.                                             */
-} ADXL345_act_inact_ctl_activity_axis_t;
+} MC3635_act_inact_ctl_activity_axis_t;
 
 
 /* Bit 3 : INACT ac/dc ( D3 ). */
 typedef enum{
     INACTIVITY_DC_Enabled     =   1,                        /*!<  Inactivity DC-Coupled Enabled.                                 */
     INACTIVITY_AC_Enabled     =   2                         /*!<  Inactivity AC-Coupled Enabled.                                 */
-} ADXL345_act_inact_ctl_inactivity_dc_coupled_t;
+} MC3635_act_inact_ctl_inactivity_dc_coupled_t;
 
 
 /* Bit 2 - 0 : INACT_X ( D2 ), INACT_X ( D1 ) y INACT_X enable ( D0 ). */
@@ -113,7 +120,7 @@ typedef enum{
     INACT_XY_Enabled    =   6,                              /*!<  XY-axis Enabled.                                               */
     INACT_XYZ_Enabled   =   7,                              /*!<  XYZ-axis Enabled.                                              */
     INACT_XYZ_Disabled  =   8                               /*!<  XYZ-axis Disabled.                                             */
-} ADXL345_act_inact_ctl_inactivity_axis_t;
+} MC3635_act_inact_ctl_inactivity_axis_t;
 
 
 
@@ -124,7 +131,7 @@ typedef enum{
 typedef enum{
     SUPPRESS_Enabled     =   1,                             /*!<  SUPPRESS Enabled.                                             */
     SUPPRESS_Disabled    =   2                              /*!<  SUPPRESS Disabled.                                            */
-} ADXL345_tap_axes_suppress_t;
+} MC3635_tap_axes_suppress_t;
 
 /* Bit 2 - 0 : TAP_X ( D2 ), TAP_Y ( D1 ) y TAP_Z enable ( D0 ). */
 typedef enum{
@@ -136,7 +143,7 @@ typedef enum{
     TAP_XY_Enabled    =   6,                                /*!<  XY-axis Enabled.                                               */
     TAP_XYZ_Enabled   =   7,                                /*!<  XYZ-axis Enabled.                                              */
     TAP_XYZ_Disabled  =   8                                 /*!<  XYZ-axis Disabled.                                             */
-} ADXL345_tap_axes_axis_t;
+} MC3635_tap_axes_axis_t;
 
 
 
@@ -240,35 +247,35 @@ typedef enum{
 typedef enum{
     DATA_FORMAT_SELF_TEST_Enabled       =       true,        /*!<  SELF_TEST Enabled.                                                */
     DATA_FORMAT_SELF_TEST_Disabled      =       false        /*!<  SELF_TEST Disabled.                                               */
-} ADXL345_data_format_self_test_t;
+} MC3635_data_format_self_test_t;
 
 
 /* Bit 6 : SPI ( D6 ). */
 typedef enum{
     DATA_FORMAT_SPI_Enabled             =       true,        /*!<  3-wire SPI Enabled.                                               */
     DATA_FORMAT_SPI_Disabled            =       false        /*!<  4-wire SPI Enabled.                                               */
-} ADXL345_data_format_spi_t;
+} MC3635_data_format_spi_t;
 
 
 /* Bit 5 : INT_INVERT ( D5 ). */
 typedef enum{
     DATA_FORMAT_INT_INVERT_Enabled             =       true,        /*!<  INT_INVERT Enabled.                                                */
     DATA_FORMAT_INT_INVERT_Disabled            =       false        /*!<  INT_INVERT Disabled.                                               */
-} ADXL345_data_format_int_invert_t;
+} MC3635_data_format_int_invert_t;
 
 
 /* Bit 3 : FULL_RES ( D3 ). */
 typedef enum{
     DATA_FORMAT_FULL_RES_Enabled             =       true,        /*!<  FULL_RES Enabled.                                                */
     DATA_FORMAT_FULL_RES_Disabled            =       false        /*!<  FULL_RES Disabled.                                               */
-} ADXL345_data_format_full_res_t;
+} MC3635_data_format_full_res_t;
 
 
 /* Bit 2 : JUSTIFY ( D2 ). */
 typedef enum{
     DATA_FORMAT_JUSTIFY_Enabled             =       true,        /*!<  JUSTIFY Enabled.                                                */
     DATA_FORMAT_JUSTIFY_Disabled            =       false        /*!<  JUSTIFY Disabled.                                               */
-} ADXL345_data_format_justify_t;
+} MC3635_data_format_justify_t;
 
 
 /* Bit 1 - 0 : RANGE ( D1 - D0 ). */
@@ -277,7 +284,7 @@ typedef enum{
     DATA_FORMAT_RANGE_4_G       =       0x01,        /*!<  RANGE ±4g.                                                         */
     DATA_FORMAT_RANGE_8_G       =       0x02,        /*!<  RANGE ±8g.                                                         */
     DATA_FORMAT_RANGE_16_G      =       0x03         /*!<  RANGE ±16g.                                                        */
-} ADXL345_data_format_range_t;
+} MC3635_data_format_range_t;
 
 
 
@@ -332,9 +339,9 @@ typedef struct{
   * @brief   INTERNAL CONSTANTS
   */
 typedef enum{
-    ADXL345_SUCCESS     =       0,
-    ADXL345_FAILURE     =       1
-} ADXL345_status_t;
+    MC3635_SUCCESS     =       0,
+    MC3635_FAILURE     =       1
+} MC3635_status_t;
 
 
 
@@ -342,45 +349,16 @@ typedef enum{
 /**
   * @brief   FUNCTION PROTOTYPES
   */
-ADXL345_status_t  ADXL345_Init                          ( I2C_parameters_t myI2Cparameters );
-ADXL345_status_t  ADXL345_Conf                          ( I2C_parameters_t myI2Cparameters, AXDL345_bw_rate_low_power_t LOWPOWER, AXDL345_bw_rate_rate_t RATE,
-                                                          ADXL345_data_format_int_invert_t INT_INVERT, ADXL345_data_format_full_res_t FULLRESOLUTION, ADXL345_data_format_justify_t JUSTIFY,
-                                                          ADXL345_data_format_range_t RANGE );
+MC3635_status_t  MC3635_Init                          ( I2C_parameters_t myI2Cparameters );
+MC3635_status_t  MC3635_Conf                          ( I2C_parameters_t myI2Cparameters, AXDL345_bw_rate_low_power_t LOWPOWER, AXDL345_bw_rate_rate_t RATE,
+                                                          MC3635_data_format_int_invert_t INT_INVERT, MC3635_data_format_full_res_t FULLRESOLUTION, MC3635_data_format_justify_t JUSTIFY,
+                                                          MC3635_data_format_range_t RANGE );
 
-ADXL345_status_t  ADXL345_PowerMode                     ( I2C_parameters_t myI2Cparameters, AXDL345_power_ctl_measure_t MEASUREMODE );
 
-ADXL345_status_t  ADXL345_GetID                         ( I2C_parameters_t myI2Cparameters, uint8_t* myID );
 
-ADXL345_status_t  ADXL345_GetRange                      ( I2C_parameters_t myI2Cparameters, ADXL345_data_format_range_t* myRANGE );
-ADXL345_status_t  ADXL345_SetRange                      ( I2C_parameters_t myI2Cparameters, ADXL345_data_format_range_t  myRANGE );
 
-ADXL345_status_t  ADXL345_GetRate                       ( I2C_parameters_t myI2Cparameters, AXDL345_bw_rate_rate_t* myRATE );
-ADXL345_status_t  ADXL345_SetRate                       ( I2C_parameters_t myI2Cparameters, AXDL345_bw_rate_rate_t  myRATE );
+#ifdef __cplusplus
+}
+#endif
 
-ADXL345_status_t  ADXL345_ReadRawData                   ( I2C_parameters_t myI2Cparameters, Vector_t* myXYZVector );
-ADXL345_status_t  ADXL345_ReadScaledData                ( I2C_parameters_t myI2Cparameters, Vector_f* myScaled_XYZVector );
-
-ADXL345_status_t  ADXL345_SetFreeFallThreshold          ( I2C_parameters_t myI2Cparameters, uint8_t myThreshold );
-ADXL345_status_t  ADXL345_SetFreeFallDuration           ( I2C_parameters_t myI2Cparameters, uint8_t myDuration );
-
-ADXL345_status_t  ADXL345_SetTapThreshold               ( I2C_parameters_t myI2Cparameters, uint8_t myThreshold );
-ADXL345_status_t  ADXL345_SetTapDuration                ( I2C_parameters_t myI2Cparameters, uint8_t myDuration );
-ADXL345_status_t  ADXL345_SetDoubleTapLatency           ( I2C_parameters_t myI2Cparameters, uint8_t myLatency );
-ADXL345_status_t  ADXL345_SetDoubleTapWindow            ( I2C_parameters_t myI2Cparameters, uint8_t myWindow );
-ADXL345_status_t  ADXL345_SetTap_Axis                   ( I2C_parameters_t myI2Cparameters, ADXL345_tap_axes_axis_t myAxisEnabled );
-ADXL345_status_t  ADXL345_SetDouble_Suppress            ( I2C_parameters_t myI2Cparameters, ADXL345_tap_axes_suppress_t mySuppressEnabled );
-
-ADXL345_status_t  ADXL345_SetActivityThreshold          ( I2C_parameters_t myI2Cparameters, uint8_t myActivityThreshold );
-ADXL345_status_t  ADXL345_SetActivity_AC_DC_Coupled     ( I2C_parameters_t myI2Cparameters, ADXL345_act_inact_ctl_activity_dc_coupled_t myDC_Coupled );
-ADXL345_status_t  ADXL345_SetActivity_Axis              ( I2C_parameters_t myI2Cparameters, ADXL345_act_inact_ctl_activity_axis_t myAxisEnabled );
-
-ADXL345_status_t  ADXL345_Read_ACT_TAP_STATUS           ( I2C_parameters_t myI2Cparameters, uint8_t* myACT_TAP_STATUS );
-
-ADXL345_status_t  ADXL345_SetInactivityThreshold        ( I2C_parameters_t myI2Cparameters, uint8_t myInactivityThreshold );
-ADXL345_status_t  ADXL345_SetTimeInactivity             ( I2C_parameters_t myI2Cparameters, uint8_t myTimeInactivity );
-ADXL345_status_t  ADXL345_SetInactivity_AC_DC_Coupled   ( I2C_parameters_t myI2Cparameters, ADXL345_act_inact_ctl_inactivity_dc_coupled_t myDC_Coupled );
-ADXL345_status_t  ADXL345_SetInactivity_Axis            ( I2C_parameters_t myI2Cparameters, ADXL345_act_inact_ctl_inactivity_axis_t myAxisEnabled );
-
-ADXL345_status_t  ADXL345_SetInterruptsMap              ( I2C_parameters_t myI2Cparameters, AXDL345_int_map_t myInterruptsMap, AXDL345_int_pin_t myInterruptPin );
-ADXL345_status_t  ADXL345_EnableInterrupts              ( I2C_parameters_t myI2Cparameters, AXDL345_int_map_t myInterruptsEnabled );
-ADXL345_status_t  ADXL345_ReadInterruptSource           ( I2C_parameters_t myI2Cparameters, uint8_t* myIntSource );
+#endif /* MC3635_H_ */
