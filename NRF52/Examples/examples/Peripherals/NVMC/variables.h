5b22028d2d75bbd1c3cec3d52407e5c5dc9d95f9
Manuel Caballero <aqueronteblog@gmail.com>
2018-04-16 19:57:05 +0100

    NRF52 NVMC Example. Board and Variables files were added

---
+++ NRF52/Examples/examples/Peripherals/NVMC/variables.h
@@ -0,0 +1,42 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        16/April/2018
+ * @version     16/April/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "board.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* CONSTANTS */
+typedef  enum{ 
+  YES      =  1,
+  NO       =  0
+}EXAMPLE_nvmc_actions;
+
+
+/* VARIABLES */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
