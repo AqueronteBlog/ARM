6af21e4e60d46a99dc9d789e4bba6f44a4da7042
Manuel Caballero <aqueronteblog@gmail.com>
2018-05-31 10:44:32 +0100

    NRF51 iAQ-Core Example. Interrupt files were added

---
+++ NRF51/Examples/Drivers/iAQ_core/Src/interrupts.c
@@ -0,0 +1,39 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        5/February/2018
+ * @version     5/February/2018    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "interrupts.h"
+
+/**
+ * @brief       void TIMER0_IRQHandler ()
+ * @details     There is a new interrupt every 0.5s, increment the myState variable
+ *              so in the main can process that information.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        5/February/2018
+ * @version     5/February/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void TIMER0_IRQHandler()
+{
+    if ( ( NRF_TIMER0->EVENTS_COMPARE[0] != 0 ) && ( ( NRF_TIMER0->INTENSET & TIMER_INTENSET_COMPARE0_Msk ) != 0 ) )
+    {
+        mySTATE++;
+
+        NRF_TIMER0->EVENTS_COMPARE[0] = 0;                  // Clear ( flag ) compare register 0 event
+    }
+}
---
+++ NRF51/Examples/Drivers/iAQ_core/Src/interrupts.h
@@ -0,0 +1,29 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        5/February/2018
+ * @version     5/February/2018    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "variables.h"
+
+
+/* INTERRUPT PRTOTYPES */
+void TIMER0_IRQHandler ( void );
+
+
+
+/* VARIABLES */
+extern uint8_t   dataToBeTX;
+extern uint32_t  mySTATE;
+extern uint32_t  TX_inProgress;
+extern uint8_t   TX_buff[];
+extern uint8_t   *myPtr;
