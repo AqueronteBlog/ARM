aec93fa4b74324b7d3c69034bcbfd7aa0ffbe802
Manuel Caballero <aqueronteblog@gmail.com>
2018-05-31 10:44:21 +0100

    NRF51 iAQ-Core Example. Functions files were added

---
+++ NRF51/Examples/Drivers/iAQ_core/Src/functions.h
@@ -0,0 +1,22 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        5/February/2018
+ * @version     5/February/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+#include "variables.h"
+
+/* FUNCTION PROTOTYPES */
+void conf_GPIO      ( void );
+void conf_TIMER0    ( void );
+
