db17ecc92f8d5ffa46dd73c69acfb8adf39523e3
Manuel Caballero <aqueronteblog@gmail.com>
2018-06-01 10:47:59 +0100

    DRIVERS. iAQ-Core. The driver was completed and it is ready to be tested.

--- Drivers/iAQ_core/iAQ_core.c
+++ Drivers/iAQ_core/iAQ_core.c
@@ -11,7 +11,7 @@
  * @version     31/May/2018    The ORIGIN
  * @pre         N/A.
  * @warning     N/A
- * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ * @pre         This code belongs to Nimbus Centre ( http://www.nimbus.cit.ie ).
  */
 
 #include "iAQ_Core.h"
@@ -53,445 +53,39 @@ iAQ_Core_status_t  iAQ_Core_Init ( I2C_parameters_t myI2Cparameters )
 
 
 /**
- * @brief       iAQ_Core_StartConvertTemperature ( I2C_parameters_t )
+ * @brief       iAQ_Core_GetNewReading ( I2C_parameters_t , iAQ_Core_vector_data_t* )
  *
- * @details     It triggers a new temperature conversion.
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- *
- * @param[out]   N/A.
- *
- *
- * @return       Status of iAQ_Core_StartConvertTemperature.
- *
- *
- * @author      Manuel Caballero
- * @date        21/January/2018
- * @version     21/January/2018     The ORIGIN
- * @pre         Temperature Conversion Time ( t_TC ) 200ms maximum.
- * @warning     N/A.
- */
-iAQ_Core_status_t  iAQ_Core_StartConvertTemperature ( I2C_parameters_t myI2Cparameters )
-{
-    uint8_t      cmd       =   iAQ_Core_START_CONVERT_T;
-    i2c_status_t aux       =   0;
-
-
-    // It triggers a new temperature conversion
-    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_STOP_BIT );
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-/**
- * @brief       iAQ_Core_StopConvertTemperature ( I2C_parameters_t )
- *
- * @details     It halts the temperature conversion.
+ * @details     It performs a new parameters reading from the sensor.
  *
  * @param[in]    myI2Cparameters:   I2C parameters.
  *
- * @param[out]   N/A.
+ * @param[out]   myData:            All parameters: Prediction + Status Flag + Resistance + Tvoc.
  *
  *
- * @return       Status of iAQ_Core_StopConvertTemperature.
+ * @return       Status of iAQ_Core_GetNewReading.
  *
  *
  * @author      Manuel Caballero
- * @date        21/January/2018
- * @version     21/January/2018     The ORIGIN
- * @pre         In continuous conversion mode, a StopConvertT command halt
- *              a continuous conversion. To restart, the StartConvertT
- *              command must be issued. In one-shot mode, a StartConvertT command
- *              must be issued for every temperature reading desired.
- * @warning     N/A.
+ * @date        31/May/2018
+ * @version     31/May/2018     The ORIGIN
+ * @pre         Measurement interval ( continuous ): 1s | Measurement interval ( pulsed ): Max. 11s.
+ * @warning     First functional reading after start up is 5 minutes.
  */
-iAQ_Core_status_t  iAQ_Core_StopConvertTemperature ( I2C_parameters_t myI2Cparameters )
+iAQ_Core_status_t  iAQ_Core_GetNewReading ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myData )
 {
-    uint8_t      cmd       =   iAQ_Core_STOP_CONVERT_T;
+    uint8_t      cmd[9]    =   { 0 };
     i2c_status_t aux       =   0;
 
 
-    // It sends the command
-    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_STOP_BIT );
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-
-/**
- * @brief       iAQ_Core_ReadRawTemperature ( I2C_parameters_t , iAQ_Core_vector_data_t* )
- *
- * @details     It reads the last raw temperature conversion result.
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- *
- * @param[out]   myRawTemperature:  Raw temperature ( two bytes ).
- *
- *
- * @return       Status of iAQ_Core_ReadRawTemperature.
- *
- *
- * @author      Manuel Caballero
- * @date        22/January/2018
- * @version     22/January/2018     The ORIGIN
- * @pre         DONE bit needs to be checked before calling this function.
- * @warning     N/A.
- */
-iAQ_Core_status_t  iAQ_Core_ReadRawTemperature  ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myRawTemperature )
-{
-    uint8_t      cmd[]     =   { iAQ_Core_READ_TEMPERATURE, 0 };
-    i2c_status_t aux       =   0;
-
-
-
-    // It sends the command and gets the result otherwise
-    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
-    aux      =   i2c_read  ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ) );
-
-
-    // Parse the data
-    myRawTemperature->MSBTemperature     =   cmd[0];
-    myRawTemperature->LSBTemperature     =   cmd[1];
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-
-/**
- * @brief       iAQ_Core_ReadTemperature ( I2C_parameters_t , iAQ_Core_vector_data_t* )
- *
- * @details     It reads the last current temperature conversion result.
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- *
- * @param[out]   myTemperature:     Current temperature.
- *
- *
- * @return       Status of iAQ_Core_ReadTemperature.
- *
- *
- * @author      Manuel Caballero
- * @date        22/January/2018
- * @version     22/January/2018     The ORIGIN
- * @pre         This function updates the raw temperature variables.
- * @pre         DONE bit needs to be checked before calling this function.
- * @warning     N/A.
- */
-iAQ_Core_status_t  iAQ_Core_ReadTemperature  ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myTemperature )
-{
-    uint8_t      cmd[]     =   { iAQ_Core_READ_TEMPERATURE, 0 };
-    i2c_status_t aux       =   0;
-    uint32_t     i         =   0;
-
-
-
-    // It sends the command and gets the result otherwise
-    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
-    aux      =   i2c_read  ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ) );
-
-
-    // Update the raw temperature value
-    myTemperature->MSBTemperature     =   cmd[0];
-    myTemperature->LSBTemperature     =   cmd[1];
-
-
-    // Calculate the current temperature
-    // Check if the temperature is positive or negative
-    if ( ( myTemperature->MSBTemperature & MSB_TEMPERATURE_SIGN_BIT_MASK ) == MSB_TEMPERATURE_SIGN_BIT_NEGATIVE )
-    {
-    // The temperature is NEGATIVE
-        myTemperature->MSBTemperature   -=   1;
-        myTemperature->MSBTemperature    =  ~myTemperature->MSBTemperature;
-
-        myTemperature->LSBTemperature   -=   1;
-        myTemperature->LSBTemperature    =   ( ~myTemperature->LSBTemperature & 0xF0 );
-
-        myTemperature->Temperature       =   ( -1.0 * myTemperature->MSBTemperature );
-    }
-    else
-    {
-    // The temperature is POSITIVE
-        myTemperature->Temperature       =   myTemperature->MSBTemperature;
-    }
-
-
-    // Update the decimal value
-    for ( i = 0; i < ( myTemperature->LSBTemperature >> 4 ); i++ )
-            myTemperature->Temperature  +=   iAQ_Core_TEMPERATURE_RESOLUTION;
-
-
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-
-/**
- * @brief       iAQ_Core_GetStatusRegister ( I2C_parameters_t , iAQ_Core_vector_data_t* )
- *
- * @details     It reads the CONFIGURATION/STATUS register.
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- *
- * @param[out]   myStatusRegister:  Current Status register value.
- *
- *
- * @return       Status of iAQ_Core_GetStatusRegister.
- *
- *
- * @author      Manuel Caballero
- * @date        23/January/2018
- * @version     23/January/2018     The ORIGIN
- * @pre         N/A.
- * @warning     N/A.
- */
-iAQ_Core_status_t  iAQ_Core_GetStatusRegister   ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myStatusRegister )
-{
-    uint8_t      cmd     =   iAQ_Core_ACCESS_CONFIG;
-    i2c_status_t aux     =   0;
-
-
-    // It sends the command and gets the result
-    aux = i2c_write ( myI2Cparameters, &cmd, 1, I2C_NO_STOP_BIT );
-    aux = i2c_read  ( myI2Cparameters, &myStatusRegister->Control_Status_Register, 1 );
-
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-
-/**
- * @brief       iAQ_Core_SetConversionMode ( I2C_parameters_t , iAQ_Core_access_config_1shot_t )
- *
- * @details     It sets 1SHOT/Continuous temperature conversion mode.
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- * @param[in]    myConversionMode:  ACCESS_CONFIG_1SHOT_ONE_TEMPERATURE_CONVERSION:             1SHOT temperature conversion mode
- *                                  ACCESS_CONFIG_1SHOT_CONTINUOUSLY_TEMPERATURE_CONVERSION:    Continuous temperature conversion mode.
- *
- * @param[out]   N/A.
- *
- *
- * @return       Status of iAQ_Core_SetConversionMode.
- *
- *
- * @author      Manuel Caballero
- * @date        23/January/2018
- * @version     23/January/2018     The ORIGIN
- * @pre         N/A
- * @warning     Since the configuration register is implemented in
- *              EEPROM, writes to the register require 10ms to complete.
- *              After issuing a command to write to the configuration register,
- *              no further accesses to the iAQ_Core should be made for at least 10ms.
- */
-iAQ_Core_status_t  iAQ_Core_SetConversionMode   ( I2C_parameters_t myI2Cparameters, iAQ_Core_access_config_1shot_t myConversionMode )
-{
-    uint8_t      cmd[]   =   { iAQ_Core_ACCESS_CONFIG, 0 };
-    i2c_status_t aux     =   0;
-
-
-    // It sends the command and gets the result
-    aux = i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
-    aux = i2c_read  ( myI2Cparameters, &cmd[1], 1 );
-
-
-    // Mask the bit and check which mode to use
-    cmd[1] &= ~ACCESS_CONFIG_1SHOT_MASK;
-
-    if ( myConversionMode == ACCESS_CONFIG_1SHOT_ONE_TEMPERATURE_CONVERSION )
-    {
-    // 1SHOT Mode
-        cmd[1] |=   ACCESS_CONFIG_1SHOT_ONE_TEMPERATURE_CONVERSION;
-    }
-    else
-    {
-    // Continuously Mode
-        cmd[1] &=  ~ACCESS_CONFIG_1SHOT_ONE_TEMPERATURE_CONVERSION;
-    }
-
-
-    // Update the CONFIGURATION/STATUS register
-    aux  =  i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-
-/**
- * @brief       iAQ_Core_IsTemperatureConversionDone ( I2C_parameters_t , iAQ_Core_vector_data_t* )
- *
- * @details     It checks if a temperature conversion is done.
- *
- * @param[in]    myI2Cparameters:                I2C parameters.
- *
- * @param[out]   myTemperatureConversionStatus:  Temperature conversion bit:
- *                                                  ACCESS_CONFIG_DONE_CONVERSION_COMPLETE
- *                                                  ACCESS_CONFIG_DONE_CONVERSION_IN_PROGRESS.
- *
- *
- * @return       Status of iAQ_Core_IsTemperatureConversionDone.
- *
- *
- * @author      Manuel Caballero
- * @date        24/January/2018
- * @version     24/January/2018     The ORIGIN
- * @pre         N/A.
- * @warning     N/A.
- */
-iAQ_Core_status_t iAQ_Core_IsTemperatureConversionDone ( I2C_parameters_t myI2Cparameters, iAQ_Core_access_config_done_t* myTemperatureConversionStatus )
-{
-    uint8_t      cmd[]     =   { iAQ_Core_ACCESS_CONFIG, 0 };
-    i2c_status_t aux       =   0;
-
-
-    // It sends the command and gets the result
-    aux = i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
-    aux = i2c_read  ( myI2Cparameters, &cmd[1], 1 );
-
-
-    // Update the value
-    *myTemperatureConversionStatus   =   ( cmd[1] & ACCESS_CONFIG_DONE_MASK );
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-
-/**
- * @brief       iAQ_Core_ReadBytesEEPROM ( I2C_parameters_t , uint8_t , uint8_t* , uint8_t )
- *
- * @details     It reads a certain number of bytes from EEPROM memory.
- *
- * @param[in]    myI2Cparameters:       I2C parameters.
- * @param[in]    myStartingAddress:     Starting address to read the EEPROM memory.
- * @param[in]    myLength:              Amount of bytes to read.
- *
- * @param[out]   myReadBytesEEPROM:     Read values from EEPROM memory.
- *
- *
- * @return       Status of iAQ_Core_ReadBytesEEPROM.
- *
- *
- * @author      Manuel Caballero
- * @date        25/January/2018
- * @version     25/January/2018     The ORIGIN
- * @pre         N/A.
- * @warning     When the address pointer reaches the end of the 256-byte memory space ( address FFh )
- *              it increments from the end of the memory back to the first location of the memory
- *              ( address 00h ).
- */
-iAQ_Core_status_t iAQ_Core_ReadBytesEEPROM ( I2C_parameters_t myI2Cparameters, uint8_t myStartingAddress, uint8_t* myReadBytesEEPROM, uint8_t myLength )
-{
-    uint8_t      cmd[]     =   { iAQ_Core_ACCESS_MEMORY, 0 };
-    i2c_status_t aux       =   0;
-
-
-
-    // It sends the command, the address location and gets the result
-    cmd[1]   =   myStartingAddress;
-    aux = i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_NO_STOP_BIT );
-    aux = i2c_read  ( myI2Cparameters, myReadBytesEEPROM, myLength );
-
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   iAQ_Core_SUCCESS;
-    else
-        return   iAQ_Core_FAILURE;
-}
-
-
-
-/**
- * @brief       iAQ_Core_WriteBytesEEPROM ( I2C_parameters_t , uint8_t , uint8_t , uint8_t )
- *
- * @details     It writes a certain number of bytes to EEPROM memory.
- *
- * @param[in]    myI2Cparameters:       I2C parameters.
- * @param[in]    myStartingAddress:     Starting address to write the EEPROM memory.
- * @param[in]    myWriteBytesEEPROM:    Bytes to write to the EEPROM memory.
- * @param[in]    myLength:              Amount of bytes to write.
- *
- * @param[out]   N/A.
- *
- *
- * @return       Status of iAQ_Core_WriteBytesEEPROM.
- *
- *
- * @author      Manuel Caballero
- * @date        26/January/2018
- * @version     26/January/2018     The ORIGIN
- * @pre         N/A.
- * @warning     The STOP condition causes the iAQ_Core to initiate the write to EEPROM sequence.
- * @warning     If the starting address is 00 and the incoming data is 00 11 22 33 44 55 66 77 88 99,
- *              the result is mem00=88 mem01=99 mem02=22 mem03=33 mem04=44 mem05=55 mem06=66 mem07=77.
- *              The data wraps around and overwrites itself.
- * @warning     EEPROM Write Cycle Time: 50ms ( maximum ). The user must take care of this time!.
- */
-iAQ_Core_status_t iAQ_Core_WriteBytesEEPROM ( I2C_parameters_t myI2Cparameters, uint8_t myStartingAddress, uint8_t myWriteBytesEEPROM[], uint8_t myLength )
-{
-    uint8_t      cmd[ myLength + 2 ];
-    uint32_t     i         =     0;
-    i2c_status_t aux       =     0;
-
-    // Prepare COMMANDS
-    cmd[0]   =   iAQ_Core_ACCESS_MEMORY;
-    cmd[1]   =   myStartingAddress;
-
-    // Prepare the payload to be stored into the EEPROM memory
-    for ( i = 2 ; i < ( myLength + 2 ); i++ )
-        cmd[ i ]       =   myWriteBytesEEPROM[ i - 2 ];
-
-    // It sends the command, the address location as well as the payload
-    aux = i2c_write ( myI2Cparameters, &cmd[0], myLength + 2, I2C_STOP_BIT );
+    /* Get the data */
+    aux = i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+    /* Update the parameters    */
+    myData->pred         =  ( ( cmd[0] << 8 ) + cmd[1] );                           // Prediction = ( byte0 * 2^8 ) + byte1
+    myData->status       =  cmd[2];
+    myData->resistance   =  ( ( cmd[4] << 16 ) + ( cmd[5] << 8 ) + cmd[6] );        // Resistance = ( byte4 * 2^16 ) + ( byte5 * 2^8 ) + byte6
+    myData->Tvoc         =  ( ( cmd[7] << 8 ) + cmd[8] );                           // Tvoc       = ( byte7 * 2^8 ) + byte8
 
 
 
--- Drivers/iAQ_core/iAQ_core.h
+++ Drivers/iAQ_core/iAQ_core.h
@@ -11,7 +11,7 @@
  * @version     31/May/2018    The ORIGIN
  * @pre         N/A.
  * @warning     N/A
- * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ * @pre         This code belongs to Nimbus Centre ( http://www.nimbus.cit.ie ).
  */
 
 
@@ -31,23 +31,11 @@ extern "C" {
   */
 typedef enum
 {
-    iAQ_Core_ADDRESS     =   0x5A                         /*!<   Address for iAQ_Core                */
+    iAQ_Core_ADDRESS     =   0x5A               /*!<   Address for iAQ_Core                                             */
 } iAQ_Core_address_t;
 
 
 // iAQ_Core DATA. NOTE: The I2C master can request up to 9 bytes.
-/**
-  * @brief   DATA POSITION
-  */
-typedef enum
-{
-    iAQ_Core_PREDICTION     =   0xFF0000000,               /*!<  Information Structure ( Prediction )                              */
-    iAQ_Core_STATUS_FLAG    =   0x00F000000,               /*!<  Information Structure ( Status Flag )                             */
-    iAQ_Core_RESIATNCE      =   0x0000FFF00,               /*!<  Information Structure ( Resistance )                              */
-    iAQ_Core_TVOC           =   0x0000000FF                /*!<  Information Structure ( TVOC )                                    */
-} iAQ_Core_data_position_t;
-
-
 /**
   * @brief   STATUS FLAG. NOTE: If status is OK the data is valid. If the status is BUSY, the data integrity is not guaranteed for
   *                             variables of size > 8 bits, because the module may be updating a part of the variable. If the status
@@ -56,46 +44,12 @@ typedef enum
   */
 typedef enum
 {
-    iAQ_Core_STATUS_OK        =   0x00,                     /*!<  Status Flag: OK                                                   */
-    iAQ_Core_STATUS_BUSY      =   0x01,                     /*!<  Status Flag: OK                                                   */
-} iAQ_Core_thermostat_commands_t;
+    iAQ_Core_STATUS_OK        =   0x00,         /*!<  Status Flag: OK      ( data valid )                               */
+    iAQ_Core_STATUS_BUSY      =   0x01,         /*!<  Status Flag: BUSY    ( module in warm up phase )                  */
+    iAQ_Core_STATUS_RUNIN     =   0x10,         /*!<  Status Flag: RUNIN   ( re-read multi byte data! )                 */
+    iAQ_Core_STATUS_ERROR     =   0x80          /*!<  Status Flag: ERROR   ( if constant: replace sensor )              */
+} iAQ_Core_status_flag_t;
 
-
-
-// ACCESS CONFIG ( CONFIGURATION/STATUS REGISTER )
-/**
-  * @brief   BIT7: DONE
-  */
-typedef enum
-{
-    ACCESS_CONFIG_DONE_MASK                     =   ( 1 << 7 ),                 /*!<  DONE Mask                                 */
-    ACCESS_CONFIG_DONE_CONVERSION_COMPLETE      =   ( 1 << 7 ),                 /*!<  Conversion complete                       */
-    ACCESS_CONFIG_DONE_CONVERSION_IN_PROGRESS   =   ( 0 << 7 )                  /*!<  Conversion in progress                    */
-} iAQ_Core_access_config_done_t;
-
-
-/**
-  * @brief   BIT0: 1SHOT
-  */
-typedef enum
-{
-    ACCESS_CONFIG_1SHOT_MASK                                =   ( 1 << 0 ),     /*!<  1SHOT Mask                                                                        */
-    ACCESS_CONFIG_1SHOT_ONE_TEMPERATURE_CONVERSION          =   ( 1 << 0 ),     /*!<  Perform one temperature conversion upon receipt of the Start Convert T protocol   */
-    ACCESS_CONFIG_1SHOT_CONTINUOUSLY_TEMPERATURE_CONVERSION =   ( 0 << 0 )      /*!<  Perform continuously temperature conversions                                      */
-} iAQ_Core_access_config_1shot_t;
-
-
-
-// TEMPERATURE REGISTER
-/**
-  * @brief   MSB BIT15: SIGN
-  */
-typedef enum
-{
-    MSB_TEMPERATURE_SIGN_BIT_MASK               =   ( 1 << 7 ),                 /*!<  Temperature sign Mask                     */
-    MSB_TEMPERATURE_SIGN_BIT_POSITIVE           =   ( 0 << 7 ),                 /*!<  Temperature is POSITIVE                   */
-    MSB_TEMPERATURE_SIGN_BIT_NEGATIVE           =   ( 1 << 7 )                  /*!<  Temperature is NEGATIVE                   */
-} iAQ_Core_msb_temperature_sign_bit_t;
 
 
 
@@ -105,10 +59,10 @@ typedef enum
 #define iAQ_Core_VECTOR_STRUCT_H
 typedef struct
 {
-    uint16_t pred;                              /*!<  Prediction (CO2 eq. ppm),  Typical Value: 450                     */
-    uint8_t  status;                            /*!<  Status,                    Typical Value: 0                       */
-    uint8_t  resistance;                        /*!<  Sensor resistance [Ohm],   Typical Value: 256431                  */
-    uint16_t Tvoc;                              /*!<  Prediction (TVOC eq. ppb), Typical Value: 125                     */
+    uint16_t                pred;               /*!<  Prediction (CO2 eq. ppm),  Typical Value: 450                     */
+    iAQ_Core_status_flag_t  status;             /*!<  Status,                    Typical Value: 0                       */
+    int32_t                 resistance;         /*!<  Sensor resistance [Ohm],   Typical Value: 256431                  */
+    uint16_t                Tvoc;               /*!<  Prediction (TVOC eq. ppb), Typical Value: 125                     */
 } iAQ_Core_vector_data_t;
 #endif
 
@@ -119,8 +73,8 @@ typedef struct
   */
 typedef enum
 {
-    iAQ_Core_SUCCESS                   =   0,
-    iAQ_Core_FAILURE                   =   1,
+    iAQ_Core_SUCCESS     =   0,
+    iAQ_Core_FAILURE     =   1
 } iAQ_Core_status_t;
 
 
@@ -131,43 +85,12 @@ typedef enum
   */
 /** It configures the SPI peripheral.
     */
-iAQ_Core_status_t iAQ_Core_Init                        ( I2C_parameters_t myI2Cparameters );
+iAQ_Core_status_t iAQ_Core_Init              ( I2C_parameters_t myI2Cparameters                                 );
 
-/** It triggers a new temperature measurement.
+/** It performs a new parameters reading from the sensor.
       */
-iAQ_Core_status_t iAQ_Core_StartConvertTemperature     ( I2C_parameters_t myI2Cparameters );
+iAQ_Core_status_t iAQ_Core_GetNewReading     ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myData );
 
-/** It stops the current temperature conversion.
-      */
-iAQ_Core_status_t iAQ_Core_StopConvertTemperature      ( I2C_parameters_t myI2Cparameters );
-
-/** It reads the last raw temperature conversion result.
-      */
-iAQ_Core_status_t iAQ_Core_ReadRawTemperature          ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myRawTemperature );
-
-/** It reads the last temperature conversion result.
-      */
-iAQ_Core_status_t iAQ_Core_ReadTemperature             ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myTemperature );
-
-/** It reads the CONFIGURATION/STATUS register.
-      */
-iAQ_Core_status_t iAQ_Core_GetStatusRegister           ( I2C_parameters_t myI2Cparameters, iAQ_Core_vector_data_t* myStatusRegister );
-
-/** It sets 1SHOT/Continuous temperature conversion mode.
-      */
-iAQ_Core_status_t iAQ_Core_SetConversionMode           ( I2C_parameters_t myI2Cparameters, iAQ_Core_access_config_1shot_t myConversionMode );
-
-/** It checks if a temperature conversion is done.
-      */
-iAQ_Core_status_t iAQ_Core_IsTemperatureConversionDone ( I2C_parameters_t myI2Cparameters, iAQ_Core_access_config_done_t* myTemperatureConversionStatus );
-
-/** It reads a certain number of bytes from EEPROM memory.
-      */
-iAQ_Core_status_t iAQ_Core_ReadBytesEEPROM             ( I2C_parameters_t myI2Cparameters, uint8_t myStartingAddress, uint8_t* myReadBytesEEPROM, uint8_t myLength );
-
-/** It writes a certain number of bytes to EEPROM memory.
-      */
-iAQ_Core_status_t iAQ_Core_WriteBytesEEPROM            ( I2C_parameters_t myI2Cparameters, uint8_t myStartingAddress, uint8_t myWriteBytesEEPROM[], uint8_t myLength );
 
 
 
