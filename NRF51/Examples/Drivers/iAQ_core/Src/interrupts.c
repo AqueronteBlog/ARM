29ed24b6c5d1e358d40c4fd83d26dd20fabe258f
Manuel Caballero <aqueronteblog@gmail.com>
2018-06-07 09:27:04 +0100

    NRF51 iAQ-Core Example. Uart subroutine was added

--- NRF51/Examples/Drivers/iAQ_core/Src/interrupts.c
+++ NRF51/Examples/Drivers/iAQ_core/Src/interrupts.c
@@ -37,3 +37,46 @@ void TIMER0_IRQHandler()
         NRF_TIMER0->EVENTS_COMPARE[0] = 0;                  // Clear ( flag ) compare register 0 event
     }
 }
+
+
+
+/**
+ * @brief       void UART0_IRQHandler ()
+ * @details     It sends the collected data from the external sensor through the UART.
+ *
+ *              One byte was just sent.
+ *
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        7/June/2018
+ * @version     7/June/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A
+ */
+void UART0_IRQHandler(void)
+{
+    /* Transmission */
+    if ( ( NRF_UART0->EVENTS_TXDRDY != 0 ) && ( NRF_UART0->INTENSET & UART_INTENSET_TXDRDY_Msk ) )
+    {
+        // Clear UART TX event flag.
+        NRF_UART0->EVENTS_TXDRDY = 0;
+
+        // 5-Bytes more have to be sent
+        if ( dataToBeTX  < 6 )                      // 1 - 7 = 6. NOTE: One byte was just transmitted previously.
+        {
+        // Transmit data
+            NRF_UART0->TXD   =   *myPtr++;
+            dataToBeTX++;
+        }
+        else
+        {
+        // Everything was transmitted, stop the UART
+            NRF_UART0->TASKS_STOPTX      =   1;
+            dataToBeTX                   =   0;
+            TX_inProgress                =   NO;
+        }
+    }
+}
