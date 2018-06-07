befe1efcd1247a5c3f9bca9315715b68850a1c01
Manuel Caballero <aqueronteblog@gmail.com>
2018-06-07 09:26:15 +0100

    NRF51 iAQ-Core Example. Board file was updated and improved.

--- NRF51/Examples/Drivers/iAQ_core/Src/board.h
+++ NRF51/Examples/Drivers/iAQ_core/Src/board.h
@@ -15,13 +15,39 @@
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 /* LEDs */
-#define LED1        21     /*!<   LED1: P0.21   */
-#define LED2        22     /*!<   LED2: P0.22   */
-#define LED3        23     /*!<   LED3: P0.23   */
-#define LED4        24     /*!<   LED4: P0.24   */
+typedef enum{
+  LED1  = 21,               /*!<   LED1: P0.21   */
+  LED2  = 22,               /*!<   LED2: P0.22   */
+  LED3  = 23,               /*!<   LED3: P0.23   */
+  LED4  = 24                /*!<   LED4: P0.24   */
+} NRF51DK_board_leds_t;
+
+
+/* UART0 */
+typedef enum{
+  UART0_TX  =   9,          /*!<   UART TX: P0.9    */
+  UART0_RX  =   11          /*!<   UART RX: P0.11   */
+} NRF51DK_board_uart0_t;
 
 
 /* TWI0 */
-#define TWI0_SCL    7      /*!<   TWI0 SCL: P0.7    */
-#define TWI0_SDA    30     /*!<   TWI0 SDA: P0.30   */
+typedef enum{
+  TWI0_SCL  =   7,          /*!<   TWI0 SCL: P0.7    */
+  TWI0_SDA  =   30          /*!<   TWI0 SDA: P0.30   */
+} NRF51DK_board_twi0_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H_ */
