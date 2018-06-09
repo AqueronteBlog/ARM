a21a2902b948e327ed3baeeb36ff968a57da7e20
Manuel Caballero <aqueronteblog@gmail.com>
2018-06-09 19:54:40 +0100

    DRIVERS. TMP102. Some comments were updated and the addresses were completed, this file is still in progress

--- Drivers/TMP102/TMP102.h
+++ Drivers/TMP102/TMP102.h
@@ -1,14 +1,14 @@
 /**
- * @brief       SI7021.h
- * @details     I2C HUMIDITY AND TEMPERATURE SENSOR.
+ * @brief       TMP102.h
+ * @details     Low-Power Digital Temperature Sensor With SMBus and Two-Wire Serial.
  *              Header file.
  *
  *
- * @return      NA
+ * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        5/February/2018
- * @version     5/February/2018    The ORIGIN
+ * @date        9/June/2018
+ * @version     9/June/2018    The ORIGIN
  * @pre         N/A.
  * @warning     N/A
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
@@ -20,38 +20,38 @@
 #include "i2c.h"
 
 
+#ifndef TMP102_H_
+#define TMP102_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
 /**
   * @brief   DEFAULT ADDRESSES
   */
 typedef enum
 {
-    SI7021_ADDRESS     =   0x40                                                         /*!<   SI7021 I2C Address                                   */
-} SI7021_address_t;
+    TMP102_ADDRESS_A0_GROUND     =   0b1001000,             /*!<   TMP102 I2C Address: A0 connected to Ground           */
+    TMP102_ADDRESS_A0_VPOS       =   0b1001001,             /*!<   TMP102 I2C Address: A0 connected to V+               */
+    TMP102_ADDRESS_A0_SDA        =   0b1001010,             /*!<   TMP102 I2C Address: A0 connected to SDA              */
+    TMP102_ADDRESS_A0_SCL        =   0b1001011              /*!<   TMP102 I2C Address: A0 connected to SCL              */
+} TMP102_addresses_t;
 
 
 // REGISTERS
 /**
-  * @brief   COMMAND CODE
+  * @brief   REGISTERS BYTE
   */
 typedef enum
 {
-    SI7021_MEASURE_RELATIVE_HUMIDITY_HOLD_MASTER_MODE           =   0xE5,               /*!<  Measure Relative Humidity, Hold Master Mode           */
-    SI7021_MEASURE_RELATIVE_HUMIDITY_NO_HOLD_MASTER_MODE        =   0xF5,               /*!<  Measure Relative Humidity, No Hold Master Mode        */
-    SI7021_MEASURE_TEMPERATURE_HOLD_MASTER_MODE                 =   0xE3,               /*!<  Measure Temperature, Hold Master Mode                 */
-    SI7021_MEASURE_TEMPERATURE_NO_HOLD_MASTER_MODE              =   0xF3,               /*!<  Measure Temperature, No Hold Master Mode              */
-    SI7021_READ_TEMPERATURE_VALUE_FROM_PREVIOUS_RH_MEASUREMENT  =   0xE0,               /*!<  Read Temperature Value from Previous RH Measurement   */
-    SI7021_RESET                                                =   0xFE,               /*!<  Reset                                                 */
-    SI7021_WRITE_RH_T_USER_REGISTER_1                           =   0xE6,               /*!<  Write RH/T User Register 1                            */
-    SI7021_READ_RH_T_USER_REGISTER_1                            =   0xE7,               /*!<  Read RH/T User Register 1                             */
-    SI7021_WRITE_HEATER_CONTROL_REGISTER                        =   0x51,               /*!<  Write Heater Control Register                         */
-    SI7021_READ_HEATER_CONTROL_REGISTER                         =   0x11,               /*!<  Read Heater Control Register                          */
-    SI7021_READ_ELECTRONIC_ID_FIRST_BYTE_CMD1                   =   0xFA,               /*!<  Read Electronic ID 1st Byte                           */
-    SI7021_READ_ELECTRONIC_ID_FIRST_BYTE_CMD2                   =   0x0F,               /*!<  Read Electronic ID 1st Byte                           */
-    SI7021_READ_ELECTRONIC_ID_SECOND_BYTE_CMD1                  =   0xFC,               /*!<  Read Electronic ID 2nd Byte                           */
-    SI7021_READ_ELECTRONIC_ID_SECOND_BYTE_CMD2                  =   0xC9,               /*!<  Read Electronic ID 2nd Byte                           */
-    SI7021_READ_FIRMWARE_VERSION_CMD1                           =   0x84,               /*!<  Read Firmware Revision                                */
-    SI7021_READ_FIRMWARE_VERSION_CMD2                           =   0xB8                /*!<  Read Firmware Revision                                */
-} SI7021_command_code_t;
+    TMP102_TEMPERATURE_REGISTER     =   0x00,               /*!<  Temperature Register      ( Read Only  )              */
+    TMP102_CONFIGURATION_REGISTER   =   0x01,               /*!<  Configuration Register    ( Read/Write )              */
+    TMP102_T_LOW_REGISTER           =   0x02,               /*!<  T LOW Register            ( Read/Write )              */
+    TMP102_T_HIGH_REGISTER          =   0x03                /*!<  T HIGH Register           ( Read/Write )              */
+} TMP102_registers_byte_t;
 
 
 
@@ -61,9 +61,9 @@ typedef enum
   */
 typedef enum
 {
-    SI7021_HOLD_MASTER_MODE                 =   0x01,           /*!<  SI7021 HOLD MASTER MODE enabled                       */
-    SI7021_NO_HOLD_MASTER_MODE              =   0x00            /*!<  SI7021 NO HOLD MASTER MODE enabled                    */
-} SI7021_master_mode_t;
+    TMP102_HOLD_MASTER_MODE                 =   0x01,           /*!<  TMP102 HOLD MASTER MODE enabled                       */
+    TMP102_NO_HOLD_MASTER_MODE              =   0x00            /*!<  TMP102 NO HOLD MASTER MODE enabled                    */
+} TMP102_master_mode_t;
 
 
 
@@ -79,12 +79,12 @@ typedef enum
   */
 typedef enum
 {
-    SI7021_RESOLUTION_MASK                  =   0x81,           /*!<  SI7021 Measurement Resolution                         */
-    SI7021_RESOLUTION_RH_12_TEMP_14         =   0x00,           /*!<  SI7021 12b RH 14b Temp.                               */
-    SI7021_RESOLUTION_RH_8_TEMP_12          =   0x01,           /*!<  SI7021 9b  RH 12b Temp.                               */
-    SI7021_RESOLUTION_RH_10_TEMP_13         =   0x80,           /*!<  SI7021 10b RH 13b Temp.                               */
-    SI7021_RESOLUTION_RH_11_TEMP_11         =   0x81            /*!<  SI7021 11b RH 11b Temp.                               */
-} SI7021_measurement_resolution_t;
+    TMP102_RESOLUTION_MASK                  =   0x81,           /*!<  TMP102 Measurement Resolution                         */
+    TMP102_RESOLUTION_RH_12_TEMP_14         =   0x00,           /*!<  TMP102 12b RH 14b Temp.                               */
+    TMP102_RESOLUTION_RH_8_TEMP_12          =   0x01,           /*!<  TMP102 9b  RH 12b Temp.                               */
+    TMP102_RESOLUTION_RH_10_TEMP_13         =   0x80,           /*!<  TMP102 10b RH 13b Temp.                               */
+    TMP102_RESOLUTION_RH_11_TEMP_11         =   0x81            /*!<  TMP102 11b RH 11b Temp.                               */
+} TMP102_measurement_resolution_t;
 
 
 
@@ -99,10 +99,10 @@ typedef enum
 */
 typedef enum
 {
-    SI7021_VDDS_STATUS_MASK                 =   0x40,           /*!<  SI7021 VDD mask.                                      */
-    SI7021_VDDS_STATUS_VDD_OK               =   ( 0 << 6 ),     /*!<  VDD OK.                                               */
-    SI7021_VDDS_STATUS_VDD_LOW              =   ( 1 << 6 )      /*!<  VDD Low.                                              */
-} SI7021_vdds_status_t;
+    TMP102_VDDS_STATUS_MASK                 =   0x40,           /*!<  TMP102 VDD mask.                                      */
+    TMP102_VDDS_STATUS_VDD_OK               =   ( 0 << 6 ),     /*!<  VDD OK.                                               */
+    TMP102_VDDS_STATUS_VDD_LOW              =   ( 1 << 6 )      /*!<  VDD Low.                                              */
+} TMP102_vdds_status_t;
 
 
 
@@ -111,18 +111,18 @@ typedef enum
   */
 typedef enum
 {
-    SI7021_HTRE_MASK                        =   0x03,           /*!<  SI7021 HTRE Mask                                   */
-    SI7021_HTRE_ENABLED                     =   ( 1 << 2 ),     /*!<  SI7021 On-chip Heater Enable                          */
-    SI7021_HTRE_DISABLED                    =   ( 0 << 2 )      /*!<  SI7021 On-chip Heater Disable                         */
-} SI7021_heater_t;
+    TMP102_HTRE_MASK                        =   0x03,           /*!<  TMP102 HTRE Mask                                   */
+    TMP102_HTRE_ENABLED                     =   ( 1 << 2 ),     /*!<  TMP102 On-chip Heater Enable                          */
+    TMP102_HTRE_DISABLED                    =   ( 0 << 2 )      /*!<  TMP102 On-chip Heater Disable                         */
+} TMP102_heater_t;
 
 
 
 
 
 
-#ifndef SI7021_VECTOR_STRUCT_H
-#define SI7021_VECTOR_STRUCT_H
+#ifndef TMP102_VECTOR_STRUCT_H
+#define TMP102_VECTOR_STRUCT_H
 typedef struct
 {
     float    RelativeHumidity;
@@ -133,7 +133,7 @@ typedef struct
 
     uint8_t  FirmwareRevision;
     uint8_t  BatteryStatus;
-} SI7021_vector_data_t;
+} TMP102_vector_data_t;
 #endif
 
 
@@ -145,9 +145,9 @@ typedef struct
   */
 typedef enum
 {
-    SI7021_SUCCESS     =       0,
-    SI7021_FAILURE     =       1
-} SI7021_status_t;
+    TMP102_SUCCESS     =       0,
+    TMP102_FAILURE     =       1
+} TMP102_status_t;
 
 
 
@@ -157,60 +157,67 @@ typedef enum
   */
 /** It configures the I2C peripheral.
   */
-SI7021_status_t  SI7021_Init                        ( I2C_parameters_t myI2Cparameters );
+TMP102_status_t  TMP102_Init                        ( I2C_parameters_t myI2Cparameters );
 
 /** It configures the device: resolution and heater.
   */
-SI7021_status_t  SI7021_Conf                        ( I2C_parameters_t myI2Cparameters, SI7021_measurement_resolution_t myResolution, SI7021_heater_t myHeater );
+TMP102_status_t  TMP102_Conf                        ( I2C_parameters_t myI2Cparameters, TMP102_measurement_resolution_t myResolution, TMP102_heater_t myHeater );
 
 /** It performs a software reset.
   */
-SI7021_status_t  SI7021_SoftReset                   ( I2C_parameters_t myI2Cparameters );
+TMP102_status_t  TMP102_SoftReset                   ( I2C_parameters_t myI2Cparameters );
 
 /** It gets the electronic serial number.
   */
-SI7021_status_t  SI7021_GetElectronicSerialNumber   ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* mySerialNumber );
+TMP102_status_t  TMP102_GetElectronicSerialNumber   ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* mySerialNumber );
 
 /** It gets the firmware revision.
   */
-SI7021_status_t  SI7021_GetFirmwareRevision         ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myFirmwareRevision );
+TMP102_status_t  TMP102_GetFirmwareRevision         ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myFirmwareRevision );
 
 /** It sets the heater current.
 */
-SI7021_status_t  SI7021_SetHeaterCurrent            ( I2C_parameters_t myI2Cparameters, uint8_t myHeaterCurrent );
+TMP102_status_t  TMP102_SetHeaterCurrent            ( I2C_parameters_t myI2Cparameters, uint8_t myHeaterCurrent );
 
 /** It performs a new temperature measurement.
   */
-SI7021_status_t  SI7021_TriggerTemperature          ( I2C_parameters_t myI2Cparameters, SI7021_master_mode_t myMode );
+TMP102_status_t  TMP102_TriggerTemperature          ( I2C_parameters_t myI2Cparameters, TMP102_master_mode_t myMode );
 
 /** It read the temperature.
   */
-SI7021_status_t  SI7021_ReadTemperature             ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myTemperature );
+TMP102_status_t  TMP102_ReadTemperature             ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myTemperature );
 
 /** It reads the raw data from temperature.
   */
-SI7021_status_t  SI7021_ReadRawTemperature          ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myTemperature );
+TMP102_status_t  TMP102_ReadRawTemperature          ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myTemperature );
 
 /** It reads the raw temperature data after a relative humidity measurement was done.
   */
-SI7021_status_t  SI7021_ReadRawTemperatureFromRH    ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myTemperature );
+TMP102_status_t  TMP102_ReadRawTemperatureFromRH    ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myTemperature );
 
 /** It reads the temperature after a relative humidity measurement was done.
   */
-SI7021_status_t  SI7021_ReadTemperatureFromRH       ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myTemperature );
+TMP102_status_t  TMP102_ReadTemperatureFromRH       ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myTemperature );
 
 /** It performs a new relative humidity measurement.
   */
-SI7021_status_t  SI7021_TriggerHumidity             ( I2C_parameters_t myI2Cparameters, SI7021_master_mode_t myMode );
+TMP102_status_t  TMP102_TriggerHumidity             ( I2C_parameters_t myI2Cparameters, TMP102_master_mode_t myMode );
 
 /** It reads the relative humidity.
   */
-SI7021_status_t  SI7021_ReadHumidity                ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myHumidity );
+TMP102_status_t  TMP102_ReadHumidity                ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myHumidity );
 
 /** It reads the raw data from relative humidity.
   */
-SI7021_status_t  SI7021_ReadRawHumidity             ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myHumidity );
+TMP102_status_t  TMP102_ReadRawHumidity             ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myHumidity );
 
 /** It gets the battery status.
   */
-SI7021_status_t  SI7021_GetBatteryStatus            ( I2C_parameters_t myI2Cparameters, SI7021_vector_data_t* myBatteryStatus );
+TMP102_status_t  TMP102_GetBatteryStatus            ( I2C_parameters_t myI2Cparameters, TMP102_vector_data_t* myBatteryStatus );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TMP102_H */
