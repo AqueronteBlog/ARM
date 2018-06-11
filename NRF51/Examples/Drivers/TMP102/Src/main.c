97bf38df4c99285159c90edde7046f7f1fb89f25
Manuel Caballero <aqueronteblog@gmail.com>
2018-06-11 15:23:33 +0100

    NRF51 TMP102 Example. The main was just started to test the driver. It is still in progress

--- NRF51/Examples/Drivers/TMP102/Src/main.c
+++ NRF51/Examples/Drivers/TMP102/Src/main.c
@@ -1,15 +1,14 @@
 /**
  * @brief       main.c
- * @details     This example shows hot to work with an external device: iAQ-Core ( Indoor Air Quality Sensor Module ).
- *              Every 1s, a new reading is performed and sent through the UART.
+ * @details     [TODO].
  *
  *              The rest of the time, the microcontroller is in low power.
  *
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        4/June/2018
- * @version     4/June/2018    The ORIGIN
+ * @date        11/June/2018
+ * @version     11/June/2018    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice
@@ -23,7 +22,7 @@
 #include "board.h"
 #include "functions.h"
 #include "variables.h"
-#include "iAQ_Core.h"
+#include "TMP102.h"
 
 
 
@@ -42,9 +41,9 @@ int main( void )
     uint32_t myWarmUpCounter  =   0;
 
 
-    I2C_parameters_t           myiAQ_Core_I2C_parameters;
-    iAQ_Core_status_t          aux;
-    iAQ_Core_vector_data_t     myiAQ_Core_Data;
+    I2C_parameters_t         myTMP102_I2C_parameters;
+    TMP102_status_t          aux;
+    TMP102_vector_data_t     myTMP102_Data;
 
 
     conf_GPIO   ();
@@ -54,25 +53,17 @@ int main( void )
 
 
     /* I2C definition   */
-    myiAQ_Core_I2C_parameters.TWIinstance =    NRF_TWI0;
-    myiAQ_Core_I2C_parameters.SDA         =    TWI0_SDA;
-    myiAQ_Core_I2C_parameters.SCL         =    TWI0_SCL;
-    myiAQ_Core_I2C_parameters.ADDR        =    iAQ_Core_ADDRESS;
-    myiAQ_Core_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K100;
-    myiAQ_Core_I2C_parameters.SDAport     =    NRF_GPIO;
-    myiAQ_Core_I2C_parameters.SCLport     =    NRF_GPIO;
+    myTMP102_I2C_parameters.TWIinstance =    NRF_TWI0;
+    myTMP102_I2C_parameters.SDA         =    TWI0_SDA;
+    myTMP102_I2C_parameters.SCL         =    TWI0_SCL;
+    myTMP102_I2C_parameters.ADDR        =    TMP102_ADDRESS_A0_GROUND;
+    myTMP102_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+    myTMP102_I2C_parameters.SDAport     =    NRF_GPIO;
+    myTMP102_I2C_parameters.SCLport     =    NRF_GPIO;
 
     /* Configure I2C peripheral */
-    aux  =   iAQ_Core_Init ( myiAQ_Core_I2C_parameters );
+    aux  =   TMP102_Init ( myTMP102_I2C_parameters );
 
-
-    /* iAQ-Core warm up is at least 5 minutes ( 300 * 1s ) or when the sensor is ready  */
-    do
-    {
-        aux      =   iAQ_Core_GetNewReading ( myiAQ_Core_I2C_parameters, &myiAQ_Core_Data );
-        nrf_delay_ms ( 1000 );
-        myWarmUpCounter++;
-    }while( ( myWarmUpCounter < 300 ) && ( myiAQ_Core_Data.status == iAQ_Core_STATUS_RUNIN ) );
 
 
     mySTATE  =   0;                             // Reset the variable
@@ -92,37 +83,37 @@ int main( void )
     	NRF_GPIO->OUTCLR             |= ( ( 1 << LED1 ) | ( 1 << LED2 ) | ( 1 << LED3 ) | ( 1 << LED4 ) );          // Turn all the LEDs on
         if ( mySTATE == 1 )
         {
-            /* New reading */
-            do
-            {
-                aux      =   iAQ_Core_GetNewReading ( myiAQ_Core_I2C_parameters, &myiAQ_Core_Data );
-            }while( myiAQ_Core_Data.status != iAQ_Core_STATUS_OK );                                         // [TODO] Dangerous!!! The uC may get stuck here if something goes wrong!
-                                                                                                            // [WORKAROUND] Insert a counter.
-
-            /* Prepare the data to be sent through the UART */
-            myTX_buff[0]                 =   ( myiAQ_Core_Data.pred >> 8 ) & 0xFF;                          // Prediction (CO2 eq. ppm): MSB
-            myTX_buff[1]                 =   ( myiAQ_Core_Data.pred & 0xFF );                               // Prediction (CO2 eq. ppm): LSB
-            myTX_buff[2]                 =   ( myiAQ_Core_Data.Tvoc >> 8 ) & 0xFF;                          // Prediction (TVOC eq. ppb): MSB
-            myTX_buff[3]                 =   ( myiAQ_Core_Data.Tvoc & 0xFF );                               // Prediction (TVOC eq. ppb): LSB
-            myTX_buff[4]                 =   ( myiAQ_Core_Data.resistance >> 16 ) & 0xFF;                   // Sensor resistance: MSB
-            myTX_buff[5]                 =   ( myiAQ_Core_Data.resistance >> 8 ) & 0xFF;                    // Sensor resistance
-            myTX_buff[6]                 =   ( myiAQ_Core_Data.resistance & 0xFF );                         // Sensor resistance: LSB
-
-
-            /* Send data through the UART   */
-            myPtr                        =   &myTX_buff[0];
-            TX_inProgress                =   YES;
-            NRF_UART0->TASKS_STARTTX     =   1;
-            NRF_UART0->TXD               =   *myPtr++;                                                      // Start transmission
-
-            /* Wait until the message is transmitted */
-            while ( TX_inProgress == YES )
-            {
-                __WFE();
-                // Make sure any pending events are cleared
-                __SEV();
-                __WFE();
-            }
+//            /* New reading */
+//            do
+//            {
+//                aux      =   TMP102_GetNewReading ( myTMP102_I2C_parameters, &myTMP102_Data );
+//            }while( myTMP102_Data.status != TMP102_STATUS_OK );                                         // [TODO] Dangerous!!! The uC may get stuck here if something goes wrong!
+//                                                                                                            // [WORKAROUND] Insert a counter.
+//
+//            /* Prepare the data to be sent through the UART */
+//            myTX_buff[0]                 =   ( myTMP102_Data.pred >> 8 ) & 0xFF;                          // Prediction (CO2 eq. ppm): MSB
+//            myTX_buff[1]                 =   ( myTMP102_Data.pred & 0xFF );                               // Prediction (CO2 eq. ppm): LSB
+//            myTX_buff[2]                 =   ( myTMP102_Data.Tvoc >> 8 ) & 0xFF;                          // Prediction (TVOC eq. ppb): MSB
+//            myTX_buff[3]                 =   ( myTMP102_Data.Tvoc & 0xFF );                               // Prediction (TVOC eq. ppb): LSB
+//            myTX_buff[4]                 =   ( myTMP102_Data.resistance >> 16 ) & 0xFF;                   // Sensor resistance: MSB
+//            myTX_buff[5]                 =   ( myTMP102_Data.resistance >> 8 ) & 0xFF;                    // Sensor resistance
+//            myTX_buff[6]                 =   ( myTMP102_Data.resistance & 0xFF );                         // Sensor resistance: LSB
+//
+//
+//            /* Send data through the UART   */
+//            myPtr                        =   &myTX_buff[0];
+//            TX_inProgress                =   YES;
+//            NRF_UART0->TASKS_STARTTX     =   1;
+//            NRF_UART0->TXD               =   *myPtr++;                                                      // Start transmission
+//
+//            /* Wait until the message is transmitted */
+//            while ( TX_inProgress == YES )
+//            {
+//                __WFE();
+//                // Make sure any pending events are cleared
+//                __SEV();
+//                __WFE();
+//            }
 
 
             mySTATE             =   0;
