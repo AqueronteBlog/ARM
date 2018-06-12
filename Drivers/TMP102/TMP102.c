3d286c8fc83d418c2503ca28a8c84d7c03b8ec96
Manuel Caballero <aqueronteblog@gmail.com>
2018-06-12 11:32:19 +0100

    DRIVERS. TMP102. All the function were declared but GetTemperature.

--- Drivers/TMP102/TMP102.c
+++ Drivers/TMP102/TMP102.c
@@ -330,3 +330,345 @@ TMP102_status_t  TMP102_ReadConfigurationRegister ( I2C_parameters_t myI2Cparame
     else
         return   TMP102_FAILURE;
 }
+
+
+
+/**
+ * @brief       TMP102_SetShutdownMode ( I2C_parameters_t , TMP102_configuration_shutdown_mode_t )
+ *
+ * @details     It enables shutdown/continuous mode operation.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    mySDMode:          Shutdown/continuous mode operation.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of TMP102_SetShutdownMode.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        12/June/2018
+ * @version     12/June/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+TMP102_status_t  TMP102_SetShutdownMode ( I2C_parameters_t myI2Cparameters, TMP102_configuration_shutdown_mode_t mySDMode )
+{
+    uint8_t      cmd[]    =    { 0, 0, 0 };
+
+    i2c_status_t aux;
+
+
+    // Read CONFIGURATION REGISTER
+    cmd[0]   =   TMP102_CONFIGURATION_REGISTER;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 2 );
+
+
+    // Mask the result and update the register
+    cmd[1]  &=  ~TMP102_CONFIGURATION_SD_MASK;
+    cmd[1]  |=   mySDMode;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   TMP102_SUCCESS;
+    else
+        return   TMP102_FAILURE;
+}
+
+
+
+/**
+ * @brief       TMP102_SetThermostatMode ( I2C_parameters_t , TMP102_configuration_thermostat_mode_t )
+ *
+ * @details     It enables comparator/interrupt mode operation.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myTMMode:          Comparator/interrupt mode operation.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of TMP102_SetThermostatMode.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        12/June/2018
+ * @version     12/June/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+TMP102_status_t  TMP102_SetThermostatMode ( I2C_parameters_t myI2Cparameters, TMP102_configuration_thermostat_mode_t myTMMode )
+{
+    uint8_t      cmd[]    =    { 0, 0, 0 };
+
+    i2c_status_t aux;
+
+
+    // Read CONFIGURATION REGISTER
+    cmd[0]   =   TMP102_CONFIGURATION_REGISTER;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 2 );
+
+
+    // Mask the result and update the register
+    cmd[1]  &=  ~TMP102_CONFIGURATION_TM_MASK;
+    cmd[1]  |=   myTMMode;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   TMP102_SUCCESS;
+    else
+        return   TMP102_FAILURE;
+}
+
+
+
+/**
+ * @brief       TMP102_SetPolarityAlertPinOutput ( I2C_parameters_t , TMP102_configuration_polarity_t )
+ *
+ * @details     The polarity bit allows the user to adjust the polarity of the ALERT pin output.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myPOLMode:         Polarity for ALERT pin output.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of TMP102_SetPolarityAlertPinOutput.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        12/June/2018
+ * @version     12/June/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+TMP102_status_t  TMP102_SetPolarityAlertPinOutput ( I2C_parameters_t myI2Cparameters, TMP102_configuration_polarity_t myPOLMode )
+{
+    uint8_t      cmd[]    =    { 0, 0, 0 };
+
+    i2c_status_t aux;
+
+
+    // Read CONFIGURATION REGISTER
+    cmd[0]   =   TMP102_CONFIGURATION_REGISTER;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 2 );
+
+
+    // Mask the result and update the register
+    cmd[1]  &=  ~TMP102_CONFIGURATION_POL_MASK;
+    cmd[1]  |=   myPOLMode;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   TMP102_SUCCESS;
+    else
+        return   TMP102_FAILURE;
+}
+
+
+
+/**
+ * @brief       TMP102_SetConsecutiveFaultsQueue ( I2C_parameters_t , TMP102_configuration_fault_queue_t )
+ *
+ * @details     The number of fault conditions required to generate an alert may be programmed using the fault queue.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myF1F0Mode:        Consecutive faults.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of TMP102_SetConsecutiveFaultsQueue.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        12/June/2018
+ * @version     12/June/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+TMP102_status_t  TMP102_SetConsecutiveFaultsQueue ( I2C_parameters_t myI2Cparameters, TMP102_configuration_fault_queue_t myF1F0Mode )
+{
+    uint8_t      cmd[]    =    { 0, 0, 0 };
+
+    i2c_status_t aux;
+
+
+    // Read CONFIGURATION REGISTER
+    cmd[0]   =   TMP102_CONFIGURATION_REGISTER;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 2 );
+
+
+    // Mask the result and update the register
+    cmd[1]  &=  ~TMP102_CONFIGURATION_F1F0_MASK;
+    cmd[1]  |=   myF1F0Mode;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   TMP102_SUCCESS;
+    else
+        return   TMP102_FAILURE;
+}
+
+
+
+/**
+ * @brief       TMP102_TriggerSingleTemperatureConversion ( I2C_parameters_t )
+ *
+ * @details     When the device is in Shutdown Mode, writing a 1 to the OS bit starts a single temperature conversion.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of TMP102_TriggerSingleTemperatureConversion.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        12/June/2018
+ * @version     12/June/2018   The ORIGIN
+ * @pre         The device has to be in shutdown mode in order to use this function.
+ * @warning     N/A.
+ */
+TMP102_status_t  TMP102_TriggerSingleTemperatureConversion ( I2C_parameters_t myI2Cparameters )
+{
+    uint8_t      cmd[]    =    { 0, 0, 0 };
+
+    i2c_status_t aux;
+
+
+    // Read CONFIGURATION REGISTER
+    cmd[0]   =   TMP102_CONFIGURATION_REGISTER;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 2 );
+
+
+    // Mask the result and update the register
+    cmd[1]  &=  ~TMP102_CONFIGURATION_OS_MASK;
+    cmd[1]  |=   TMP102_CONFIGURATION_OS_START_SINGLE_TEMPERATURE_CONVERSION;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   TMP102_SUCCESS;
+    else
+        return   TMP102_FAILURE;
+}
+
+
+
+/**
+ * @brief       TMP102_SetModeOperation ( I2C_parameters_t , TMP102_configuration_extended_mode_bit_t )
+ *
+ * @details     It sets the device into Normal/Extended mode operation.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myEMMode:          Normal/Extended mode operation.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of TMP102_SetModeOperation.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        12/June/2018
+ * @version     12/June/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+TMP102_status_t  TMP102_SetModeOperation ( I2C_parameters_t myI2Cparameters, TMP102_configuration_extended_mode_bit_t myEMMode  )
+{
+    uint8_t      cmd[]    =    { 0, 0, 0 };
+
+    i2c_status_t aux;
+
+
+    // Read CONFIGURATION REGISTER
+    cmd[0]   =   TMP102_CONFIGURATION_REGISTER;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 2 );
+
+
+    // Mask the result and update the register
+    cmd[1]  &=  ~TMP102_CONFIGURATION_EM_MASK;
+    cmd[1]  |=   myEMMode;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   TMP102_SUCCESS;
+    else
+        return   TMP102_FAILURE;
+}
+
+
+
+/**
+ * @brief       TMP102_SetConversionRate ( I2C_parameters_t , TMP102_configuration_conversion_rate_t )
+ *
+ * @details     It sets the conversion rate for the device.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myCR:              Conversion rate.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of TMP102_SetConversionRate.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        12/June/2018
+ * @version     12/June/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+TMP102_status_t  TMP102_SetConversionRate ( I2C_parameters_t myI2Cparameters, TMP102_configuration_conversion_rate_t myCR  )
+{
+    uint8_t      cmd[]    =    { 0, 0, 0 };
+
+    i2c_status_t aux;
+
+
+    // Read CONFIGURATION REGISTER
+    cmd[0]   =   TMP102_CONFIGURATION_REGISTER;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 2 );
+
+
+    // Mask the result and update the register
+    cmd[1]  &=  ~TMP102_CONFIGURATION_CR_MASK;
+    cmd[1]  |=   myCR;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+        return   TMP102_SUCCESS;
+    else
+        return   TMP102_FAILURE;
+}
