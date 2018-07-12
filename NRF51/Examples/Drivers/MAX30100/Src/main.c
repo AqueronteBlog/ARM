5781e4e9abbdd6c6fcf9866bc4ac8c8b6cb1313c
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-12 17:34:33 +0100

    NRF51 MAX30100 Example. The code is still in progress to test the driver

--- NRF51/Examples/Drivers/MAX30100/Src/main.c
+++ NRF51/Examples/Drivers/MAX30100/Src/main.c
@@ -1,6 +1,6 @@
 /**
  * @brief       main.c
- * @details     This project shows how to work with the external sensor: TMP102. It
+ * @details     This project shows how to work with the external sensor: MAX30100. It
  *              performs a new reading every one second and send the result through UART.
  *
  *              The rest of the time, the microcontroller is in low power.
@@ -8,14 +8,15 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        11/June/2018
- * @version     11/June/2018    The ORIGIN
+ * @date        9/July/2018
+ * @version     9/July/2018    The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     Softdevice S310 was used although the file's name is S130. The softdevice
  *              is not used in this example anyway because of Bluetooth was not used.
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
+#include <stdio.h>
 
 #include "nrf.h"
 #include "nrf_delay.h"
@@ -23,27 +24,31 @@
 #include "board.h"
 #include "functions.h"
 #include "variables.h"
-#include "TMP102.h"
+#include "MAX30100.h"
 
 
 
-/* GLOBAL VARIABLES */
-uint32_t volatile mySTATE;                       /*!<   It indicates the next action to be performed                                       */
-uint8_t  volatile dataToBeTX;                    /*!<   A counter. It indicates how many data it will be transmitted through the UART      */
-uint32_t volatile TX_inProgress;                 /*!<   It indicates if a transmission is in progress                                      */
-uint8_t  volatile *myPtr;                        /*!<   Pointer to point out the data from the external sensor                             */
+/**@brief CONSTANTS.
+ */
+#define TX_BUFF_SIZE  32                        /*!<   UART buffer size                               */
 
 
 
-/* MAIN */
+/**@brief VARIABLES.
+ */
+volatile uint8_t  myMessage[ TX_BUFF_SIZE ];    /*!<   Message to be transmitted through the UART     */
+volatile uint8_t  *myPtr;                       /*!<   Pointer to point out myMessage                 */
+volatile uint32_t myState;                      /*!<   Variable to perform a new reading              */
+
+
+
+/**@brief FUNCTION FOR APPLICATION MAIN ENTRY.
+ */
 int main( void )
 {
-    uint8_t  myTX_buff[2]     =  { 0 };
-
-
-    I2C_parameters_t         myTMP102_I2C_parameters;
-    TMP102_status_t          aux;
-    TMP102_vector_data_t     myTMP102_Data;
+    I2C_parameters_t         myMAX30100_I2C_parameters;
+    MAX30100_status_t        aux;
+    MAX30100_vector_data_t   myMAX30100_Data;
 
 
     conf_GPIO   ();
@@ -53,38 +58,64 @@ int main( void )
 
 
     /* I2C definition   */
-    myTMP102_I2C_parameters.TWIinstance =    NRF_TWI0;
-    myTMP102_I2C_parameters.SDA         =    TWI0_SDA;
-    myTMP102_I2C_parameters.SCL         =    TWI0_SCL;
-    myTMP102_I2C_parameters.ADDR        =    TMP102_ADDRESS_A0_GROUND;
-    myTMP102_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
-    myTMP102_I2C_parameters.SDAport     =    NRF_GPIO;
-    myTMP102_I2C_parameters.SCLport     =    NRF_GPIO;
+    myMAX30100_I2C_parameters.TWIinstance =    NRF_TWI0;
+    myMAX30100_I2C_parameters.SDA         =    TWI0_SDA;
+    myMAX30100_I2C_parameters.SCL         =    TWI0_SCL;
+    myMAX30100_I2C_parameters.ADDR        =    MAX30100_ADDRESS;
+    myMAX30100_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+    myMAX30100_I2C_parameters.SDAport     =    NRF_GPIO;
+    myMAX30100_I2C_parameters.SCLport     =    NRF_GPIO;
 
     /* Configure I2C peripheral */
-    aux  =   TMP102_Init                        ( myTMP102_I2C_parameters );
+    aux  =   MAX30100_Init                      ( myMAX30100_I2C_parameters );
 
-    /* Read TLOW value */
-    aux  =   TMP102_Read_T_LOW_Register         ( myTMP102_I2C_parameters, &myTMP102_Data );
+    /* Software reset   */
+    aux  =   MAX30100_SoftwareReset             ( myMAX30100_I2C_parameters );
 
-    /* Read THIGH value */
-    aux  =   TMP102_Read_T_HIGH_Register        ( myTMP102_I2C_parameters, &myTMP102_Data );
+    /* Wait until the software reset is finished    */
+    do
+    {
+        aux  =   MAX30100_PollingSoftwareReset ( myMAX30100_I2C_parameters, &myMAX30100_Data );
+    }while( ( myMAX30100_Data.ResetFlag & MODE_CONFIGURATION_RESET_MASK ) != MODE_CONFIGURATION_RESET_DISABLE );    // [TODO] Dangerous!!! The uC may get stuck here if something goes wrong!
+                                                                                                                    // [WORKAROUND] Insert a counter.
 
-    /* Set polarity LOW */
-    aux  =   TMP102_SetPolarityAlertPinOutput   ( myTMP102_I2C_parameters, TMP102_CONFIGURATION_POL_ALERT_PIN_ACTIVE_LOW );
+    /* Shutdown enabled   */
+    aux  =   MAX30100_ShutdownControl           ( myMAX30100_I2C_parameters, MODE_CONFIGURATION_SHDN_ENABLE );
 
-    /* Set Normal mode operation */
-    aux  =   TMP102_SetModeOperation            ( myTMP102_I2C_parameters, TMP102_CONFIGURATION_EM_NORMAL_MODE_OPERATION );
+    /* Get Revision ID   */
+    aux  =   MAX30100_GetRevisionID             ( myMAX30100_I2C_parameters, &myMAX30100_Data );
 
-    /* Set Conversion Rate: 4Hz */
-    aux  =   TMP102_SetConversionRate           ( myTMP102_I2C_parameters, TMP102_CONFIGURATION_CR_4_HZ );
+    /* Get Part ID   */
+    aux  =   MAX30100_GetPartID                 ( myMAX30100_I2C_parameters, &myMAX30100_Data );
 
-    /* Set Shutdown mode */
-    aux  =   TMP102_SetShutdownMode             ( myTMP102_I2C_parameters, TMP102_CONFIGURATION_SD_ENABLED );
+    /* Set RED LED current: 27.1mA */
+    aux  =   MAX30100_SetRed_LED_CurrentControl ( myMAX30100_I2C_parameters, LED_CONFIGURATION_RED_PA_27_1_MA );
+
+    /* Set IR LED current: 50.0mA */
+    aux  =   MAX30100_SetIR_LED_CurrentControl  ( myMAX30100_I2C_parameters, LED_CONFIGURATION_IR_PA_50_0_MA );
+
+    /* Set sample rate: 50 samples per second    */
+    aux  =   MAX30100_SpO2_SampleRateControl    ( myMAX30100_I2C_parameters, SPO2_CONFIGURATION_SPO2_SR_50 );
+
+    /* Set pulse width/resolution: 200us, 13-bit    */
+    myMAX30100_Data.Resolution   =   SPO2_CONFIGURATION_LED_PW_200US_13BITS;
+    aux  =   MAX30100_LED_PulseWidthControl     ( myMAX30100_I2C_parameters, myMAX30100_Data );
+
+    /* Mode Control: SpO2 and HR   */
+    aux  =   MAX30100_ModeControl               ( myMAX30100_I2C_parameters, MODE_CONFIGURATION_MODE_SPO2_ENABLED );
+
+    /* Reset the FIFO    */
+    aux  =   MAX30100_ClearFIFO                 ( myMAX30100_I2C_parameters, &myMAX30100_Data );
+
+//    /* Mode Control: SpO2 and HR   */
+//    aux  =   MAX30100_InterrupEnable            ( myMAX30100_I2C_parameters, uint8_t myInterruptEnable  );
+
+    /* Shutdown disabled   */
+    aux  =   MAX30100_ShutdownControl           ( myMAX30100_I2C_parameters, MODE_CONFIGURATION_SHDN_DISABLE );
 
 
 
-    mySTATE  =   0;                             // Reset the variable
+    myState  =   0;                             // Reset the variable
     NRF_TIMER0->TASKS_START  =   1;             // Start Timer0
 
     //NRF_POWER->SYSTEMOFF = 1;
@@ -99,44 +130,41 @@ int main( void )
 
 
     	NRF_GPIO->OUTCLR             |= ( ( 1 << LED1 ) | ( 1 << LED2 ) | ( 1 << LED3 ) | ( 1 << LED4 ) );          // Turn all the LEDs on
-        if ( mySTATE == 1 )
+        if ( myState == 1 )
         {
             /* New reading */
-            aux  =   TMP102_TriggerSingleTemperatureConversion ( myTMP102_I2C_parameters );
+            aux  =   MAX30100_TriggerTemperature ( myMAX30100_I2C_parameters );
 
             /* Wait until the conversion is finished    */
             do
             {
-                aux      =   TMP102_ReadConfigurationRegister ( myTMP102_I2C_parameters, &myTMP102_Data );
-            }while( ( myTMP102_Data.ConfigurationRegister & TMP102_CONFIGURATION_OS_MASK ) == TMP102_CONFIGURATION_OS_BUSY );   // [TODO] Dangerous!!! The uC may get stuck here if something goes wrong!
-                                                                                                                                // [WORKAROUND] Insert a counter.
+                aux      =   MAX30100_PollingTemperatureConversion ( myMAX30100_I2C_parameters, &myMAX30100_Data );
+            }while( ( myMAX30100_Data.TemperatureFlag & MODE_CONFIGURATION_TEMP_EN_MASK ) == MODE_CONFIGURATION_TEMP_EN_ENABLE );   // [TODO] Dangerous!!! The uC may get stuck here if something goes wrong!
+                                                                                                                                    // [WORKAROUND] Insert a counter.
 
             /* Read the new temperature value */
-            aux  =   TMP102_GetTemperature ( myTMP102_I2C_parameters, &myTMP102_Data );
+            aux  =   MAX30100_GetTemperature ( myMAX30100_I2C_parameters, &myMAX30100_Data );
+
+            /* Read the FIFO */
+            aux  =   MAX30100_ReadFIFO ( myMAX30100_I2C_parameters, &myMAX30100_Data, 16 );
+
 
 
             /* Prepare the data to be sent through the UART */
-            myTX_buff[0]                 =   ( myTMP102_Data.TemperatureRegister >> 8 ) & 0xFF;                         // Temperature Register: MSB
-            myTX_buff[1]                 =   ( myTMP102_Data.TemperatureRegister & 0xFF );                              // Temperature Register: LSB
+            sprintf ( (char*)myMessage, "Temperature: %d C\r\n", (int)myMAX30100_Data.Temperature );
 
 
             /* Send data through the UART   */
-            myPtr                        =   &myTX_buff[0];
-            TX_inProgress                =   YES;
-            NRF_UART0->TASKS_STARTTX     =   1;
-            NRF_UART0->TXD               =   *myPtr++;                                                                  // Start transmission
+            NRF_UART0->TASKS_STOPRX  =   1;
+            NRF_UART0->TASKS_STOPTX  =   1;
+            myPtr                    =   &myMessage[0];
 
-            /* Wait until the message is transmitted */
-            while ( TX_inProgress == YES )
-            {
-                __WFE();
-                // Make sure any pending events are cleared
-                __SEV();
-                __WFE();
-            }
+            NRF_UART0->TASKS_STARTTX =   1;
+            NRF_UART0->TXD           =   *myPtr;
 
 
-            mySTATE             =   0;
+            /* Reset the variables   */
+            myState                  =   0;
     	}
 
         NRF_GPIO->OUTSET             |= ( ( 1 << LED1 ) | ( 1 << LED2 ) | ( 1 << LED3 ) | ( 1 << LED4 ) );          // Turn all the LEDs off
