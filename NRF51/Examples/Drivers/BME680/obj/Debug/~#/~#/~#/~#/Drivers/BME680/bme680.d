864ae6bd15e70d1953c811e299b9f8eb07bbca57
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-16 11:35:55 +0100

    NRF51 BME680 Example. Medium changes, last compilation without any errors, Bosh driver is being adapted

--- NRF51/Examples/Drivers/BME680/obj/Debug/~#/~#/~#/~#/Drivers/BME680/bme680.d
+++ NRF51/Examples/Drivers/BME680/obj/Debug/~#/~#/~#/~#/Drivers/BME680/bme680.d
@@ -1,8 +1,3 @@
 obj\debug\~\#\~\#\~\#\~\#\drivers\bme680\bme680.o: \
- ..\..\..\..\Drivers\BME680\BME680.c ..\..\..\..\Drivers\BME680\BME680.h \
- ..\..\Basics\I2C/i2c.h Inc/nrf.h Inc/nrf51.h \
- c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cm0.h \
- c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cminstr.h \
- c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cmfunc.h \
- Inc/system_nrf51.h Inc/nrf51_bitfields.h Inc/nrf51_deprecated.h \
- Inc/compiler_abstraction.h Inc/nrf51_bitfields.h
+ ..\..\..\..\Drivers\BME680\bme680.c ..\..\..\..\Drivers\BME680\bme680.h \
+ ..\..\..\..\Drivers\BME680\bme680_defs.h
