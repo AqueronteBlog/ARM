3ce4b90e62d26581a3eeefb90cd2ef5e93a33593
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-16 14:54:09 +0100

    NRF51 BME680 Example. Some comments were improved

--- NRF51/Examples/Drivers/BME680/Src/board.h
+++ NRF51/Examples/Drivers/BME680/Src/board.h
@@ -15,16 +15,42 @@
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
-/* LEDs */
-#define LED1        21     /*!<   LED1: P0.21   */
-#define LED2        22     /*!<   LED2: P0.22   */
-#define LED3        23     /*!<   LED3: P0.23   */
-#define LED4        24     /*!<   LED4: P0.24   */
+#ifndef BOARD_H_
+#define BOARD_H_
 
-/* UART0 */
-#define UART0_TX    9      /*!<   UART TX: P0.9    */
-#define UART0_RX    11     /*!<   UART RX: P0.11   */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-/* TWI0 */
-#define TWI0_SCL    7      /*!<   TWI0 SCL: P0.7    */
-#define TWI0_SDA    30     /*!<   TWI0 SDA: P0.30   */
+
+/**@brief LEDS.
+ */
+typedef enum{
+  LED1  = 21,               /*!<   LED1: P0.21   */
+  LED2  = 22,               /*!<   LED2: P0.22   */
+  LED3  = 23,               /*!<   LED3: P0.23   */
+  LED4  = 24                /*!<   LED4: P0.24   */
+} NRF51DK_board_leds_t;
+
+
+/**@brief UART0.
+ */
+typedef enum{
+  UART0_TX  =   9,          /*!<   UART TX: P0.9    */
+  UART0_RX  =   11          /*!<   UART RX: P0.11   */
+} NRF51DK_board_uart0_t;
+
+
+/**@brief TWI0.
+ */
+typedef enum{
+  TWI0_SCL  =   7,          /*!<   TWI0 SCL: P0.7    */
+  TWI0_SDA  =   30          /*!<   TWI0 SDA: P0.30   */
+} NRF51DK_board_twi0_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H_ */
