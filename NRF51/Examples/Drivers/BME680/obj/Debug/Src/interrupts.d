3ff6458a0078aaac3d45d04073b3816723b1c7dc
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-16 14:53:49 +0100

    NRF51 BME680 Example. Minor changes, last compilation without any errors.

--- NRF51/Examples/Drivers/BME680/obj/Debug/Src/interrupts.d
+++ NRF51/Examples/Drivers/BME680/obj/Debug/Src/interrupts.d
@@ -1,7 +1,7 @@
-obj\debug\src\interrupts.o: Src\interrupts.c Src\interrupts.h \
+obj\debug\src\interrupts.o: Src\interrupts.c Src\interrupts.h Src\board.h \
  Src\variables.h Inc/nrf.h Inc/nrf51.h \
  c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cm0.h \
  c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cminstr.h \
  c:\program\ files\ (x86)\embitz\1.11\share\em_armgcc\lib\cmsis\include\core_cmfunc.h \
  Inc/system_nrf51.h Inc/nrf51_bitfields.h Inc/nrf51_deprecated.h \
- Inc/compiler_abstraction.h Inc/nrf51_bitfields.h Src\board.h
+ Inc/compiler_abstraction.h Inc/nrf51_bitfields.h
