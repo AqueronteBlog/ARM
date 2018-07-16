cfd0519f078018ff22308ac0e77f6cbc3d4215e6
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-16 14:55:54 +0100

    NRF51 BME680 Example. Some comments were improved

--- NRF51/Examples/Drivers/BME680/Src/variables.h
+++ NRF51/Examples/Drivers/BME680/Src/variables.h
@@ -6,8 +6,8 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        13/February/2018
- * @version     13/February/2018     The ORIGIN
+ * @date        9/July/2018
+ * @version     9/July/2018     The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     N/A
@@ -18,17 +18,21 @@
 #include "nrf51_bitfields.h"
 #include "board.h"
 
-/* CONSTANTS */
-#define ON              1
-#define OFF             0
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
 
-#define YES             1
-#define NO              0
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**@brief CONSTANTS.
+ */
 
 
-/* VARIABLES */
-uint32_t mySTATE;                       /*!<   It indicates the next action to be performed                                       */
-uint8_t  dataToBeTX;                    /*!<   A counter. It indicates how many data it will be transmitted through the UART      */
-uint32_t TX_inProgress;                 /*!<   It indicates if a transmission is in progress                                      */
-uint8_t *myPtr;                         /*!<   Pointer to point out the data from the external sensor                             */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
+
 
