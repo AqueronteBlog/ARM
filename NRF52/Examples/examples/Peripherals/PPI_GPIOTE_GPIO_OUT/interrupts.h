87e7daa6234477a493f97befe1275525b5383c92
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-17 19:23:15 +0100

    NRF52 PPI GPIOTE GPIO OUT Example. Interrupts files were added.

---
+++ NRF52/Examples/examples/Peripherals/PPI_GPIOTE_GPIO_OUT/interrupts.h
@@ -0,0 +1,49 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        17/July/2018
+ * @version     17/July/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "board.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+void UART0_IRQHandler ( void );
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+extern volatile uint8_t  myMessage[];
+extern volatile uint8_t  *myPtr;
+extern volatile uint32_t myState;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
