a0e034fa89e46071768ee92f8410c1d9470aa56c
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-26 07:12:44 +0100

    NRF52 PPI GPIOTE GPIO OUT TIMER Example. This example was completed and tested, it works as expected.

--- NRF52/Examples/examples/Peripherals/PPI_GPIOTE_GPIO_OUT_TIMER/main.c
+++ NRF52/Examples/examples/Peripherals/PPI_GPIOTE_GPIO_OUT_TIMER/main.c
@@ -1,7 +1,7 @@
 /**
  * @brief       main.c
- * @details     [TODO]This example shows how to work with the PPI peripheral. The LED1
- *              blinks every 125ms. PPI interconnects the RTC event with the GPIOTE task.
+ * @details     This example shows how to work with the PPI peripheral. The LED1
+ *              blinks every 500ms. PPI interconnects the TIMER0 CC[0] event with the GPIOTE task.
  *
  *              The microcontroller is in low power the rest of the time.
  *
@@ -41,14 +41,14 @@ int main(void)
   conf_GPIO   ();
   conf_GPIOTE ();
   conf_PPI    ();
-  conf_RTC1   ();
+  conf_TIMER0 ();
 
   
 
-  NRF_RTC1->TASKS_START  =   1;       // Start RTC1
+  NRF_TIMER0->TASKS_START  =   1U;    // Start TIMER0
 
-//  NRF_POWER->SYSTEMOFF = 1;
-  NRF_POWER->TASKS_LOWPWR = 1;        // Sub power mode: Low power.
+//  NRF_POWER->SYSTEMOFF = 1U;
+  NRF_POWER->TASKS_LOWPWR = 1U;       // Sub power mode: Low power.
   while( 1 )
   {
     /* Enter System ON sleep mode  */
