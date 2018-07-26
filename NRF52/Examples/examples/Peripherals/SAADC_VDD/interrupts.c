02656af7cb1973c48c3eebb7c67cbd5031cfe0a3
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-26 09:10:48 +0200

    NRF52 SAADC VDD Example. Interrupts files were added

---
+++ NRF52/Examples/examples/Peripherals/SAADC_VDD/interrupts.c
@@ -0,0 +1,14 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        26/July/2018
+ * @version     26/July/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
---
+++ NRF52/Examples/examples/Peripherals/SAADC_VDD/interrupts.h
@@ -0,0 +1,46 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        26/July/2018
+ * @version     26/July/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "board.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
