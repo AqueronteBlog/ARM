62341fa51b446567a6aa8e566537e604ea1ea8bd
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-26 09:11:05 +0200

    NRF52 SAADC VDD Example. This project was just started, it is still in progress

---
+++ NRF52/Examples/examples/Peripherals/SAADC_VDD/main.c
@@ -0,0 +1,65 @@
+/**
+ * @brief       main.c
+ * @details     [TODO] This example shows how to work with the PPI peripheral. The LED1
+ *              blinks every 500ms. PPI interconnects the TIMER0 CC[0] event with the GPIOTE task.
+ *
+ *              The microcontroller is in low power the rest of the time.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        26/July/2018
+ * @version     26/July/2018    The ORIGIN
+ * @pre         This firmware was tested on the nrf52-DK with Segger Embedded Studio v3.40
+ *              ( SDK 14.2.0 ).
+ * @warning     The softdevice (s132) is taken into account, Bluetooth was not used although.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+#include <stdio.h>
+
+#include "nordic_common.h"
+#include "nrf.h"
+#include "board.h"
+#include "functions.h"
+#include "interrupts.h"
+
+
+/**@brief Constants.
+ */
+
+
+/**@brief Variables.
+ */
+
+
+
+/**@brief Function for application main entry.
+ */
+int main(void)
+{
+  conf_GPIO   ();
+  conf_GPIOTE ();
+  conf_PPI    ();
+  conf_TIMER0 ();
+
+  
+
+  NRF_TIMER0->TASKS_START  =   1U;    // Start TIMER0
+
+//  NRF_POWER->SYSTEMOFF = 1U;
+  NRF_POWER->TASKS_LOWPWR = 1U;       // Sub power mode: Low power.
+  while( 1 )
+  {
+    /* Enter System ON sleep mode  */
+    __WFE();
+    /* Make sure any pending events are cleared  */
+    __SEV();
+    __WFE();
+  }
+}
+
+
+/**
+ * @}
+ */
