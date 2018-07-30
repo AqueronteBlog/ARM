fbfb9ec7ecfd3cb7c1a9d5cf8b3d2bcdcb334512
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-30 22:45:08 +0200

    DRIVERS. DS1307, function file is ready to be tested, reading/writing the RAM is still in progress

--- Drivers/DS1307/DS1307.c
+++ Drivers/DS1307/DS1307.c
@@ -53,47 +53,726 @@ DS1307_status_t  DS1307_Init ( I2C_parameters_t myI2Cparameters )
 
 
 /**
- * @brief       DS1307_ReadTemperatureRegister ( I2C_parameters_t , DS1307_vector_data_t* )
+ * @brief       DS1307_SquareWaveOutput ( I2C_parameters_t , DS1307_control_sqwe_t , DS1307_control_rate_select_t )
  *
- * @details     It gets the temperature register value ( raw temperature ).
+ * @details     It configures the square-wave output.
  *
- * @param[in]    myI2Cparameters:       I2C parameters.
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    mySQWE:            Square-wave output enabled/disabled.
+ * @param[in]    myRateSelect:      Rate select.
  *
- * @param[out]   myTemperatureRegister: Temperature register value ( raw temperature ).
+ * @param[out]   N/A.
  *
  *
- * @return       Status of DS1307_ReadTemperatureRegister.
+ * @return       Status of DS1307_SquareWaveOutput.
  *
  *
  * @author      Manuel Caballero
- * @date        28/July/2018
- * @version     28/July/2018   The ORIGIN
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
  * @pre         N/A.
  * @warning     N/A.
  */
-DS1307_status_t  DS1307_ReadTemperatureRegister ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t* myTemperatureRegister )
+DS1307_status_t  DS1307_SquareWaveOutput ( I2C_parameters_t myI2Cparameters, DS1307_control_sqwe_t mySQWE, DS1307_control_rate_select_t myRateSelect )
 {
     uint8_t      cmd[]    =    { 0, 0 };
 
     i2c_status_t aux;
 
 
-    /* Read TEMPERATURE REGISTER to mask it */
-    cmd[0]   =   DS1307_TEMPERATURE_REGISTER;
-    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1, I2C_NO_STOP_BIT );
-    aux      =   i2c_read  ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ) );
+    /* Read CONTROL REGISTER to mask it */
+    cmd[0]   =   DS1307_CONTROL;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
 
 
-    /* Parse data   */
-    myTemperatureRegister->TemperatureRegister   =   ( cmd[0] << 8 );
-    myTemperatureRegister->TemperatureRegister  |=   cmd[1];
+    /* Update the register   */
+    cmd[1]  &=  ~( CONTROL_SQWE_MASK | CONTROL_RS_MASK );
+    cmd[1]  |=   ( mySQWE | myRateSelect );
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
 
 
 
 
     if ( aux == I2C_SUCCESS )
+    {
         return   DS1307_SUCCESS;
+    }
     else
+    {
         return   DS1307_FAILURE;
+    }
 }
 
+
+
+/**
+ * @brief       DS1307_ConfOutput ( I2C_parameters_t , DS1307_control_out_t )
+ *
+ * @details     It configures the output pin ( OUT ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myOUT:             OUT pin High/Low.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of DS1307_ConfOutput.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     Square-wave output must be disabled.
+ */
+DS1307_status_t  DS1307_ConfOutput ( I2C_parameters_t myI2Cparameters, DS1307_control_out_t myOUT )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Read CONTROL REGISTER to mask it */
+    cmd[0]   =   DS1307_CONTROL;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Update the register   */
+    cmd[1]  &=  ~( CONTROL_OUT_MASK );
+    cmd[1]  |=   myOUT;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_SetTime ( I2C_parameters_t , DS1307_vector_data_t )
+ *
+ * @details     It sets the time ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myTime:            Time in BCD mode.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of DS1307_SetTime.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: 20:35.59 24h --> 0x203559 24h Enabled.
+ *              i.e2: 08:35.59 12h --> 0x083559 12h Enabled ( PM ).
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_SetTime ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t myTime )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* SECONDS   */
+    /* Read SECONDS REGISTER to mask it */
+    cmd[0]   =   DS1307_SECONDS;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Update the register   */
+    cmd[1]  &=  ~( SECONDS_10SECONDS_MASK | SECONDS_SECONDS_MASK );
+    cmd[1]  |=   ( myTime.BCDTime & ( SECONDS_10SECONDS_MASK | SECONDS_SECONDS_MASK ) );
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+    /* MINUTES   */
+    /* Read MINUTES REGISTER to mask it */
+    cmd[0]   =   DS1307_MINUTES;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Update the register   */
+    cmd[1]  &=  ~( MINUTES_10MINUTES_MASK | MINUTES_MINUTES_MASK );
+    cmd[1]  |=   ( ( myTime.BCDTime >> 8U ) & ( MINUTES_10MINUTES_MASK | MINUTES_MINUTES_MASK ) );
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+    /* HOURS   */
+    /* Read HOURS REGISTER to mask it */
+    cmd[0]   =   DS1307_HOURS;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Mask the register   */
+    cmd[1]  &=  ~( HOURS_MODE_MASK | HOUR_10HOUR_MASK | HOUR_HOURS_MASK );
+
+    /* Check if it is 12h or 24h mode    */
+    if ( myTime.Time12H_24HMode ==  HOURS_MODE_24H )
+    {
+        cmd[1]  |=   ( ( myTime.BCDTime >> 16U ) & ( HOUR_10HOUR_MASK | HOUR_HOURS_MASK ) ) | myTime.Time12H_24HMode );
+    }
+    else
+    {
+        cmd[1]  |=   ( ( myTime.BCDTime >> 16U ) & ( HOUR_10HOUR_MASK | HOUR_HOURS_MASK ) ) | ( myTime.Time12H_24HMode | myTime.TimeAM_PM_Mode ) );
+    }
+
+    /* Update the register   */
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_GetTime ( I2C_parameters_t , DS1307_vector_data_t* )
+ *
+ * @details     It gets the time ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myTime:            Time in BCD mode.
+ *
+ *
+ * @return       Status of DS1307_GetTime.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: 20:35.59 24h --> 0x203559 24h Enabled.
+ *              i.e2: 08:35.59 12h --> 0x083559 12h Enabled ( PM ).
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_GetTime ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t* myTime )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* HOURS   */
+    /* Read HOURS REGISTER  */
+    cmd[0]   =   DS1307_HOURS;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Parse the data    */
+    cmd[1]  &=   ( HOUR_10HOUR_MASK | HOUR_HOURS_MASK );
+    myTime->BCDTime   =   cmd[1];
+
+
+    /* Check if it is 12h or 24h mode    */
+    if ( ( cmd[1] & HOURS_MODE_MASK ) ==  HOURS_MODE_24H )
+    {
+        /* 24h mode  */
+        myTime->Time12H_24HMode  =   HOURS_MODE_24H;
+
+        /* Update the PM/AM mode variable    */
+        if ( ( cmd[1] & ( HOUR_10HOUR_MASK | HOUR_HOURS_MASK ) ) >=  0x12 )
+        {
+            myTime->TimeAM_PM_Mode   =   HOURS_MODE_PM;
+        }
+        else
+        {
+            myTime->TimeAM_PM_Mode   =   HOURS_MODE_AM;
+        }
+    }
+    else
+    {
+        /* 12h mode  */
+        myTime->Time12H_24HMode  =   HOURS_MODE_12H;
+        myTime->TimeAM_PM_Mode   =   ( cmd[1] & HOURS_MODE_AM_PM_MASK );
+        myTime->BCDTime         &=  ~( HOURS_MODE_AM_PM_MASK );
+    }
+
+    myTime->BCDTime <<=   16U;
+
+
+
+    /* MINUTES   */
+    /* Read MINUTES REGISTER to mask it */
+    cmd[0]   =   DS1307_MINUTES;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Parse the data    */
+    cmd[1]  &=   ( MINUTES_10MINUTES_MASK | MINUTES_MINUTES_MASK );
+    myTime->BCDTime  |=   cmd[1];
+    myTime->BCDTime <<=   8U;
+
+
+
+    /* SECONDS   */
+    /* Read SECONDS REGISTER to mask it */
+    cmd[0]   =   DS1307_SECONDS;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Parse the data   */
+    cmd[1]  &=   ( SECONDS_10SECONDS_MASK | SECONDS_SECONDS_MASK );
+    myTime->BCDTime |=   cmd[1];
+
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_SetDayOfTheWeek ( I2C_parameters_t , DS1307_vector_data_t )
+ *
+ * @details     It sets the day of the week.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myDayOfTheWeek:    Day of the week.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of DS1307_SetDayOfTheWeek.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_SetDayOfTheWeek ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t myDayOfTheWeek )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Read DAY REGISTER to mask it */
+    cmd[0]   =   DS1307_DAY;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Update the register   */
+    cmd[1]  &=  ~( DAY_MASK );
+    cmd[1]  |=   myDayOfTheWeek.DayOfTheWeek;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_GetDayOfTheWeek ( I2C_parameters_t , DS1307_vector_data_t )
+ *
+ * @details     It gets the day of the week.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myDayOfTheWeek:    Day of the week.
+ *
+ *
+ * @return       Status of DS1307_GetDayOfTheWeek.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_GetDayOfTheWeek ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t* myDayOfTheWeek )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Read DAY REGISTER to mask it */
+    cmd[0]   =   DS1307_DAY;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Parse the data   */
+    myDayOfTheWeek->DayOfTheWeek     =   (DS1307_day_t)( cmd[1] & DAY_MASK );
+
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_SetDate ( I2C_parameters_t , DS1307_vector_data_t )
+ *
+ * @details     It sets the date ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myDate:            Date.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of DS1307_SetDate.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: 01:  0x01.
+ *              i.e2: 12:  0x12.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_SetDate ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t myDate )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Check date limits     */
+    if ( ( myDate.BCDDate >= 0x01 ) && ( myDate.BCDDate <= 0x31 ) )
+    {
+        /* Read DATE REGISTER to mask it */
+        cmd[0]   =   DS1307_DATE;
+        aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+        aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+        /* Update the register   */
+        cmd[1]  &=  ~( DATE_10DATE_MASK | DATE_DATE_MASK );
+        cmd[1]  |=   myDate.BCDDate;
+        aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+    }
+    else
+    {
+        return DS1307_FAILURE;
+    }
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_GetDate ( I2C_parameters_t , DS1307_vector_data_t* )
+ *
+ * @details     It gets the date ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myDate:            Date.
+ *
+ *
+ * @return       Status of DS1307_GetDate.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: 01:  0x01.
+ *              i.e2: 12:  0x12.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_GetDate ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t* myDate )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Read DATE REGISTER to mask it */
+    cmd[0]   =   DS1307_DATE;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Parse the data   */
+    myDate->BCDDate   =  ( cmd[1] & ( DATE_10DATE_MASK | DATE_DATE_MASK ) );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_SetMonth ( I2C_parameters_t , DS1307_vector_data_t )
+ *
+ * @details     It sets the month ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myMonth:           Month.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of DS1307_SetMonth.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: January:  0x01.
+ *              i.e2: December: 0x12.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_SetMonth ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t myMonth )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Read MONTH REGISTER to mask it */
+    cmd[0]   =   DS1307_MONTH;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Update the register   */
+    cmd[1]  &=  ~( MONTH_10MONTH_MASK | MONTH_MONTH_MASK );
+    cmd[1]  |=   myMonth.BCDMonth;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_GetMonth ( I2C_parameters_t , DS1307_vector_data_t* )
+ *
+ * @details     It gets the month ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myMonth:           Month.
+ *
+ *
+ * @return       Status of DS1307_GetMonth.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: January:  0x01.
+ *              i.e2: December: 0x12.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_GetMonth ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t* myMonth )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Read MONTH REGISTER to mask it */
+    cmd[0]   =   DS1307_MONTH;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &cmd[1], 1U );
+
+
+    /* Parse the data   */
+    myMonth->BCDMonth   =   (DS1307_month_t)( cmd[1] & ( MONTH_10MONTH_MASK | MONTH_MONTH_MASK ) );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_SetYear ( I2C_parameters_t , DS1307_vector_data_t )
+ *
+ * @details     It sets the year ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myYear:            Year.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of DS1307_SetYear.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: 2000:  0x00.
+ *              i.e2: 2018:  0x18.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_SetYear ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t myYear )
+{
+    uint8_t      cmd[]    =    { 0, 0 };
+
+    i2c_status_t aux;
+
+
+    /* Update the register   */
+    cmd[0]   =   DS1307_YEAR;
+    cmd[1]   =   myYear.BCDYear;
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       DS1307_GetYear ( I2C_parameters_t , DS1307_vector_data_t* )
+ *
+ * @details     It gets the year ( BCD mode ).
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myYear:            Year.
+ *
+ *
+ * @return       Status of DS1307_GetYear.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        30/July/2018
+ * @version     30/July/2018   The ORIGIN
+ * @pre         i.e1: 2000:  0x00.
+ *              i.e2: 2018:  0x18.
+ * @warning     N/A.
+ */
+DS1307_status_t  DS1307_GetYear ( I2C_parameters_t myI2Cparameters, DS1307_vector_data_t* myYear )
+{
+    uint8_t      cmd    =    0;
+
+    i2c_status_t aux;
+
+
+    /* Read YEAR REGISTER to mask it */
+    cmd      =   DS1307_YEAR;
+    aux      =   i2c_write ( myI2Cparameters, &cmd, 1U, I2C_NO_STOP_BIT );
+    aux      =   i2c_read  ( myI2Cparameters, &myYear->BCDYear, 1U );
+
+
+
+
+
+    if ( aux == I2C_SUCCESS )
+    {
+        return   DS1307_SUCCESS;
+    }
+    else
+    {
+        return   DS1307_FAILURE;
+    }
+}
