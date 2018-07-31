016c400bf8463b2e3dfb81e6845dccede7ac1e3f
Manuel Caballero <aqueronteblog@gmail.com>
2018-07-31 08:04:07 +0200

    NRF51 DS1307 Example. Some comments were improved

--- NRF51/Examples/Drivers/DS1307/Src/variables.h
+++ NRF51/Examples/Drivers/DS1307/Src/variables.h
@@ -6,8 +6,8 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        4/June/2018
- * @version     4/June/2018     The ORIGIN
+ * @date        30/July/2018
+ * @version     30/July/2018     The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0
  *              ( SDK 1.1.0 ).
  * @warning     N/A
@@ -25,12 +25,15 @@
 extern "C" {
 #endif
 
-/* CONSTANTS */
-#define ON              1
-#define OFF             0
 
-#define YES             1
-#define NO              0
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
 
 #ifdef __cplusplus
 }
