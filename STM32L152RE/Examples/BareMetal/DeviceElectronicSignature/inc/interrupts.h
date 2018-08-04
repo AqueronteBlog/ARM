5728b4660f515fe3779881a1a77a14042132415c
Manuel Caballero <vszener@gmail.com>
2018-08-04 10:41:59 +0200

    STM32L152RE Device Electronic Signature Example. Interrupts files were added

---
+++ STM32L152RE/Examples/BareMetal/DeviceElectronicSignature/inc/interrupts.h
@@ -0,0 +1,46 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        4/August/2018
+ * @version     4/August/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "stm32l1xx.h"
+#include "variables.h"
+#include "board.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+void RTC_WKUP_IRQHandler ( void );
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+extern volatile uint32_t myLEDstate;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
---
+++ STM32L152RE/Examples/BareMetal/DeviceElectronicSignature/src/interrupts.c
@@ -0,0 +1,62 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        4/August/2018
+ * @version     4/August/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "interrupts.h"
+
+
+
+/**
+ * @brief       void RTC_WKUP_IRQHandler ( void )
+ * @details     RTC Wake-up timer subroutine.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        4/August/2018
+ * @version     4/August/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void RTC_WKUP_IRQHandler ( void )
+{
+	/* Disable backup write protection	*/
+	PWR->CR		|=	 PWR_CR_DBP;
+
+	/* Disable the write protection for RTC registers */
+	RTC->WPR   	 =	 ( 0xCA & RTC_WPR_KEY_Msk );
+	RTC->WPR	 = 	 ( 0x53 & RTC_WPR_KEY_Msk );
+
+	/* WAKE-UP Interrupt	 */
+	if ( ( RTC->ISR & RTC_ISR_WUTF_Msk ) == RTC_ISR_WUTF )
+	{
+		if ( myLEDstate	 ==	 0 )
+		{
+			GPIOA->BSRR	 =	 ( 1 << LED_1 );				// LED1 ON
+			myLEDstate	 =	 1;
+		}
+		else
+		{
+			GPIOA->BRR	 =	 ( 1 << LED_1 );				// LED1 OFF
+			myLEDstate	 =	 0;
+		}
+
+		RTC->ISR	&=	~( RTC_ISR_WUTF | RTC_ISR_INIT );	// Clear flag
+	}
+
+	/* Enable the write protection for RTC registers */
+	RTC->WPR   	 =	 0xFF;
+	RTC->WPR   	 =	 0xFF;
+
+	/* Access to RTC, RTC Backup and RCC CSR registers disabled */
+	PWR->CR &= ~PWR_CR_DBP;
+}
