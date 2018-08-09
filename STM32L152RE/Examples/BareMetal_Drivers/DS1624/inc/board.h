6cb5b77977ea1538f985022e2687fe9fee674ba1
Manuel Caballero <aqueronteblog@gmail.com>
2018-08-09 13:31:26 +0200

    STM32L152RE DS1624 Example. Comments in general were improved and USART pinout was declared

--- STM32L152RE/Examples/BareMetal_Drivers/DS1624/inc/board.h
+++ STM32L152RE/Examples/BareMetal_Drivers/DS1624/inc/board.h
@@ -3,13 +3,13 @@
  * @details     It characterizes the board ( for this example ): NUCLEO-L152RE.
  *
  *
- * @return      NA
+ * @return      N/A
  *
  * @author      Manuel Caballero
  * @date        5/January/2018
  * @version     5/January/2018   The ORIGIN
- * @pre         NaN
- * @warning     NaN
+ * @pre         N/A
+ * @warning     N/A
  */
 
 #ifndef BOARD_H_
@@ -20,31 +20,43 @@ extern "C" {
 #endif
 
 /**
-      * @brief   NUCLEO-L152RE LEDs PINOUT
-      */
+ * @brief   NUCLEO-L152RE LEDs PINOUT
+ */
 typedef enum
 {
     LED_1         =   5              /*!<  NUCLEO-L152RE LED1: 		PA_5  		*/
-} NUCLEOL152_led_pinout_t;
+} nucleol152re_led_pinout_t;
 
 
 /**
-      * @brief   NUCLEO-L152RE BUTTONs PINOUT
-      */
+  * @brief   NUCLEO-L152RE BUTTONs PINOUT
+  */
 typedef enum
 {
 	BUTTON_1      =   13             /*!<  NUCLEO-L152RE USER BUTTON: 	PC_13	*/
-} NUCLEOL152_button_pinout_t;
+} nucleol152re_button_pinout_t;
 
 
 /**
-      * @brief   NUCLEO-L152RE UARTs PINOUT
-      */
+  * @brief   NUCLEO-L152RE UARTs PINOUT
+  */
 typedef enum
 {
 	UART5_RX      =   2,             /*!<  NUCLEO-L152RE BASIC UART5 RX: 	PD_2	*/
 	UART5_TX      =   12             /*!<  NUCLEO-L152RE BASIC UART5 TX: 	PC_12	*/
-} NUCLEOL152_uart_pinout_t;
+} nucleol152re_uart_pinout_t;
+
+
+/**
+  * @brief   NUCLEO-L152RE USARTs PINOUT
+  */
+typedef enum
+{
+	USART2_RX     =   3,             /*!<  NUCLEO-L152RE BASIC USART2 RX: 	PA_3	*/
+	USART2_TX     =   2              /*!<  NUCLEO-L152RE BASIC USART2 TX: 	PA_2	*/
+} nucleol152re_usart_pinout_t;
+
+
 
 #ifdef __cplusplus
 }
