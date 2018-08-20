55fb6802573c35ec14910cb2f8ea6e12ebe9ba03
Manuel Caballero <aqueronteblog@gmail.com>
2018-08-20 11:15:01 +0100

    NRF51 DS1307 Example. Some new functionalities were added

--- NRF51/Examples/Drivers/DS1307/Src/functions.c
+++ NRF51/Examples/Drivers/DS1307/Src/functions.c
@@ -19,6 +19,36 @@
 
 
 /**
+ * @brief       void conf_CLK  ( void )
+ * @details     It activates the external HFCLK crystal oscillator ( 16MHz ).
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        20/August/2018
+ * @version     20/August/2018      The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void conf_CLK  ( void )
+{
+    NRF_CLOCK->EVENTS_HFCLKSTARTED  =   0UL;                                                        // Reset flag
+    NRF_CLOCK->TASKS_HFCLKSTART     =   ( CLOCK_HFCLKSTAT_SRC_Xtal << CLOCK_HFCLKSTAT_SRC_Pos );    // Start External crystal CLK
+
+    while ( NRF_CLOCK->EVENTS_HFCLKSTARTED  !=   1UL );                                             // [TODO]       This is DANGEROUS, if something goes wrong, the uC will get stuck here!!!.
+                                                                                                    // [WORKAROUND] Insert a counter.
+    NRF_CLOCK->EVENTS_HFCLKSTARTED   =   0UL;
+
+}
+
+
+
+/**
  * @brief       void conf_GPIO  ( void )
  * @details     It configures GPIO to work with the LEDs.
  *
@@ -38,11 +68,11 @@ void conf_GPIO  ( void )
     for ( i = LED1; i < ( LED4 + 1 ); i++ ){
             NRF_GPIO->OUTSET        =   ( 1 << i );
 
-            NRF_GPIO->PIN_CNF[i]    =   GPIO_PIN_CNF_DIR_Output         <<  GPIO_PIN_CNF_DIR_Pos    |
-                                        GPIO_PIN_CNF_INPUT_Disconnect   <<  GPIO_PIN_CNF_INPUT_Pos  |
-                                        GPIO_PIN_CNF_PULL_Disabled      <<  GPIO_PIN_CNF_PULL_Pos   |
-                                        GPIO_PIN_CNF_DRIVE_S0S1         <<  GPIO_PIN_CNF_DRIVE_Pos  |
-                                        GPIO_PIN_CNF_SENSE_Disabled     <<  GPIO_PIN_CNF_SENSE_Pos;
+            NRF_GPIO->PIN_CNF[i]    =   ( GPIO_PIN_CNF_DIR_Output         <<  GPIO_PIN_CNF_DIR_Pos    )|
+                                        ( GPIO_PIN_CNF_INPUT_Disconnect   <<  GPIO_PIN_CNF_INPUT_Pos  )|
+                                        ( GPIO_PIN_CNF_PULL_Disabled      <<  GPIO_PIN_CNF_PULL_Pos   )|
+                                        ( GPIO_PIN_CNF_DRIVE_S0S1         <<  GPIO_PIN_CNF_DRIVE_Pos  )|
+                                        ( GPIO_PIN_CNF_SENSE_Disabled     <<  GPIO_PIN_CNF_SENSE_Pos  );
     }
 }
 
@@ -57,25 +87,26 @@ void conf_GPIO  ( void )
  *                  * Interrupt ENABLE.
  *
  *                 --- Channel 0:
- *                  * Overflow:             ( 1000000 * ( f_Timer0 )^( -1 ) ) = ( 1000000 * ( 500kHz )^( -1 ) ) ~ 1s.
+ *                  * Overflow:             ( 8*62500 * (f_Timer0)^(-1) ) = ( 8*62500 * (500kHz)^(-1) ) ~ 1s
  *
  * @return      N/A
  *
  * @author      Manuel Caballero
  * @date        30/July/2018
- * @version     30/July/2018   The ORIGIN
+ * @version     20/August/2018  Timer overflow every 1 second instead of 2 second.
+ *              30/July/2018    The ORIGIN
  * @pre         N/A
  * @warning     N/A.
  */
 void conf_TIMER0  ( void )
 {
-    NRF_TIMER0->TASKS_STOP  =   1;
+    NRF_TIMER0->TASKS_STOP  =   1UL;
     NRF_TIMER0->MODE        =   TIMER_MODE_MODE_Timer;
-    NRF_TIMER0->PRESCALER   =   5U;                                                                         // f_Timer0 = ( 16MHz / 2^5 ) = 500kHz
+    NRF_TIMER0->PRESCALER   =   5UL;                                                                        // f_Timer0 = ( 16MHz / 2^5 ) = 500kHz
     NRF_TIMER0->BITMODE     =   TIMER_BITMODE_BITMODE_32Bit << TIMER_BITMODE_BITMODE_Pos;                   // 32 bit mode.
-    NRF_TIMER0->TASKS_CLEAR =   1;                                                                          // clear the task first to be usable for later.
+    NRF_TIMER0->TASKS_CLEAR =   1UL;                                                                        // clear the task first to be usable for later.
 
-    NRF_TIMER0->CC[0]       =   1000000;                                                                    // ( 1000000 * ( f_Timer0 )^( -1 ) ) = ( 1000000 * ( 500kHz )^( -1 ) ) ~ 1s
+    NRF_TIMER0->CC[0]       =   ( 8*62500 );                                                                // ( 8*62500 * (f_Timer0)^(-1) ) = ( 8*62500 * (500kHz)^(-1) ) ~ 1s
 
     NRF_TIMER0->INTENSET    =   ( TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos );
 
@@ -91,7 +122,7 @@ void conf_TIMER0  ( void )
  * @brief       void conf_Uart  ( void )
  * @details     Uart0 with the following features:
  *
- *                  * Baud Rate:    115200
+ *                  * Baud Rate:    230400
  *                  * No Parity.
  *                  * No Flow Control.
  *
@@ -99,7 +130,8 @@ void conf_TIMER0  ( void )
  *
  * @author      Manuel Caballero
  * @date        30/July/2018
- * @version     30/July/2018   The ORIGIN
+ * @version     20/August/2018  230400 Baud rate
+ *              30/July/2018    The ORIGIN
  * @pre         Only TX pin will be configured.
  * @warning     N/A.
  */
@@ -123,7 +155,7 @@ void conf_UART  ( void )
     NRF_UART0->PSELRXD           =   0xFFFFFFFF;
 
     /* BaudRate & Configuration */
-    NRF_UART0->BAUDRATE          =   UART_BAUDRATE_BAUDRATE_Baud115200 << UART_BAUDRATE_BAUDRATE_Pos;
+    NRF_UART0->BAUDRATE          =   UART_BAUDRATE_BAUDRATE_Baud230400 << UART_BAUDRATE_BAUDRATE_Pos;
     NRF_UART0->CONFIG            =   ( UART_CONFIG_HWFC_Disabled   << UART_CONFIG_HWFC_Pos   ) |
                                      ( UART_CONFIG_PARITY_Excluded << UART_CONFIG_PARITY_Pos );
     /* Configure Interrupts */
