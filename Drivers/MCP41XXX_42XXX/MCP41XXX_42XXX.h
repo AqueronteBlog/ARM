ed5152207445629ec8e6ac0b10f4ae8f02279dec
Manuel Caballero <aqueronteblog@gmail.com>
2018-08-21 11:10:00 +0100

    DRIVERS. MCP41XXX/42XXX, header file is ready to be tested

--- Drivers/MCP41XXX_42XXX/MCP41XXX_42XXX.h
+++ Drivers/MCP41XXX_42XXX/MCP41XXX_42XXX.h
@@ -1,96 +1,63 @@
 /**
- * @brief       MCP4725.h
- * @details     12-Bit Digital-to-Analog Converter with EEPROM Memory.
+ * @brief       MCP41XXX_42XXX.h
+ * @details     Single/Dual Digital Potentiometer with SPI™ Interface.
  *              Header file.
  *
  *
- * @return      NA
+ * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        7/September/2017
- * @version     7/September/2017    The ORIGIN
- * @pre         NaN.
- * @warning     NaN
+ * @date        21/August/2018
+ * @version     21/August/2018    The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
  */
 
 
 #include "stdint.h"
 #include "stdbool.h"
-#include "i2c.h"
+#include "spi.h"
 
 
+#ifndef MCP41XXX_42XXX_H_
+#define MCP41XXX_42XXXH_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Command Byte */
 /**
-  * @brief   DEFAULT ADDRESSES ( NOTE1: The A2 and A1 are programmed to '00' (default), if not requested by customer.
-  *                              NOTE2: On my board, the A2 and A1 are programmed to '01'. )
+  * @brief   COMMAND SELECTION BITS
   */
 typedef enum{
-    MCP4725_ADDRESS_LOW     =   0x62,                   /*!<   A0 pin ties to GND                                            */
-    MCP4725_ADDRESS_HIGH    =   0x63                    /*!<   A0 pin ties to VDD                                            */
-} MCP4725_address_t;
+    MCP41XXX_42XXX_COMMAND_NONE         =   ( 0b00 << 4 ),      /*!<   No Command will be executed                                                                                                          */
+    MCP41XXX_42XXX_COMMAND_WRITE_DATA   =   ( 0b01 << 4 ),      /*!<   Write the data contained in Data Byte to the potentiometer(s) determined by the potentiometer selection bits                         */
+    MCP41XXX_42XXX_COMMAND_SHUTDOWN     =   ( 0b10 << 4 )       /*!<   Potentiometer(s) determined by potentiometer selection bits will enter Shutdown Mode. Data bits for this command are 'don't cares'   */
+} MCP41XXX_42XXX_command_selection_bits_t;
 
 
 /**
-  * @brief   COMMANDS
-  */
-#define MCP4725_GENERAL_CALL             0x00              /*!<   The MCP4725 device acknowledges the general call address                             */
-
-/* General Call Commands */
-/**
-  * @brief   GENERAL CALL COMMANDS
-  */
-#define MCP4725_GENERAL_CALL_RESET       0x06              /*!<  Perform an internal reset similar to a power-on-reset (POR).                          */
-#define MCP4725_GENERAL_CALL_WAKE_UP     0x09              /*!<  The power-down bits of the DAC register are set to a normal operation.                */
-
-
-
-
-
-/* Commands Registers */
-/**
-  * @brief   WRITE COMMAND TYPE
+  * @brief   POTENTIOMETER SELECTION BITS
   */
 typedef enum{
-    FAST_MODE                              =   0,           /*!<  This command is used to change the DAC register. EEPROM is not affected.              */
-    WRITE_DAC_REGISTER_MODE                =   1,           /*!<  Load configuration bits and data code to the DAC Register.                            */
-    WRITE_DAC_AND_EEPROM_REGISTER_MODE     =   2            /*!<  Load configuration bits and data code to the DAC Register and also write the EEPROM.  */
-} MCP4725_write_command_type_t;
-
-
-
-/**
-  * @brief   POWER-DOWN MODE
-  */
-typedef enum{
-    NORMAL_MODE                                 =   0,      /*!<  Normal Mode.                                                                          */
-    POWER_DOWN_1KOHM_RESISTIVE_LOAD_MODE        =   1,      /*!<  Power-Down Mode. 1 k‎Ω resistor to ground.                                             */
-    POWER_DOWN_100KOHM_RESISTIVE_LOAD_MODE      =   2,      /*!<  Power-Down Mode. 100 k‎Ω resistor to ground.                                           */
-    POWER_DOWN_500KOHM_RESISTIVE_LOAD_MODE      =   3       /*!<  Power-Down Mode. 500 k‎Ω resistor to ground.                                           */
-} MCP4725_operation_mode_t;
-
-
-
-/**
-  * @brief   READY/#BUSY BIT
-  */
-typedef enum{
-    EEPROM_BUSY                                 =   0,      /*!<  EEPROM write is not completed.                                                        */
-    EEPROM_READY                                =   1       /*!<  EEPROM write is complete.                                                             */
-} MCP4725_eeprom_status_t;
+    MCP41XXX_42XXX_POTENTIOMETER_DUMMY_CODE                     =   ( 0b00 << 0 ),      /*!<   Dummy Code: Neither Potentiometer affected               */
+    MCP41XXX_42XXX_POTENTIOMETER_COMMAND_POTENTIOMETER_0        =   ( 0b01 << 0 ),      /*!<   Command executed on Potentiometer 0                      */
+    MCP41XXX_42XXX_POTENTIOMETER_COMMAND_POTENTIOMETER_1        =   ( 0b10 << 0 ),      /*!<   Command executed on Potentiometer 1                      */
+    MCP41XXX_42XXX_POTENTIOMETER_COMMAND_BOTH_POTENTIOMETERS    =   ( 0b11 << 0 )       /*!<   Command executed on both Potentiometers */
+} MCP41XXX_42XXX_potentiometer_selection_bits_t;
 
 
 
 
-#ifndef VECTOR_STRUCT_H
-#define VECTOR_STRUCT_H
+
+#ifndef MCP41XXX_42XXX_VECTOR_STRUCT_H
+#define MCP41XXX_42XXX_VECTOR_STRUCT_H
 typedef struct{
-    uint16_t EEPROM_Data;
-    uint16_t DAC_Data;
-} Vector_data_t;
-
-typedef struct{
-    uint32_t DAC_New_Value;
-} Vector_new_dac_value_t;
+    uint8_t Dn;
+} MCP41XXX_42XXX_vector_data_t;
 #endif
 
 
@@ -99,9 +66,9 @@ typedef struct{
   * @brief   INTERNAL CONSTANTS
   */
 typedef enum{
-    MCP4725_SUCCESS     =       0,
-    MCP4725_FAILURE     =       1
-} MCP4725_status_t;
+    MCP41XXX_42XXX_SUCCESS     =       0,
+    MCP41XXX_42XXX_FAILURE     =       1
+} MCP41XXX_42XXX_status_t;
 
 
 
@@ -109,11 +76,21 @@ typedef enum{
 /**
   * @brief   FUNCTION PROTOTYPES
   */
-MCP4725_status_t  MCP4725_Init                  ( I2C_parameters_t myI2Cparameters );
-MCP4725_status_t  MCP4725_Reset                 ( I2C_parameters_t myI2Cparameters );
-MCP4725_status_t  MCP4725_WakeUp                ( I2C_parameters_t myI2Cparameters );
-MCP4725_status_t  MCP4725_PowerMode             ( I2C_parameters_t myI2Cparameters, MCP4725_write_command_type_t myWriteCMD, MCP4725_operation_mode_t myPowerMode );
-MCP4725_status_t  MCP4725_SetNewValue           ( I2C_parameters_t myI2Cparameters, MCP4725_write_command_type_t myWriteCMD, Vector_new_dac_value_t myDACNewValue );
-MCP4725_status_t  MCP4725_EEPROM_Status         ( I2C_parameters_t myI2Cparameters, MCP4725_eeprom_status_t* myEEPROM_Status );
-MCP4725_status_t  MCP4725_GetEEPROM_Data        ( I2C_parameters_t myI2Cparameters, Vector_data_t* myEEPROMData );
-MCP4725_status_t  MCP4725_GetDAC_Data           ( I2C_parameters_t myI2Cparameters, Vector_data_t* myDACData );
+/** It configures the SPI peripheral.
+    */
+MCP41XXX_42XXX_status_t  MCP41XXX_42XXX_Init                ( SPI_parameters_t mySPIparameters                                                                                                     );
+
+/** It updates the wiper value on the given channel.
+    */
+MCP41XXX_42XXX_status_t  MCP41XXX_42XXX_SetWiper            ( SPI_parameters_t mySPIparameters, MCP41XXX_42XXX_potentiometer_selection_bits_t myChannel, MCP41XXX_42XXX_vector_data_t myWiperValue );
+
+/** It performs a software shutdown.
+    */
+MCP41XXX_42XXX_status_t  MCP41XXX_42XXX_SoftwareShutdown    ( SPI_parameters_t mySPIparameters, MCP41XXX_42XXX_potentiometer_selection_bits_t myChannel                                            );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MCP41XXX_42XXX_H_ */
