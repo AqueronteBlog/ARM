429b65bfcc9ef12dbf11b91abd1b23794729483f
Manuel Caballero <aqueronteblog@gmail.com>
2018-08-21 11:13:30 +0100

    NRF51 MCP41XXX/42XXX Example. Interrupts files were added

---
+++ NRF51/Examples/Drivers/MCP41XXX_42XXX/Src/interrupts.h
@@ -0,0 +1,53 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        21/August/2018
+ * @version     21/August/2018    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "board.h"
+#include "variables.h"
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Subroutine prototypes.
+ */
+void TIMER0_IRQHandler ( void );
+void UART0_IRQHandler  ( void );
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+extern volatile uint32_t myState;
+extern volatile uint8_t  myMessage[];
+extern volatile uint8_t  *myPtr;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
