e6ce79e398d942147a45b255c803cfe7bc7ac098
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-07 10:12:29 +0100

    Drivers. BMP180 header file is ready to be tested

--- Drivers/BMP180/BMP180.h
+++ Drivers/BMP180/BMP180.h
@@ -1,14 +1,14 @@
 /**
- * @brief       SHT2X.h
- * @details     Humidity and Temperature Sensor IC.
+ * @brief       BMP180.h
+ * @details     Digital Pressure Sensor
  *              Header file.
  *
  *
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        3/September/2018
- * @version     3/September/2018    The ORIGIN
+ * @date        6/September/2018
+ * @version     6/September/2018    The ORIGIN
  * @pre         N/A
  * @warning     N/A
  * @pre         This code belongs to Nimbus Centre ( http://www.nimbus.cit.ie ).
@@ -19,8 +19,8 @@
 #include "i2c.h"
 
 
-#ifndef SHT2X_H_
-#define SHT2X_H_
+#ifndef BMP180_H_
+#define BMP180_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,115 +32,180 @@ extern "C" {
   */
 typedef enum
 {
-    SHT2X_ADDRESS     =   0b1000000                                             /*!<   SHT2X I2C Address                */
-} SHT2X_address_t;
+    BMP180_ADDRESS     =   0b1110111            /*!<   BMP180 I2C Address           */
+} BMP180_address_t;
 
 
 
 /**
-  * @brief   COMMAND REGISTERS
+  * @brief   CALIBRATION COEFFICIENTS
   */
 typedef enum
 {
-    SHT2X_TRIGGER_TEMPERATURE_MEASUREMENT_HOLD_MASTER      =   0xE3,            /*!<  Hold master                           */
-    SHT2X_TRIGGER_HUMIDITY_MEASUREMENT_HOLD_MASTER         =   0xE5,            /*!<  Hold master                           */
-    SHT2X_TRIGGER_TEMPERATURE_MEASUREMENT_NO_HOLD_MASTER   =   0xF3,            /*!<  No Hold master                        */
-    SHT2X_TRIGGER_HUMIDITY_MEASUREMENT_NO_HOLD_MASTER      =   0xF5,            /*!<  No Hold master                        */
-    SHT2X_WRITE_USER_REGISTER                              =   0xE6,            /*!<  Write user register                   */
-    SHT2X_READ_USER_REGISTER                               =   0xE7,            /*!<  Read user register                    */
-    SHT2X_SOFT_RESET                                       =   0xFE,            /*!<  Software reset                        */
-    SHT2X_SERIAL_NUMBER_FIRST_MEMORY_ACCESS_MSB            =   0xFA,            /*!<  Serial number first memory access     */
-    SHT2X_SERIAL_NUMBER_FIRST_MEMORY_ACCESS_LSB            =   0x0F,            /*!<  Serial number first memory access     */
-    SHT2X_SERIAL_NUMBER_SECOND_MEMORY_ACCESS_MSB           =   0xFC,            /*!<  Serial number second memory access    */
-    SHT2X_SERIAL_NUMBER_SECOND_MEMORY_ACCESS_LSB           =   0xC9             /*!<  Serial number second memory access    */
-} SHT2X_command_registers_t;
+    BMP180_AC1_MSB      =   0xAA,               /*!<  MSB AC1 coefficient           */
+    BMP180_AC1_LSB      =   0xAB,               /*!<  LSB AC1 coefficient           */
+    BMP180_AC2_MSB      =   0xAC,               /*!<  MSB AC2 coefficient           */
+    BMP180_AC2_LSB      =   0xAD,               /*!<  LSB AC2 coefficient           */
+    BMP180_AC3_MSB      =   0xAE,               /*!<  MSB AC3 coefficient           */
+    BMP180_AC3_LSB      =   0xAF,               /*!<  LSB AC3 coefficient           */
+    BMP180_AC4_MSB      =   0xB0,               /*!<  MSB AC4 coefficient           */
+    BMP180_AC4_LSB      =   0xB1,               /*!<  LSB AC4 coefficient           */
+    BMP180_AC5_MSB      =   0xB2,               /*!<  MSB AC5 coefficient           */
+    BMP180_AC5_LSB      =   0xB3,               /*!<  LSB AC5 coefficient           */
+    BMP180_AC6_MSB      =   0xB4,               /*!<  MSB AC6 coefficient           */
+    BMP180_AC6_LSB      =   0xB5,               /*!<  LSB AC6 coefficient           */
+    BMP180_B1_MSB       =   0xB6,               /*!<  MSB B1 coefficient            */
+    BMP180_B1_LSB       =   0xB7,               /*!<  LSB B1 coefficient            */
+    BMP180_B2_MSB       =   0xB8,               /*!<  MSB B2 coefficient            */
+    BMP180_B2_LSB       =   0xB9,               /*!<  LSB B2 coefficient            */
+    BMP180_MB_MSB       =   0xBA,               /*!<  MSB MB coefficient            */
+    BMP180_MB_LSB       =   0xBB,               /*!<  LSB MB coefficient            */
+    BMP180_MC_MSB       =   0xBC,               /*!<  MSB MC coefficient            */
+    BMP180_MC_LSB       =   0xBD,               /*!<  LSB MC coefficient            */
+    BMP180_MD_MSB       =   0xBE,               /*!<  MSB MD coefficient            */
+    BMP180_MD_LSB       =   0xBF                /*!<  LSB MD coefficient            */
+} BMP180_calibration_coefficients_t;
 
 
 
-// MASTER MODE
 /**
-  * @brief   MODE
+  * @brief   REGISTERS MAP
   */
 typedef enum
 {
-    SHT2X_HOLD_MASTER_MODE                 =   0x01,           /*!<  SHT2X HOLD MASTER MODE enabled                       */
-    SHT2X_NO_HOLD_MASTER_MODE              =   0x00            /*!<  SHT2X NO HOLD MASTER MODE enabled                    */
-} SHT2X_master_mode_t;
+    BMP180_OUT_XLSB     =   0xF8,               /*!<  ADC OUT XLSB                              | Reset: 0x00   */
+    BMP180_OUT_LSB      =   0xF7,               /*!<  ADC OUT MSB                               | Reset: 0x00   */
+    BMP180_OUT_MSB      =   0xF6,               /*!<  ADC OUT LSB                               | Reset: 0x80   */
+    BMP180_CTRL_MEAS    =   0xF4,               /*!<  Measurement control ( Control register )  | Reset: 0x00   */
+    BMP180_SOFT         =   0xE0,               /*!<  Software reset                            | Reset: 0x00   */
+    BMP180_ID           =   0xD0                /*!<  Chip-ID                                   | Reset: 0x55   */
+} BMP180_registers map_t;
 
 
 
-// USER REGISTER
+/* MEASUREMENT CONTROL  */
+/**
+  * @brief   START OF CONVERSION ( SCO, BIT<5> )
+  */
 /*
-    NOTE:   Reserved bits must not be changed and default
-            values of respective reserved bits may change over time without prior notice. Therefore, for any writing to user
-            register, default values of reserved bits must be read first.
+    NOTE: The value of this bit stays '1' during conversion and is reset to '0' after conversion is complete ( data registers
+          are filled ).
+*/
+typedef enum
+{
+    CTRL_MEAS_SCO_MASK                      =   ( 1U << 5U ),       /*!<  SCO Mask                         */
+    CTRL_MEAS_SCO_CONVERSION_IN_PROGRESS    =   ( 1U << 5U ),       /*!<  SCO conversion in progress       */
+    CTRL_MEAS_SCO_CONVERSION_COMPLETED      =   ( 0U << 5U )        /*!<  SCO conversion is complete       */
+} BMP180_sco_t;
+
+
+
+/**
+  * @brief   CONTROLS THE OVERSAMPLING RATIO OF THE PRESSURE MEASUREMENT ( OSS, BIT<7:6> )
+  */
+typedef enum
+{
+    CTRL_MEAS_OSS_MASK                      =   ( 0b11 << 6U ),       /*!<  OSS Mask                         */
+    CTRL_MEAS_OSS_OVERSAMPLING_SINGLE       =   ( 0b00 << 6U ),       /*!<  OSS oversampling ratio: single   */
+    CTRL_MEAS_OSS_OVERSAMPLING_2_TIMES      =   ( 0b01 << 6U ),       /*!<  OSS oversampling ratio: 2 times  */
+    CTRL_MEAS_OSS_OVERSAMPLING_4_TIMES      =   ( 0b10 << 6U ),       /*!<  OSS oversampling ratio: 4 times  */
+    CTRL_MEAS_OSS_OVERSAMPLING_8_TIMES      =   ( 0b11 << 6U )        /*!<  OSS oversampling ratio: 8 times  */
+} BMP180_oss_t;
+
+
+
+/* SOFT RESET   */
+/**
+  * @brief   SOFT RESET
+  */
+typedef enum
+{
+    SOFT_SOFT_RESET         =   0xB6                  /*!<  It will perform the same sequence as power on reset   */
+} BMP180_soft_reset_t;
+
+
+
+/* ID   */
+/**
+  * @brief   CHIP-ID
+  */
+typedef enum
+{
+    ID_CHIP_ID              =   0x55                  /*!<  Fixed value                         */
+} BMP180_chip_id_t;
+
+
+
+/*
+    AUXILIAR REGISTERS
+
+    NOTE: These definitions are for better understanding in order to use the driver
 */
 /**
-  * @brief   RESOLUTION
+  * @brief   COMMANDS
   */
 typedef enum
 {
-    USER_REGISTER_RESOLUTION_MASK                =   0x81,           /*!<  SHT2X Measurement Resolution                         */
-    USER_REGISTER_RESOLUTION_12RH_14TEMP         =   0x00,           /*!<  SHT2X 12b RH 14b Temp.                               */
-    USER_REGISTER_RESOLUTION_8RH_12TEMP          =   0x01,           /*!<  SHT2X 9b  RH 12b Temp.                               */
-    USER_REGISTER_RESOLUTION_10RH_13TEMP         =   0x80,           /*!<  SHT2X 10b RH 13b Temp.                               */
-    USER_REGISTER_RESOLUTION_11RH_11TEMP         =   0x81            /*!<  SHT2X 11b RH 11b Temp.                               */
-} SHT2X_measurement_resolution_t;
+    BMP180_TRIGGER_TEMPERATURE      =   0x2E,           /*!<  It triggers a new temperature measurement  */
+    BMP180_TRIGGER_PRESSURE         =   0x34            /*!<  It triggers a new pressure measurement     */
+} BMP180_commands_t;
 
 
 
 /**
-  * @brief   STATUS END OF BATTERY
+  * @brief   PRESSURE
   */
 typedef enum
 {
-    USER_REGISTER_STATUS_END_BATTERY_MASK         =   0x40,           /*!<  End battery mask                                      */
-    USER_REGISTER_STATUS_END_BATTERY_HIGH_2V25    =   0x00,           /*!<  VDD > 2.25V.                                          */
-    USER_REGISTER_STATUS_END_BATTERY_LOW_2V25     =   0x40            /*!<  VDD < 2.25V.                                          */
-} SHT2X_status_end_battery_t;
+    PRESSURE_ULTRA_LOW_POWER_MODE   =   CTRL_MEAS_OSS_OVERSAMPLING_SINGLE,      /*!<  OSS oversampling ratio: single   */
+    PRESSURE_STANDARD_MODE          =   CTRL_MEAS_OSS_OVERSAMPLING_2_TIMES,     /*!<  OSS oversampling ratio: 2 times  */
+    PRESSURE_HIGH_RESOLUTION_MODE   =   CTRL_MEAS_OSS_OVERSAMPLING_4_TIMES,     /*!<  OSS oversampling ratio: 4 times  */
+    PRESSURE_ULTRA_HIGH_RES_MODE    =   CTRL_MEAS_OSS_OVERSAMPLING_8_TIMES      /*!<  OSS oversampling ratio: 8 times  */
+} BMP180_pressure_resolution_t;
 
 
 
-/**
-  * @brief   ON-CHIP HEATER
-  */
-typedef enum
-{
-    USER_REGISTER_HEATER_MASK                     =   0x04,           /*!<  ON-chip heater mask                                   */
-    USER_REGISTER_HEATER_ENABLED                  =   0x04,           /*!<  Heater enabled.                                       */
-    USER_REGISTER_HEATER_DISABLED                 =   0x00            /*!<  Heater disabled.                                      */
-} SHT2X_on_chip_heater_t;
 
 
-
-/**
-  * @brief   OTP RELOAD
-  */
-typedef enum
-{
-    USER_REGISTER_OTP_MASK                        =   0x02,           /*!<  OTP heater mask                                       */
-    USER_REGISTER_OTP_ENABLED                     =   0x00,           /*!<  OTP enabled.                                          */
-    USER_REGISTER_OTP_DISABLED                    =   0x02            /*!<  OTP disabled.                                         */
-} SHT2X_otp_t;
-
-
-
-#ifndef SHT2X_VECTOR_STRUCT_H
-#define SHT2X_VECTOR_STRUCT_H
+#ifndef BMP180_VECTOR_STRUCT_H
+#define BMP180_VECTOR_STRUCT_H
+/* Calibration data  */
 typedef struct
 {
-    float    RelativeHumidity;
-    float    Temperature;
+    int16_t  ac1;
+    int16_t  ac2;
+    int16_t  ac3;
+    uint16_t ac4;
+    uint16_t ac5;
+    uint16_t ac6;
 
-    uint16_t RawRelativeHumidity;
-    uint16_t RawTemperature;
+    int16_t  b1;
+    int16_t  b2;
 
-    uint8_t  BatteryStatus;
-    uint64_t SerialNumber;
-} SHT2X_vector_data_t;
+    int16_t  mb;
+    int16_t  mc;
+    int16_t  md;
+} BMP180_calibration_data_t;
+
+
+/* Uncompensated data  */
+typedef struct
+{
+    int32_t  ut;
+    int32_t  up;
+} BMP180_uncompensated_data_t;
+
+
+/* Data  */
+typedef struct
+{
+    uint8_t  id;
+    int32_t  b5;
+
+    float    temp;
+    float    press;
+} BMP180_data_t;
 #endif
-
-
 
 
 
@@ -149,9 +214,12 @@ typedef struct
   */
 typedef enum
 {
-    SHT2X_SUCCESS     =       0,
-    SHT2X_FAILURE     =       1
-} SHT2X_status_t;
+    BMP180_SUCCESS     =       0,
+    BMP180_FAILURE     =       1
+} BMP180_status_t;
+
+
+
 
 
 
@@ -160,51 +228,39 @@ typedef enum
   */
 /** It configures the I2C peripheral.
   */
-SHT2X_status_t  SHT2X_Init               ( I2C_parameters_t myI2Cparameters                                                                                 );
+BMP180_status_t  BMP180_Init                ( I2C_parameters_t myI2Cparameters                                                                                                          );
 
-/** It configures the SHT2X device.
+/** It performs a soft reset.
   */
-SHT2X_status_t  SHT2X_Conf               ( I2C_parameters_t myI2Cparameters, SHT2X_measurement_resolution_t myResolution, SHT2X_on_chip_heater_t myHeater   );
+BMP180_status_t  BMP180_SoftReset           ( I2C_parameters_t myI2Cparameters                                                                                                          );
 
-/** Rebooting the SHT2X sensor switching the power off and on again.
+/** It gets the chip-ID.
   */
-SHT2X_status_t  SHT2X_SoftReset          ( I2C_parameters_t myI2Cparameters                                                                                 );
+BMP180_status_t  BMP180_GetID               ( I2C_parameters_t myI2Cparameters, BMP180_data_t myID                                                                                      );
 
-/** It triggers a new temperature measurement.
+/** It reads the calibration data.
   */
-SHT2X_status_t  SHT2X_TriggerTemperature ( I2C_parameters_t myI2Cparameters, SHT2X_master_mode_t myMode                                                     );
+BMP180_status_t  BMP180_Get_Cal_Param       ( I2C_parameters_t myI2Cparameters, BMP180_calibration_data_t* myCalibrationData                                                            );
 
-/** It reads a new temperature measurement.
+/** It reads uncompensated temperature value.
   */
-SHT2X_status_t  SHT2X_ReadTemperature    ( I2C_parameters_t myI2Cparameters, SHT2X_vector_data_t* myTemperature                                             );
+BMP180_status_t  BMP180_Get_UT              ( I2C_parameters_t myI2Cparameters, BMP180_uncompensated_data_t* myUT                                                                       );
 
-/** It reads a new raw temperature measurement.
+/** It reads uncompensated pressure value.
   */
-SHT2X_status_t  SHT2X_ReadRawTemperature ( I2C_parameters_t myI2Cparameters, SHT2X_vector_data_t* myRawTemperature                                          );
+BMP180_status_t  BMP180_Get_UP              ( I2C_parameters_t myI2Cparameters, BMP180_pressure_resolution_t myPressureResolutionMode, BMP180_uncompensated_data_t* myUP                );
 
-/** It triggers a new humidity measurement.
+/** It calculates true temperature.
   */
-SHT2X_status_t  SHT2X_TriggerHumidity    ( I2C_parameters_t myI2Cparameters, SHT2X_master_mode_t myMode                                                     );
+BMP180_data_t    BMP180_Get_Temperature     ( I2C_parameters_t myI2Cparameters, BMP180_calibration_data_t myCalibrationData, BMP180_uncompensated_data_t myUT                           );
 
-/** It reads a new humidity measurement.
+/** It calculates true pressure.
   */
-SHT2X_status_t  SHT2X_ReadHumidity       ( I2C_parameters_t myI2Cparameters, SHT2X_vector_data_t* myHumidity                                                );
-
-/** It reads a new raw humidity measurement.
-  */
-SHT2X_status_t  SHT2X_ReadRawHumidity    ( I2C_parameters_t myI2Cparameters, SHT2X_vector_data_t* myRawHumidity                                             );
-
-/** It reads the user register to check the battery status.
-  */
-SHT2X_status_t  SHT2X_BatteryStatus      ( I2C_parameters_t myI2Cparameters, SHT2X_vector_data_t* myBattStatus                                              );
-
-/** It gets the serial number ( Electronic Identification Code ).
-  */
-SHT2X_status_t  SHT2X_GetSerialNumber    ( I2C_parameters_t myI2Cparameters, SHT2X_vector_data_t* mySerialNumber                                            );
+BMP180_data_t    BMP180_Get_CalPressure     ( I2C_parameters_t myI2Cparameters, BMP180_calibration_data_t myCalibrationData, BMP180_data_t myB5, BMP180_uncompensated_data_t myUP       );
 
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* SHT2X_H */
+#endif /* BMP180_H */
