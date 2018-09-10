f0041e9334bfdb4b02c2a1da663b6333c5856574
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-10 14:57:36 +0100

    NRF51 BMP180 Example. Comments improvement into the variable file.

--- NRF51/Examples/Drivers/BMP180/Src/variables.h
+++ NRF51/Examples/Drivers/BMP180/Src/variables.h
@@ -6,8 +6,8 @@
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        3/September/2018
- * @version     3/September/2018     The ORIGIN
+ * @date        10/September/2018
+ * @version     10/September/2018     The ORIGIN
  * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0 ( SDK 1.1.0 ).
  * @warning     N/A
  * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
