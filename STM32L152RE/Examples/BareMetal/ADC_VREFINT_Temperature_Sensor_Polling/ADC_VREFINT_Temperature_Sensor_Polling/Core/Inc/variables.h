54bd794699b19fcb734324fb641004863e8de81b
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-24 12:20:32 +0100

    STM32L152RE ADC Vrefint and Internal Temperature Sensor Polling Example. Variables file was added

---
+++ STM32L152RE/Examples/BareMetal/ADC_VREFINT_Temperature_Sensor_Polling/ADC_VREFINT_Temperature_Sensor_Polling/Core/Inc/variables.h
@@ -0,0 +1,38 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        4/August/2018
+ * @version     4/August/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "stm32l1xx.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Macros
+ */
+#define _ROUND32U( x )  ( uint32_t )( x + 0.5 )
+
+/**
+ * @brief   Variables
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
