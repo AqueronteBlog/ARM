1c3bdf90503c01e868d8ff79c5b7cf43ef27af5a
Manuel Caballero <aqueronteblog@gmail.com>
2018-09-28 13:14:08 +0100

    STM32L152RE ADC Vrefint and Internal Temperature Sensor Interrupt Example. Variables file was added

---
+++ STM32L152RE/Examples/BareMetal/ADC_VREFINT_Temperature_Sensor_Interrupt/Inc/variables.h
@@ -0,0 +1,38 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        28/September/2018
+ * @version     28/September/2018   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "stm32l1xx.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Macros
+ */
+#define _ROUND32U( x )  ( uint32_t )( x + 0.5 )
+
+/**
+ * @brief   Variables
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
