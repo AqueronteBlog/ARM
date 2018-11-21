c24154aa195d725a5a8ecce6306666d13012bf51
Manuel Caballero <aqueronteblog@gmail.com>
2018-11-21 16:58:19 +0000

    DRIVERS. SSD1306, it is in progress, new functions added

--- Drivers/SSD1306/SSD1306.h
+++ Drivers/SSD1306/SSD1306.h
@@ -103,6 +103,10 @@ typedef enum
 
     /* Hardware Configuration (Panel resolution & layout related) Command Table  */
     /* Check it!   */
+    SSD1306_SET_DISPLAY_START_LINE                      =   0b01111111,            /*!<  Set display RAM display start line register from 0-63                                                                 ( RESET = 40h ) */
+    SSD1306_SET_MULTIPLEX_RATIO                         =   0xA8,                  /*!<  Set MUX ratio to N + 1 MUX                                                                                                            */
+    SSD1306_SET_DISPLAY_OFFSET                          =   0xD3,                  /*!<  Set vertical shift by COM from 0-63                                                                                                   */
+    SSD1306_SET_COM_PINS_HARDWARE_CONFIGURATION         =   0xDA,                  /*!<  Set COM Pins Hardware configuration                                                                                                   */
 
     /* Timing & Driving Scheme Setting Command Table  */
     /* Check it!   */
@@ -135,6 +139,113 @@ typedef enum
     SET_DISPLAY_ON_OFF_DISPLAY_OFF          =   ( 0U << 0U ),     /*!<  Display OFF ( sleep mode ) ( RESET )                                */
     SET_DISPLAY_ON_OFF_DISPLAY_ON           =   ( 1U << 0U )      /*!<  Display ON in normal mode                                           */
 } SSD1306_set_display_t;
+
+
+
+/* SET MULTIPLEX RATIO
+    NOTE: Set MUX ratio to N + 1 MUX
+*/
+typedef enum
+{
+    SET_MULTIPLEX_RATIO_MASK                =   ( 111111U << 0U ),  /*!<  Set Multiplex ratio mask                                          */
+    SET_MULTIPLEX_RATIO_16MUX               =   15U,                /*!<  Set Multiplex ratio: 16MUX                                        */
+    SET_MULTIPLEX_RATIO_17MUX               =   16U,                /*!<  Set Multiplex ratio: 17MUX                                        */
+    SET_MULTIPLEX_RATIO_18MUX               =   17U,                /*!<  Set Multiplex ratio: 18MUX                                        */
+    SET_MULTIPLEX_RATIO_19MUX               =   18U,                /*!<  Set Multiplex ratio: 19MUX                                        */
+    SET_MULTIPLEX_RATIO_20MUX               =   19U,                /*!<  Set Multiplex ratio: 20MUX                                        */
+    SET_MULTIPLEX_RATIO_21MUX               =   20U,                /*!<  Set Multiplex ratio: 21MUX                                        */
+    SET_MULTIPLEX_RATIO_22MUX               =   21U,                /*!<  Set Multiplex ratio: 22MUX                                        */
+    SET_MULTIPLEX_RATIO_23MUX               =   22U,                /*!<  Set Multiplex ratio: 23MUX                                        */
+    SET_MULTIPLEX_RATIO_24MUX               =   23U,                /*!<  Set Multiplex ratio: 24MUX                                        */
+    SET_MULTIPLEX_RATIO_25MUX               =   24U,                /*!<  Set Multiplex ratio: 25MUX                                        */
+    SET_MULTIPLEX_RATIO_26MUX               =   25U,                /*!<  Set Multiplex ratio: 26MUX                                        */
+    SET_MULTIPLEX_RATIO_27MUX               =   26U,                /*!<  Set Multiplex ratio: 27MUX                                        */
+    SET_MULTIPLEX_RATIO_28MUX               =   27U,                /*!<  Set Multiplex ratio: 28MUX                                        */
+    SET_MULTIPLEX_RATIO_29MUX               =   28U,                /*!<  Set Multiplex ratio: 29MUX                                        */
+    SET_MULTIPLEX_RATIO_30MUX               =   29U,                /*!<  Set Multiplex ratio: 30MUX                                        */
+    SET_MULTIPLEX_RATIO_31MUX               =   30U,                /*!<  Set Multiplex ratio: 31MUX                                        */
+    SET_MULTIPLEX_RATIO_32MUX               =   31U,                /*!<  Set Multiplex ratio: 32MUX                                        */
+    SET_MULTIPLEX_RATIO_33MUX               =   32U,                /*!<  Set Multiplex ratio: 33MUX                                        */
+    SET_MULTIPLEX_RATIO_34MUX               =   33U,                /*!<  Set Multiplex ratio: 34MUX                                        */
+    SET_MULTIPLEX_RATIO_35MUX               =   34U,                /*!<  Set Multiplex ratio: 35MUX                                        */
+    SET_MULTIPLEX_RATIO_36MUX               =   35U,                /*!<  Set Multiplex ratio: 36MUX                                        */
+    SET_MULTIPLEX_RATIO_37MUX               =   36U,                /*!<  Set Multiplex ratio: 37MUX                                        */
+    SET_MULTIPLEX_RATIO_38MUX               =   37U,                /*!<  Set Multiplex ratio: 38MUX                                        */
+    SET_MULTIPLEX_RATIO_39MUX               =   38U,                /*!<  Set Multiplex ratio: 39MUX                                        */
+    SET_MULTIPLEX_RATIO_40MUX               =   39U,                /*!<  Set Multiplex ratio: 40MUX                                        */
+    SET_MULTIPLEX_RATIO_41MUX               =   40U,                /*!<  Set Multiplex ratio: 41MUX                                        */
+    SET_MULTIPLEX_RATIO_42MUX               =   41U,                /*!<  Set Multiplex ratio: 42MUX                                        */
+    SET_MULTIPLEX_RATIO_43MUX               =   42U,                /*!<  Set Multiplex ratio: 43MUX                                        */
+    SET_MULTIPLEX_RATIO_44MUX               =   43U,                /*!<  Set Multiplex ratio: 44MUX                                        */
+    SET_MULTIPLEX_RATIO_45MUX               =   44U,                /*!<  Set Multiplex ratio: 45MUX                                        */
+    SET_MULTIPLEX_RATIO_46MUX               =   45U,                /*!<  Set Multiplex ratio: 46MUX                                        */
+    SET_MULTIPLEX_RATIO_47MUX               =   46U,                /*!<  Set Multiplex ratio: 47MUX                                        */
+    SET_MULTIPLEX_RATIO_48MUX               =   47U,                /*!<  Set Multiplex ratio: 48MUX                                        */
+    SET_MULTIPLEX_RATIO_49MUX               =   48U,                /*!<  Set Multiplex ratio: 49MUX                                        */
+    SET_MULTIPLEX_RATIO_50MUX               =   49U,                /*!<  Set Multiplex ratio: 50MUX                                        */
+    SET_MULTIPLEX_RATIO_51MUX               =   50U,                /*!<  Set Multiplex ratio: 51MUX                                        */
+    SET_MULTIPLEX_RATIO_52MUX               =   51U,                /*!<  Set Multiplex ratio: 52MUX                                        */
+    SET_MULTIPLEX_RATIO_53MUX               =   52U,                /*!<  Set Multiplex ratio: 53MUX                                        */
+    SET_MULTIPLEX_RATIO_54MUX               =   53U,                /*!<  Set Multiplex ratio: 54MUX                                        */
+    SET_MULTIPLEX_RATIO_55MUX               =   54U,                /*!<  Set Multiplex ratio: 55MUX                                        */
+    SET_MULTIPLEX_RATIO_56MUX               =   55U,                /*!<  Set Multiplex ratio: 56MUX                                        */
+    SET_MULTIPLEX_RATIO_57MUX               =   56U,                /*!<  Set Multiplex ratio: 57MUX                                        */
+    SET_MULTIPLEX_RATIO_58MUX               =   57U,                /*!<  Set Multiplex ratio: 58MUX                                        */
+    SET_MULTIPLEX_RATIO_59MUX               =   58U,                /*!<  Set Multiplex ratio: 59MUX                                        */
+    SET_MULTIPLEX_RATIO_60MUX               =   59U,                /*!<  Set Multiplex ratio: 60MUX                                        */
+    SET_MULTIPLEX_RATIO_61MUX               =   60U,                /*!<  Set Multiplex ratio: 61MUX                                        */
+    SET_MULTIPLEX_RATIO_62MUX               =   61U,                /*!<  Set Multiplex ratio: 62MUX                                        */
+    SET_MULTIPLEX_RATIO_63MUX               =   62U,                /*!<  Set Multiplex ratio: 63MUX                                        */
+    SET_MULTIPLEX_RATIO_64MUX               =   63U                 /*!<  Set Multiplex ratio: 64MUX (reset)                                */
+} SSD1306_set_multiplex_ratio_t;
+
+
+/* SET SEGMENT RE-MAP
+*/
+typedef enum
+{
+    SET_SEGMENT_RE_MAP_MASK                 =   0b10100001,         /*!<  Set Segment re-map mask                                           */
+    SET_SEGMENT_RE_MAP_COL_0_SEG0           =   0b10100000,         /*!<  Set column address 0 is mapped to SEG0                  ( RESET ) */
+    SET_SEGMENT_RE_MAP_COL_127_SEG0         =   0b10100001          /*!<  Set column address 127 is mapped to SEG0                          */
+} SSD1306_set_segment_re_map_t;
+
+
+
+/* SET COM OUTPUT SCAN DIRECTION
+*/
+typedef enum
+{
+    SET_SCAN_DIR_MASK                       =   0b11001000,         /*!<  Set COM Output Scan Direction mask                                        */
+    SET_SCAN_DIR_NORMAL_MODE                =   0b11000000,         /*!<  Normal mode. Scan from COM0 to COM[N –1]                        ( RESET ) */
+    SET_SCAN_DIR_REMAPPED_MODE              =   0b11001000          /*!<  Remapped mode. Scan from COM[N-1] to COM0. Where N is the Multiplex ratio */
+} SSD1306_set_com_output_scan_direction_t;
+
+
+
+/**
+  * @brief   SET COM PINS HARDWARE CONFIGURATION
+  */
+/* COM PIN CONFIGURATION
+*/
+typedef enum
+{
+    COM_PIN_CONFIGURATION_MASK                =   0b00010000,         /*!<  COM pin configuration mask                                                */
+    COM_PIN_CONFIGURATION_SEQUENCTIAL_COM_PIN =   0b00000000,         /*!<  COM pin configuration: Sequential mode                          ( RESET ) */
+    COM_PIN_CONFIGURATION_ALTERNATIVE_COM_PIN =   0b00010000          /*!<  COM pin configuration: Alternative mode                                   */
+} SSD1306_com_pin_configuration_t;
+
+
+/* COM LEFT/RIGHT RE-MAP
+*/
+typedef enum
+{
+    COM_LEFT_RIGHT_REMAP_MASK               =   0b00100000,           /*!<  COM left/right re-map mask                                                */
+    COM_LEFT_RIGHT_REMAP_DISABLED           =   0b00000000,           /*!<  COM left/right re-map: Disabled                                 ( RESET ) */
+    COM_LEFT_RIGHT_REMAP_ENABLED            =   0b00100000            /*!<  COM left/right re-map: Enabled                                            */
+} SSD1306_com_left_right_re_map_t;
+
+
+
 
 
 
@@ -182,6 +293,33 @@ SSD1306_status_t  SSD1306_SetNormalInverseDisplay ( I2C_parameters_t myI2Cparame
 /** It sets display ON/OFF.
   */
 SSD1306_status_t  SSD1306_SetDisplay              ( I2C_parameters_t myI2Cparameters, SSD1306_set_display_t myDisplayMode                         );
+
+
+
+/** This command switches the default 63 multiplex mode to any multiplex ratio, ranging from 16 to 63. The
+    output pads COM0-COM63 will be switched to the corresponding COM signal.
+  */
+SSD1306_status_t  SSD1306_SetMultiplexRatio                 ( I2C_parameters_t myI2Cparameters, SSD1306_set_multiplex_ratio_t myMultiplexRatio                                                                );
+
+/** Set vertical shift by COM from 0-63.
+  */
+SSD1306_status_t  SSD1306_SetDisplayOffset                  ( I2C_parameters_t myI2Cparameters, uint8_t myDisplayOffset                                                                                       );
+
+/** Set display RAM display start line register from 0-63.
+  */
+SSD1306_status_t  SSD1306_SetDisplayStartLine               ( I2C_parameters_t myI2Cparameters, uint8_t myDisplayStartLine                                                                                    );
+
+/** Set Segment Re-map.
+  */
+SSD1306_status_t  SSD1306_SetSegmentReMap                   ( I2C_parameters_t myI2Cparameters, SSD1306_set_segment_re_map_t mySegmentReMap                                                                   );
+
+/** Set COM Output Scan Direction.
+  */
+SSD1306_status_t  SSD1306_SetCOM_OutputScanDirection        ( I2C_parameters_t myI2Cparameters, SSD1306_set_com_output_scan_direction_t myScanDirection                                                       );
+
+/** Set COM Pins Hardware Configuration.
+  */
+SSD1306_status_t  SSD1306_SetCOM_PinsHardwareConfiguration  ( I2C_parameters_t myI2Cparameters, SSD1306_com_pin_configuration_t myCOM_PinConfiguration, SSD1306_com_left_right_re_map_t myCOM_LeftRightEnable );
 
 
 #ifdef __cplusplus
