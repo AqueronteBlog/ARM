0eebb2147edb9bf21ffb531ab7b1f06bc9062de0
Manuel Caballero <aqueronteblog@gmail.com>
2018-11-22 15:38:30 +0000

    DRIVERS. SSD1306, Command and Data functions were added, they have to be tested, the driver is still in progress

--- Drivers/SSD1306/SSD1306.c
+++ Drivers/SSD1306/SSD1306.c
@@ -57,6 +57,97 @@ SSD1306_status_t  SSD1306_Init ( I2C_parameters_t myI2Cparameters )
 
 
 /**
+ * @brief       SSD1306_SendCommand ( I2C_parameters_t , uint8_t )
+ *
+ * @details     It sends a command byte to SSD1306.
+ *
+ * @param[in]    myI2Cparameters: I2C parameters.
+ * @param[in]    myCommand:       Command.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SSD1306_SendCommand.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        22/November/2018
+ * @version     22/November/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SSD1306_status_t  SSD1306_SendCommand ( I2C_parameters_t myI2Cparameters, uint8_t myCommand )
+{
+    uint8_t      cmd[]  =    { 0U, 0U };
+    i2c_status_t aux;
+
+
+    /* Send a new command value    */
+    cmd[0]   =   ( SSD1306_DATA_COMMAND_BIT_COMMAND & ( SSD1306_CO_DATA_BYTES & SSD1306_CONTROL_BYTE ) );   // Control byte
+    cmd[1]   =   myCommand;                                                              
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+    
+    if ( aux == I2C_SUCCESS )
+    {
+        return   SSD1306_SUCCESS;
+    }
+    else
+    {
+        return   SSD1306_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SSD1306_SendData ( I2C_parameters_t , uint8_t* , uint8_t )
+ *
+ * @details     It sends a command byte to SSD1306.
+ *
+ * @param[in]    myI2Cparameters: I2C parameters.
+ * @param[in]    myData:          Data.
+ * @param[in]    myDataSize:      Amount of data to be sent.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SSD1306_SendData.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        22/November/2018
+ * @version     22/November/2018   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SSD1306_status_t  SSD1306_SendData ( I2C_parameters_t myI2Cparameters, uint8_t* myData, uint8_t myDataSize )
+{
+    uint8_t      *cmd;
+    i2c_status_t aux;
+
+    *cmd++   =   ( SSD1306_DATA_COMMAND_BIT_DATA | ( SSD1306_CO_DATA_BYTES & SSD1306_CONTROL_BYTE ) );   // Control byte
+    cmd   =   myData;
+
+    /* Send a new data value    */                                                             
+    aux      =   i2c_write ( myI2Cparameters, &cmd[0], myDataSize, I2C_STOP_BIT );
+
+
+    
+    if ( aux == I2C_SUCCESS )
+    {
+        return   SSD1306_SUCCESS;
+    }
+    else
+    {
+        return   SSD1306_FAILURE;
+    }
+}
+
+
+
+/**
  * @brief       SSD1306_SetContrastControl ( I2C_parameters_t , SSD1306_vector_data_t )
  *
  * @details     It sets the constrast value to select 1 out of 256 contrast steps.
@@ -78,14 +169,13 @@ SSD1306_status_t  SSD1306_Init ( I2C_parameters_t myI2Cparameters )
  */
 SSD1306_status_t  SSD1306_SetContrastControl ( I2C_parameters_t myI2Cparameters, SSD1306_vector_data_t myContrastStep )
 {
-    uint8_t      cmd[]  =    { 0U, 0U,  };
-    i2c_status_t aux;
+    uint8_t          cmd[]  =    { 0U, 0U };
+    SSD1306_status_t aux;
 
 
     /* Set new constrast value    */
-    cmd[0]   =   ( SSD1306_CO_DATA_BYTES | SSD1306_DATA_COMMAND_BIT_COMMAND | SSD1306_CONTROL_BYTE );   // Control byte
-    cmd[1]   =   myContrastStep.contrast;                                                               // New contrast value
-    aux      =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+    aux  =   SSD1306_SendCommand ( myI2Cparameters, myContrastStep.contrast );
+
 
 
     
