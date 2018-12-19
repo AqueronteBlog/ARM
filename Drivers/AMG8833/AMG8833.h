4971823763d60f03b7bce76b59b5b2bc56c66db9
Manuel Caballero <aqueronteblog@gmail.com>
2018-12-19 22:45:43 +0000

    DRIVER. AMG8833, header file is in progress, some comments were improved

--- Drivers/AMG8833/AMG8833.h
+++ Drivers/AMG8833/AMG8833.h
@@ -1,14 +1,14 @@
 /**
- * @brief       TMP006.h
- * @details     Infrared Thermopile Sensor in Chip-Scale Package.
+ * @brief       AMG8833.h
+ * @details     Infrared Array Sensor "Grid-EYE".
  *              Header file.
  *
  *
  * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        7/December/2018
- * @version     7/December/2018    The ORIGIN
+ * @date        19/December/2018
+ * @version     19/December/2018    The ORIGIN
  * @pre         N/A.
  * @warning     N/A
  * @pre         This code belongs to Nimbus Centre ( http://www.nimbus.cit.ie ). All rights reserved.
@@ -21,8 +21,8 @@
 #include "i2c.h"
 
 
-#ifndef TMP006_H_
-#define TMP006_H_
+#ifndef AMG8833_H_
+#define AMG8833_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -35,15 +35,15 @@ extern "C" {
   */
 typedef enum
 {
-    TMP006_ADDRESS_ADR1_0_ADR0_0     =   0b1000000,   /*!<   TMP006 I2C Address: ADR1 = 0 | ADR0 = 0             */
-    TMP006_ADDRESS_ADR1_0_ADR0_1     =   0b1000001,   /*!<   TMP006 I2C Address: ADR1 = 0 | ADR0 = 1             */
-    TMP006_ADDRESS_ADR1_0_ADR0_SDA   =   0b1000010,   /*!<   TMP006 I2C Address: ADR1 = 0 | ADR0 = SDA           */
-    TMP006_ADDRESS_ADR1_0_ADR0_SCL   =   0b1000011,   /*!<   TMP006 I2C Address: ADR1 = 0 | ADR0 = SCL           */
-    TMP006_ADDRESS_ADR1_1_ADR0_0     =   0b1000100,   /*!<   TMP006 I2C Address: ADR1 = 1 | ADR0 = 0             */
-    TMP006_ADDRESS_ADR1_1_ADR0_1     =   0b1000101,   /*!<   TMP006 I2C Address: ADR1 = 1 | ADR0 = 1             */
-    TMP006_ADDRESS_ADR1_1_ADR0_SDA   =   0b1000110,   /*!<   TMP006 I2C Address: ADR1 = 1 | ADR0 = SDA           */
-    TMP006_ADDRESS_ADR1_1_ADR0_SCL   =   0b1000111    /*!<   TMP006 I2C Address: ADR1 = 1 | ADR0 = SCL           */
-} TMP006_addresses_t;
+    AMG8833_ADDRESS_ADR1_0_ADR0_0     =   0b1000000,   /*!<   AMG8833 I2C Address: ADR1 = 0 | ADR0 = 0             */
+    AMG8833_ADDRESS_ADR1_0_ADR0_1     =   0b1000001,   /*!<   AMG8833 I2C Address: ADR1 = 0 | ADR0 = 1             */
+    AMG8833_ADDRESS_ADR1_0_ADR0_SDA   =   0b1000010,   /*!<   AMG8833 I2C Address: ADR1 = 0 | ADR0 = SDA           */
+    AMG8833_ADDRESS_ADR1_0_ADR0_SCL   =   0b1000011,   /*!<   AMG8833 I2C Address: ADR1 = 0 | ADR0 = SCL           */
+    AMG8833_ADDRESS_ADR1_1_ADR0_0     =   0b1000100,   /*!<   AMG8833 I2C Address: ADR1 = 1 | ADR0 = 0             */
+    AMG8833_ADDRESS_ADR1_1_ADR0_1     =   0b1000101,   /*!<   AMG8833 I2C Address: ADR1 = 1 | ADR0 = 1             */
+    AMG8833_ADDRESS_ADR1_1_ADR0_SDA   =   0b1000110,   /*!<   AMG8833 I2C Address: ADR1 = 1 | ADR0 = SDA           */
+    AMG8833_ADDRESS_ADR1_1_ADR0_SCL   =   0b1000111    /*!<   AMG8833 I2C Address: ADR1 = 1 | ADR0 = SCL           */
+} AMG8833_addresses_t;
 
 
 
@@ -52,12 +52,12 @@ typedef enum
   */
 typedef enum
 {
-    TMP006_SENSOR_VOLTAGE     =   0x00,               /*!<  Sensor voltage      ( Read Only  )                    */
-    TMP006_LOCAL_TEMPERATURE  =   0x01,               /*!<  Local temperature   ( Read Only )                     */
-    TMP006_CONFIGURATION      =   0x02,               /*!<  Configuration       ( Read/Write )                    */
-    TMP006_MANUFACTURER_ID    =   0xFE,               /*!<  Manufacturer ID     ( Read Only )                     */
-    TMP006_DEVICE_ID          =   0xFF                /*!<  Device ID           ( Read Only )                     */
-} TMP006_register_map_t;
+    AMG8833_SENSOR_VOLTAGE     =   0x00,               /*!<  Sensor voltage      ( Read Only  )                    */
+    AMG8833_LOCAL_TEMPERATURE  =   0x01,               /*!<  Local temperature   ( Read Only )                     */
+    AMG8833_CONFIGURATION      =   0x02,               /*!<  Configuration       ( Read/Write )                    */
+    AMG8833_MANUFACTURER_ID    =   0xFE,               /*!<  Manufacturer ID     ( Read Only )                     */
+    AMG8833_DEVICE_ID          =   0xFF                /*!<  Device ID           ( Read Only )                     */
+} AMG8833_register_map_t;
 
 
 
@@ -71,7 +71,7 @@ typedef enum
     RST_BIT_MASK                      =   ( 1U << 15U ),      /*!<  Software reset bit mask                                                 */
     RST_NORMAL_OPERATION              =   ( 0U << 15U ),      /*!<  Software reset bit: Normal operation                          ( RESET ) */
     RST_SOFTWARE_RESET                =   ( 1U << 15U )       /*!<  Software reset bit: Software reset                                      */
-} TMP006_rst_t;
+} AMG8833_rst_t;
 
 
 /* MODE OF OPERATION <14:12>
@@ -81,7 +81,7 @@ typedef enum
     MOD_MASK                           =   ( 0b111 << 12U ),   /*!<  Mode of operation mask                                                  */
     MOD_POWER_DOWN                     =   ( 0b000 << 12U ),   /*!<  Mode of operation: Power-down                                           */
     MOD_SENSOR_AND_DIE_CONT_CONVERSION =   ( 0b111 << 12U )    /*!<  Mode of operation: Sensor and die continuous conversion (MOD) ( RESET ) */
-} TMP006_mod_t;
+} AMG8833_mod_t;
 
 
 /* ADC CONVERSION RATE <11:9>
@@ -94,7 +94,7 @@ typedef enum
     CR_4_AVERAGED_SAMPLES             =   ( 0b010 << 9U ),    /*!<  ADC Conversion rate:  4 number of averaged sample             ( RESET ) */
     CR_8_AVERAGED_SAMPLES             =   ( 0b011 << 9U ),    /*!<  ADC Conversion rate:  8 number of averaged sample                       */
     CR_16_AVERAGED_SAMPLES            =   ( 0b100 << 9U )     /*!<  ADC Conversion rate: 16 number of averaged sample                       */
-} TMP006_cr_t;
+} AMG8833_cr_t;
 
 
 /* #DRDY ENABLE BIT <8>
@@ -104,7 +104,7 @@ typedef enum
     EN_MASK                           =   ( 1U << 8U ),       /*!<  #DRDY enable bit mask                                                   */
     EN_nDRDY_PIN_DISABLED             =   ( 0U << 8U ),       /*!<  #DRDY enable bit: Pin disabled                                ( RESET ) */
     EN_nDRDY_PIN_ENABLED              =   ( 1U << 8U )        /*!<  #DRDY enable bit: Pin enabled                                           */
-} TMP006_en_t;
+} AMG8833_en_t;
 
 
 /* DATA READY BIT <7>
@@ -114,7 +114,7 @@ typedef enum
     nDRDY_MASK                        =   ( 1U << 7U ),       /*!<  Data ready bit mask                                                     */
     nDRDY_CONVERSION_IN_PROGRESS      =   ( 0U << 7U ),       /*!<  Data ready bit: Conversion in progress                        ( RESET ) */
     nDRDY_CONVERSION_COMPLETED        =   ( 1U << 7U )        /*!<  Data ready bit: Conversion completed                                    */
-} TMP006_ndrdy_t;
+} AMG8833_ndrdy_t;
 
 
 
@@ -126,7 +126,7 @@ typedef enum
 {
     MANUFACTURER_ID_MASK              =   0xFFFF,             /*!<  Manufacturer ID mask                                                    */
     MANUFACTURER_ID_VALUE             =   0x5449              /*!<  Manufacturer ID value                                         ( RESET ) */
-} TMP006_manufacturer_id_register_t;
+} AMG8833_manufacturer_id_register_t;
 
 
 
@@ -137,7 +137,7 @@ typedef enum
 {
     DEVICE_ID_MASK                    =   0xFFFF,             /*!<  Device ID mask                                                          */
     DEVICE_ID_VALUE                   =   0x0067              /*!<  Device ID value                                               ( RESET ) */
-} TMP006_device_id_register_t;
+} AMG8833_device_id_register_t;
 
 
 
@@ -159,8 +159,8 @@ typedef enum
 
 
 
-#ifndef TMP006_VECTOR_STRUCT_H
-#define TMP006_VECTOR_STRUCT_H
+#ifndef AMG8833_VECTOR_STRUCT_H
+#define AMG8833_VECTOR_STRUCT_H
 typedef struct
 {
     float    ObjectTemperatureC;             /*!<  Temperature of the target object in Celsius degrees                              */
@@ -177,7 +177,7 @@ typedef struct
     
     uint16_t ManufacturerID;                /*!<  Manufacturer ID                                                                   */
     uint16_t DeviceID;                      /*!<  Device ID                                                                         */
-} TMP006_data_t;
+} AMG8833_data_t;
 #endif
 
 
@@ -189,9 +189,9 @@ typedef struct
   */
 typedef enum
 {
-    TMP006_SUCCESS     =       0,
-    TMP006_FAILURE     =       1
-} TMP006_status_t;
+    AMG8833_SUCCESS     =       0,
+    AMG8833_FAILURE     =       1
+} AMG8833_status_t;
 
 
 
@@ -201,55 +201,55 @@ typedef enum
   */
 /** It configures the I2C peripheral.
   */
-TMP006_status_t  TMP006_Init                      ( I2C_parameters_t myI2Cparameters                                  );
+AMG8833_status_t  AMG8833_Init                      ( I2C_parameters_t myI2Cparameters                                  );
 
 /** It gets the manufacturer ID.
   */
-TMP006_status_t  TMP006_GetManufacturerID         ( I2C_parameters_t myI2Cparameters, TMP006_data_t* myManufacturerID );
+AMG8833_status_t  AMG8833_GetManufacturerID         ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myManufacturerID );
 
 /** It gets the device ID.
   */
-TMP006_status_t  TMP006_GetDeviceID               ( I2C_parameters_t myI2Cparameters, TMP006_data_t* myDeviceID       );
+AMG8833_status_t  AMG8833_GetDeviceID               ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myDeviceID       );
 
 /** It reads the configuration register.
   */
-TMP006_status_t  TMP006_ReadConfigurationRegister ( I2C_parameters_t myI2Cparameters, TMP006_data_t* myConfReg        );
+AMG8833_status_t  AMG8833_ReadConfigurationRegister ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myConfReg        );
 
 /** It performs a software reset.
   */
-TMP006_status_t  TMP006_SoftwareReset             ( I2C_parameters_t myI2Cparameters                                  );
+AMG8833_status_t  AMG8833_SoftwareReset             ( I2C_parameters_t myI2Cparameters                                  );
 
 /** It sets mode of operation.
   */
-TMP006_status_t  TMP006_SetModeOperation          ( I2C_parameters_t myI2Cparameters, TMP006_mod_t myModeOpreation    );
+AMG8833_status_t  AMG8833_SetModeOperation          ( I2C_parameters_t myI2Cparameters, AMG8833_mod_t myModeOpreation    );
 
 /** It sets conversion rate.
   */
-TMP006_status_t  TMP006_SetConversionRate         ( I2C_parameters_t myI2Cparameters, TMP006_cr_t myConversionRate    );
+AMG8833_status_t  AMG8833_SetConversionRate         ( I2C_parameters_t myI2Cparameters, AMG8833_cr_t myConversionRate    );
 
 /** It sets #DRDY control.
   */
-TMP006_status_t  TMP006_SetnDRDY_EnableBit        ( I2C_parameters_t myI2Cparameters, TMP006_en_t myEnableBit         );
+AMG8833_status_t  AMG8833_SetnDRDY_EnableBit        ( I2C_parameters_t myI2Cparameters, AMG8833_en_t myEnableBit         );
 
 /** It reads raw temperature ( T_DIE ) register.
   */
-TMP006_status_t  TMP006_GetRawTemperature         ( I2C_parameters_t myI2Cparameters, TMP006_data_t* myRawTemperature );
+AMG8833_status_t  AMG8833_GetRawTemperature         ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myRawTemperature );
 
 /** It reads raw sensor voltage result ( V_SENSOR ) register.
   */
-TMP006_status_t  TMP006_GetRawSensorVoltage       ( I2C_parameters_t myI2Cparameters, TMP006_data_t* myRawVoltage     );
+AMG8833_status_t  AMG8833_GetRawSensorVoltage       ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myRawVoltage     );
 
 /** It calculates the real temperature ( T_DIE ) value.
   */
-TMP006_status_t  TMP006_CalculateTemperature      ( TMP006_data_t* myTemperature                                      );
+AMG8833_status_t  AMG8833_CalculateTemperature      ( AMG8833_data_t* myTemperature                                      );
 
 /** It calculates the real sensor voltage ( V_SENSOR ) value.
   */
-TMP006_status_t  TMP006_CalculateSensorVoltage    ( TMP006_data_t* myV_sensor                                         );
+AMG8833_status_t  AMG8833_CalculateSensorVoltage    ( AMG8833_data_t* myV_sensor                                         );
 
 /** It calculates the object temperature ( T_OBJ ) value.
   */
-TMP006_status_t  TMP006_CalculateObjectTemperature( TMP006_data_t* myObjTemperature                                   );
+AMG8833_status_t  AMG8833_CalculateObjectTemperature( AMG8833_data_t* myObjTemperature                                   );
 
 
 
@@ -257,4 +257,4 @@ TMP006_status_t  TMP006_CalculateObjectTemperature( TMP006_data_t* myObjTemperat
 }
 #endif
 
-#endif /* TMP006_H */
+#endif /* AMG8833_H */
