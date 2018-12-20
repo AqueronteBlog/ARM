b63fb7c0c51b467b5a669e4ce5febc633f4741d9
Manuel Caballero <aqueronteblog@gmail.com>
2018-12-20 09:27:33 +0000

    DRIVER. AMG8833, registers definition is in progress ( up to n. 5 )

--- Drivers/AMG8833/AMG8833.h
+++ Drivers/AMG8833/AMG8833.h
@@ -202,100 +202,72 @@ typedef enum
 
 
 /**
-  * @brief   CONFIGURATION REGISTER
+  * @brief   POWER CONTROL REGISTER
   */
-/* SOFTWARE RESET BIT <15>
+/* PCTL: OPERATING MODE
+ *    NOTE: Writing operation in Sleep mode is only active in return to Normal mode. Reading operation in Sleep mode is invalid.
 */
 typedef enum
 {
-    RST_BIT_MASK                      =   ( 1U << 15U ),      /*!<  Software reset bit mask                                                 */
-    RST_NORMAL_OPERATION              =   ( 0U << 15U ),      /*!<  Software reset bit: Normal operation                          ( RESET ) */
-    RST_SOFTWARE_RESET                =   ( 1U << 15U )       /*!<  Software reset bit: Software reset                                      */
+    PCTL_MASK                 =   0xFF,         /*!<  PCTL bit mask                                                 */
+    PCTL_NORMAL_MODE          =   0x00,         /*!<  PCTL: Normal mode                                   ( RESET ) */
+    PCTL_SLEEP_MODE           =   0x10,         /*!<  PCTL: Sleep mode                                              */
+    PCTL_STANDBY_60SEC_MODE   =   0x20,         /*!<  PCTL: Stand-by mode ( 60sec intermittence )                   */
+    PCTL_STANDBY_10SEC_MODE   =   0x21          /*!<  PCTL: Stand-by mode ( 10sec intermittence )                   */
+} AMG8833_pctl_t;
+
+
+/**
+  * @brief   RESET REGISTER
+  */
+typedef enum
+{
+    RST_MASK                  =   0xFF,         /*!<  RST mask                                                      */
+    RST_FLAG_RESET            =   0x30,         /*!<  Flag reset                                                    */
+    RST_INITIAL_RESET         =   0x3F          /*!<  Initial reset reset                                           */
 } AMG8833_rst_t;
 
 
-/* MODE OF OPERATION <14:12>
-*/
+
+/**
+  * @brief   FRAME RATE REGISTER ( SETTING FRAME MODE )
+  */
 typedef enum
 {
-    MOD_MASK                           =   ( 0b111 << 12U ),   /*!<  Mode of operation mask                                                  */
-    MOD_POWER_DOWN                     =   ( 0b000 << 12U ),   /*!<  Mode of operation: Power-down                                           */
-    MOD_SENSOR_AND_DIE_CONT_CONVERSION =   ( 0b111 << 12U )    /*!<  Mode of operation: Sensor and die continuous conversion (MOD) ( RESET ) */
-} AMG8833_mod_t;
-
-
-/* ADC CONVERSION RATE <11:9>
-*/
-typedef enum
-{
-    CR_MASK                           =   ( 0b111 << 9U ),    /*!<  ADC Conversion rate mask                                                */
-    CR_1_AVERAGED_SAMPLES             =   ( 0b000 << 9U ),    /*!<  ADC Conversion rate:  1 number of averaged sample                       */
-    CR_2_AVERAGED_SAMPLES             =   ( 0b001 << 9U ),    /*!<  ADC Conversion rate:  2 number of averaged sample                       */
-    CR_4_AVERAGED_SAMPLES             =   ( 0b010 << 9U ),    /*!<  ADC Conversion rate:  4 number of averaged sample             ( RESET ) */
-    CR_8_AVERAGED_SAMPLES             =   ( 0b011 << 9U ),    /*!<  ADC Conversion rate:  8 number of averaged sample                       */
-    CR_16_AVERAGED_SAMPLES            =   ( 0b100 << 9U )     /*!<  ADC Conversion rate: 16 number of averaged sample                       */
-} AMG8833_cr_t;
-
-
-/* #DRDY ENABLE BIT <8>
-*/
-typedef enum
-{
-    EN_MASK                           =   ( 1U << 8U ),       /*!<  #DRDY enable bit mask                                                   */
-    EN_nDRDY_PIN_DISABLED             =   ( 0U << 8U ),       /*!<  #DRDY enable bit: Pin disabled                                ( RESET ) */
-    EN_nDRDY_PIN_ENABLED              =   ( 1U << 8U )        /*!<  #DRDY enable bit: Pin enabled                                           */
-} AMG8833_en_t;
-
-
-/* DATA READY BIT <7>
-*/
-typedef enum
-{
-    nDRDY_MASK                        =   ( 1U << 7U ),       /*!<  Data ready bit mask                                                     */
-    nDRDY_CONVERSION_IN_PROGRESS      =   ( 0U << 7U ),       /*!<  Data ready bit: Conversion in progress                        ( RESET ) */
-    nDRDY_CONVERSION_COMPLETED        =   ( 1U << 7U )        /*!<  Data ready bit: Conversion completed                                    */
-} AMG8833_ndrdy_t;
-
+    FPSC_MASK                 =   ( 1U << 0U ), /*!<  FPSC mask                                                     */
+    FPSC_1FPS                 =   ( 1U << 0U ), /*!<  FPSC: 1 FPSC                                                  */
+    FPSC_10FPS                =   ( 0U << 0U )  /*!<  FPSC: 10 FPSC                                       ( RESET ) */
+} AMG8833_fpsc_t;
 
 
 
 /**
-  * @brief   MANUFACTURER ID REGISTER
+  * @brief   INTERRUPT CONTROL REGISTER
+  */
+/**
+  * INTMOD ( INTERRUPT MODE ) <1>
   */
 typedef enum
 {
-    MANUFACTURER_ID_MASK              =   0xFFFF,             /*!<  Manufacturer ID mask                                                    */
-    MANUFACTURER_ID_VALUE             =   0x5449              /*!<  Manufacturer ID value                                         ( RESET ) */
-} AMG8833_manufacturer_id_register_t;
+    INTMOD_MASK                       =   ( 1U << 1U ), /*!<  INTMOD mask                                           */
+    INTMOD_ABSOLUTE_INTERRUPT_MODE    =   ( 1U << 1U ), /*!<  Absolute interrupt mode                               */
+    INTMOD_DIFFERENCE_INTERRUPT_MODE  =   ( 0U << 1U )  /*!<  Difference Interrupt Mode                   ( RESET ) */
+} AMG8833_intmod_t;
 
 
 
 /**
-  * @brief   DEVICE ID REGISTER
+  * INTEN ( INTERRUPT MODE ) <0>
   */
 typedef enum
 {
-    DEVICE_ID_MASK                    =   0xFFFF,             /*!<  Device ID mask                                                          */
-    DEVICE_ID_VALUE                   =   0x0067              /*!<  Device ID value                                               ( RESET ) */
-} AMG8833_device_id_register_t;
+    INTEN_MASK                        =   ( 1U << 0U ), /*!<  INTEN mask                                            */
+    INTEN_INT_OUTPUT_ACTIVE           =   ( 1U << 0U ), /*!<  INT Output active                                     */
+    INTEN_INT_OUTPUT_REACTIVE         =   ( 0U << 0U )  /*!<  INT Output reactive ( Hi-Z )                ( RESET ) */
+} AMG8833_inten_t;
 
 
 
-
-/**
-  * @brief   CONSTANS TO BE USED IN THE FORMULAS
-  *           NOTE: User Guide ( sbou107.pdf ) 5.1 Equations for Calculating Target Object Temperatures, p10.
-  */
-#define A1        0.00175                                         /*!<  A1                                                                */
-#define A2        -0.00001678                                     /*!<  A2                                                                */
-#define T_REF     298.15                                          /*!<  T_REF, Kelvin                                                     */
-#define B0        -0.0000294                                      /*!<  B0                                                                */
-#define B1        -0.00000057                                     /*!<  B1                                                                */
-#define B2        -0.00000000463                                  /*!<  B2                                                                */
-#define C2        13.4                                            /*!<  C2                                                                */
-#define S0        ( 0.00000000000005 + 0.00000000000007 ) / 2.0   /*!<  Primary calibration sensitivity factor ( mean of typical values ) */
-#define TEMP_1LSB 0.03125                                         /*!<  Temperature: 1 LSB = 1 / 32°C = 0.03125                           */
-#define SVOL_1LSB 156.25                                          /*!<  Sensor voltage: 1 LSB = 156.25 nV                                 */
 
 
 
@@ -343,53 +315,53 @@ typedef enum
   */
 AMG8833_status_t  AMG8833_Init                      ( I2C_parameters_t myI2Cparameters                                  );
 
-/** It gets the manufacturer ID.
-  */
-AMG8833_status_t  AMG8833_GetManufacturerID         ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myManufacturerID );
-
-/** It gets the device ID.
-  */
-AMG8833_status_t  AMG8833_GetDeviceID               ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myDeviceID       );
-
-/** It reads the configuration register.
-  */
-AMG8833_status_t  AMG8833_ReadConfigurationRegister ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myConfReg        );
-
-/** It performs a software reset.
-  */
-AMG8833_status_t  AMG8833_SoftwareReset             ( I2C_parameters_t myI2Cparameters                                  );
-
-/** It sets mode of operation.
-  */
-AMG8833_status_t  AMG8833_SetModeOperation          ( I2C_parameters_t myI2Cparameters, AMG8833_mod_t myModeOpreation    );
-
-/** It sets conversion rate.
-  */
-AMG8833_status_t  AMG8833_SetConversionRate         ( I2C_parameters_t myI2Cparameters, AMG8833_cr_t myConversionRate    );
-
-/** It sets #DRDY control.
-  */
-AMG8833_status_t  AMG8833_SetnDRDY_EnableBit        ( I2C_parameters_t myI2Cparameters, AMG8833_en_t myEnableBit         );
-
-/** It reads raw temperature ( T_DIE ) register.
-  */
-AMG8833_status_t  AMG8833_GetRawTemperature         ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myRawTemperature );
-
-/** It reads raw sensor voltage result ( V_SENSOR ) register.
-  */
-AMG8833_status_t  AMG8833_GetRawSensorVoltage       ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myRawVoltage     );
-
-/** It calculates the real temperature ( T_DIE ) value.
-  */
-AMG8833_status_t  AMG8833_CalculateTemperature      ( AMG8833_data_t* myTemperature                                      );
-
-/** It calculates the real sensor voltage ( V_SENSOR ) value.
-  */
-AMG8833_status_t  AMG8833_CalculateSensorVoltage    ( AMG8833_data_t* myV_sensor                                         );
-
-/** It calculates the object temperature ( T_OBJ ) value.
-  */
-AMG8833_status_t  AMG8833_CalculateObjectTemperature( AMG8833_data_t* myObjTemperature                                   );
+///** It gets the manufacturer ID.
+//  */
+//AMG8833_status_t  AMG8833_GetManufacturerID         ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myManufacturerID );
+//
+///** It gets the device ID.
+//  */
+//AMG8833_status_t  AMG8833_GetDeviceID               ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myDeviceID       );
+//
+///** It reads the configuration register.
+//  */
+//AMG8833_status_t  AMG8833_ReadConfigurationRegister ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myConfReg        );
+//
+///** It performs a software reset.
+//  */
+//AMG8833_status_t  AMG8833_SoftwareReset             ( I2C_parameters_t myI2Cparameters                                  );
+//
+///** It sets mode of operation.
+//  */
+//AMG8833_status_t  AMG8833_SetModeOperation          ( I2C_parameters_t myI2Cparameters, AMG8833_mod_t myModeOpreation    );
+//
+///** It sets conversion rate.
+//  */
+//AMG8833_status_t  AMG8833_SetConversionRate         ( I2C_parameters_t myI2Cparameters, AMG8833_cr_t myConversionRate    );
+//
+///** It sets #DRDY control.
+//  */
+//AMG8833_status_t  AMG8833_SetnDRDY_EnableBit        ( I2C_parameters_t myI2Cparameters, AMG8833_en_t myEnableBit         );
+//
+///** It reads raw temperature ( T_DIE ) register.
+//  */
+//AMG8833_status_t  AMG8833_GetRawTemperature         ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myRawTemperature );
+//
+///** It reads raw sensor voltage result ( V_SENSOR ) register.
+//  */
+//AMG8833_status_t  AMG8833_GetRawSensorVoltage       ( I2C_parameters_t myI2Cparameters, AMG8833_data_t* myRawVoltage     );
+//
+///** It calculates the real temperature ( T_DIE ) value.
+//  */
+//AMG8833_status_t  AMG8833_CalculateTemperature      ( AMG8833_data_t* myTemperature                                      );
+//
+///** It calculates the real sensor voltage ( V_SENSOR ) value.
+//  */
+//AMG8833_status_t  AMG8833_CalculateSensorVoltage    ( AMG8833_data_t* myV_sensor                                         );
+//
+///** It calculates the object temperature ( T_OBJ ) value.
+//  */
+//AMG8833_status_t  AMG8833_CalculateObjectTemperature( AMG8833_data_t* myObjTemperature                                   );
 
 
 
