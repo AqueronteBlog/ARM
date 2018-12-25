e119737960394b60cbaea56b7f09041614be21ff
Manuel Caballero <aqueronteblog@gmail.com>
2018-12-25 22:15:39 +0100

    NRF52 AMG8833 Example. The driver is ready to be tested

--- NRF52/Examples/examples/Drivers/AMG8833/main.c
+++ NRF52/Examples/examples/Drivers/AMG8833/main.c
@@ -49,9 +49,9 @@ int main(void)
 {
   uint8_t  myMessage[ TX_BUFF_SIZE ];
 
-//  I2C_parameters_t myAMG8833_I2C_parameters;
-//  AMG8833_status_t  aux;
-//  AMG8833_data_t    myAMG8833_Data;
+  I2C_parameters_t myAMG8833_I2C_parameters;
+  AMG8833_status_t  aux;
+  AMG8833_data_t    myAMG8833_Data;
 
 
   conf_CLK    ();
@@ -61,62 +61,38 @@ int main(void)
 
   
 
-//  /* I2C definition   */
-//  myAMG8833_I2C_parameters.TWIinstance =    NRF_TWI0;
-//  myAMG8833_I2C_parameters.SDA         =    TWI0_SDA;
-//  myAMG8833_I2C_parameters.SCL         =    TWI0_SCL;
-//  myAMG8833_I2C_parameters.ADDR        =    AMG8833_ADDRESS_ADR1_0_ADR0_0;
-//  myAMG8833_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
-//  myAMG8833_I2C_parameters.SDAport     =    NRF_P0;
-//  myAMG8833_I2C_parameters.SCLport     =    NRF_P0;
-//
-//  /* Configure I2C peripheral  */
-//  aux  =   AMG8833_Init              ( myAMG8833_I2C_parameters );
-//
-//  /* Reset the device by software  */
-//  aux  =   AMG8833_SoftwareReset     ( myAMG8833_I2C_parameters );
-//  
-//  /* Wait until the device is ready  */
-//  do
-//  {
-//    /* Read configuration register  */
-//    aux  =   AMG8833_ReadConfigurationRegister ( myAMG8833_I2C_parameters, &myAMG8833_Data );
-//  }while ( ( myAMG8833_Data.ConfigurationRegister & RST_BIT_MASK ) != RST_NORMAL_OPERATION );  // [TODO]       This is DANGEROUS, if something goes wrong, the uC will get stuck here!!!.
-//                                                                                              // [WORKAROUND] Insert a timeout.
-//  /* Turn off the device  */
-//  aux  =   AMG8833_SetModeOperation ( myAMG8833_I2C_parameters, MOD_POWER_DOWN );
-//
-//  /* Get manufacturer ID */
-//  aux  =   AMG8833_GetManufacturerID ( myAMG8833_I2C_parameters, &myAMG8833_Data );
-//  sprintf ( (char*)myMessage, "Manufacturer ID: %02x\r\n", myAMG8833_Data.ManufacturerID );
-//  NRF_UART0->TASKS_STOPRX  =   1UL;
-//  NRF_UART0->TASKS_STOPTX  =   1UL;
-//  myPtr                    =   &myMessage[0];
-//
-//  NRF_UART0->TASKS_STARTTX =   1UL;
-//  NRF_UART0->TXD           =   *myPtr;
-//  nrf_delay_ms ( 500 );
-//
-//  /* Get device ID */
-//  aux  =   AMG8833_GetDeviceID       ( myAMG8833_I2C_parameters, &myAMG8833_Data );
-//  sprintf ( (char*)myMessage, "Device ID: %02x\r\n", myAMG8833_Data.DeviceID );
-//  NRF_UART0->TASKS_STOPRX  =   1UL;
-//  NRF_UART0->TASKS_STOPTX  =   1UL;
-//  myPtr                    =   &myMessage[0];
-//
-//  NRF_UART0->TASKS_STARTTX =   1UL;
-//  NRF_UART0->TXD           =   *myPtr;
-//  nrf_delay_ms ( 500 );
-//  
-//  /* Conversion Rate: 1 conversions/sec ( 4 averaged samples ) */
-//  aux  =   AMG8833_SetConversionRate ( myAMG8833_I2C_parameters, CR_4_AVERAGED_SAMPLES );
-//
-//  /* Disbale #DRDY pin */
-//  aux  =   AMG8833_SetnDRDY_EnableBit ( myAMG8833_I2C_parameters, EN_nDRDY_PIN_DISABLED );
-//
-//  /* Turn on the device  */
-//  aux  =   AMG8833_SetModeOperation ( myAMG8833_I2C_parameters, MOD_SENSOR_AND_DIE_CONT_CONVERSION );
+  /* I2C definition   */
+  myAMG8833_I2C_parameters.TWIinstance =    NRF_TWI0;
+  myAMG8833_I2C_parameters.SDA         =    TWI0_SDA;
+  myAMG8833_I2C_parameters.SCL         =    TWI0_SCL;
+  myAMG8833_I2C_parameters.ADDR        =    AMG8833_ADDRESS_AD_SELECT_GND;
+  myAMG8833_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+  myAMG8833_I2C_parameters.SDAport     =    NRF_P0;
+  myAMG8833_I2C_parameters.SCLport     =    NRF_P0;
 
+  /* Configure I2C peripheral  */
+  aux  =   AMG8833_Init           ( myAMG8833_I2C_parameters );
+
+  /* Reset the device by software  */
+  aux  =   AMG8833_SoftwareReset  ( myAMG8833_I2C_parameters, RST_INITIAL_RESET );
+  nrf_delay_ms ( 500 );
+
+  /* Put the device in Sleep mode  */
+  myAMG8833_Data.operationMode   =   PCTL_SLEEP_MODE;
+  aux  =   AMG8833_SetOperationMode ( myAMG8833_I2C_parameters, myAMG8833_Data );
+    
+  /* Set frame rate: 10 FPSC   */
+  aux  =   AMG8833_SetFrameMode     ( myAMG8833_I2C_parameters, FPSC_10FPS );
+
+  /* Set moving average Output Mode: OFF   */
+  aux  =   AMG8833_SetAverageOutputMode ( myAMG8833_I2C_parameters, MAMOD_WICE_MOVING_AVERAGE_OUTPUT_MODE_OFF );
+  
+  /* Clear all flags   */
+  aux  =   AMG8833_ClearFlags       ( myAMG8833_I2C_parameters, ( OVT_CLR_THERMISTOR_TEMPERATURE_OVERFLOW_CLEAR_FLAG | OVS_CLR_TEMPERATURE_OVERFLOW_CLEAR_FLAG | INTCLR_INTERRUPT_OUTBREAK_CLEAR_FLAG ) );
+  
+  /* Put the device in Stand-by ( 10 seconds ) mode  */
+  myAMG8833_Data.operationMode   =   PCTL_STANDBY_10SEC_MODE;
+  aux  =   AMG8833_SetOperationMode ( myAMG8833_I2C_parameters, myAMG8833_Data );
 
   
   myState  =   0;                             // Reset the variable
@@ -138,43 +114,26 @@ int main(void)
     {
       NRF_P0->OUTCLR  |= ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );          // Turn all the LEDs on
 
-//      /* Wait until a new data is ready to be read  */
-//      do
-//      {
-//        /* Read configuration register  */
-//        aux  =   AMG8833_ReadConfigurationRegister ( myAMG8833_I2C_parameters, &myAMG8833_Data );
-//      }while ( ( myAMG8833_Data.ConfigurationRegister & nDRDY_MASK ) == nDRDY_CONVERSION_IN_PROGRESS );  // [TODO]       This is DANGEROUS, if something goes wrong, the uC will get stuck here!!!.
-//                                                                                                        // [WORKAROUND] Insert a timeout.
-//      /* Get raw temperature ( T_DIE ) */
-//      aux  =   AMG8833_GetRawTemperature           ( myAMG8833_I2C_parameters, &myAMG8833_Data );
-//
-//      /* Get raw sensor voltage result ( V_SENSOR ) */
-//      aux  =   AMG8833_GetRawSensorVoltage         ( myAMG8833_I2C_parameters, &myAMG8833_Data );
-//
-//      /* Calculate temperature ( T_DIE ) */
-//      aux  =   AMG8833_CalculateTemperature        ( &myAMG8833_Data );
-//
-//      /* Calculates the real sensor voltage ( V_SENSOR ) value */
-//      aux  =   AMG8833_CalculateSensorVoltage      ( &myAMG8833_Data );
-//
-//      /* Calculate object temperature ( T_OBJ ) */
-//      myAMG8833_Data.s0   =   S0;                                                                        // Typical values for S0 are between 5×10^–14 and 7×10^–14
-//      aux  =   AMG8833_CalculateObjectTemperature  ( &myAMG8833_Data );
-//
-//
-//      /* Transmit result through the UART  */
-//      sprintf ( (char*)myMessage, "T_DIE: %d mC | T_OBJ: %d mC\r\n", (int32_t)( myAMG8833_Data.TemperatureC * 1000.0 ), (int32_t)( myAMG8833_Data.ObjectTemperatureC * 1000.0 ) );
-//
-//      NRF_UART0->TASKS_STOPRX  =   1UL;
-//      NRF_UART0->TASKS_STOPTX  =   1UL;
-//      myPtr                    =   &myMessage[0];
-//
-//      NRF_UART0->TASKS_STARTTX =   1UL;
-//      NRF_UART0->TXD           =   *myPtr;
-//
-//      
-//      /* Reset the variables   */
-//      myState          =   0UL;
+      /* Get thermistor temperature value  */
+      aux  =   AMG8833_GetThermistorValue   ( myAMG8833_I2C_parameters, &myAMG8833_Data );
+
+      /* Get pixel temperature values  */
+      aux  =   AMG8833_GetPixelTemperatures ( myAMG8833_I2C_parameters, &myAMG8833_Data );
+
+
+      /* Transmit result through the UART  */
+      sprintf ( (char*)myMessage, "Temp: %d C\r\n", (int32_t)( myAMG8833_Data.termistorOutputValue ) );
+
+      NRF_UART0->TASKS_STOPRX  =   1UL;
+      NRF_UART0->TASKS_STOPTX  =   1UL;
+      myPtr                    =   &myMessage[0];
+
+      NRF_UART0->TASKS_STARTTX =   1UL;
+      NRF_UART0->TXD           =   *myPtr;
+
+      
+      /* Reset the variables   */
+      myState          =   0UL;
       NRF_P0->OUTSET  |=   ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );          // Turn all the LEDs off
     }
     //__NOP();
