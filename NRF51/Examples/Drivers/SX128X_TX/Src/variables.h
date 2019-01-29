70c1edd5bc19ef66c73832ac59995a1731d025c5
Manuel Caballero <aqueronteblog@gmail.com>
2019-01-29 20:21:49 +0000

    NRF51 SX1280_TX Example. Variables file was added

---
+++ NRF51/Examples/Drivers/SX128X_TX/Src/variables.h
@@ -0,0 +1,40 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        10/September/2018
+ * @version     10/September/2018     The ORIGIN
+ * @pre         This firmware was tested on the nrf51-DK with EmBitz 1.11 rev 0 ( SDK 1.1.0 ).
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "nrf.h"
+#include "nrf51_bitfields.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* VARIABLES_H_ */
