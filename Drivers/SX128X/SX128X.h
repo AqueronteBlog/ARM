c17ddbf89245b13f9b76c1a90b581089a7e5d042
Manuel Caballero <aqueronteblog@gmail.com>
2019-02-07 20:43:00 +0000

    DRIVER. SX1280. Header file is still in progress. GetStatus function was declared

--- Drivers/SX128X/SX128X.h
+++ Drivers/SX128X/SX128X.h
@@ -66,356 +66,52 @@ typedef enum
 
 
 /**
-  * @brief   Register Control_1
+  * @brief   GET_STATUS
   */
-/* EXT_TEST <7>: EXTERNAL CLOCK TEST MODE
+/* CIRCUIT_MODE <7:5>: CIRCUIT MODE
  *    NOTE: N/A.
  */
 typedef enum
 {
-    CONTROL_1_EXT_TEST_MASK                     =   ( 1U << 7U ),     /*!<  EXT_TEST mask                                   */
-    CONTROL_1_EXT_TEST_NORMAL_MODE              =   ( 0U << 7U ),     /*!<  EXT_TEST: normal mode               [ Default ] */
-    CONTROL_1_EXT_TEST_EXTERNAL_CLOCK_TEST_MODE =   ( 1U << 7U )      /*!<  EXT_TEST: external clock test mode              */
-} SX128X_control_1_ext_test_t;
+    GET_STATUS_CIRCUIT_MODE_MASK       =   ( 0b111 << 5U ),             /*!<  CIRCUIT_MODE mask                                   */
+    GET_STATUS_CIRCUIT_MODE_STDBY_RC   =   ( 0x02  << 5U ),             /*!<  CIRCUIT_MODE: STDBY_RC                              */
+    GET_STATUS_CIRCUIT_MODE_STDBY_XOSC =   ( 0x03  << 5U ),             /*!<  CIRCUIT_MODE: STDBY_XOSC                            */
+    GET_STATUS_CIRCUIT_MODE_FS         =   ( 0x04  << 5U ),             /*!<  CIRCUIT_MODE: FS                                    */
+    GET_STATUS_CIRCUIT_MODE_RX         =   ( 0x05  << 5U ),             /*!<  CIRCUIT_MODE: RX                                    */
+    GET_STATUS_CIRCUIT_MODE_TX         =   ( 0x06  << 5U )              /*!<  CIRCUIT_MODE: TX                                    */
+} SX128X_get_status_circuit_mode_t;
 
 
 
-/* STOP <5>: STOP BIT
+/* COMMAND_STATUS <4:2>: COMMAND STATUS
  *    NOTE: N/A.
  */
 typedef enum
 {
-    CONTROL_1_STOP_MASK                         =   ( 1U << 5U ),     /*!<  STOP mask                                       */
-    CONTROL_1_STOP_RTC_CLOCK_RUNS               =   ( 0U << 5U ),     /*!<  STOP: RTC clock runs                [ Default ] */
-    CONTROL_1_STOP_RTC_CLOCK_STOPPED            =   ( 1U << 5U )      /*!<  STOP: RTC clock is stopped                      */
-} SX128X_control_1_stop_t;
+    GET_STATUS_COMMAND_STATUS_MASK                                              =   ( 0b111 << 2U ),  /*!<  COMMAND_STATUS mask                                                                             */
+    GET_STATUS_COMMAND_STATUS_TRANSCEIVER_HAS_SUCCESSFULLY_PROCESSED_COMMAND    =   ( 0x01  << 2U ),  /*!<  The command has been terminated correctly                                                       */
+    GET_STATUS_COMMAND_STATUS_DATA_ARE_AVAILABLE_TO_HOST                        =   ( 0x02  << 2U ),  /*!<  A packet has been successfully received and data can be retrieved                               */
+    GET_STATUS_COMMAND_STATUS_COMMAND_TIMEOUT                                   =   ( 0x03  << 2U ),  /*!<  A transaction from the host took too long to complete and triggered an internal watchdog        */
+    GET_STATUS_COMMAND_STATUS_COMMAND_PROCESSING_ERROR                          =   ( 0x04  << 2U ),  /*!<  The transceiver was unable to process command                                                   */
+    GET_STATUS_COMMAND_STATUS_FAILURE_TO_EXECUTE_COMMAND                        =   ( 0x05  << 2U ),  /*!<  The command was successfully processed, however the transceiver could not execute the command   */
+    GET_STATUS_COMMAND_STATUS_COMMAND_TX_DONE                                   =   ( 0x06  << 2U )   /*!<  The transmission of the current packet has terminated                                           */
+} SX128X_get_status_command_status_t;
 
 
 
-/* SR <4>: SOFTWARE RESET
+/* BUSY <0>: BUSY
  *    NOTE: N/A.
  */
 typedef enum
 {
-    CONTROL_1_SR_MASK                           =   ( 1U << 4U ),     /*!<  SR mask                                         */
-    CONTROL_1_SR_NO_SOFTWARE_RESET              =   ( 0U << 4U ),     /*!<  SR: no software reset               [ Default ] */
-    CONTROL_1_SR_SOFTWARE_RESET                 =   ( 1U << 4U )      /*!<  SR: initiate software reset                     */
-} SX128X_control_1_sr_t;
+    GET_STATUS_BUSY_MASK                =   ( 0b1 << 0U ),              /*!<  BUSY mask                                                     */
+    GET_STATUS_BUSY_BUSY                =   ( 0b1 << 0U ),              /*!<  Transceiver is processing command or doing internal operation */
+    GET_STATUS_BUSY_NOT_BUSY            =   ( 0b0 << 0U )               /*!<  Transceiver is released                                       */
+} SX128X_get_status_busy_t;
 
 
 
-/* CIE <2>: CORRECTION INTERRUPT ENABLE
- *    NOTE: N/A.
- */
-typedef enum
-{
-    CONTROL_1_CIE_MASK                              =   ( 1U << 2U ), /*!<  CIE mask                                                      */
-    CONTROL_1_CIE_NO_CORRECTION_INTERRUPT_GENERATED =   ( 0U << 2U ), /*!<  CIE: no correction interrupt generated [ Default ]            */
-    CONTROL_1_CIE_INTERRUPT_PULSES_GENERATED        =   ( 1U << 2U )  /*!<  CIE: interrupt pulses are generated at every correction cycle */
-} SX128X_control_1_cie_t;
 
-
-
-/* 12_24 <1>: SOFTWARE RESET
- *    NOTE: N/A.
- */
-typedef enum
-{
-    CONTROL_1_12_24_MASK                        =   ( 1U << 1U ),     /*!<  12_24 mask                                      */
-    CONTROL_1_12_24_24_HOUR_MODE                =   ( 0U << 1U ),     /*!<  12_24: 24 hour mode is selected     [ Default ] */
-    CONTROL_1_12_24_12_HOUR_MODE                =   ( 1U << 1U )      /*!<  12_24: 12 hour mode is selected                 */
-} SX128X_control_1_12_24_t;
-
-
-
-/* CAP_SEL <0>: INTERNAL OSCILLATOR CAPACITOR SELECTION
- *    NOTE: N/A.
- */
-typedef enum
-{
-    CONTROL_1_CAP_SEL_MASK                      =   ( 1U << 0U ),     /*!<  CAP_SEL mask                                    */
-    CONTROL_1_CAP_SEL_7_PF                      =   ( 0U << 0U ),     /*!<  CAP_SEL: 7 pF                       [ Default ] */
-    CONTROL_1_CAP_SEL_12_5_PF                   =   ( 1U << 0U )      /*!<  CAP_SEL: 12.5 pF                                */
-} SX128X_control_1_cap_sel_t;
-
-
-
-/**
-  * @brief   Register Control_2
-  */
-/* MI <5>: MINUTE INTERRUPT
- *    NOTE: N/A.
- */
-typedef enum
-{
-    CONTROL_2_MI_MASK                           =   ( 1U << 5U ),     /*!<  MI mask                                         */
-    CONTROL_2_MI_MINUTE_INTERRUPT_DISABLED      =   ( 0U << 5U ),     /*!<  Minute interrupt disabled           [ Default ] */
-    CONTROL_2_MI_MINUTE_INTERRUPT_ENABLED       =   ( 1U << 5U )      /*!<  Minute interrupt enabled                        */
-} SX128X_control_2_mi_t;
-
-
-
-/* HMI <4>: HALF MINUTE INTERRUPT
- *    NOTE: N/A.
- */
-typedef enum
-{
-    CONTROL_2_HMI_MASK                            =   ( 1U << 4U ),   /*!<  HMI mask                                        */
-    CONTROL_2_HMI_HALF_MINUTE_INTERRUPT_DISABLED  =   ( 0U << 4U ),   /*!<  Half Minute interrupt disabled      [ Default ] */
-    CONTROL_2_HMI_HALF_MINUTE_INTERRUPT_ENABLED   =   ( 1U << 4U )    /*!<  Half Minute interrupt enabled                   */
-} SX128X_control_2_hmi_t;
-
-
-
-/* TF <3>: TIMER FLAG
- *    NOTE: N/A.
- */
-typedef enum
-{
-    CONTROL_2_TF_MASK                           =   ( 1U << 3U ),     /*!<  TF mask                                         */
-    CONTROL_2_TF_TIMER_INTERRUPT_NOT_GENERATED  =   ( 0U << 3U ),     /*!<  No Timer interrupt generated        [ Default ] */
-    CONTROL_2_TF_TIMER_INTERRUPT_GENERATED      =   ( 1U << 3U )      /*!<  Timer interrupt generated                       */
-} SX128X_control_2_tf_t;
-
-
-
-/* COF <2:0>: CLKOUT CONTROL
- *    NOTE: N/A.
- */
-typedef enum
-{
-    CONTROL_2_COF_MASK                          =   ( 0b111 << 0U ),  /*!<  COF mask                                        */
-    CONTROL_2_COF_CLKOUT_32768_HZ               =   ( 0b000 << 0U ),  /*!<  CLKOUT: 32768 Hz                    [ Default ] */
-    CONTROL_2_COF_CLKOUT_16384_HZ               =   ( 0b001 << 0U ),  /*!<  CLKOUT: 16384 Hz                                */
-    CONTROL_2_COF_CLKOUT_8192_HZ                =   ( 0b010 << 0U ),  /*!<  CLKOUT: 8192  Hz                                */
-    CONTROL_2_COF_CLKOUT_4096_HZ                =   ( 0b011 << 0U ),  /*!<  CLKOUT: 4096  Hz                                */
-    CONTROL_2_COF_CLKOUT_2048_HZ                =   ( 0b100 << 0U ),  /*!<  CLKOUT: 2048  Hz                                */
-    CONTROL_2_COF_CLKOUT_1024_HZ                =   ( 0b101 << 0U ),  /*!<  CLKOUT: 1024  Hz                                */
-    CONTROL_2_COF_CLKOUT_1_HZ                   =   ( 0b110 << 0U ),  /*!<  CLKOUT: 1     Hz                                */
-    CONTROL_2_COF_CLKOUT_LOW                    =   ( 0b111 << 0U )   /*!<  CLKOUT: LOW                                     */
-} SX128X_control_2_cof_t;
-
-
-
-/**
-  * @brief   Register Offset
-  */
-/* MODE <7>: OFFSET MODE
- *    NOTE: N/A.
- */
-typedef enum
-{
-    OFFSET_MODE_MASK                            =   ( 1U << 7U ),     /*!<  MODE mask                                                   */
-    OFFSET_MODE_NORMAL_MODE                     =   ( 0U << 7U ),     /*!<  Normal mode: offset is made once every two hours[ Default ] */
-    OFFSET_MODE_COURSE_MODE                     =   ( 1U << 7U )      /*!<  Course mode: offset is made every 4 minutes                 */
-} SX128X_offset_mode_t;
-
-
-
-/**
-  * @brief   Register RAM_byte
-  */
-/* B <7:0>: RAM CONTENT
- *    NOTE: N/A.
- */
-typedef enum
-{
-    RAM_BYTE_B_MASK                             =   0xFF              /*!<  RAM_byte mask                                     */
-} SX128X_ram_byte_b_t;
-
-
-
-/**
-  * @brief   Register Seconds
-  */
-/* OS <7>: OSCILLATOR STOP
- *    NOTE: N/A.
- */
-typedef enum
-{
-    SECONDS_OS_MASK                           =   ( 1U << 7U ),     /*!<  OS mask                                                                                         */
-    SECONDS_OS_CLOCK_INTEGRITY_IS_GUARANTEED  =   ( 0U << 7U ),     /*!<  Clock integrity is guaranteed                                                                   */
-    SECONDS_OS_CLOCK_INTEGRITY_NOT_GUARANTEED =   ( 1U << 7U )      /*!<  Clock integrity is not guaranteed, oscillator has stopped or has been interrupted  [ Default ]  */
-} SX128X_seconds_os_t;
-
-
-
-/* SECONDS, TEN'S PLACE <6:4>: ACTUAL SECONDS, TEN'S PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    SECONDS_SECONDS_TEN_PLACE_MASK            =   ( 0b111 << 4U )   /*!<  SECONDS TEN'S PLACE mask                              */
-} SX128X_seconds_ten_place_t;
-
-
-
-/* SECONDS, UNIT PLACE <3:0>: ACTUAL SECONDS, UNIT PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    SECONDS_SECONDS_UNIT_PLACE_MASK           =   ( 0b1111 << 0U )  /*!<  SECONDS UNIT PLACE mask                               */
-} SX128X_seconds_unit_place_t;
-
-
-
-/**
-  * @brief   Register Minutes
-  */
-/* MINUTES, TEN'S PLACE <6:4>: ACTUAL MINUTES, TEN'S PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    MINUTES_MINUTES_TEN_PLACE_MASK            =   ( 0b111 << 4U )   /*!<  MINUTES TEN'S PLACE mask                              */
-} SX128X_minutes_ten_place_t;
-
-
-
-/* MINUTES, UNIT PLACE <3:0>: ACTUAL MINUTES, UNIT PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    MINUTES_MINUTES_UNIT_PLACE_MASK           =   ( 0b1111 << 0U )  /*!<  MINUTES UNIT PLACE mask                               */
-} SX128X_minutes_unit_place_t;
-
-
-
-/**
-  * @brief   Register HOURS
-  */
-/* AMPM <5>: AM/PM INDICATOR
- *    NOTE: ONLY FOR 12-HOUR MODE.
- */
-typedef enum
-{
-    HOURS_AMPM_MASK                           =   ( 1U << 5U ),     /*!<  AMPM mask                                             */
-    HOURS_AMPM_AM                             =   ( 0U << 5U ),     /*!<  AMPM: AM mode                                         */
-    HOURS_AMPM_PM                             =   ( 1U << 5U )      /*!<  AMPM: PM mode                                         */
-} SX128X_hours_ampm_t;
-
-
-
-/* HOURS, TEN'S PLACE <4>: ACTUAL HOURS, TEN'S PLACE
- *    NOTE: ONLY FOR 12-HOUR MODE, CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    HOURS_12_HOUR_MODE_TEN_PLACE_MASK         =   ( 1U << 4U )      /*!<  Hours TEN'S PLACE mask                                */
-} SX128X_12_hour_mode_ten_place_t;
-
-
-
-/* HOURS, TEN'S PLACE <5:4>: ACTUAL HOURS, TEN'S PLACE
- *    NOTE: ONLY FOR 24-HOUR MODE, CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    HOURS_24_HOUR_MODE_TEN_PLACE_MASK         =   ( 0b11 << 4U )    /*!<  Hours TEN'S PLACE mask                                */
-} SX128X_24_hour_mode_ten_place_t;
-
-
-
-/* HOURS, UNIT PLACE <3:0>: ACTUAL HOURS, UNIT PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    HOURS_HOURS_UNIT_PLACE_MASK               =   ( 0b1111 << 0U )  /*!<  HOURS UNIT PLACE mask                                 */
-} SX128X_hours_unit_place_t;
-
-
-
-/**
-  * @brief   Register Days
-  */
-/* DAYS, TEN'S PLACE <5:4>: ACTUAL DAYS, TEN'S PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    DAYS_DAYS_TEN_PLACE_MASK                  =   ( 0b11 << 4U )    /*!<  DAYS TEN'S PLACE mask                                 */
-} SX128X_days_ten_place_t;
-
-
-
-/* DAYS, UNIT PLACE <3:0>: ACTUAL DAYS, UNIT PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    DAYS_DAYS_UNIT_PLACE_MASK                 =   ( 0b1111 << 0U )  /*!<  DAYS UNIT PLACE mask                                  */
-} SX128X_days_unit_place_t;
-
-
-
-/**
-  * @brief   Register Weekdays
-  */
-/* WEEKDAYS <2:0>: ACTUAL WEEKDAY
- *    NOTE: N/A.
- */
-typedef enum
-{
-    WEEKDAYS_WEEKDAYS_MASK                    =   ( 0b111 << 0U ),  /*!<  WEEKDAYS mask                                          */
-    WEEKDAYS_WEEKDAYS_SUNDAY                  =   ( 0b000 << 0U ),  /*!<  WEEKDAYS Sunday                                        */
-    WEEKDAYS_WEEKDAYS_MONDAY                  =   ( 0b001 << 0U ),  /*!<  WEEKDAYS Monday                                        */
-    WEEKDAYS_WEEKDAYS_TUESDAY                 =   ( 0b010 << 0U ),  /*!<  WEEKDAYS Tuesday                                       */
-    WEEKDAYS_WEEKDAYS_WEDNESDAY               =   ( 0b011 << 0U ),  /*!<  WEEKDAYS Wednesday                                     */
-    WEEKDAYS_WEEKDAYS_THURSDAY                =   ( 0b100 << 0U ),  /*!<  WEEKDAYS Thursday                                      */
-    WEEKDAYS_WEEKDAYS_FRIDAY                  =   ( 0b101 << 0U ),  /*!<  WEEKDAYS Friday                                        */
-    WEEKDAYS_WEEKDAYS_SATURDAY                =   ( 0b111 << 0U )   /*!<  WEEKDAYS Saturday                          [ Default ] */
-} SX128X_weekdays_weekdays_t;
-
-
-
-/**
-  * @brief   Register Months
-  */
-/* MONTHS <4:0>: ACTUAL MONTH
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-   MONTHS_MONTHS_MASK                        =   ( 0b11111 << 0U ),  /*!<  MONTHS mask                                            */
-   MONTHS_MONTHS_JANUARY                     =   ( 0b00001 << 0U ),  /*!<  MONTHS January                             [ Default ] */
-   MONTHS_MONTHS_FEBRUARY                    =   ( 0b00010 << 0U ),  /*!<  MONTHS February                                        */
-   MONTHS_MONTHS_MARCH                       =   ( 0b00011 << 0U ),  /*!<  MONTHS March                                           */
-   MONTHS_MONTHS_APRIL                       =   ( 0b00100 << 0U ),  /*!<  MONTHS April                                           */
-   MONTHS_MONTHS_MAY                         =   ( 0b00101 << 0U ),  /*!<  MONTHS May                                             */
-   MONTHS_MONTHS_JUNE                        =   ( 0b00110 << 0U ),  /*!<  MONTHS June                                            */
-   MONTHS_MONTHS_JULY                        =   ( 0b00111 << 0U ),  /*!<  MONTHS July                                            */
-   MONTHS_MONTHS_AUGUST                      =   ( 0b01000 << 0U ),  /*!<  MONTHS August                                          */
-   MONTHS_MONTHS_SEPTEMBER                   =   ( 0b01001 << 0U ),  /*!<  MONTHS September                                       */
-   MONTHS_MONTHS_OCTOBER                     =   ( 0b10000 << 0U ),  /*!<  MONTHS October                                         */
-   MONTHS_MONTHS_NOVEMBER                    =   ( 0b10001 << 0U ),  /*!<  MONTHS November                                        */
-   MONTHS_MONTHS_DECEMBER                    =   ( 0b10010 << 0U )   /*!<  MONTHS December                                        */
-} SX128X_months_months_t;
-
-
-
-/**
-  * @brief   Register Years
-  */
-/* YEARS, TEN'S PLACE <7:4>: ACTUAL YEARS, TEN'S PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    YEARS_YEARS_TEN_PLACE_MASK                =   ( 0b1111 << 4U )   /*!<  YEARS TEN'S PLACE mask                                 */
-} SX128X_years_ten_place_t;
-
-
-
-/* YEARS, UNIT PLACE <3:0>: ACTUAL YEARS, UNIT PLACE
- *    NOTE: CODED IN BCD FORMAT.
- */
-typedef enum
-{
-    YEARS_YEARS_UNIT_PLACE_MASK               =   ( 0b1111 << 0U )   /*!<  YEARS UNIT PLACE mask                                  */
-} SX128X_years_unit_place_t;
 
 
 
@@ -425,17 +121,7 @@ typedef enum
 #define SX128X_VECTOR_STRUCT_H
 typedef struct
 {
-    SX128X_control_1_12_24_t    Time12H_24HMode;                    /*!<  Time mode: 12-hour or 24-hour mode                    */
-    SX128X_hours_ampm_t         TimeAM_PM_Mode;                     /*!<  AM/PM mode ( only for 12-hour mode )                  */
-
-    uint32_t                      BCDtime;                            /*!<  Time ( HHMMSS ) in BCD format                         */
-    uint8_t                       BCDday;                             /*!<  Day number in BCD format                              */
-    SX128X_weekdays_weekdays_t  weekday;                            /*!<  Weekday                                               */
-    SX128X_months_months_t      BCDmonth;                           /*!<  Month  in BCD format                                  */
-    uint8_t                       BCDyear;                            /*!<  Year in BCD format                                    */
-
-    int8_t                        ramByte;                            /*!<  RAM byte                                              */
-    SX128X_seconds_os_t         os;                                 /*!<  Oscillator flag                                       */
+    uint8_t status;             /*!<  Status            */
 } SX128X_data_t;
 #endif
 
@@ -457,13 +143,13 @@ typedef enum
 /**
   * @brief   FUNCTION PROTOTYPES
   */
-/** It configures the I2C peripheral.
+/** It configures the SPI peripheral.
   */
-SX128X_status_t  SX128X_Init                              ( SPI_parameters_t mySPI_parameters                                                                );
+SX128X_status_t  SX128X_Init    ( SPI_parameters_t mySPI_parameters                             );
 
-/** It sets the external clock test mode.
+/** It gets the transceiver status.
   */
-// SX128X_status_t  SetTx                                   ( SPI_parameters_t mySPI_parameters, periodBase, periodBaseCount );
+SX128X_status_t  GetStatus      ( SPI_parameters_t mySPI_parameters, SX128X_data_t* myStatus    );
 
 /** It sets the RTC clock mode.
   */
