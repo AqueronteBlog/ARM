5d6d8d302e82873350a9adbd76269030da12fc05
Manuel Caballero <aqueronteblog@gmail.com>
2019-02-13 21:00:50 +0000

    DRIVER. SX1280. Function file is still in progress. Several functions were defined.

--- Drivers/SX128X/SX128X.c
+++ Drivers/SX128X/SX128X.c
@@ -245,7 +245,7 @@ SX128X_status_t SetFs ( SPI_parameters_t mySPI_parameters )
  *                  Time-out duration = periodBase * periodBaseCount
  *
  * @param[in]    mySPI_parameters:  SPI parameters.
- * @param[in]    myTimeoutStep:     periodBase.
+ * @param[in]    myPeriodBase:      periodBase.
  * @param[in]    myperiodBaseCount: periodBaseCount.
  *
  * @param[out]   N/A.
@@ -264,14 +264,14 @@ SX128X_status_t SetFs ( SPI_parameters_t mySPI_parameters )
  *              disabled to allow complete reception of the packet.
  * @warning     Clear IRQ status before using this command.
  */
-SX128X_status_t SetTx ( SPI_parameters_t mySPI_parameters, SX128X_set_tx_rx_timeout_definition_t myTimeoutStep, uint16_t myperiodBaseCount )
+SX128X_status_t SetTx ( SPI_parameters_t mySPI_parameters, SX128X_set_tx_rx_timeout_definition_t myPeriodBase, uint16_t myperiodBaseCount )
 {
     uint8_t      cmd[4]  =   { 0U };
     spi_status_t aux;
 
     /* Send command  */
     cmd[0]   =   SX128X_SET_TX;
-    cmd[1]   =   myTimeoutStep;
+    cmd[1]   =   myPeriodBase;
     cmd[2]   =   (uint8_t)( ( myperiodBaseCount & 0xFF00 ) >> 8U );
     cmd[3]   =   (uint8_t)( myperiodBaseCount & 0x00FF );
     aux      =   spi_transfer ( mySPI_parameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), &cmd[0], 0U );
@@ -299,7 +299,7 @@ SX128X_status_t SetTx ( SPI_parameters_t mySPI_parameters, SX128X_set_tx_rx_time
  *                  Time-out duration = periodBase * periodBaseCount
  *
  * @param[in]    mySPI_parameters:  SPI parameters.
- * @param[in]    myTimeoutStep:     periodBase.
+ * @param[in]    myPeriodBase:      periodBase.
  * @param[in]    myperiodBaseCount: periodBaseCount.
  *
  * @param[out]   N/A.
@@ -321,16 +321,383 @@ SX128X_status_t SetTx ( SPI_parameters_t mySPI_parameters, SX128X_set_tx_rx_time
  *              disabled to allow complete reception of the packet.
  * @warning     The IRQ status should be cleared prior to using this command.
  */
-SX128X_status_t SetRx ( SPI_parameters_t mySPI_parameters, SX128X_set_tx_rx_timeout_definition_t myTimeoutStep, uint16_t myperiodBaseCount )
+SX128X_status_t SetRx ( SPI_parameters_t mySPI_parameters, SX128X_set_tx_rx_timeout_definition_t myPeriodBase, uint16_t myperiodBaseCount )
 {
     uint8_t      cmd[4]  =   { 0U };
     spi_status_t aux;
 
     /* Send command  */
     cmd[0]   =   SX128X_SET_RX;
-    cmd[1]   =   myTimeoutStep;
+    cmd[1]   =   myPeriodBase;
     cmd[2]   =   (uint8_t)( ( myperiodBaseCount & 0xFF00 ) >> 8U );
     cmd[3]   =   (uint8_t)( myperiodBaseCount & 0x00FF );
+    aux      =   spi_transfer ( mySPI_parameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), &cmd[0], 0U );
+
+
+
+
+    if ( aux == SPI_SUCCESS )
+    {
+        return   SX128X_SUCCESS;
+    }
+    else
+    {
+        return   SX128X_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SetRxDutyCycle ( SPI_parameters_t , SX128X_set_tx_rx_timeout_definition_t, uint16_t , uint16_t )
+ *
+ * @details     It sets the transceiver in sniff mode, so that it regularly looks for new packets ( duty cycled operation ).
+ *
+ *                  The Sleep mode duration is defined by:
+ *
+ *                      - Sleep Duration = PeriodBase * sleepPeriodBaseCount
+ *
+ *                  The Rx mode duration is defined by:
+ *
+ *                      - Rx Duration = PeriodBase * rxPeriodBaseCount
+ *
+ * @param[in]    mySPI_parameters:          SPI parameters.
+ * @param[in]    myPeriodBase:              periodBase.
+ * @param[in]    myRxPeriodBaseCount:       rxPeriodBaseCount.
+ * @param[in]    mySleepPeriodBaseCount:    sleepPeriodBaseCount.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SetRxDutyCycle.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        13/February/2019
+ * @version     13/February/2019   The ORIGIN
+ * @pre         If rxPeriodBaseCount = 0x0000, The transceiver waits until a packet is found. Once found, the transceiver goes to STDBY_RC mode
+ *              after sending an RxDone IRQ to the host.
+ * @pre         If rxPeriodBaseCount = Others, The device will stay in Rx Mode until the end of the timer when the device returns in Sleep mode for
+ *              Sleep duration.
+ * @pre         If myperiodBaseCount = Others, Time-out active. The device remains in Rx mode, it returns automatically to STDBY_RC mode
+ *              on timer end-of-count or when a packet has been received. As soon as a packet is detected, the timer is automatically
+ *              disabled to allow complete reception of the packet.
+ * @warning     The command SetLongPreamble must be issued prior to SetRxDutyCycle.
+ * @warning     To use the RxDone interrupt, you have to enable the corresponding IRQ prior to enter Duty cycled operation. To enable the RxDone IRQ,
+ *              refer to the command SetDioIrqParams().
+ */
+SX128X_status_t SetRxDutyCycle ( SPI_parameters_t mySPI_parameters, SX128X_set_tx_rx_timeout_definition_t myPeriodBase, uint16_t myRxPeriodBaseCount, uint16_t mySleepPeriodBaseCount )
+{
+    uint8_t      cmd[6]  =   { 0U };
+    spi_status_t aux;
+
+    /* Send command  */
+    cmd[0]   =   SX128X_SET_RX_DUTY_CYCLE;
+    cmd[1]   =   myPeriodBase;
+    cmd[2]   =   (uint8_t)( ( myRxPeriodBaseCount & 0xFF00 ) >> 8U );
+    cmd[3]   =   (uint8_t)( myRxPeriodBaseCount & 0x00FF );
+    cmd[4]   =   (uint8_t)( ( mySleepPeriodBaseCount & 0xFF00 ) >> 8U );
+    cmd[5]   =   (uint8_t)( mySleepPeriodBaseCount & 0x00FF );
+    aux      =   spi_transfer ( mySPI_parameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), &cmd[0], 0U );
+
+
+
+
+    if ( aux == SPI_SUCCESS )
+    {
+        return   SX128X_SUCCESS;
+    }
+    else
+    {
+        return   SX128X_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SetLongPreamble ( SPI_parameters_t , SX128X_set_long_preamble_t )
+ *
+ * @details     It sets the transceiver into Long Preamble mode, and can only be used with either the LoRa mode and GFSK mode.
+ *
+ *               - In GFSK only, the SetTx arguments do not define a timeout anymore, but the duration of the preamble part of GFSK
+ *                 packet. Therefore, there is no TxTimeout interrupt generated in GFSK mode. In LoRa, SetTx behavior is not changed.
+ *
+ *               - In GFSK only with LongPreamble mode, the preamble detection mode is activated. The command SetRx can then
+ *                 generate an interrupt for Preamble detection.
+ *
+ *               - In GFSK and LoRa, the behavior of RxDutyCycle is modified so that if a preamble is detected, the Rx window is
+ *                 extended by SleepPeriod + 2 * RxPeriod.
+ *
+ *
+ * @param[in]    mySPI_parameters:        SPI parameters.
+ * @param[in]    myLongPreambleStatus:    Enable/Disable.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SetLongPreamble.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        13/February/2019
+ * @version     13/February/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SX128X_status_t SetLongPreamble ( SPI_parameters_t mySPI_parameters, SX128X_set_long_preamble_t myLongPreambleStatus )
+{
+    uint8_t      cmd[2]  =   { 0U };
+    spi_status_t aux;
+
+    /* Send command  */
+    cmd[0]   =   SX128X_SET_LONG_PREAMBLE;
+    cmd[1]   =   myLongPreambleStatus;
+    aux      =   spi_transfer ( mySPI_parameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), &cmd[0], 0U );
+
+
+
+
+    if ( aux == SPI_SUCCESS )
+    {
+        return   SX128X_SUCCESS;
+    }
+    else
+    {
+        return   SX128X_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SetCAD ( SPI_parameters_t )
+ *
+ * @details     ( Channel Activity Detection ) can be used only in LoRa packet type. The Channel Activity Detection is a LoRa specific mode of operation
+ *              where the device searches for a LoRa signal. After search has completed, the device returns to STDBY_RC mode. The length of the search is
+ *              configured via SetCadParams() command. At the end of search period the device always sends the CadDone IRQ. If a valid signal has been
+ *              detected it also generates the CadDetected IRQ.
+ *
+ *              This mode of operation is especially useful in all the applications requiring Listen before Talk.
+ *
+ *
+ * @param[in]    mySPI_parameters:        SPI parameters.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SetCAD.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        13/February/2019
+ * @version     13/February/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SX128X_status_t SetCAD ( SPI_parameters_t mySPI_parameters )
+{
+    uint8_t      cmd  =   0U;
+    spi_status_t aux;
+
+    /* Send command  */
+    cmd   =   SX128X_SET_CAD;
+    aux   =   spi_transfer ( mySPI_parameters, &cmd, 1U, &cmd, 0U );
+
+
+
+
+    if ( aux == SPI_SUCCESS )
+    {
+        return   SX128X_SUCCESS;
+    }
+    else
+    {
+        return   SX128X_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SetTxContinuousWave ( SPI_parameters_t )
+ *
+ * @details     It is a test command to generate a Continuous Wave (RF tone) at a selected frequency and output power.
+ *              The device remains in Tx Continuous Wave until the host sends a mode configuration command. This
+ *              command is available for all packet types.
+ *
+ *
+ * @param[in]    mySPI_parameters:  SPI parameters.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SetTxContinuousWave.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        13/February/2019
+ * @version     13/February/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SX128X_status_t SetTxContinuousWave ( SPI_parameters_t mySPI_parameters )
+{
+    uint8_t      cmd  =   0U;
+    spi_status_t aux;
+
+    /* Send command  */
+    cmd   =   SX128X_SET_TX_CONTINUOUS_WAVE;
+    aux   =   spi_transfer ( mySPI_parameters, &cmd, 1U, &cmd, 0U );
+
+
+
+
+    if ( aux == SPI_SUCCESS )
+    {
+        return   SX128X_SUCCESS;
+    }
+    else
+    {
+        return   SX128X_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SetTxContinuousPreamble ( SPI_parameters_t )
+ *
+ * @details     It is a test command to generate an infinite sequence of alternating '0's and '1's in GFSK,
+ *              BLE, or FLRC modulation and symbol 0 in LoRa. The device remains in Tx Continuous Wave until
+ *              the host sends a mode configuration command
+ *
+ *
+ * @param[in]    mySPI_parameters:  SPI parameters.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SetTxContinuousPreamble.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        13/February/2019
+ * @version     13/February/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SX128X_status_t SetTxContinuousPreamble ( SPI_parameters_t mySPI_parameters )
+{
+    uint8_t      cmd  =   0U;
+    spi_status_t aux;
+
+    /* Send command  */
+    cmd   =   SX128X_SET_TX_CONTINUOUS_PREAMBLE;
+    aux   =   spi_transfer ( mySPI_parameters, &cmd, 1U, &cmd, 0U );
+
+
+
+
+    if ( aux == SPI_SUCCESS )
+    {
+        return   SX128X_SUCCESS;
+    }
+    else
+    {
+        return   SX128X_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SetAutoTx ( SPI_parameters_t , uint16_t )
+ *
+ * @details     BLE requires the transceiver to be able to send back a response 150us after a packet reception.
+ *              This is carried out by sending the command SetAutoTx() which allows the transceiver to send a packet
+ *              at a user programmable time ( time ) after the end of a packet reception. SetAutoTx() must be issued in STDBY_RC mode.
+ *
+ *              time is expressed in us. The delay between the packet reception end and the next packet transmission start is defined by:
+ *
+ *                  - Tx_Delay =  time + Offset
+ *
+ *              Here Offset is a time needed for the transceiver to switch modes and is equal to 33us. When this command is issued, each
+ *              time the transceiver goes in Rx mode, it automatically switches to Tx and sends a packet in a predefined time TxDelay. To
+ *              resume STDBY_RC after Rx, the command SetAutoTx is issued with 0x00 as time argument.
+ *
+ *
+ * @param[in]    mySPI_parameters:  SPI parameters.
+ * @param[in]    myTime:            Time.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SetAutoTx.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        13/February/2019
+ * @version     13/February/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SX128X_status_t SetAutoTx ( SPI_parameters_t mySPI_parameters, uint16_t myTime )
+{
+    uint8_t      cmd[3]  =   { 0U };
+    spi_status_t aux;
+
+    /* Send command  */
+    cmd[0]   =   SX128X_SET_AUTO_TX;
+    cmd[2]   =   (uint8_t)( ( myTime & 0xFF00 ) >> 8U );
+    cmd[3]   =   (uint8_t)( myTime & 0x00FF );
+    aux      =   spi_transfer ( mySPI_parameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), &cmd[0], 0U );
+
+
+
+
+    if ( aux == SPI_SUCCESS )
+    {
+        return   SX128X_SUCCESS;
+    }
+    else
+    {
+        return   SX128X_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       SetAutoFs ( SPI_parameters_t , SX128X_set_auto_fs_t )
+ *
+ * @details     This feature modifies the chip behavior so that the state following a Rx or Tx operation is FS and not STDBY.
+ *              This feature is to be used to reduce the switching time between consecutive Rx and/or Tx operations.
+ *
+ *
+ * @param[in]    mySPI_parameters:  SPI parameters.
+ * @param[in]    myAutoFsStatus:    Time.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of SetAutoFs.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        13/February/2019
+ * @version     13/February/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+SX128X_status_t SetAutoFs ( SPI_parameters_t mySPI_parameters, SX128X_set_auto_fs_t myAutoFsStatus )
+{
+    uint8_t      cmd[2]  =   { 0U };
+    spi_status_t aux;
+
+    /* Send command  */
+    cmd[0]   =   SX128X_SET_AUTO_FS;
+    cmd[2]   =   myAutoFsStatus;
     aux      =   spi_transfer ( mySPI_parameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), &cmd[0], 0U );
 
 
