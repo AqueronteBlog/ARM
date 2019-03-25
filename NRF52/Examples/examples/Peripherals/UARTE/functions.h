4a82e9c2bde2123999a028ff6bfd86f2a4d296c8
Manuel Caballero <aqueronteblog@gmail.com>
2019-03-25 20:39:09 +0000

    NRF52 UARTE Example. Functions files were added

---
+++ NRF52/Examples/examples/Peripherals/UARTE/functions.h
@@ -0,0 +1,35 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        25/March/2019
+ * @version     25/March/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "nrf.h"
+#include "nrf52_bitfields.h"
+#include "board.h"
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* FUNCTION PROTOTYPES */
+void conf_GPIO    ( void );
+void conf_UART0   ( void );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTION_H_ */
