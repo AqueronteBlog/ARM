84da8f963cf52497fa5ebc7d2411b15130cf2275
Manuel Caballero <aqueronteblog@gmail.com>
2019-05-26 21:41:32 +0100

    NRF52 HTS221 Example. Testing the driver.

--- NRF52/Examples/examples/Drivers/HTS221/main.c
+++ NRF52/Examples/examples/Drivers/HTS221/main.c
@@ -50,9 +50,8 @@ int main(void)
   uint8_t  myMessage[ TX_BUFF_SIZE ];
 
   I2C_parameters_t     myHTS221_I2C_parameters;
-//  HTS221_status_t      aux;
-//  HTS221_config_reg_t  myHTS221_Config;
-//  HTS221_data_t        myHTS221_Data;
+  HTS221_status_t      aux;
+  HTS221_data_t        myHTS221_Data;
 
 
   conf_CLK    ();
@@ -63,52 +62,36 @@ int main(void)
   
 
   /* I2C definition   */
-//  myHTS221_I2C_parameters.TWIinstance =    NRF_TWI0;
-//  myHTS221_I2C_parameters.SDA         =    TWI0_SDA;
-//  myHTS221_I2C_parameters.SCL         =    TWI0_SCL;
-//  myHTS221_I2C_parameters.ADDR        =    HTS221_ADDRESS_0;
-//  myHTS221_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
-//  myHTS221_I2C_parameters.SDAport     =    NRF_P0;
-//  myHTS221_I2C_parameters.SCLport     =    NRF_P0;
-//
-//  /* Configure I2C peripheral  */
-//  aux  =   HTS221_Init  ( myHTS221_I2C_parameters );
-//
-//  /* Shutdown the device, low-power mode enabled  */
-//  aux  =   HTS221_GetCONFIG ( myHTS221_I2C_parameters, &myHTS221_Config );
-//  
-//  myHTS221_Config.shdn  =   CONFIG_SHDN_SHUTDOWN;
-//  aux  =   HTS221_SetCONFIG ( myHTS221_I2C_parameters, myHTS221_Config );
-//  
-//  /* Get manufacturer ID  */
-//  aux  =   HTS221_GetManufacturerID ( myHTS221_I2C_parameters, &myHTS221_Data );
-//
-//  /* Get device ID and device revision  */
-//  aux  =   HTS221_GetDeviceID ( myHTS221_I2C_parameters, &myHTS221_Data );
-//  
-//  /* Configure the device
-//   *  - T_UPPER and T_LOWER limit hysteresis at 0C
-//   *  - Continuous conversion mode
-//   *  - T_CRIT unlocked
-//   *  - Window lock unlocked
-//   *  - Alert output control disabled
-//   *  - Alert output select: Alert for T_UPPER, T_LOWER and T_CRIT
-//   *  - Alert output polaruty: Active-low
-//   *  - Alert output mode: Comparator output
-//   */
-//  myHTS221_Config.t_hyst      =   CONFIG_T_HYST_0_C;
-//  myHTS221_Config.shdn        =   CONFIG_SHDN_CONTINUOUS_CONVERSION;
-//  myHTS221_Config.t_crit      =   CONFIG_CRIT_LOCK_UNLOCKED;
-//  myHTS221_Config.t_win_lock  =   CONFIG_WIN_LOCK_UNLOCKED;
-//  myHTS221_Config.alert_cnt   =   CONFIG_ALERT_CNT_DISABLED;
-//  myHTS221_Config.alert_sel   =   CONFIG_ALERT_SEL_TUPPER_TLOWER_TCRIT;
-//  myHTS221_Config.alert_pol   =   CONFIG_ALERT_POL_ACTIVE_LOW;
-//  myHTS221_Config.alert_mod   =   CONFIG_ALERT_MOD_COMPARATOR_OUTPUT;
-//  aux  =   HTS221_SetCONFIG ( myHTS221_I2C_parameters, myHTS221_Config );
-//
-//  /* Set resolution: +0.0625C ( t_CON ~ 250ms )  */
-//  myHTS221_Data.resolution  =   RESOLUTION_0_0625_C;
-//  aux  =   HTS221_SetResolution ( myHTS221_I2C_parameters, myHTS221_Data );
+  myHTS221_I2C_parameters.TWIinstance =    NRF_TWI0;
+  myHTS221_I2C_parameters.SDA         =    TWI0_SDA;
+  myHTS221_I2C_parameters.SCL         =    TWI0_SCL;
+  myHTS221_I2C_parameters.ADDR        =    HTS221_ADDRESS;
+  myHTS221_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+  myHTS221_I2C_parameters.SDAport     =    NRF_P0;
+  myHTS221_I2C_parameters.SCLport     =    NRF_P0;
+
+  /* Configure I2C peripheral  */
+  aux  =   HTS221_Init  ( myHTS221_I2C_parameters );
+
+  /* Get device ID  */
+  aux  =   HTS221_GetDeviceID ( myHTS221_I2C_parameters, &myHTS221_Data );
+
+  /* Set device in ACTIVE mode  */
+  aux  =   HTS221_SetPowerDown ( myHTS221_I2C_parameters, CTRL_REG1_PD_ACTIVE_MODE );
+
+  /* Get calibration coefficients  */
+  aux  =   HTS221_GetCalibrationCoefficients ( myHTS221_I2C_parameters, &myHTS221_Data );
+  
+  /* Set resolution: 16 AVGT, 32 AVGH  */
+  myHTS221_Data.temperatureResolution  =   AV_CONF_AVGT_16;
+  myHTS221_Data.humidityResolution     =   AV_CONF_AVGH_32;
+  aux  =   HTS221_SetResolution ( myHTS221_I2C_parameters, myHTS221_Data );
+  
+  /* ODR  */
+  myHTS221_Data.odr  =   CTRL_REG1_ODR_ONE_SHOT;
+  aux  =   HTS221_SetOutputDataRate ( myHTS221_I2C_parameters, myHTS221_Data );
+
+  
   
   
     
@@ -131,9 +114,23 @@ int main(void)
     {
       NRF_P0->OUTCLR  |= ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );          // Turn all the LEDs on
 
-//      /* Get ambient temperature  */
-//      aux  =   HTS221_GetTA ( myHTS221_I2C_parameters, &myHTS221_Data );
-//
+      /* Trigger to get a new data value  */
+      aux  =   HTS221_SetOneShot ( myHTS221_I2C_parameters );
+
+      /* Wait until there is a new data  */
+      do{
+        aux  =   HTS221_GetOneShot ( myHTS221_I2C_parameters, &myHTS221_Data );
+      }while( myHTS221_Data.one_shot == CTRL_REG2_ONE_SHOT_WAITING );
+
+      
+      aux  =   HTS221_GetCalibrationCoefficients ( myHTS221_I2C_parameters, &myHTS221_Data );
+
+      /* Get temperature  */
+      aux  =   HTS221_GetTemperature ( myHTS221_I2C_parameters, &myHTS221_Data );
+
+      /* Get humidity  */
+      aux  =   HTS221_GetHumidity ( myHTS221_I2C_parameters, &myHTS221_Data );
+
 //      /* Transmit result through the UART  */
 //      sprintf ( (char*)myMessage, "T: %d.%d C\r\n", (int32_t)myHTS221_Data.t_a, (int32_t)( ( myHTS221_Data.t_a - (uint32_t)myHTS221_Data.t_a ) * 10000.0f ) );
 //
