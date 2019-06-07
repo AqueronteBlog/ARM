8865ada159187cdbfb92c49f9fc7c396d6ef385d
Manuel Caballero <aqueronteblog@gmail.com>
2019-06-07 20:34:17 +0100

    ADuCM3029. GPIO Output Example. Functions files were added

---
+++ ADuCM3029/Examples/GPIO_LEDs/src/functions.c
@@ -0,0 +1,65 @@
+/**
+ * @brief       functions.c
+ * @details     Functions.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        07/June/2019
+ * @version     07/June/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "functions.h"
+
+
+
+/**
+ * @brief       void conf_CLK  ( void )
+ * @details     It activates the external HFCLK crystal oscillator ( 64MHz ).
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        07/June/2019
+ * @version     07/June/2019      The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void conf_CLK  ( void )
+{
+  
+}
+
+
+
+/**
+ * @brief       void conf_GPIO  ( void )
+ * @details     It configures GPIO to work with the LEDs.
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        07/June/2019
+ * @version     07/June/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void conf_GPIO  ( void )
+{
+  uint32_t i = 0UL;
+  
+  
+}
