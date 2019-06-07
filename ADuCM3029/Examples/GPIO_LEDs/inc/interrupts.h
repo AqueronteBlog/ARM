175764a0fe19a814f72e7f85f5131c2e840feac5
Manuel Caballero <aqueronteblog@gmail.com>
2019-06-07 20:34:37 +0100

    ADuCM3029. GPIO Output Example. Interrupts files were added

---
+++ ADuCM3029/Examples/GPIO_LEDs/inc/interrupts.h
@@ -0,0 +1,50 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        07/June/2019
+ * @version     07/June/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "board.h"
+#include <sys/platform.h>
+#include "adi_initialize.h"
+#include <sys/ADuCM302x_device.h>
+
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "board.h"
+
+
+/**@brief Subroutine prototypes.
+ */
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INTERRUPTS_H_ */
---
+++ ADuCM3029/Examples/GPIO_LEDs/src/interrupts.c
@@ -0,0 +1,15 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        07/June/2019
+ * @version     07/June/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
