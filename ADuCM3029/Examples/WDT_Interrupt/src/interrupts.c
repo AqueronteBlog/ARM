5a690b9b1128f891363ce6787646dd33fccc7c70
Manuel Caballero <aqueronteblog@gmail.com>
2019-06-13 20:07:01 +0100

    ADuCM3029. WDT Interrupt Example. Interrupts files were added

---
+++ ADuCM3029/Examples/WDT_Interrupt/src/interrupts.c
@@ -0,0 +1,15 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        13/June/2019
+ * @version     13/June/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
