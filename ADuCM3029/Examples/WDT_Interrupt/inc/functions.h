d817bd51fb09707dae616182db5ec6c4b89cc74f
Manuel Caballero <aqueronteblog@gmail.com>
2019-06-13 20:06:48 +0100

    ADuCM3029. WDT Interrupt Example. Functions files were added

---
+++ ADuCM3029/Examples/WDT_Interrupt/inc/functions.h
@@ -0,0 +1,49 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        13/June/2019
+ * @version     13/June/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "board.h"
+#include <sys/platform.h>
+#include "adi_initialize.h"
+#include <sys/ADuCM302x_device.h>
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/**@brief Function prototypes.
+ */
+void conf_CLK    ( void );
+void conf_GPIO   ( void );
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTION_H_ */
