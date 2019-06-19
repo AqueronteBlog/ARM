653086fb9f47a659facaf7e43f40ebb1da2de853
Manuel Caballero <aqueronteblog@gmail.com>
2019-06-19 20:02:34 +0100

    ADuCM3029. Timers Interrupt Example. This project has just started, it is still in progress

---
+++ ADuCM3029/Examples/Timers/src/main.c
@@ -0,0 +1,91 @@
+/**
+ * @brief       main.c
+ * @details     [todo]This example shows how to work with the internal peripheral: WDT on interrupt configuration.
+ * 				Both LEDs blink every 1 second.
+ *
+ * 				The rest of the time, the microcontroller is in low-power mode.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        19/June/2019
+ * @version     19/June/2019    The ORIGIN
+ * @pre         This firmware was tested on the ADICUP3029 with CrossCore Embedded Studio v2.8.3.0.
+ * @warning     N/A.
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ). All rights reserved.
+ */
+
+#include <sys/platform.h>
+#include "adi_initialize.h"
+#include "board.h"
+#include "functions.h"
+#include "interrupts.h"
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+volatile uint32_t myState	 =	 0UL;		/*!<   State that indicates when to perform the next action     */
+
+
+
+
+/**@brief Function for application main entry.
+ */
+int main(int argc, char *argv[])
+{
+	/**
+	 * Initialize managed drivers and/or services that have been added to 
+	 * the project.
+	 * @return zero on success 
+	 */
+	adi_initComponents();
+	
+	/* Begin adding your custom code here */
+	SystemInit ();
+
+	conf_CLK  ();
+	conf_GPIO ();
+	conf_WDT  ();
+
+	/* Enable interrupts	 */
+	__enable_irq ();
+
+
+	while ( 1 )
+	{
+		/* Low power mode: Flexi Mode	 */
+		pADI_PMG0->PWRKEY	 =	 0x4859;
+		pADI_PMG0->PWRMOD	&=	~( 0b11 << BITP_PMG_PWRMOD_MODE );
+		pADI_PMG0->PWRKEY	 =	 0x0000;
+		__WFI();
+
+
+		/* Check new action	 */
+		if ( myState == 1UL )
+		{
+			/* Disable interrupts	 */
+			__disable_irq ();
+
+			/* Blink LEDs	 */
+			pADI_GPIO2->TGL	|=	 DS3;
+			pADI_GPIO1->TGL	|=	 DS4;
+
+
+			/* Reset variables	 */
+			myState	 =	 0UL;
+
+			/* Enable interrupts	 */
+			__enable_irq ();
+		}
+	}
+
+	/* It should never reach here	 */
+	return 0;
+}
+
