44f9740eb77f11803ef12eb55ebe6d7224773dc2
Manuel Caballero <aqueronteblog@gmail.com>
2019-07-05 19:49:16 +0100

    ADuCM3029. WDT Watchdog Example. Interrupts files were added.

---
+++ ADuCM3029/Examples/WDT_Watchdog/src/interrupts.c
@@ -0,0 +1,46 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        05/July/2019
+ * @version     05/July/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
+
+/**
+ * @brief       void WDog_Tmr_Int_Handler  ( void )
+ * @details     WDT subroutine.
+ *
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        05/July/2019
+ * @version     05/July/2019    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void WDog_Tmr_Int_Handler ( void )
+{
+	/* Check if WDT interrupt pending	 */
+	if ( ( pADI_WDT0->STAT & ( 1U << BITP_WDT_STAT_IRQ ) ) == ( 1U << BITP_WDT_STAT_IRQ ) )
+	{
+		/* New action	 */
+		myState	 =	 1UL;
+
+		/* Clear IRQ	 */
+		pADI_WDT0->RESTART	 =	 0xCCCC;
+	}
+}
