48e821c1bc4946d1bb1c881f5b61af1c838118ed
Manuel Caballero <aqueronteblog@gmail.com>
2019-07-09 20:39:21 +0100

    ADuCM3029. UART Example. Board file was updated with the pins for the UART

--- ADuCM3029/Examples/UART/inc/board.h
+++ ADuCM3029/Examples/UART/inc/board.h
@@ -7,7 +7,9 @@
  *
  * @author      Manuel Caballero
  * @date        07/June/2019
- * @version     07/June/2019   The ORIGIN
+ * @version     09/July/2019   UART was added
+ * 				04/July/2019   Switches were added
+ * 				07/June/2019   The ORIGIN
  * @pre         N/A
  * @warning     N/A
  */
@@ -31,6 +33,21 @@ typedef enum{
   DS4  = (uint16_t)( 1U << 15U )	 	/*!<   LED2: P1_15   */
 } ADICUP3029_board_leds_t;
 
+
+/**@brief SWITCHES.
+ */
+typedef enum{
+  S4  = (uint16_t)( 1U << 2U ),			/*!<   WIFI_RST<-->GPIO34_WIFI: P2_02   */
+  S3  = (uint16_t)( 1U << 15U )	 		/*!<   S3: P1_01   						*/
+} ADICUP3029_board_switches_t;
+
+
+/**@brief UART.
+ */
+typedef enum{
+  UART_TX  = (uint16_t)( 1U << 10U ),	/*!<   UART_TX: P0_10   				*/
+  UART_RX  = (uint16_t)( 1U << 11U )	/*!<   UART_RX: P0_11   				*/
+} ADICUP3029_board_uart_t;
 
 
 #ifdef __cplusplus
