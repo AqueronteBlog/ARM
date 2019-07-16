01e810c79d53963a0a058919641b4e57fb902532
Manuel Caballero <aqueronteblog@gmail.com>
2019-07-16 13:36:33 +0100

    NRF52 BMA456 Example. This project was completed and tested, it works as expected.

--- NRF52/Examples/examples/Drivers/BMA456/main.c
+++ NRF52/Examples/examples/Drivers/BMA456/main.c
@@ -1,7 +1,9 @@
 /**
  * @brief       main.c
- * @details     [todo]This example shows how to work with the external device: BMA456. Every 1 seconds, a new
- *              pressure/temperature value is read and the data is transmitted through the UART ( Baud Rate: 230400 ).
+ * @details     This example shows how to work with the external device: BMA456. Every 1 seconds, a new
+ *              pressure/temperature/wake-up event value is read and the data is transmitted through the UART ( Baud Rate: 230400 ).
+ *
+ *              The wake-up event is triggered by a double tap.
  *
  *              The microcontroller is in low power the rest of the time.
  *
@@ -24,21 +26,22 @@
 #include "functions.h"
 #include "interrupts.h"
 #if defined (WORK_FROM_HOME)
-#include "D:/Workspace/ARM/Drivers/BMA456/BMA456.h" 
+#include "D:/Workspace/ARM/Drivers/BMA456/bma456_port.h" 
 #else
-#include "D:/vsProjects/ARM/Drivers/BMA456/BMA456.h" 
+#include "D:/vsProjects/ARM/Drivers/BMA456/bma456_port.h" 
 #endif
 
 
 /**@brief Constants.
  */
-#define TX_BUFF_SIZE  64                          /*!<   UART buffer size                                       */
+#define TX_BUFF_SIZE  64                              /*!<   UART buffer size                     */
 
 
 /**@brief Variables.
  */
-volatile uint32_t myState;                        /*!<   State that indicates when to perform an ADC sample     */
-volatile uint8_t  *myPtr;                         /*!<   Pointer to point out myMessage                         */
+volatile uint32_t           myState;                  /*!<   State that indicates next action     */
+volatile uint8_t            *myPtr;                   /*!<   Pointer to point out myMessage       */
+volatile I2C_parameters_t   myBMA456_I2C_parameters;  /*!<   I2C configuration                    */
 
 
 
@@ -47,90 +50,89 @@ volatile uint8_t  *myPtr;                         /*!<   Pointer to point out my
  */
 int main(void)
 {
-  uint8_t  myMessage[ TX_BUFF_SIZE ];
-
-  I2C_parameters_t    myBMA456_I2C_parameters;
-  BMA456_status_t     aux;
-  BMA456_data_t       myBMA456_Data;
+  uint8_t           myMessage[ TX_BUFF_SIZE ];
+  uint8_t           myWakeUpInfo[]  = { "OFF" };
+  uint16_t          int_status      = 0U;
+  struct bma4_accel sens_data;
+  struct bma4_dev   dev;
+  uint16_t          rslt            = BMA4_OK;
+  uint8_t           init_seq_status = 0U;
+  BMA456_status_t   aux;
+  int16_t           myRange         = 0;
 
 
   conf_CLK    ();
   conf_GPIO   ();
   conf_UART   ();
   conf_TIMER0 ();
-
   
   /* I2C definition   */
   myBMA456_I2C_parameters.TWIinstance =    NRF_TWI0;
   myBMA456_I2C_parameters.SDA         =    TWI0_SDA;
   myBMA456_I2C_parameters.SCL         =    TWI0_SCL;
-  myBMA456_I2C_parameters.ADDR        =    BMA456_ADDRESS_SDO_GND;
+  myBMA456_I2C_parameters.ADDR        =    BMA4_I2C_ADDR_PRIMARY;
   myBMA456_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
   myBMA456_I2C_parameters.SDAport     =    NRF_P0;
   myBMA456_I2C_parameters.SCLport     =    NRF_P0;
 
   /* Configure I2C peripheral  */
-  aux  =   BMA456_Init  ( myBMA456_I2C_parameters );
+  aux  =   BMA456_Init_I2C  ( myBMA456_I2C_parameters );
 
-  /* Check that the device is not in Fatal error state ( Boot, powersystem )   */
-  do{
-    aux  =   BMA456_GetSensorErrorConditions ( myBMA456_I2C_parameters, &myBMA456_Data );   
-  }while( myBMA456_Data.fatal_err == ERR_REG_FATAL_ERR_ERROR );                               // Dangerous!!! The uC may get stuck here...
-                                                                                              // [WORKAROUND] Insert a counter
-  /* Trigger a softreset  */
-  myBMA456_Data.cmd  =   CMD_CMD_SOFTRESET;
-  aux  =   BMA456_SetCMD  ( myBMA456_I2C_parameters, myBMA456_Data );
-
-  /* Check that the device is not in Fatal error state ( Boot, powersystem )   */
-  do{
-    aux  =   BMA456_GetSensorErrorConditions ( myBMA456_I2C_parameters, &myBMA456_Data );   
-  }while( myBMA456_Data.fatal_err == ERR_REG_FATAL_ERR_ERROR );                               // Dangerous!!! The uC may get stuck here...
-                                                                                              // [WORKAROUND] Insert a counter                                                                                    
+  /* Modify the parameters */
+  dev.dev_addr        = BMA4_I2C_ADDR_PRIMARY;
+  dev.interface       = BMA4_I2C_INTERFACE;
+  dev.bus_read        = user_i2c_read;
+  dev.bus_write       = user_i2c_write;
+  dev.delay           = user_delay_ms;
+  dev.read_write_len  = 8;
+  dev.resolution      = 16;
+  dev.feature_len     = BMA456_FEATURE_SIZE;
   
-  /* Read event to make sure there was a POR detected  */
-  do{
-    aux  =   BMA456_GetEvent ( myBMA456_I2C_parameters, &myBMA456_Data );   
-  }while( myBMA456_Data.por_detected == EVENT_POR_DETECTED_HIGH );                            // Dangerous!!! The uC may get stuck here...
-                                                                                              // [WORKAROUND] Insert a counter  
-  /* Accelerometer disabled, Auxiliary sensor disabled  */
-  myBMA456_Data.acc_en   =   PWR_CTRL_ACC_EN_ACC_OFF;
-  myBMA456_Data.aux_en   =   PWR_CTRL_AUX_EN_MAG_OFF;
-  aux  =   BMA456_SetPWR_Ctrl ( myBMA456_I2C_parameters, myBMA456_Data );
-  nrf_delay_ms ( 1UL );
+  /* Softreset   */
+  bma4_set_command_register(0xB6, &dev); 
+  dev.delay(1);
 
-  /* Get the ID  */
-  aux  =   BMA456_GetID ( myBMA456_I2C_parameters, &myBMA456_Data );
+  /* a. Reading the chip id. */
+  rslt = bma456_init(&dev);
+  dev.delay(1);
+
+  /* b. Performing initialization sequence. 
+   * c. Checking the correct status of the initialization sequence.
+   */
+  rslt = bma456_write_config_file(&dev);
+  dev.delay(1);
+
+  /* Enable the accelerometer */
+  bma4_set_accel_enable(BMA4_ENABLE, &dev);
+  dev.delay(10);
+
+  /* Declare an accelerometer configuration structure */
+  struct bma4_accel_config accel_conf;
+	
+  /* Assign the desired settings */
+  accel_conf.odr        = BMA4_OUTPUT_DATA_RATE_200HZ;
+  accel_conf.range      = BMA4_ACCEL_RANGE_4G;
+  accel_conf.bandwidth  = BMA4_ACCEL_OSR2_AVG2;
+  accel_conf.perf_mode  = BMA4_CIC_AVG_MODE;
+
+  /* Set the configuration */
+  rslt = bma4_set_accel_config(&accel_conf, &dev);
+  dev.delay(1);
+
+  /* Feature: Doble tap ( wakeup feature ) mapped to INT1 pin */
+  rslt = bma4_set_advance_power_save(BMA4_DISABLE, &dev);
+  dev.delay(1);
+
+  rslt = bma456_map_interrupt(BMA4_INTR1_MAP, BMA456_WAKEUP_INT, BMA4_ENABLE, &dev);
+  dev.delay(1);
+  rslt = bma456_wakeup_set_sensitivity(5, &dev);
+  dev.delay(1);
+  rslt = bma456_tap_selection(0, &dev);
+  dev.delay(1);
+  rslt = bma456_feature_enable(BMA456_WAKEUP, BMA4_ENABLE, &dev);
+  dev.delay(1);
   
-  /* Accelerometer. Range: +/- 16g  */
-  myBMA456_Data.acc_range   =   ACC_RANGE_ACC_RANGE_RANGE_16G;
-  aux  =   BMA456_SetAccRange ( myBMA456_I2C_parameters, myBMA456_Data );
-  nrf_delay_ms ( 1UL );
-
-  /* Accelerometer. Bandwidth: Average 2 samples | ODR: 50Hz | Filter performance: Average mode  */
-  myBMA456_Data.acc_bwp        =   ACC_CONF_ACC_BWP_OSR2_AVG2;
-  myBMA456_Data.acc_odr        =   ACC_CONF_ACC_ODR_ODR_50;
-  myBMA456_Data.acc_perf_mode  =   ACC_CONF_ACC_PERF_MODE_CIC_AVG;
-  aux  =   BMA456_SetAccConf ( myBMA456_I2C_parameters, myBMA456_Data );
-  nrf_delay_ms ( 1UL );
-
-  /* Accelerometer. Advanced power mode enabled  */
-  myBMA456_Data.adv_power_save   =   PWR_CONF_ADV_POWER_SAVE_APS_ON;
-  aux  =   BMA456_SetPWR_Conf ( myBMA456_I2C_parameters, myBMA456_Data );
-  nrf_delay_ms ( 1UL );
-
-  /* Check that there is not any error after configuring ACC_CONF   */
-  do{
-    aux  =   BMA456_GetSensorErrorConditions ( myBMA456_I2C_parameters, &myBMA456_Data );   
-  }while( myBMA456_Data.error_code == ERR_REG_ERROR_CODE_ACC_ERR );                           // Dangerous!!! The uC may get stuck here...
-                                                                                              // [WORKAROUND] Insert a counter 
-  /* Accelerometer enabled, Auxiliary sensor disabled  */
-  myBMA456_Data.acc_en   =   PWR_CTRL_ACC_EN_ACC_ON;
-  myBMA456_Data.aux_en   =   PWR_CTRL_AUX_EN_MAG_OFF;
-  aux  =   BMA456_SetPWR_Ctrl ( myBMA456_I2C_parameters, myBMA456_Data );
-  nrf_delay_ms ( 1UL );
-
   
-
   myState  =   0UL;                           // Reset the variable
   NRF_TIMER0->TASKS_START  =   1UL;           // Start Timer0
 
@@ -150,19 +152,56 @@ int main(void)
     {
       NRF_P0->OUTCLR  |= ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );   // Turn all the LEDs on
       
-      aux  =   BMA456_GetTemperature ( myBMA456_I2C_parameters, &myBMA456_Data );
+      /* Read the sensor data into the sensor data instance */
+      rslt = bma4_read_accel_xyz(&sens_data, &dev);
 
-      /* Wait until there is a new data ready for accelerometer  */
-      do{
-        aux  =   BMA456_GetSensorStatusFlags ( myBMA456_I2C_parameters, &myBMA456_Data );
-      }while( myBMA456_Data.drdy_acc == STATUS_DRDY_ACC_DATA_NO_READY );                    // Dangerous!!! The uC may get stuck here...
-                                                                                            // [WORKAROUND] Insert a counter 
-      /* Get the acceleration data from the device   */
-      aux  =   BMA456_GetAccData ( myBMA456_I2C_parameters, &myBMA456_Data );
+      /* Get temperature   */
+      int32_t temp = 0;
+      rslt = bma4_get_temperature(&temp, BMA4_DEG, &dev);
 
+      /* Read the Interrupt status reg. */
+      rslt = bma456_read_int_status(&int_status, &dev);
+      
+      /* Check if double tap happened  */
+      if ( int_status == BMA456_WAKEUP_INT )
+      {
+        myWakeUpInfo[0]  =   'O';
+        myWakeUpInfo[1]  =   'N';
+        myWakeUpInfo[2]  =   ' ';
+      }
+      else
+      {
+        myWakeUpInfo[0]  =   'O';
+        myWakeUpInfo[1]  =   'F';
+        myWakeUpInfo[2]  =   'F';
+      }
 
+      /* Get the configuration */
+      rslt = bma4_get_accel_config(&accel_conf, &dev);
+
+      /* Get the range   */
+      switch ( accel_conf.range )
+      {
+        case BMA4_ACCEL_RANGE_2G:
+          myRange  =   16348;
+          break;
+        
+        default:
+        case BMA4_ACCEL_RANGE_4G:
+          myRange  =   8192;
+          break;
+
+        case BMA4_ACCEL_RANGE_8G:
+          myRange  =   4096;
+          break;
+
+        case BMA4_ACCEL_RANGE_16G:
+          myRange  =   2048;
+          break;
+      }
+		
       /* Transmit result through the UART  */
-      sprintf ( (char*)myMessage, "X: %d mg | Y: %d mg | Z: %d mg | T: %d C\r\n", (int16_t)( myBMA456_Data.acc_x * 1000.0 ), (int16_t)( myBMA456_Data.acc_y * 1000.0 ), (int16_t)( myBMA456_Data.acc_z * 1000.0 ), myBMA456_Data.temperature );
+      sprintf ( (char*)myMessage, "X: %d mg | Y: %d mg | Z: %d mg | T: %d C | WakeUp: %s\r\n", ( 1000 * sens_data.x ) / myRange, ( 1000 * sens_data.y ) / myRange, ( 1000 * sens_data.z ) / myRange, temp / BMA4_SCALE_TEMP, myWakeUpInfo );
 
       NRF_UART0->TASKS_STOPRX  =   1UL;
       NRF_UART0->TASKS_STOPTX  =   1UL;
