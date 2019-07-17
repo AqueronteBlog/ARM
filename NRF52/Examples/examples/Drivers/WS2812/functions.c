4ea9a8eafa72971313dede8202faefec7df9451d
Manuel Caballero <aqueronteblog@gmail.com>
2019-07-17 12:32:23 +0100

    NRF52 WS2812 Example. Functions file, Timer was configured accordingly to produces an overflow every 125ns

--- NRF52/Examples/examples/Drivers/WS2812/functions.c
+++ NRF52/Examples/examples/Drivers/WS2812/functions.c
@@ -82,15 +82,15 @@ void conf_GPIO  ( void )
 
 /**
  * @brief       void conf_TIMER0  ( void )
- * @details     One channel. Channel zero at 1s.
+ * @details     One channel. Channel zero at 125ns.
  *
  *              Timer0:
- *                  * Prescaler:            5   ( f_Timer0 = 500kHz ( PCLK1M ) ).
+ *                  * Prescaler:            1   ( f_Timer0 = 8MHz ( PCLK16M ) ).
  *                  * 32-bits mode.
- *                  * Interrupt Enabled.
+ *                  * Interrupt Disabled.
  *
  *                 --- Channel 0:
- *                  * Overflow:             ( 500000 * (f_Timer0)^(-1) ) = ( 500000 * (500kHz)^(-1) ) ~ 1s
+ *                  * Overflow:             ( 1 * (f_Timer0)^(-1) ) = ( 1 * (8MHz)^(-1) ) = 125ns
  *
  *
  * @param[in]    N/A.
@@ -110,16 +110,16 @@ void conf_TIMER0  ( void )
 {
   NRF_TIMER0->TASKS_STOP  =   1UL;
   NRF_TIMER0->MODE        =   ( TIMER_MODE_MODE_Timer << TIMER_MODE_MODE_Pos );
-  NRF_TIMER0->PRESCALER   =   5UL;                                                                        // f_Timer0 = ( 16MHz / 2^5 ) = 500kHz
+  NRF_TIMER0->PRESCALER   =   1UL;                                                                        // f_Timer0 = ( 16MHz / 2^1 ) = 8MHz
   NRF_TIMER0->BITMODE     =   ( TIMER_BITMODE_BITMODE_32Bit << TIMER_BITMODE_BITMODE_Pos );               // 32 bit mode.
   NRF_TIMER0->TASKS_CLEAR =   1UL;                                                                        // clear the task first to be usable for later.
 
-  NRF_TIMER0->CC[0]       =   ( 500000 );                                                                 // ( 500000 * (f_Timer0)^(-1) ) = ( 500000 * (500kHz)^(-1) ) ~ 1s
+  NRF_TIMER0->CC[0]       =   ( 1UL );                                                                    // ( 1 * (f_Timer0)^(-1) ) = ( 1 * (8MHz)^(-1) ) ~ 125ns
 
-  NRF_TIMER0->INTENSET    =   ( TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos );
-  
+//  NRF_TIMER0->INTENSET    =   ( TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos );
+//  
   NRF_TIMER0->SHORTS      =   ( TIMER_SHORTS_COMPARE0_CLEAR_Enabled << TIMER_SHORTS_COMPARE0_CLEAR_Pos ); // Create an Event-Task shortcut to clear TIMER0 on COMPARE[0] event.
-
-
-  NVIC_EnableIRQ ( TIMER0_IRQn );                                                                         // Enable Interrupt for the Timer0 in the core.
+//
+//
+//  NVIC_EnableIRQ ( TIMER0_IRQn );                                                                         // Enable Interrupt for the Timer0 in the core.
 }
