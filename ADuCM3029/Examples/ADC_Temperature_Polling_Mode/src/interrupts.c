a5771b9b1f9326a406d934d13dd14bcf8cf3a1ea
Manuel Caballero <aqueronteblog@gmail.com>
2019-07-18 19:25:24 +0100

    ADuCM3029. ADC Temperature Polling Mode Example. Interrupts files were added

---
+++ ADuCM3029/Examples/ADC_Temperature_Polling_Mode/src/interrupts.c
@@ -0,0 +1,59 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        18/July/2019
+ * @version     18/July/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "interrupts.h"
+
+
+/**
+ * @brief       void UART_Int_Handler  ( void )
+ * @details     UART subroutine.
+ *
+ *
+ * @param[in]    N/A.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        18/July/2019
+ * @version     18/July/2019    The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void UART_Int_Handler ( void )
+{
+	/* Rx	 */
+	if ( ( pADI_UART0->LSR & ( 1U << BITP_UART_LSR_DR ) ) == ( 1U << BITP_UART_LSR_DR ) )
+	{
+		/* Next action	 */
+		myState	 =	 (uint8_t)( pADI_UART0->RX );
+	}
+
+	/* Tx	 */
+	if ( ( ( pADI_UART0->LSR & ( ( 1U << BITP_UART_LSR_THRE ) | ( 1U << BITP_UART_LSR_TEMT ) ) ) ==  ( ( 1U << BITP_UART_LSR_THRE ) | ( 1U << BITP_UART_LSR_TEMT ) ) ) )
+	{
+		/* Stop transmitting data when that character is found */
+		if ( *myPtr  == '\n' )
+		{
+			pADI_UART0->IEN	&=	~( 1U << BITP_UART_IEN_ETBEI );
+		}
+		else
+		{
+			pADI_UART0->TX	 =	 *++myPtr;
+		}
+	}
+}
+
+
