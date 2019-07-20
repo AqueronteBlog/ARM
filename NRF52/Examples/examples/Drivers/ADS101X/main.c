b0a3cf704657c92922d2de1098c727992de1f2ad
Manuel Caballero <aqueronteblog@gmail.com>
2019-07-20 21:03:34 +0100

    NRF52 ADS101X Example. Major changes, last compilation without any errors, testing the driver.

--- NRF52/Examples/examples/Drivers/ADS101X/main.c
+++ NRF52/Examples/examples/Drivers/ADS101X/main.c
@@ -50,8 +50,9 @@ volatile uint8_t            *myPtr;                   /*!<   Pointer to point ou
 int main(void)
 {
   uint8_t           myMessage[ TX_BUFF_SIZE ];
-//  I2C_parameters_t  myADS101X_I2C_parameters;
-//  ADS101X_status_t  aux;
+  I2C_parameters_t  myADS101X_I2C_parameters;
+  ADS101X_data_t    myADS101X_Data;
+  ADS101X_status_t  aux;
 
 
 
@@ -60,19 +61,42 @@ int main(void)
   conf_UART   ();
   conf_TIMER0 ();
   
-//  /* I2C definition   */
-//  myADS101X_I2C_parameters.TWIinstance =    NRF_TWI0;
-//  myADS101X_I2C_parameters.SDA         =    TWI0_SDA;
-//  myADS101X_I2C_parameters.SCL         =    TWI0_SCL;
-//  myADS101X_I2C_parameters.ADDR        =    ADS101X_ADDRESS_0;
-//  myADS101X_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
-//  myADS101X_I2C_parameters.SDAport     =    NRF_P0;
-//  myADS101X_I2C_parameters.SCLport     =    NRF_P0;
-//
-//  /* Configure I2C peripheral  */
-//  aux  =   ADS101X_Init  ( myADS101X_I2C_parameters );
+  /* I2C definition   */
+  myADS101X_I2C_parameters.TWIinstance =    NRF_TWI0;
+  myADS101X_I2C_parameters.SDA         =    TWI0_SDA;
+  myADS101X_I2C_parameters.SCL         =    TWI0_SCL;
+  myADS101X_I2C_parameters.ADDR        =    ADS101X_ADDRESS_GND;
+  myADS101X_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+  myADS101X_I2C_parameters.SDAport     =    NRF_P0;
+  myADS101X_I2C_parameters.SCLport     =    NRF_P0;
 
+  /* Configure I2C peripheral  */
+  myADS101X_Data.device  =   DEVICE_ADS1015;
+  aux  =   ADS101X_Init  ( myADS101X_I2C_parameters, myADS101X_Data );
 
+  /* Perform a softreset   */
+  aux  =   ADS101X_SoftReset  ( myADS101X_I2C_parameters );
+  nrf_delay_ms ( 500U );
+
+  /* Input multiplexor configuration ( channels ): AINp = AIN0 | AINn = GND  */
+  myADS101X_Data.mux  =   CONFIG_MUX_AINP_AIN0_AND_AINN_GND;
+  aux  =   ADS101X_SetMux  ( myADS101X_I2C_parameters, myADS101X_Data );
+
+  /* Gain: ±4.096V  */
+  myADS101X_Data.pga  =   CONFIG_PGA_FSR_4_096_V;
+  aux  =   ADS101X_SetGain  ( myADS101X_I2C_parameters, myADS101X_Data );
+
+  /* Mode: Single-shot  */
+  myADS101X_Data.mode  =   CONFIG_MODE_SINGLE_SHOT;
+  aux  =   ADS101X_SetMode  ( myADS101X_I2C_parameters, myADS101X_Data );
+
+  /* Data rate: 1600 SPS  */
+  myADS101X_Data.dr  =   CONFIG_DR_1600_SPS;
+  aux  =   ADS101X_SetDataRate  ( myADS101X_I2C_parameters, myADS101X_Data );
+
+  /* Comparator: Disabled  */
+  myADS101X_Data.comp_que  =   CONFIG_COMP_QUE_DISABLED;
+  aux  =   ADS101X_SetComparator  ( myADS101X_I2C_parameters, myADS101X_Data );
 
   
   
@@ -95,10 +119,21 @@ int main(void)
     {
       NRF_P0->OUTCLR  |= ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );   // Turn all the LEDs on
       
+      /* Trigger a new conversion  */
+      aux  =   ADS101X_StartSingleConversion ( myADS101X_I2C_parameters );
       
-		
+      /* Wait until the conversion is completed  */
+      do{
+        aux  =   ADS101X_GetOS ( myADS101X_I2C_parameters, &myADS101X_Data );
+      }while( ( myADS101X_Data.os & CONFIG_OS_MASK ) == CONFIG_OS_BUSY );       // [TODO] Too dangerous! the uC may get stuck here
+                                                                                // [WORKAROUND] Insert a counter.
+      /* Get the result  */
+      aux  =   ADS101X_GetRawConversion ( myADS101X_I2C_parameters, &myADS101X_Data );
+
+
+
 //      /* Transmit result through the UART  */
-//      sprintf ( (char*)myMessage, "X: %d mg | Y: %d mg | Z: %d mg | T: %d C | WakeUp: %s\r\n", ( 1000 * sens_data.x ) / myRange, ( 1000 * sens_data.y ) / myRange, ( 1000 * sens_data.z ) / myRange, temp / BMA4_SCALE_TEMP, myWakeUpInfo );
+//      sprintf ( (char*)myMessage, "V = mV\r\n",  );
 //
 //      NRF_UART0->TASKS_STOPRX  =   1UL;
 //      NRF_UART0->TASKS_STOPTX  =   1UL;
@@ -106,10 +141,10 @@ int main(void)
 //
 //      NRF_UART0->TASKS_STARTTX =   1UL;
 //      NRF_UART0->TXD           =   *myPtr;
-//
-//      /* Reset the variables   */
-//      myState          =   0UL;
-//      NRF_P0->OUTSET  |=   ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );  // Turn all the LEDs off
+
+      /* Reset the variables   */
+      myState          =   0UL;
+      NRF_P0->OUTSET  |=   ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );  // Turn all the LEDs off
     }
     //__NOP();
   }
