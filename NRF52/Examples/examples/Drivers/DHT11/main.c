073cdbcf9ea6b5c099e5e50aff584cd33471bea5
Manuel Caballero <aqueronteblog@gmail.com>
2019-08-02 07:51:19 +0200

    NRF52 DHT11 Example. Minor changes, last compilation without any errors

--- NRF52/Examples/examples/Drivers/DHT11/main.c
+++ NRF52/Examples/examples/Drivers/DHT11/main.c
@@ -47,9 +47,9 @@ volatile uint8_t            *myPtr;                   /*!<   Pointer to point ou
 int main(void)
 {
   uint8_t           myMessage[ TX_BUFF_SIZE ];
-  I2C_parameters_t  myDHT11_I2C_parameters;
-  DHT11_data_t    myDHT11_Data;
-  DHT11_status_t  aux;
+//  I2C_parameters_t  myDHT11_I2C_parameters;
+//  DHT11_data_t    myDHT11_Data;
+//  DHT11_status_t  aux;
 
 
   conf_CLK    ();
@@ -57,42 +57,20 @@ int main(void)
   conf_UART   ();
   conf_TIMER0 ();
   
-  /* I2C definition   */
-  myDHT11_I2C_parameters.TWIinstance =    NRF_TWI0;
-  myDHT11_I2C_parameters.SDA         =    TWI0_SDA;
-  myDHT11_I2C_parameters.SCL         =    TWI0_SCL;
-  myDHT11_I2C_parameters.ADDR        =    DHT11_ADDRESS_GND;
-  myDHT11_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
-  myDHT11_I2C_parameters.SDAport     =    NRF_P0;
-  myDHT11_I2C_parameters.SCLport     =    NRF_P0;
+//  /* I2C definition   */
+//  myDHT11_I2C_parameters.TWIinstance =    NRF_TWI0;
+//  myDHT11_I2C_parameters.SDA         =    TWI0_SDA;
+//  myDHT11_I2C_parameters.SCL         =    TWI0_SCL;
+//  myDHT11_I2C_parameters.ADDR        =    DHT11_ADDRESS_GND;
+//  myDHT11_I2C_parameters.Freq        =    TWI_FREQUENCY_FREQUENCY_K400;
+//  myDHT11_I2C_parameters.SDAport     =    NRF_P0;
+//  myDHT11_I2C_parameters.SCLport     =    NRF_P0;
+//
+//  /* Configure I2C peripheral  */
+//  myDHT11_Data.device  =   DEVICE_ADS1015;
+//  aux  =   DHT11_Init  ( myDHT11_I2C_parameters, myDHT11_Data );
 
-  /* Configure I2C peripheral  */
-  myDHT11_Data.device  =   DEVICE_ADS1015;
-  aux  =   DHT11_Init  ( myDHT11_I2C_parameters, myDHT11_Data );
 
-  /* Perform a softreset   */
-  aux  =   DHT11_SoftReset  ( myDHT11_I2C_parameters );
-  nrf_delay_ms ( 500U );
-
-  /* Input multiplexor configuration ( channels ): AINp = AIN0 | AINn = GND  */
-  myDHT11_Data.mux  =   CONFIG_MUX_AINP_AIN0_AND_AINN_GND;
-  aux  =   DHT11_SetMux  ( myDHT11_I2C_parameters, myDHT11_Data );
-
-  /* Gain: ±4.096V  */
-  myDHT11_Data.pga  =   CONFIG_PGA_FSR_4_096_V;
-  aux  =   DHT11_SetGain  ( myDHT11_I2C_parameters, myDHT11_Data );
-
-  /* Mode: Single-shot  */
-  myDHT11_Data.mode  =   CONFIG_MODE_SINGLE_SHOT;
-  aux  =   DHT11_SetMode  ( myDHT11_I2C_parameters, myDHT11_Data );
-
-  /* Data rate: 1600 SPS  */
-  myDHT11_Data.dr  =   CONFIG_DR_1600_SPS;
-  aux  =   DHT11_SetDataRate  ( myDHT11_I2C_parameters, myDHT11_Data );
-
-  /* Comparator: Disabled  */
-  myDHT11_Data.comp_que  =   CONFIG_COMP_QUE_DISABLED;
-  aux  =   DHT11_SetComparator  ( myDHT11_I2C_parameters, myDHT11_Data );
 
    
   myState  =   0UL;                           // Reset the variable
@@ -114,27 +92,20 @@ int main(void)
     {
       NRF_P0->OUTCLR  |= ( ( 1U << LED1 ) | ( 1U << LED2 ) | ( 1U << LED3 ) | ( 1U << LED4 ) );   // Turn all the LEDs on
       
-      /* Trigger a new conversion  */
-      aux  =   DHT11_StartSingleConversion ( myDHT11_I2C_parameters );
-      
-      /* Wait until the conversion is completed  */
-      do{
-        aux  =   DHT11_GetOS ( myDHT11_I2C_parameters, &myDHT11_Data );
-      }while( ( myDHT11_Data.os & CONFIG_OS_MASK ) == CONFIG_OS_BUSY );       // [TODO] Too dangerous! the uC may get stuck here
-                                                                                // [WORKAROUND] Insert a counter.
-      /* Get the result  */
-      aux  =   DHT11_GetConversion ( myDHT11_I2C_parameters, &myDHT11_Data );
-
-
-      /* Transmit result through the UART  */
-      sprintf ( (char*)myMessage, "V = %d mV\r\n", (int32_t)( 1000 * myDHT11_Data.conversion ) );
-
-      NRF_UART0->TASKS_STOPRX  =   1UL;
-      NRF_UART0->TASKS_STOPTX  =   1UL;
-      myPtr                    =   &myMessage[0];
-
-      NRF_UART0->TASKS_STARTTX =   1UL;
-      NRF_UART0->TXD           =   *myPtr;
+//      /* Trigger a new conversion  */
+//      aux  =   DHT11_StartSingleConversion ( myDHT11_I2C_parameters );
+//      
+//
+//
+//      /* Transmit result through the UART  */
+//      sprintf ( (char*)myMessage, "V = %d mV\r\n", (int32_t)( 1000 * myDHT11_Data.conversion ) );
+//
+//      NRF_UART0->TASKS_STOPRX  =   1UL;
+//      NRF_UART0->TASKS_STOPTX  =   1UL;
+//      myPtr                    =   &myMessage[0];
+//
+//      NRF_UART0->TASKS_STARTTX =   1UL;
+//      NRF_UART0->TXD           =   *myPtr;
 
       /* Reset the variables   */
       myState          =   0UL;
