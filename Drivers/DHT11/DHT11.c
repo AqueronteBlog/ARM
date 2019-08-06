911227fe484ab93bf05b15f24a3a87089edb1f60
Manuel Caballero <aqueronteblog@gmail.com>
2019-08-06 22:52:01 +0200

    DRIVERS. DHT11, some comments were improved

--- Drivers/DHT11/DHT11.c
+++ Drivers/DHT11/DHT11.c
@@ -20,7 +20,7 @@
 /**
  * @brief       DHT11_Init ( DHT11_comm_t myDHT11 )
  *
- * @details     It configures the GPIO for the DHT11 device. Output pin and high output ( bus released )
+ * @details     It sets the pin 'high' level to release the bus.
  *
  * @param[in]    myDHT11: GPIO parameters for this device.
  *
@@ -32,7 +32,7 @@
  *
  * @author      Manuel Caballero
  * @date        01/August/2019
- * @version     02/August/2019   [todo]
+ * @version     06/August/2019   The bus is released when the pin is 'High'.
  *              01/August/2019   The ORIGIN
  * @pre         N/A
  * @warning     N/A.
@@ -40,8 +40,8 @@
 DHT11_status_t DHT11_Init ( DHT11_comm_t myDHT11 )
 {
   DHT11_status_t aux =  DHT11_SUCCESS;
-
-  //aux  =   i2c_init ( myI2Cparameters );
+  
+  aux |=   myDHT11.dht11_set_high ( myDHT11.pin );
 
 
 
@@ -49,4 +49,47 @@ DHT11_status_t DHT11_Init ( DHT11_comm_t myDHT11 )
 }
 
 
+
+/**
+ * @brief       DHT11_GetRawData ( DHT11_comm_t myDHT11 , DHT11_data_t* )
+ *
+ * @details     It gets the raw data: Temperature, Humidity and Checksum.
+ *
+ * @param[in]    myDHT11: GPIO parameters for this device.
+ *
+ * @param[out]   myRawData: Raw data: Temperature, Humidity and Checksum.
+ *
+ *
+ * @return       Status of DHT11_Init.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        06/August/2019
+ * @version     06/August/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
+ */
+DHT11_status_t DHT11_GetRawData ( DHT11_comm_t myDHT11, DHT11_data_t* myRawData )
+{
+  DHT11_device_bus_status_t* pinStatus;
+  DHT11_status_t             aux       =  DHT11_SUCCESS;
+  
+  /* The communication starts with the bus on 'high' level   */
+  aux |=   myDHT11.dht11_set_high ( myDHT11.pin );
+
+  /* 1. MCU sends a start signal  */
+  aux |=   myDHT11.dht11_set_low ( myDHT11.pin );
+  aux |=   myDHT11.dht11_delay_us ( DHT11_START_SIGNAL );
+  
+  /* 2. MCU waits for DHT11 response   */
+  aux |=   myDHT11.dht11_set_high ( myDHT11.pin );
+  
+  *pinStatus   =   DHT11_PIN_UNKNOWN;
+  do{
+    aux |=   myDHT11.dht11_read_pin ( myDHT11.pin, &(*pinStatus) );
+  }while ( *pinStatus == ( DHT11_PIN_HIGH << myDHT11.pin ) );
+
+
+  return  aux;
+}
 
