009019426c6d948d1cc2a5d8b7d483e5bd8b03de
Manuel Caballero <aqueronteblog@gmail.com>
2019-09-29 20:15:13 +0100

    STM32L072CZ LEDs Example. Board file was added.

---
+++ STM32L072CZ/Examples/Peripherals/LEDs/Core/Inc/board.h
@@ -0,0 +1,28 @@
+/**
+ * @brief       board.h
+ * @details     It characterizes the board ( for this example ): B-L072Z-LRWAN1.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        29/September/2019
+ * @version     29/September/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+
+/**
+  * @brief   LEDs
+*/
+typedef enum
+{
+    LED1         =   5              /*!<  PA_5                        */
+} B_L072Z_LRWAN1_leds_t;
+
+
+#endif /* BOARD_H_ */
