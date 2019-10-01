90c5ecb534fd316ca75fc9293b7321fc0eb6df9a
Manuel Caballero <aqueronteblog@gmail.com>
2019-10-01 19:18:27 +0100

    STM32L072CZ Timers Example. Interrupts files were added.

---
+++ STM32L072CZ/Examples/Peripherals/Timers/Core/Inc/interrupts.h
@@ -0,0 +1,36 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        01/October/2019
+ * @version     01/October/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "variables.h"
+
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+
+/**@brief Subroutine prototypes.
+ */
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#endif /* INTERRUPTS_H_ */
---
+++ STM32L072CZ/Examples/Peripherals/Timers/Core/Src/interrupts.c
@@ -0,0 +1,13 @@
+/**
+ * @brief       interrupts.c
+ * @details     Interruption sources.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        01/October/2019
+ * @version     01/October/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "interrupts.h"
