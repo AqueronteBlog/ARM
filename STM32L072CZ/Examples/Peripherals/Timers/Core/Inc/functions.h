fa5dc779c0f4f40760debd9e64eb749daa27416c
Manuel Caballero <aqueronteblog@gmail.com>
2019-10-01 19:18:12 +0100

    STM32L072CZ Timers Example. Functions files were added.

---
+++ STM32L072CZ/Examples/Peripherals/Timers/Core/Inc/functions.h
@@ -0,0 +1,26 @@
+/**
+ * @brief       functions.h
+ * @details     Functions' header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        01/October/2019
+ * @version     01/October/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include "stm32l0xx_hal.h"
+
+
+/**@brief Functions prototypes.
+ */
+void Conf_GPIO 	   ( void );
+
+
+
+#endif /* FUNCTIONS_H_ */
---
+++ STM32L072CZ/Examples/Peripherals/Timers/Core/Src/functions.c
@@ -0,0 +1,74 @@
+/**
+ * @brief       functions.c
+ * @details     Functions.
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        01/October/2019
+ * @version     01/October/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+
+#include "functions.h"
+
+/**
+ * @brief       void Conf_GPIO  ( void )
+ * @details     It configures GPIO to work with the LEDs.
+ *
+ *					LEDs:
+ * 						- LD1:	PB_5
+ * 						- LD2:	PA_5
+ * 						- LD3:	PB_6
+ * 						- LD4:	PB_7
+ *
+ *
+ *
+ * @return      NA
+ *
+ * @author      Manuel Caballero
+ * @date        01/October/2019
+ * @version		01/October/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+void Conf_GPIO  ( void )
+{
+	/* GPIOA and GPIOB Periph clocks enable	 */
+	RCC->IOPENR	|=	 ( RCC_IOPENR_GPIOAEN | RCC_IOPENR_GPIOBEN );
+
+	/* GPIOB Mode: General purpose output mode	 */
+	GPIOB->MODER	&=	~( GPIO_MODER_MODE5_Msk | GPIO_MODER_MODE6_Msk | GPIO_MODER_MODE7_Msk );
+	GPIOB->MODER	|=	 ( GPIO_MODER_MODE5_0 | GPIO_MODER_MODE6_0 | GPIO_MODER_MODE7_0 );
+
+	/* GPIOB Output type: Output push-pull	 */
+	GPIOB->OTYPER	&=	~( GPIO_OTYPER_OT_5 | GPIO_OTYPER_OT_6 | GPIO_OTYPER_OT_7 );
+
+	/* GPIOB Output speed: Low speed	 */
+	GPIOB->OSPEEDR	&=	~( GPIO_OSPEEDER_OSPEED5_Msk | GPIO_OSPEEDER_OSPEED6_Msk | GPIO_OSPEEDER_OSPEED7_Msk );
+
+	/* GPIOB Output: No pull-up, pull-down	 */
+	GPIOB->PUPDR	&=	~( GPIO_PUPDR_PUPD5_Msk | GPIO_PUPDR_PUPD6_Msk | GPIO_PUPDR_PUPD7_Msk );
+
+	/* GPIOB Output: PA_5, PA_6 and PA_7 reset	 */
+	GPIOB->BSRR	|=	 ( GPIO_BSRR_BR_5 | GPIO_BSRR_BR_6 | GPIO_BSRR_BR_7 );
+
+	/* GPIOA Mode: General purpose output mode	 */
+	GPIOA->MODER	&=	~( GPIO_MODER_MODE5_Msk );
+	GPIOA->MODER	|=	 GPIO_MODER_MODE5_0;
+
+	/* GPIOA Output type: Output push-pull	 */
+	GPIOA->OTYPER	&=	~GPIO_OTYPER_OT_5;
+
+	/* GPIOA Output speed: Low speed	 */
+	GPIOA->OSPEEDR	&=	~GPIO_OSPEEDER_OSPEED5_Msk;
+
+	/* GPIOA Output: No pull-up, pull-down	 */
+	GPIOA->PUPDR	&=	~GPIO_PUPDR_PUPD5_Msk;
+
+	/* GPIOA Output: PB_6 reset	 */
+	GPIOA->BSRR	|=	 GPIO_BSRR_BR_5;
+}
