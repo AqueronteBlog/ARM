a0bbdbdb3099b3cb9bc1befaca4bf9606f532a88
Manuel Caballero <aqueronteblog@gmail.com>
2019-10-03 20:25:16 +0100

    STM32L072CZ LPTIM Example. Variables file was added.

---
+++ STM32L072CZ/Examples/Peripherals/LPTIM/Core/Inc/variables.h
@@ -0,0 +1,22 @@
+/**
+ * @brief       variables.h
+ * @details     Constants and variables used by the example.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        03/October/2019
+ * @version     03/October/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "stm32l0xx_hal.h"
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+
+
+
+#endif /* VARIABLES_H_ */
