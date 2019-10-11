cf427e8a432d6d219ff7fb63676a447a26f430a3
Manuel Caballero <aqueronteblog@gmail.com>
2019-10-11 19:18:48 +0100

    Drivers HDC2080. The driver is still in progress, some registers were defined.

--- Drivers/HDC2080/HDC2080.h
+++ Drivers/HDC2080/HDC2080.h
@@ -21,43 +21,184 @@
 
 
 /**
-  * @brief   DEFAULT ADDRESS
+  * @brief   DEFAULT ADDRESSES
   */
 typedef enum
 {
-  HDC2080_ADDRESS     =   0b1011101          /*!<   I2C slave address byte                       */
-} HDC2080_address_t;
+  HDC2080_ADDRESS_GND  =   0b1000000,         	/*!<   ADDR connected to GND                        */
+  HDC2080_ADDRESS_VDD  =   0b1000001          	/*!<   ADDR connected to VDD                        */
+} HDC2080_addresses_t;
 
 
 
 /**
-  * @brief   REGISTERS
+  * @brief   REGISTER MAP
   */
 typedef enum
 {
-  HDC2080_ID1        	 =   0x0F,           /*!<  Device ID1			                         */
-  HDC2080_ID2         	 =   0x10,           /*!<  Device ID2								     */
-  HDC2080_POWER_DOWN      =   0x12,           /*!<  Power down register                           */
-  HDC2080_RESET	         =   0x13,           /*!<  Reset register	                             */
-  HDC2080_MODE_CONTROL    =   0x14,           /*!<  Mode control register                         */
-  HDC2080_STATUS		     =   0x19,           /*!<  Status register                               */
-  HDC2080_PRESSURE_MSB    =   0x1A,           /*!<  Pressure data (MSB)  		                 */
-  HDC2080_PRESSURE_LSB    =   0x1B,           /*!<  Pressure data (LSB)  		                 */
-  HDC2080_PRESSURE_XLSB   =   0x1C,           /*!<  Pressure data (XLSB)  		                 */
-  HDC2080_TEMPERATURE_MSB =   0x1D,           /*!<  Temperature data (MSB)                        */
-  HDC2080_TEMPERATURE_LSB =   0x1E            /*!<  Temperature data (LSB)                        */
-} HDC2080_registers_t;
+  HDC2080_TEMPERATURE_LOW        	=   0x00,	/*!<  Temperature [7:0]		                         						*/
+  HDC2080_TEMPERATURE_HIGH         	=   0x01,   /*!<  Temperature [15:8]							     					*/
+  HDC2080_HUMIDITY_LOW      		=   0x02,   /*!<  Humidity [7:0]                        								*/
+  HDC2080_HUMIDITY_HIGH	         	=   0x03,   /*!<  Humidity [15:8]	                             						*/
+  HDC2080_INTERRUPT_DRDY    		=   0x04,   /*!<  DataReady and interrupt configuration                        			*/
+  HDC2080_TEMPERATURE_MAX		    =   0x05,   /*!<  Maximum measured temperature (Not supported in Auto Measurement Mode)	*/
+  HDC2080_HUMIDITY_MAX    			=   0x06,   /*!<  Maximum measured humidity (Not supported in Auto Measurement Mode)	*/
+  HDC2080_INTERRUPT_ENABLE    		=   0x07,   /*!<  Interrupt Enable	                 									*/
+  HDC2080_TEMP_OFFSET_ADJUST   		=   0x08,   /*!<  Temperature offset adjustment	                 						*/
+  HDC2080_HUM_OFFSET_ADJUST 		=   0x09,   /*!<  Humidity offset adjustment                      						*/
+  HDC2080_TEMP_THR_L 				=   0x0A,   /*!<  Temperature Threshold Low                      						*/
+  HDC2080_TEMP_THR_H 				=   0x0B,   /*!<  Temperature Threshold High                        					*/
+  HDC2080_RH_THR_L 					=   0x0C,   /*!<  Humidity threshold Low                       							*/
+  HDC2080_RH_THR_H 					=   0x0D,   /*!<  Humidity threshold High                       						*/
+  HDC2080_RESET_DRDY_INT_CONF 		=   0x0E,   /*!<  Soft Reset and Interrupt Configuration                        		*/
+  HDC2080_MEASUREMENT_CONFIGURATION	=   0x0F,   /*!<  Measurement configuration                       						*/
+  HDC2080_MANUFACTURER_ID_LOW 		=   0xFC,   /*!<  Manufacturer ID Low                       							*/
+  HDC2080_MANUFACTURER_ID_HIGH 		=   0xFD,   /*!<  Manufacturer ID High                      							*/
+  HDC2080_DEVICE_ID_LOW 			=   0xFE,   /*!<  Device ID Low                       									*/
+  HDC2080_DEVICE_ID_HIGH 			=   0xFF    /*!<  Device ID High                      									*/
+} HDC2080_register_map_t;
 
 
 
 /**
-  * @brief   ID REGISTERS
+  * @brief   INTERRUPT/DRDY REGISTER
   */
+/* DRDY_STATUS <7>
+ *    NOTE: DataReady bit status. DRDY_STATUS is cleared to 0 when read.
+ */
 typedef enum
 {
-    ID_ID1           	=   0xE0,             			/*!<  ID1 value	                                */
-    ID_ID2          	=   0x32              			/*!<  ID2 value	                                */
-} HDC2080_id_t;
+    INTERRUPT_DRDY_DRDY_STATUS_MASK				=   ( 1U << 7U ),	/*!<  DRDY_STATUS mask                          */
+	INTERRUPT_DRDY_DRDY_STATUS_DATA_NOT_READY	=   ( 0U << 7U ),	/*!<  DRDY_STATUS data not ready                */
+	INTERRUPT_DRDY_DRDY_STATUS_DATA_READY		=   ( 1U << 7U )	/*!<  DRDY_STATUS data ready 		            */
+} HDC2080_interrupt_drdy_drdy_status_t;
+
+
+/* TH_STATUS <6>
+ *    NOTE: Temperature threshold HIGH Interrupt status. TH_STATUS is cleared to 0 when read.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_TH_STATUS_MASK				=   ( 1U << 6U ),	/*!<  TH_STATUS mask 	                        */
+	INTERRUPT_DRDY_TH_STATUS_NO_INTERRUPT		=   ( 0U << 6U ),	/*!<  No interrupt				                */
+	INTERRUPT_DRDY_TH_STATUS_INTERRUPT			=   ( 1U << 6U )	/*!<  Interrupt				 		            */
+} HDC2080_interrupt_drdy_th_status_t;
+
+
+/* TL_STATUS <5>
+ *    NOTE: Temperature threshold LOW Interrupt status. TL_STATUS is cleared to 0 when read.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_TL_STATUS_MASK				=   ( 1U << 5U ),	/*!<  TL_STATUS mask 	                        */
+	INTERRUPT_DRDY_TL_STATUS_NO_INTERRUPT		=   ( 0U << 5U ),	/*!<  No interrupt				                */
+	INTERRUPT_DRDY_TL_STATUS_INTERRUPT			=   ( 1U << 5U )	/*!<  Interrupt				 		            */
+} HDC2080_interrupt_drdy_tl_status_t;
+
+
+/* HH_STATUS <4>
+ *    NOTE: Humidity threshold HIGH Interrupt status. HH_STATUS is cleared to 0 when read.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_HH_STATUS_MASK				=   ( 1U << 4U ),	/*!<  HH_STATUS mask 	                        */
+	INTERRUPT_DRDY_HH_STATUS_NO_INTERRUPT		=   ( 0U << 4U ),	/*!<  No interrupt				                */
+	INTERRUPT_DRDY_HH_STATUS_INTERRUPT			=   ( 1U << 4U )	/*!<  Interrupt				 		            */
+} HDC2080_interrupt_drdy_hh_status_t;
+
+
+/* HL_STATUS <3>
+ *    NOTE: Humidity threshold LOW Interrupt status. HL_STATUS is cleared to 0 when read.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_HL_STATUS_MASK				=   ( 1U << 3U ),	/*!<  HL_STATUS mask 	                        */
+	INTERRUPT_DRDY_HL_STATUS_NO_INTERRUPT		=   ( 0U << 3U ),	/*!<  No interrupt				                */
+	INTERRUPT_DRDY_HL_STATUS_INTERRUPT			=   ( 1U << 3U ) 	/*!<  Interrupt				 		            */
+} HDC2080_interrupt_drdy_hl_status_t;
+
+
+
+/**
+  * @brief   INTERRUPT CONFIGURATION REGISTER
+  */
+/* DRDY_STATUS <7>
+ *    NOTE: DataReady interrupt enable.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_DRDY_ENABLE_MASK					=   ( 1U << 7U ),	/*!<  DRDY_ENABLE mask                          */
+	INTERRUPT_DRDY_DRDY_ENABLE_INTERRUPT_DISABLE	=   ( 0U << 7U ),	/*!<  DRDY_ENABLE interrupt disable             */
+	INTERRUPT_DRDY_DRDY_ENABLE_INTERRUPT_ENABLE		=   ( 1U << 7U )	/*!<  DRDY_ENABLE interrupt enable 		        */
+} HDC2080_interrupt_enable_drdy_enable_t;
+
+
+/* TH_ENABLE <6>
+ *    NOTE: Temperature threshold HIGH Interrupt enable.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_TH_ENABLE_MASK					=   ( 1U << 6U ),	/*!<  TH_ENABLE mask 	                        */
+	INTERRUPT_DRDY_TH_ENABLE_INTERRUPT_DISABLE		=   ( 0U << 6U ),	/*!<  Interrupt disable			                */
+	INTERRUPT_DRDY_TH_ENABLE_INTERRUPT_ENABLE		=   ( 1U << 6U ),	/*!<  Interrupt	enable		 		            */
+} HDC2080_interrupt_enable_th_enable_t;
+
+
+/* TL_ENABLE <5>
+ *    NOTE: Temperature threshold LOW Interrupt enable.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_TL_ENABLE_MASK					=   ( 1U << 5U ),	/*!<  TL_ENABLE mask 	                        */
+	INTERRUPT_DRDY_TL_ENABLE_INTERRUPT_DISABLE		=   ( 0U << 5U ),	/*!<  Interrupt disable			                */
+	INTERRUPT_DRDY_TL_ENABLE_INTERRUPT_ENABLE		=   ( 1U << 5U ),	/*!<  Interrupt	enable		 		            */
+} HDC2080_interrupt_enable_tl_enable_t;
+
+
+/* HH_ENABLE <4>
+ *    NOTE: Humidity threshold HIGH Interrupt enable.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_HH_ENABLE_MASK					=   ( 1U << 4U ),	/*!<  HH_ENABLE mask 	                        */
+	INTERRUPT_DRDY_HH_ENABLE_INTERRUPT_DISABLE		=   ( 0U << 4U ),	/*!<  Interrupt disable			                */
+	INTERRUPT_DRDY_HH_ENABLE_INTERRUPT_ENABLE		=   ( 1U << 4U ),	/*!<  Interrupt	enable		 		            */
+} HDC2080_interrupt_enable_hh_enable_t;
+
+
+/* HL_ENABLE <3>
+ *    NOTE: Humidity threshold LOW Interrupt enable.
+ */
+typedef enum
+{
+    INTERRUPT_DRDY_HL_ENABLE_MASK					=   ( 1U << 3U ),	/*!<  HL_ENABLE mask 	                        */
+	INTERRUPT_DRDY_HL_ENABLE_INTERRUPT_DISABLE		=   ( 0U << 3U ),	/*!<  Interrupt disable			                */
+	INTERRUPT_DRDY_HL_ENABLE_INTERRUPT_ENABLE		=   ( 1U << 3U ),	/*!<  Interrupt	enable		 		            */
+} HDC2080_interrupt_enable_hl_enable_t;
+
+
+/**
+  * @brief   TEMP_OFFSET_ADJUST REGISTER
+  *				NOTE: Temperature offset adjustment. Added to the converted temperature value.
+ */
+typedef enum
+{
+	TEMP_OFFSET_ADJUST_MASK				=   0xFF,						/*!<  TEMP_OFFSET_ADJUST mask       	        */
+	TEMP_OFFSET_ADJUST_MINUS_20_62_C	=   ( 1U << 7U ),				/*!<  TEMP_OFFSET_ADJUST: –20.62°C  			*/
+	TEMP_OFFSET_ADJUST_10_32_C			=   ( 1U << 6U ),				/*!<  TEMP_OFFSET_ADJUST:  10.32°C  			*/
+	TEMP_OFFSET_ADJUST_5_16_C			=   ( 1U << 5U ),				/*!<  TEMP_OFFSET_ADJUST:   5.16°C  			*/
+	TEMP_OFFSET_ADJUST_2_58_C			=   ( 1U << 4U ),				/*!<  TEMP_OFFSET_ADJUST:   2.58°C  			*/
+	TEMP_OFFSET_ADJUST_1_28_C			=   ( 1U << 3U ),				/*!<  TEMP_OFFSET_ADJUST:   1.28°C  			*/
+	TEMP_OFFSET_ADJUST_0_64_C			=   ( 1U << 2U ),				/*!<  TEMP_OFFSET_ADJUST:   0.64°C  			*/
+	TEMP_OFFSET_ADJUST_0_32_C			=   ( 1U << 1U ),				/*!<  TEMP_OFFSET_ADJUST:   0.32°C  			*/
+	TEMP_OFFSET_ADJUST_0_16_C			=   ( 1U << 0U ) 				/*!<  TEMP_OFFSET_ADJUST:   0.16°C  			*/
+} HDC2080_temp_offset_adjust_t;
+
+
+
+
+
+
 
 
 
