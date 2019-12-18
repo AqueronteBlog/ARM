ecd711e4ab028050c7d6837456487ea459b2f81a
Manuel Caballero <aqueronteblog@gmail.com>
2019-12-18 20:15:48 +0000

    STM32L072CZ UART Example. Variables file was added.

---
+++ STM32L072CZ/Examples/Peripherals/UART/Core/Inc/interrupts.h
@@ -0,0 +1,36 @@
+/**
+ * @brief       interrupts.h
+ * @details     Interrupts header.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        18/December/2019
+ * @version     18/December/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+#include "board.h"
+
+
+#ifndef INTERRUPTS_H_
+#define INTERRUPTS_H_
+
+
+/**@brief Subroutine prototypes.
+ */
+extern void LPTIM1_IRQHandler (void);
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#endif /* INTERRUPTS_H_ */
