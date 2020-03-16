d200a845d545f89bc12479201d3557557143ec5b
Manuel Caballero <aqueronteblog@gmail.com>
2020-03-16 20:39:34 +0000

    Drivers VEML6030. Both header file and source file were added, they are still in progress.

---
+++ Drivers/VEML6035/VEML6035.c
@@ -0,0 +1,493 @@
+/**
+ * @brief       VEML6030.c
+ * @details     High Accuracy Ambient Light Sensor With I2C Interface.
+ *              Functions file.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        29/November/2019
+ * @version     29/November/2019    The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+#include "VEML6030.h"
+
+
+/**
+ * @brief       VEML6030_Init ( I2C_parameters_t )
+ *
+ * @details     It configures the I2C peripheral.
+ *
+ * @param[in]    myI2Cparameters:       I2C parameters.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of VEML6030_Init.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        04/November/2019
+ * @version     04/November/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_Init ( I2C_parameters_t myI2Cparameters )
+{
+	i2c_status_t aux;
+
+	aux  =   i2c_init ( myI2Cparameters );
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_SetConfiguration ( I2C_parameters_t , VEML6030_data_t )
+ *
+ * @details     It sets the configuration register.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myALS_Conf:   		ALS gain, integration time, interrupt, and shut down.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of VEML6030_SetConfiguration.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        06/December/2019
+ * @version     06/December/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_SetConfiguration ( I2C_parameters_t myI2Cparameters, VEML6030_data_t myALS_Conf )
+{
+	uint8_t		 cmd[3]  = { 0 };
+	uint16_t	 aux_reg = 0U;
+	i2c_status_t aux;
+
+	/* Update the register	 */
+	aux_reg	 =	 ( myALS_Conf.als_gain | myALS_Conf.als_it | myALS_Conf.als_pers | myALS_Conf.als_int_en | myALS_Conf.als_sd );
+
+	cmd[0]	 =   VEML6030_ALS_CONF;
+	cmd[1]	 =	 (uint8_t)( aux_reg >> 2U );
+	cmd[2]	 =	 (uint8_t)( aux_reg & 0xFF );
+	aux	 	 =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_SetHighThreshold ( I2C_parameters_t , VEML6030_data_t )
+ *
+ * @details     It sets the high threshold windows value.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myALS_WH:   		High threshold windows value.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of VEML6030_SetHighThreshold.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        07/December/2019
+ * @version     07/December/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_SetHighThreshold ( I2C_parameters_t myI2Cparameters, VEML6030_data_t myALS_WH )
+{
+	uint8_t		 cmd[3]  = { 0 };
+	i2c_status_t aux;
+
+	/* Update the register	 */
+	cmd[0]	 =   VEML6030_ALS_WH;
+	cmd[1]	 =	 (uint8_t)( myALS_WH.high_threshold_windows_setting >> 2U );
+	cmd[2]	 =	 (uint8_t)( myALS_WH.high_threshold_windows_setting & 0xFF );
+	aux	 	 =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_SetLowThreshold ( I2C_parameters_t , VEML6030_data_t )
+ *
+ * @details     It sets the low threshold windows value.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myALS_WL:   		Low threshold windows value.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of VEML6030_SetHighThreshold.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        07/December/2019
+ * @version     07/December/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_SetLowThreshold ( I2C_parameters_t myI2Cparameters, VEML6030_data_t myALS_WL )
+{
+	uint8_t		 cmd[3]  = { 0 };
+	i2c_status_t aux;
+
+	/* Update the register	 */
+	cmd[0]	 =   VEML6030_ALS_WL;
+	cmd[1]	 =	 (uint8_t)( myALS_WL.low_threshold_windows_setting >> 2U );
+	cmd[2]	 =	 (uint8_t)( myALS_WL.low_threshold_windows_setting & 0xFF );
+	aux	 	 =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_SetPowerSavingModes ( I2C_parameters_t , VEML6030_data_t )
+ *
+ * @details     It sets the power saving modes.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myPSM:		   		PSM and PSM_EN.
+ *
+ * @param[out]   N/A.
+ *
+ *
+ * @return       Status of VEML6030_SetPowerSavingModes.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        07/December/2019
+ * @version     07/December/2019   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_SetPowerSavingModes ( I2C_parameters_t myI2Cparameters, VEML6030_data_t myPSM )
+{
+	uint8_t		 cmd[3]  = { 0 };
+	uint16_t	 aux_reg = 0U;
+	i2c_status_t aux;
+
+	/* Update the register	 */
+	aux_reg	 =	 ( myPSM.psm | myPSM.psm_en );
+
+	cmd[0]	 =   VEML6030_POWER_SAVING;
+	cmd[1]	 =	 (uint8_t)( aux_reg >> 2U );
+	cmd[2]	 =	 (uint8_t)( aux_reg & 0xFF );
+	aux	 	 =   i2c_write ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ), I2C_STOP_BIT );
+
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_GetALS_OuputData ( I2C_parameters_t , VEML6030_data_t* )
+ *
+ * @details     It gets the ALS high resolution output data.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myALS:		   		ALS high resolution output data.
+ *
+ *
+ * @return       Status of VEML6030_GetALS_OuputData.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        07/December/2019
+ * @version     07/December/2019   The ORIGIN
+ * @pre         This function returns raw data.
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_GetALS_OuputData ( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myALS )
+{
+	uint8_t		 cmd[2]  = { 0 };
+	i2c_status_t aux;
+
+	/* Read the register	 */
+	cmd[0]	 =   VEML6030_ALS;
+	aux	 	 =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+	aux	 	 =   i2c_read  ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ) );
+
+	/* Parse the data	 */
+	myALS->als_high_resolution_output_data	 =	 cmd[0];
+	myALS->als_high_resolution_output_data <<=	 8U;
+	myALS->als_high_resolution_output_data	|=	 cmd[1];
+
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_GetWhiteChannelOuputData ( I2C_parameters_t , VEML6030_data_t* )
+ *
+ * @details     It gets the WHITE output data.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myWhite:	   		WHITE output data.
+ *
+ *
+ * @return       Status of VEML6030_GetWhiteChannelOuputData.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        07/December/2019
+ * @version     07/December/2019   The ORIGIN
+ * @pre         This function returns raw data.
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_GetWhiteChannelOuputData	( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myWhite )
+{
+	uint8_t		 cmd[2]  = { 0 };
+	i2c_status_t aux;
+
+	/* Read the register	 */
+	cmd[0]	 =   VEML6030_WHITE;
+	aux	 	 =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+	aux	 	 =   i2c_read  ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ) );
+
+	/* Parse the data	 */
+	myWhite->white_output_data	 =	 cmd[0];
+	myWhite->white_output_data <<=	 8U;
+	myWhite->white_output_data	|=	 cmd[1];
+
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_GetInterruptStatus ( I2C_parameters_t , VEML6030_data_t* )
+ *
+ * @details     It gets the Interrupt status value.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ *
+ * @param[out]   myIntStatus:	   	Status for: int_th_low and int_th_high.
+ *
+ *
+ * @return       Status of VEML6030_GetInterruptStatus.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        07/December/2019
+ * @version     07/December/2019   The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_GetInterruptStatus ( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myIntStatus )
+{
+	uint8_t		 cmd[2]  = { 0 };
+	uint16_t	 aux_reg = 0U;
+	i2c_status_t aux;
+
+	/* Read the register	 */
+	cmd[0]	 =   VEML6030_ALS_INT;
+	aux	 	 =   i2c_write ( myI2Cparameters, &cmd[0], 1U, I2C_NO_STOP_BIT );
+	aux	 	 =   i2c_read  ( myI2Cparameters, &cmd[0], sizeof( cmd )/sizeof( cmd[0] ) );
+
+	/* Parse the data	 */
+	aux_reg						 =	 cmd[0];
+	aux_reg						<<=	 8U;
+	aux_reg						|=	 cmd[1];
+
+	myIntStatus->int_th_high	 =	(VEML6030_als_int_int_th_high_t)( aux_reg & ALS_INT_INT_TH_HIGH_MASK );
+	myIntStatus->int_th_low		|=	(VEML6030_als_int_int_th_low_t)( aux_reg & ALS_INT_INT_TH_LOW_MASK );
+
+
+
+	if ( aux == I2C_SUCCESS )
+	{
+		return   VEML6030_SUCCESS;
+	}
+	else
+	{
+		return   VEML6030_FAILURE;
+	}
+}
+
+
+
+/**
+ * @brief       VEML6030_CalculateLuxValue ( I2C_parameters_t , VEML6030_data_t* )
+ *
+ * @details     It calculates the total lux value.
+ *
+ * @param[in]    myI2Cparameters:   I2C parameters.
+ * @param[in]    myLuxValue:   		Raw ALS output data.
+ *
+ * @param[out]   myLuxValue:	   	Lux value, resolution.
+ *
+ *
+ * @return       Status of VEML6030_CalculateLuxValue.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        09/December/2019
+ * @version     17/December/2019   Compensation formula was added.
+ * 				09/December/2019   The ORIGIN
+ * @pre         NOT Tested!.
+ * @warning     N/A.
+ */
+VEML6030_status_t VEML6030_CalculateLuxValue ( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myLuxValue )
+{
+	uint32_t	auxRes	 =	 0U;
+	double		a1 = 0, a2 = 0, a3 = 0, a4 = 0;
+
+
+	/* Initial resolution	 */
+	auxRes	 =	 TYPICAL_RESOLUTION_GAIN_2_IT_800MS;
+
+
+	/* Calculate the resolution regarding the integration time	 */
+	switch ( myLuxValue->als_it )
+	{
+		case ALS_CONF_ALS_IT_25MS:
+			auxRes <<=	 5U;	// auxRes *= 32
+			break;
+
+		case ALS_CONF_ALS_IT_50MS:
+			auxRes <<=	 4U;	// auxRes *= 16
+			break;
+
+		case ALS_CONF_ALS_IT_100MS:
+			auxRes <<=	 3U;	// auxRes *= 8
+			break;
+
+		case ALS_CONF_ALS_IT_200MS:
+			auxRes <<=	 2U;	// auxRes *= 4
+			break;
+
+		case ALS_CONF_ALS_IT_400MS:
+			auxRes <<=	 1U;	// auxRes *= 2
+			break;
+
+		default:
+		case ALS_CONF_ALS_IT_800MS:
+			break;
+	}
+
+	/* Calculate the resolution regarding the gain	 */
+	switch ( myLuxValue->als_gain )
+	{
+		case ALS_CONF_ALS_GAIN_X1:
+			auxRes <<=	 1U;	// auxRes *= 2
+			break;
+
+		default:
+		case ALS_CONF_ALS_GAIN_X2:
+			break;
+
+		case ALS_CONF_ALS_GAIN_X1_4:
+			auxRes <<=	 3U;	// auxRes *= 8
+			break;
+
+		case ALS_CONF_ALS_GAIN_X1_8:
+			auxRes <<=	 4U;	// auxRes *= 16
+			break;
+	}
+
+
+	/* Calculate the resolution	 */
+	myLuxValue->resolution	 =	 (float)( auxRes / 10000.0 );
+
+	/* Calculate the light level	 */
+	myLuxValue->light_level	 =	 (float)( ( myLuxValue->als_high_resolution_output_data * auxRes ) / 10000.0 );
+
+	/* Check if a correction formula needs to be applied	 */
+	if ( myLuxValue->light_level > 1000 )
+	{
+		a1	 =	 0.00000000000060135*pow((uint32_t)myLuxValue->light_level, 4);
+		a2	 =	 0.0000000093924*pow((uint32_t)myLuxValue->light_level, 3);
+		a3	 =	 0.000081488*pow((uint32_t)myLuxValue->light_level, 2);
+		a4	 =	 1.0023*myLuxValue->light_level;
+
+		myLuxValue->light_level	 =	(float)( a1 - a2 + a3 + a4 );
+	}
+
+	return   VEML6030_SUCCESS;
+}
+
+
+
---
+++ Drivers/VEML6035/VEML6035.h
@@ -0,0 +1,272 @@
+/**
+ * @brief       VEML6030.h
+ * @details     High Accuracy Ambient Light Sensor With I2C Interface.
+ *              Header file.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        29/November/2019
+ * @version     29/November/2019    The ORIGIN
+ * @pre         N/A.
+ * @warning     NOT Tested!
+ * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ */
+
+
+#include "stdint.h"
+#include "stdbool.h"
+#include "math.h"
+#include "i2c.h"
+
+
+/**
+  * @brief   DEFAULT ADDRESSES
+  */
+typedef enum
+{
+	VEML6030_ADDRESS_GND  =   0b0010000,     /*!<   ADDR = GND                   	*/
+	VEML6030_ADDRESS_VDD  =   0b1001000      /*!<   ADDR = VDD                   	*/
+} VEML6030_addresses_t;
+
+
+
+/**
+  * @brief   COMMAND REGISTER FORMAT
+  */
+typedef enum
+{
+  VEML6030_ALS_CONF     		=   0x00,	/*!<  Configuration register            */
+  VEML6030_ALS_WH			    =   0x01,   /*!<  High Threshold Windows Setting	*/
+  VEML6030_ALS_WL		      	=   0x02,   /*!<  Low Threshold Windows Setting		*/
+  VEML6030_POWER_SAVING		    =   0x03,   /*!<  Power Saving Mode: PSM		    */
+  VEML6030_ALS			    	=   0x04,   /*!<  ALS High Resolution Output Data   */
+  VEML6030_WHITE				=   0x05,   /*!<  White Channel Output Data			*/
+  VEML6030_ALS_INT			 	=   0x06    /*!<  Interrupt status				    */
+} VEML6030_command_register_format_t;
+
+
+
+/**
+  * @brief   CONFIGURATION REGISTER
+ */
+/* ALS_GAIN <12:11>
+ *    NOTE: Gain selection.
+ */
+typedef enum
+{
+	ALS_CONF_ALS_GAIN_MASK					=   ( 0b11 << 11U ),	/*!<  ALS_GAIN mask       	    			    */
+	ALS_CONF_ALS_GAIN_X1					=   ( 0b00 << 11U ),	/*!<  ALS gain x 1								*/
+	ALS_CONF_ALS_GAIN_X2					=   ( 0b01 << 11U ), 	/*!<  ALS gain x 2					 			*/
+	ALS_CONF_ALS_GAIN_X1_8					=   ( 0b10 << 11U ), 	/*!<  ALS gain x 1/8				 			*/
+	ALS_CONF_ALS_GAIN_X1_4					=   ( 0b11 << 11U ) 	/*!<  ALS gain x 1/4				 			*/
+} VEML6030_als_conf_als_gain_t;
+
+
+/* ALS_IT <9:6>
+ *    NOTE: ALS integration time setting.
+ */
+typedef enum
+{
+	ALS_CONF_ALS_IT_MASK					=   ( 0b1111 << 6U ),	/*!<  ALS_IT mask       	    			    */
+	ALS_CONF_ALS_IT_25MS					=   ( 0b1100 << 6U ),	/*!<   25 ms	 								*/
+	ALS_CONF_ALS_IT_50MS					=   ( 0b1000 << 6U ), 	/*!<   50 ms						 			*/
+	ALS_CONF_ALS_IT_100MS					=   ( 0b0000 << 6U ), 	/*!<  100 ms						 			*/
+	ALS_CONF_ALS_IT_200MS					=   ( 0b0001 << 6U ), 	/*!<  200 ms						 			*/
+	ALS_CONF_ALS_IT_400MS					=   ( 0b0010 << 6U ), 	/*!<  400 ms						 			*/
+	ALS_CONF_ALS_IT_800MS					=   ( 0b0011 << 6U )  	/*!<  800 ms						 			*/
+} VEML6030_als_conf_als_it_t;
+
+
+/* ALS_PERS <5:4>
+ *    NOTE: ALS persistence protect number setting.
+ */
+typedef enum
+{
+	ALS_CONF_ALS_PERS_MASK					=   ( 0b11 << 4U ),		/*!<  ALS_PERS mask       	    			    */
+	ALS_CONF_ALS_PERS_1	  					=   ( 0b00 << 4U ),		/*!<  ALS 1										*/
+	ALS_CONF_ALS_PERS_2						=   ( 0b01 << 4U ), 	/*!<  ALS 2					 					*/
+	ALS_CONF_ALS_PERS_4						=   ( 0b10 << 4U ), 	/*!<  ALS 4							 			*/
+	ALS_CONF_ALS_PERS_8						=   ( 0b11 << 4U ) 		/*!<  ALS 8							 			*/
+} VEML6030_als_conf_als_pers_t;
+
+
+/* ALS_INT_EN <1>
+ *    NOTE: ALS interrupt enable setting.
+ */
+typedef enum
+{
+	ALS_CONF_ALS_INT_EN_MASK				=   ( 1U << 1U ),		/*!<  ALS_INT_EN mask      	    			    */
+	ALS_CONF_ALS_INT_EN_DISABLE  			=   ( 0U << 1U ),		/*!<  ALS INT disable							*/
+	ALS_CONF_ALS_INT_EN_ENABLE				=   ( 1U << 1U )  		/*!<  ALS INT enable		 					*/
+} VEML6030_als_conf_als_int_en_t;
+
+
+/* ALS_SD <0>
+ *    NOTE: ALS shut down setting.
+ */
+typedef enum
+{
+	ALS_CONF_ALS_SD_MASK					=   ( 1U << 0U ),		/*!<  ALS_SD mask      		    			    */
+	ALS_CONF_ALS_SD_POWER_ON  				=   ( 0U << 0U ),		/*!<  ALS power on								*/
+	ALS_CONF_ALS_SD_SHUTDOWN				=   ( 1U << 0U )  		/*!<  ALS shut down			 					*/
+} VEML6030_als_conf_als_sd_t;
+
+
+
+/**
+  * @brief   POWER SAVING MODES
+ */
+/* PSM <2:1>
+ *    NOTE: Power saving mode; see table "Refresh time".
+ */
+typedef enum
+{
+	POWER_SAVING_PSM_MASK					=   ( 0b11 << 1U ),		/*!<  PSM mask  	     	    			    */
+	POWER_SAVING_PSM_MODE_1					=   ( 0b00 << 1U ),		/*!<  mode 1									*/
+	POWER_SAVING_PSM_MODE_2					=   ( 0b01 << 1U ), 	/*!<  mode 2					 				*/
+	POWER_SAVING_PSM_MODE_3					=   ( 0b10 << 1U ), 	/*!<  mode 3				 					*/
+	POWER_SAVING_PSM_MODE_4					=   ( 0b11 << 1U ) 		/*!<  mode 4						 			*/
+} VEML6030_power_saving_psm_t;
+
+
+/* PSM_EN <0>
+ *    NOTE: Power saving mode; see table "Refresh time".
+ */
+typedef enum
+{
+	POWER_SAVING_PSM_EN_MASK				=   ( 1U << 0U ),		/*!<  PSM_EN mask  	     	    			    */
+	POWER_SAVING_PSM_EN_DISABLE				=   ( 0U << 0U ),		/*!<  Disable									*/
+	POWER_SAVING_PSM_EN_ENABLE				=   ( 1U << 0U )  		/*!<  Enable					 				*/
+} VEML6030_power_saving_psm_en_t;
+
+
+
+/**
+  * @brief   INTERRUPT STATUS
+ */
+/* INT_TH_LOW <15>
+ *    NOTE: Read bit. Indicated a low threshold exceed.
+ */
+typedef enum
+{
+	ALS_INT_INT_TH_LOW_MASK					=   ( 1U << 15U ),		/*!<  INT_TH_LOW mask  	   	    			    */
+	ALS_INT_INT_TH_LOW_NO_EXCEEDED			=   ( 0U << 15U ),		/*!<  A low threshold no exceed					*/
+	ALS_INT_INT_TH_LOW_EXCEEDED				=   ( 1U << 15U ) 		/*!<  Indicated a low threshold exceed 			*/
+} VEML6030_als_int_int_th_low_t;
+
+
+/* INT_TH_HIGH <14>
+ *    NOTE: Read bit. Indicated a high threshold exceed.
+ */
+typedef enum
+{
+	ALS_INT_INT_TH_HIGH_MASK				=   ( 1U << 14U ),		/*!<  INT_TH_HIGH mask  		   			    */
+	ALS_INT_INT_TH_HIGH_NO_EXCEEDED			=   ( 0U << 14U ),		/*!<  A high threshold no exceed				*/
+	ALS_INT_INT_TH_HIGH_EXCEEDED			=   ( 1U << 14U ) 		/*!<  Indicated a high threshold exceed 		*/
+} VEML6030_als_int_int_th_high_t;
+
+
+
+/**
+  * @brief   APPLICATION
+ */
+#define TYPICAL_RESOLUTION_GAIN_2_IT_800MS	(uint32_t)36			/*!<  Typical resolution: Gain 2 | IT 800ms ( 0.0036 * 10000 = 36    */
+//#define TYPICAL_RESOLUTION_GAIN_2_IT_800MS	(float)0.0036			/*!<  Typical resolution: Gain 2 | IT 800ms    */
+
+
+
+
+
+
+#ifndef VEML6030_VECTOR_STRUCT_H
+#define VEML6030_VECTOR_STRUCT_H
+typedef struct
+{
+	/* Raw ALS high resolution output data	 */
+	uint16_t als_high_resolution_output_data;				/*!< Raw ALS high resolution output data					*/
+
+	/* Raw WHITE output data	 */
+	uint16_t white_output_data;								/*!< Raw WHITE output data									*/
+
+	/* Raw threshold windows setting	 */
+	uint16_t high_threshold_windows_setting;				/*!< Raw ALS high threshold window setting					*/
+	uint16_t low_threshold_windows_setting;					/*!< Raw ALS low threshold window setting					*/
+
+	/* Configuration register	 */
+	VEML6030_als_conf_als_gain_t	als_gain;				/*!< Gain selection											*/
+	VEML6030_als_conf_als_it_t		als_it;					/*!< ALS integration time setting							*/
+	VEML6030_als_conf_als_pers_t	als_pers;				/*!< ALS persistence protect number setting					*/
+	VEML6030_als_conf_als_int_en_t	als_int_en;				/*!< ALS interrupt enable setting							*/
+	VEML6030_als_conf_als_sd_t		als_sd;					/*!< ALS shut down setting									*/
+
+	/* Power saving modes	 */
+	VEML6030_power_saving_psm_t		psm;					/*!< Power saving mode										*/
+	VEML6030_power_saving_psm_en_t	psm_en;					/*!< Power saving mode enable setting						*/
+
+	/* Interrupt status	 */
+	VEML6030_als_int_int_th_low_t	int_th_low;				/*!< Indicated if a low threshold exceed					*/
+	VEML6030_als_int_int_th_high_t	int_th_high;			/*!< Indicated if a high threshold exceed					*/
+
+	/* Application information	 */
+	float	light_level;									/*!< Light level [lux]										*/
+	float	resolution;										/*!< Resolution regarding the integration time and the gain	*/
+} VEML6030_data_t;
+#endif
+
+
+
+
+/**
+  * @brief   INTERNAL CONSTANTS
+  */
+typedef enum
+{
+    VEML6030_SUCCESS     =       0,
+    VEML6030_FAILURE     =       1
+} VEML6030_status_t;
+
+
+
+
+/**
+  * @brief   FUNCTION PROTOTYPES
+  */
+/** It configures the I2C peripheral.
+  */
+VEML6030_status_t VEML6030_Init               		( I2C_parameters_t myI2Cparameters                                		);
+
+/** It sets the configuration register.
+  */
+VEML6030_status_t VEML6030_SetConfiguration			( I2C_parameters_t myI2Cparameters, VEML6030_data_t myALS_Conf			);
+
+/** It sets the high threshold windows value.
+  */
+VEML6030_status_t VEML6030_SetHighThreshold			( I2C_parameters_t myI2Cparameters, VEML6030_data_t myALS_WH			);
+
+/** It sets the low threshold windows value.
+  */
+VEML6030_status_t VEML6030_SetLowThreshold			( I2C_parameters_t myI2Cparameters, VEML6030_data_t myALS_WL			);
+
+/** It sets the power saving modes.
+  */
+VEML6030_status_t VEML6030_SetPowerSavingModes		( I2C_parameters_t myI2Cparameters, VEML6030_data_t myPSM				);
+
+/** It gets the ALS high resolution output data.
+  */
+VEML6030_status_t VEML6030_GetALS_OuputData			( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myALS				);
+
+/** It gets the WHITE output data.
+  */
+VEML6030_status_t VEML6030_GetWhiteChannelOuputData	( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myWhite			);
+
+/** It gets the Interrupt status value.
+  */
+VEML6030_status_t VEML6030_GetInterruptStatus		( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myIntStatus		);
+
+/** It calculates the total lux value.
+  */
+VEML6030_status_t VEML6030_CalculateLuxValue		( I2C_parameters_t myI2Cparameters, VEML6030_data_t* myLuxValue			);
+
+
