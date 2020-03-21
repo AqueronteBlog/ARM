c8f751493b6ba6904beb4b6f53a95e7d2ce0bd6c
Manuel Caballero <aqueronteblog@gmail.com>
2020-03-21 21:06:57 +0000

    ADuCM3029 VEML6035 Example. Last compilation without any errors.

--- ADuCM3029/Examples_Drivers/VEML6035/src/main.c
+++ ADuCM3029/Examples_Drivers/VEML6035/src/main.c
@@ -1,6 +1,6 @@
 /**
  * @brief       main.c
- * @details     This example shows how to work with the external device: VEML6030.
+ * @details     This example shows how to work with the external device: VEML6035.
  * 				A new sample is read every 1 second and transmitted through
  * 				the UART (115200 baud).
  *
@@ -42,9 +42,9 @@ volatile uint8_t   *myPtr;             	/*!<   Pointer to point out myMessage   
 int main(int argc, char *argv[])
 {
 	uint8_t  			myMessage[ TX_BUFF_SIZE ];
-	/*I2C_parameters_t    myVEML6030_I2C_parameters;
-	VEML6030_data_t 	myVEML6030_Data;
-	VEML6030_status_t  	aux;*/
+	I2C_parameters_t    myVEML6035_I2C_parameters;
+	VEML6035_data_t 	myVEML6035_Data;
+	VEML6035_status_t  	aux;
 
 	/**
 	 * Initialize managed drivers and/or services that have been added to 
@@ -63,30 +63,30 @@ int main(int argc, char *argv[])
 
 
 	/* I2C definition   */
-//	myVEML6030_I2C_parameters.i2cInstance 	 =    pADI_I2C0;
-//	myVEML6030_I2C_parameters.sda         	 =    I2C0_SDA;
-//	myVEML6030_I2C_parameters.scl         	 =    I2C0_SCL;
-//	myVEML6030_I2C_parameters.addr        	 =    VEML6030_ADDRESS_GND;
-//	myVEML6030_I2C_parameters.freq        	 =    100000;
-//	myVEML6030_I2C_parameters.pclkFrequency =	  6400000;
-//	myVEML6030_I2C_parameters.sdaPort     	 =    pADI_GPIO0;
-//	myVEML6030_I2C_parameters.sclPort     	 =    pADI_GPIO0;
-//
-//	/* Configure I2C peripheral */
-//	aux  =   VEML6030_Init ( myVEML6030_I2C_parameters );
-//
+	myVEML6035_I2C_parameters.i2cInstance 	 =    pADI_I2C0;
+	myVEML6035_I2C_parameters.sda         	 =    I2C0_SDA;
+	myVEML6035_I2C_parameters.scl         	 =    I2C0_SCL;
+	myVEML6035_I2C_parameters.addr        	 =    VEML6035_ADDRESS;
+	myVEML6035_I2C_parameters.freq        	 =    100000;
+	myVEML6035_I2C_parameters.pclkFrequency =	  6400000;
+	myVEML6035_I2C_parameters.sdaPort     	 =    pADI_GPIO0;
+	myVEML6035_I2C_parameters.sclPort     	 =    pADI_GPIO0;
+
+	/* Configure I2C peripheral */
+	aux  =   VEML6035_Init ( myVEML6035_I2C_parameters );
+
 //	/* Power on the device	 */
-//	myVEML6030_Data.als_sd	 =	 ALS_CONF_ALS_SD_POWER_ON;
-//	aux	 =	 VEML6030_SetConfiguration ( myVEML6030_I2C_parameters, myVEML6030_Data );
+//	myVEML6035_Data.als_sd	 =	 ALS_CONF_ALS_SD_POWER_ON;
+//	aux	 =	 VEML6035_SetConfiguration ( myVEML6035_I2C_parameters, myVEML6035_Data );
 //
 //	/* Wait until the device is stable ( wait >= 2.5ms )	 */
 //	for ( uint32_t i = 0UL; i < 0x2323; i++);
 //
 //	/* Configure the device: Integration time 100ms | Gain x1 | Interrupt disabled	 */
-//	myVEML6030_Data.als_it	 	 =	 ALS_CONF_ALS_IT_100MS;
-//	myVEML6030_Data.als_gain 	 =	 ALS_CONF_ALS_GAIN_X1;
-//	myVEML6030_Data.als_int_en	 =	 ALS_CONF_ALS_INT_EN_DISABLE;
-//	aux	 =	 VEML6030_SetConfiguration ( myVEML6030_I2C_parameters, myVEML6030_Data );
+//	myVEML6035_Data.als_it	 	 =	 ALS_CONF_ALS_IT_100MS;
+//	myVEML6035_Data.als_gain 	 =	 ALS_CONF_ALS_GAIN_X1;
+//	myVEML6035_Data.als_int_en	 =	 ALS_CONF_ALS_INT_EN_DISABLE;
+//	aux	 =	 VEML6035_SetConfiguration ( myVEML6035_I2C_parameters, myVEML6035_Data );
 
 
 	/* Enable Timer0	 */
@@ -119,17 +119,17 @@ int main(int argc, char *argv[])
 			pADI_GPIO2->SET	|=	 DS3;
 
 			/* Get the raw light data	 */
-//			aux  	 =   VEML6030_GetALS_OuputData ( myVEML6030_I2C_parameters, &myVEML6030_Data );
+//			aux  	 =   VEML6035_GetALS_OuputData ( myVEML6035_I2C_parameters, &myVEML6035_Data );
 //
 //			/* Calculate light data and resolution	 */
-//			aux  	 =   VEML6030_CalculateLuxValue ( myVEML6030_I2C_parameters, &myVEML6030_Data );
+//			aux  	 =   VEML6035_CalculateLuxValue ( myVEML6035_I2C_parameters, &myVEML6035_Data );
 //
 //			/* Power on the device: New sample	 */
-//			myVEML6030_Data.als_sd	 =	 ALS_CONF_ALS_SD_POWER_ON;
-//			aux	 =	 VEML6030_SetConfiguration ( myVEML6030_I2C_parameters, myVEML6030_Data );
+//			myVEML6035_Data.als_sd	 =	 ALS_CONF_ALS_SD_POWER_ON;
+//			aux	 =	 VEML6035_SetConfiguration ( myVEML6035_I2C_parameters, myVEML6035_Data );
 //
 //			/* Transmit data through the UART	 */
-//			sprintf ( (char*)myMessage, "Raw Lux: %d | Lux: %0.4f lx\r\n", myVEML6030_Data.als_high_resolution_output_data, myVEML6030_Data.light_level );
+//			sprintf ( (char*)myMessage, "Raw Lux: %d | Lux: %0.4f lx\r\n", myVEML6035_Data.als_high_resolution_output_data, myVEML6035_Data.light_level );
 
 			/* Check that is safe to send data	 */
 			while( ( pADI_UART0->LSR & ( ( 1U << BITP_UART_LSR_THRE ) | ( 1U << BITP_UART_LSR_TEMT ) ) ) == ~( ( 1U << BITP_UART_LSR_THRE ) | ( 1U << BITP_UART_LSR_TEMT ) ) );
