a5e960f432fce62eba297c1f00a468173b3444f0
Manuel Caballero <aqueronteblog@gmail.com>
2020-04-01 17:51:06 +0100

    Drivers AS7263. HW version register was defined.

--- Drivers/AS7263/AS7263.h
+++ Drivers/AS7263/AS7263.h
@@ -60,200 +60,38 @@ typedef enum
   AS7263_U_CAL			 	=   0x20,   	/*!<  Channel U Calibrated Data			*/
   AS7263_V_CAL			 	=   0x24,   	/*!<  Channel V Calibrated Data			*/
   AS7263_W_CAL			 	=   0x28    	/*!<  Channel W Calibrated Data			*/
-} AS7263_command_register_format_t;
+} AS7263_command_register_set_t;
 
 
 
 /**
-  * @brief   CONFIGURATION REGISTER
+  * @brief   HARDWARE VERSION
  */
-/* SENS <12>
- *    NOTE: Sensitivity.
- */
-typedef enum
-{
-	ALS_CONF_SENS_MASK					=   ( 1UL << 12U ),			/*!<  SENS mask       	    			    	*/
-	ALS_CONF_SENS_HIGH_SENSITIVITY		=   ( 0UL << 12U ),			/*!<  High sensitivity (1 x)					*/
-	ALS_CONF_SENS_LOW_SENSITIVITY		=   ( 1UL << 12U ) 			/*!<  Low sensitivity (1/8 x)			 		*/
-} AS7263_als_conf_sens_t;
-
-
-/* DG <11>
- *    NOTE: Digital gain.
+/* DEVICE_TYPE <7:0>
+ *    NOTE: Device type number.
  */
 typedef enum
 {
-	ALS_CONF_DG_MASK					=   ( 1UL << 11U ),			/*!<  DG mask       	    			    	*/
-	ALS_CONF_DG_NORMAL					=   ( 0UL << 11U ),			/*!<  Normal									*/
-	ALS_CONF_DG_DOUBLE					=   ( 1UL << 11U ) 			/*!<  Double							 		*/
-} AS7263_als_conf_dg_t;
+	HW_VERSION_DEVICE_TYPE			=   0b0100000					/*!<  Device type number    			    	*/
+} AS7263_hw_version_device_type;
 
 
-/* GAIN <10>
- *    NOTE: Gain.
+/* HW_VERSION <7:0>
+ *    NOTE: Hardware version.
  */
 typedef enum
 {
-	ALS_CONF_GAIN_MASK					=   ( 1UL << 10U ),			/*!<  GAIN mask       	    			    	*/
-	ALS_CONF_GAIN_NORMAL_SENSITIVITY	=   ( 0UL << 10U ),			/*!<  Normal sensitivity						*/
-	ALS_CONF_GAIN_DOUBLE_SENSITIVITY	=   ( 1UL << 10U ) 			/*!<  Double sensitivity				 		*/
-} AS7263_als_conf_gain_t;
-
-
-/* ALS_IT <9:6>
- *    NOTE: ALS integration time setting which represents how long ALS can update the readout value.
- */
-typedef enum
-{
-	ALS_CONF_ALS_IT_MASK					=   ( 0b1111 << 6U ),	/*!<  ALS_IT mask       	    			    */
-	ALS_CONF_ALS_IT_25MS					=   ( 0b1100 << 6U ),	/*!<   25 ms	 								*/
-	ALS_CONF_ALS_IT_50MS					=   ( 0b1000 << 6U ), 	/*!<   50 ms						 			*/
-	ALS_CONF_ALS_IT_100MS					=   ( 0b0000 << 6U ), 	/*!<  100 ms						 			*/
-	ALS_CONF_ALS_IT_200MS					=   ( 0b0001 << 6U ), 	/*!<  200 ms						 			*/
-	ALS_CONF_ALS_IT_400MS					=   ( 0b0010 << 6U ), 	/*!<  400 ms						 			*/
-	ALS_CONF_ALS_IT_800MS					=   ( 0b0011 << 6U )  	/*!<  800 ms						 			*/
-} AS7263_als_conf_als_it_t;
-
-
-/* ALS_PERS <5:4>
- *    NOTE: ALS interrupt persistence setting. The interrupt pin is triggered while sensor reading is out of threshold windows
- *    		after consecutive number of measurement cycle.
- */
-typedef enum
-{
-	ALS_CONF_ALS_PERS_MASK					=   ( 0b11 << 4U ),		/*!<  ALS_PERS mask       	    			    */
-	ALS_CONF_ALS_PERS_1	  					=   ( 0b00 << 4U ),		/*!<  ALS 1										*/
-	ALS_CONF_ALS_PERS_2						=   ( 0b01 << 4U ), 	/*!<  ALS 2					 					*/
-	ALS_CONF_ALS_PERS_4						=   ( 0b10 << 4U ), 	/*!<  ALS 4							 			*/
-	ALS_CONF_ALS_PERS_8						=   ( 0b11 << 4U ) 		/*!<  ALS 8							 			*/
-} AS7263_als_conf_als_pers_t;
-
-
-/* INT_CHANNEL <3>
- *    NOTE: Selection for which channel the interrupt should trigger.
- */
-typedef enum
-{
-	ALS_CONF_ALS_INT_CHANNEL_MASK			=   ( 1U << 3U ),		/*!<  ALS_INT_EN mask      	    			    */
-	ALS_CONF_ALS_INT_CHANNEL_ALS_CHANNEL	=   ( 0U << 3U ),		/*!<  ALS CH interrupt							*/
-	ALS_CONF_ALS_INT_CHANNEL_WHITE_CHANNEL	=   ( 1U << 3U )  		/*!<  WHITE CH interrupt	 					*/
-} AS7263_als_conf_als_int_channel_t;
-
-
-/* CHANNEL_EN <2>
- *    NOTE: Channel enable function.
- */
-typedef enum
-{
-	ALS_CONF_ALS_CHANNEL_EN_MASK			=   ( 1U << 2U ),		/*!<  CHANNEL_EN mask  		    			    */
-	ALS_CONF_ALS_CHANNEL_EN_ALS_CH_ONLY		=   ( 0U << 2U ),		/*!<  ALS CH enable only						*/
-	ALS_CONF_ALS_CHANNEL_EN_ALS_WHITE_CH	=   ( 1U << 2U )  		/*!<  ALS and WHITE CH enable					*/
-} AS7263_als_conf_als_channel_en_t;
-
-
-/* INT_EN <1>
- *    NOTE: Interrupt enable setting.
- */
-typedef enum
-{
-	ALS_CONF_ALS_INT_EN_MASK				=   ( 1U << 1U ),		/*!<  INT_EN mask  			    			    */
-	ALS_CONF_ALS_INT_EN_INT_DISABLE			=   ( 0U << 1U ),		/*!<  INT disable								*/
-	ALS_CONF_ALS_INT_EN_INT_ENABLE			=   ( 1U << 1U )  		/*!<  INT enable								*/
-} AS7263_als_conf_als_int_en_t;
-
-
-/* SD <0>
- *    NOTE: Shut down setting.
- */
-typedef enum
-{
-	ALS_CONF_ALS_SD_MASK					=   ( 1U << 0U ),		/*!<  SD mask  				    			    */
-	ALS_CONF_ALS_SD_POWER_ON				=   ( 0U << 0U ),		/*!<  Power on									*/
-	ALS_CONF_ALS_SD_SHUTDOWN				=   ( 1U << 0U )  		/*!<  Shutdown									*/
-} AS7263_als_conf_als_sd_t;
+	HW_VERSION_HW_VERSION			=   0b00111111					/*!<  Hardware version	    			    	*/
+} AS7263_hw_version_hw_version;
 
 
 
-/**
-  * @brief   POWER SAVING MODES
- */
-/* PSM_WAIT <2:1>
- *    NOTE: Power saving mode; see table "Refresh time".
- */
-typedef enum
-{
-	POWER_SAVING_PSM_WAIT_MASK				=   ( 0b11 << 1U ),		/*!<  PSM_WAIT mask       	    			    */
-	POWER_SAVING_PSM_WAIT_0_4_S				=   ( 0b00 << 1U ),		/*!<  0.4 s										*/
-	POWER_SAVING_PSM_WAIT_0_8_S				=   ( 0b01 << 1U ), 	/*!<  0.8 s						 				*/
-	POWER_SAVING_PSM_WAIT_1_6_S				=   ( 0b10 << 1U ), 	/*!<  1.6 s					 					*/
-	POWER_SAVING_PSM_WAIT_3_2_S				=   ( 0b11 << 1U ) 		/*!<  3.2 s							 			*/
-} AS7263_power_saving_psm_wait_t;
-
-
-/* PSM_EN <0>
- *    NOTE: Power saving mode; see table "Refresh time".
- */
-typedef enum
-{
-	POWER_SAVING_PSM_EN_MASK				=   ( 1U << 0U ),		/*!<  PSM_EN mask  	     	    			    */
-	POWER_SAVING_PSM_EN_DISABLE				=   ( 0U << 0U ),		/*!<  Disable									*/
-	POWER_SAVING_PSM_EN_ENABLE				=   ( 1U << 0U )  		/*!<  Enable					 				*/
-} AS7263_power_saving_psm_en_t;
-
-
-
-/**
-  * @brief   INTERRUPT STATUS
- */
-/* INT_TH_LOW <15>
- *    NOTE: Read bit. Low threshold interrupt flag.
- */
-typedef enum
-{
-	ALS_INT_INT_TH_LOW_MASK					=   ( 1U << 15U ),		/*!<  INT_TH_LOW mask  	   	    			    */
-	ALS_INT_INT_TH_LOW_INT_NOT_TRIGGERED	=   ( 0U << 15U ),		/*!<  Interrupt not triggered					*/
-	ALS_INT_INT_TH_LOW_INT_OCCURRED			=   ( 1U << 15U ) 		/*!<  Interrupt occurred			 			*/
-} AS7263_als_int_int_th_low_t;
-
-
-/* INT_TH_HIGH <14>
- *    NOTE: Read bit. High threshold interrupt flag.
- */
-typedef enum
-{
-	ALS_INT_INT_TH_HIGH_MASK				=   ( 1U << 14U ),		/*!<  INT_TH_HIGH mask  		   			    */
-	ALS_INT_INT_TH_HIGH_INT_NOT_TRIGGERED	=   ( 0U << 14U ),		/*!<  Interrupt not triggered					*/
-	ALS_INT_INT_TH_HIGH_INT_OCCURRED		=   ( 1U << 14U ) 		/*!<  Interrupt occurred			 			*/
-} AS7263_als_int_int_th_high_t;
-
-
-
-/**
-  * @brief   APPLICATION
- */
-#define AS7263_MAXIMUM_RESOLUTION	(float)0.0004					/*!<  Maximum resolution    							*/
 
 
 
 
 #ifndef AS7263_VECTOR_STRUCT_H
 #define AS7263_VECTOR_STRUCT_H
-/* Configuration register	 */
-typedef struct
-{
-	AS7263_als_conf_sens_t			als_sens;			/*!< ALS Sensitivity selection										*/
-	AS7263_als_conf_dg_t				als_dg;				/*!< ALS digital gain												*/
-	AS7263_als_conf_gain_t			als_gain;			/*!< ALS gain														*/
-	AS7263_als_conf_als_it_t			als_it;				/*!< ALS integration time											*/
-	AS7263_als_conf_als_pers_t		als_pers;			/*!< ALS interrupt persistence setting								*/
-	AS7263_als_conf_als_int_channel_t	als_int_channel;	/*!< ALS selection for which channel the interrupt should trigger	*/
-	AS7263_als_conf_als_channel_en_t	als_channel_en;		/*!< ALS channel enable function									*/
-	AS7263_als_conf_als_int_en_t		als_int_en;			/*!< ALS interrupt enable setting									*/
-	AS7263_als_conf_als_sd_t			als_sd;				/*!< ALS Shut down setting											*/
-} AS7263_configuration_register_t;
-
-
-
 typedef struct
 {
 	/* Raw ALS high resolution output data	 */
@@ -265,21 +103,6 @@ typedef struct
 	/* Raw threshold windows setting	 */
 	uint16_t high_threshold_windows_setting;				/*!< Raw ALS high threshold window setting							*/
 	uint16_t low_threshold_windows_setting;					/*!< Raw ALS low threshold window setting							*/
-
-	/* Configuration register	 */
-	AS7263_configuration_register_t	configuration;		/*!< Configuration parameters										*/
-
-	/* Power saving modes	 */
-	AS7263_power_saving_psm_wait_t	psm_wait;			/*!< Defines the wait time between the measurements					*/
-	AS7263_power_saving_psm_en_t		psm_en;				/*!< Power saving mode enable setting								*/
-
-	/* Interrupt status	 */
-	AS7263_als_int_int_th_low_t		int_th_low;			/*!< Low threshold interrupt flag									*/
-	AS7263_als_int_int_th_high_t		int_th_high;		/*!< High threshold interrupt flag									*/
-
-	/* Application information	 */
-	float	light_level;									/*!< Light level [lux]												*/
-	float	resolution;										/*!< Resolution regarding the integration time and the gain			*/
 } AS7263_data_t;
 #endif
 
@@ -304,14 +127,6 @@ typedef enum
 /** It configures the I2C peripheral.
   */
 AS7263_status_t AS7263_Init               		( I2C_parameters_t myI2Cparameters                              					);
-
-/** It reads the configuration register.
-  */
-AS7263_status_t AS7263_GetConfigurationRegister	( I2C_parameters_t myI2Cparameters, AS7263_configuration_register_t* myConfReg	);
-
-/** It writes the configuration register.
-  */
-AS7263_status_t AS7263_SetConfigurationRegister	( I2C_parameters_t myI2Cparameters, AS7263_configuration_register_t myConfReg		);
 
 /** It sets the sensitivity value.
   */
