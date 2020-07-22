1d7bc557a706b68792b48db0eff2a17bd81198ea
Manuel Caballero <aqueronteblog@gmail.com>
2020-07-22 20:30:40 +0100

    ADuCM3029 SFM4100 Example. Functions files were added.

---
+++ ADuCM3029/Examples_Drivers/SFM4100/inc/functions.h
@@ -0,0 +1,49 @@
+/**
+ * @brief       functions.h
+ * @details     Functions's header.
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        22/July/2020
+ * @version     22/July/2020   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A
+ */
+
+#include "board.h"
+
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/**@brief Function prototypes.
+ */
+void conf_CLK    ( void );
+void conf_GPIO   ( void );
+void conf_UART   ( void );
+void conf_Timer0 ( void );
+
+
+
+/**@brief Constants.
+ */
+
+
+
+/**@brief Variables.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCTION_H_ */
