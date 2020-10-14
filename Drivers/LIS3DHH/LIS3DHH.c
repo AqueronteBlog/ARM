80394a1ee85a791bf8b0bbf1fe8d33d0248430fd
Manuel Caballero <aqueronteblog@gmail.com>
2020-10-14 20:04:11 +0100

    Drivers. LIS3DHH. Functions spi_init and get the device identifier were declared.

--- Drivers/LIS3DHH/LIS3DHH.c
+++ Drivers/LIS3DHH/LIS3DHH.c
@@ -20,7 +20,7 @@
 /**
  * @brief       LIS3DHH_Init   ( spi_parameters_t )
  *
- * @details     It configures the SPI pins and resets all the LIS3DHH register ( default value ) and resets its status.
+ * @details     It configures the SPI pins.
  *
  * @param[in]    mySPI_parameters:  SPI instance, MOSI pin, MISO pin, SCLK pin, CS pin, SPI frequency and the port for each pin.
  *
@@ -31,26 +31,71 @@
  *
  *
  * @author      Manuel Caballero
- * @date        22/February/2018
- * @version     27/February/2018   PRESET_DEFAULT and CLEAR_WAKE functions were added.
- *              22/February/2018   The ORIGIN
+ * @date        14/October/2020
+ * @version     14/October/2020   The ORIGIN
  * @pre         N/A
  * @warning     N/A.
  */
 LIS3DHH_status_t  LIS3DHH_Init ( spi_parameters_t mySPI_parameters )
 {
-    uint8_t             cmd   =    0;
-    spi_status_t        mySPI_status;
+    spi_status_t mySPI_status;
 
 
-    // Configure the SPI peripheral
+    /* Configure the SPI peripheral	 */
     mySPI_status     =   spi_init ( mySPI_parameters );
 
 
 
 
     if ( mySPI_status == SPI_SUCCESS )
+    {
         return   LIS3DHH_SUCCESS;
+    }
     else
+    {
         return   LIS3DHH_FAILURE;
+    }
+}
+
+
+
+/**
+ * @brief       LIS3DHH_GetDeviceIdentification ( spi_parameters_t , LIS3DHH_data_t* )
+ *
+ * @details     It gets the device identification.
+ *
+ * @param[in]    mySPI_parameters:  SPI instance, MOSI pin, MISO pin, SCLK pin, CS pin, SPI frequency and the port for each pin.
+ *
+ * @param[out]   myID:				Device identifier.
+ *
+ *
+ * @return       Status of LIS3DHH_GetDeviceIdentification.
+ *
+ *
+ * @author      Manuel Caballero
+ * @date        14/October/2020
+ * @version     14/October/2020   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
+ */
+LIS3DHH_status_t  LIS3DHH_GetDeviceIdentification ( spi_parameters_t mySPI_parameters, LIS3DHH_data_t* myID )
+{
+	uint8_t      cmd   =    ( LIS3DHH_READ | LIS3DHH_WHO_AM_I );
+	spi_status_t mySPI_status;
+
+
+    /* Read the device identifier	 */
+    mySPI_status	 =   spi_transfer ( mySPI_parameters, &cmd, 1U, (uint8_t*)(&myID->who_am_i), 1U );
+
+
+
+
+    if ( mySPI_status == SPI_SUCCESS )
+    {
+        return   LIS3DHH_SUCCESS;
+    }
+    else
+    {
+        return   LIS3DHH_FAILURE;
+    }
 }
