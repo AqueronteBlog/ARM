33ab97268b92d77ab914e0cb4377fbf22a5b0ee4
Manuel Caballero <aqueronteblog@gmail.com>
2020-11-07 21:22:06 +0000

    ADuCM3029 Basics. SPI driver. SPI transfer function and init function are still in progress.

--- ADuCM3029/Basics/SPI/spi.c
+++ ADuCM3029/Basics/SPI/spi.c
@@ -18,6 +18,7 @@
 
 /**
  * @brief       spi_init   ( spi_parameters_t )
+ *
  * @details     It configures the SPI peripheral.
  *
  * @param[in]    mySPIparameters:       SPI parameters.
@@ -30,10 +31,41 @@
  * @author      Manuel Caballero
  * @date        11/October/2020
  * @version     11/October/2020     The ORIGIN
- * @pre         SPI communication is by polling mode.
+ * @pre         N/A.
  * @warning     N/A.
  */
 spi_status_t spi_init ( spi_parameters_t mySPIparameters )
+{
+
+
+	/* Peripheral configured successfully	 */
+	return SPI_SUCCESS;
+}
+
+
+
+/**
+ * @brief       spi_transfer ( spi_parameters_t , uint8_t* , uint32_t , uint8_t* , uint32_t  )
+ *
+ * @details     Send/Receive data through the SPI bus.
+ *
+ * @param[in]    mySPI_parameters:      SPI instance, MOSI pin, MISO pin, SCLK pin, CS pin, SPI frequency and the port for each pin.
+ * @param[in]    spi_tx_buff:           Data to be transmitted through SPI bus.
+ * @param[in]    spi_tx_length:         Amount of data to be transmitted through SPI bus.
+ * @param[in]    spi_rx_length:         Amount of data to be read through SPI bus.
+ *
+ * @param[out]   spi_rx_buff:           Data from the SPI bus.
+ *
+ *
+ * @return      N/A
+ *
+ * @author      Manuel Caballero
+ * @date        07/October/2020
+ * @version     07/October/2020     The ORIGIN
+ * @pre         SPI communication is by polling mode.
+ * @warning     N/A.
+ */
+spi_status_t spi_transfer ( spi_parameters_t mySPIparameters, uint8_t* spi_tx_buff, uint32_t spi_tx_length, uint8_t* spi_rx_buff, uint32_t spi_rx_length )
 {
 
 
