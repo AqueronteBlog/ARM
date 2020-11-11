116a9abf9908eea4e4a87aa0ef0769d623d4a54f
Manuel Caballero <aqueronteblog@gmail.com>
2020-11-11 20:27:25 +0000

    ADuCM3029 Basics. SPI driver.  New parameters were added to complete the configuration of the SPI. Those parameters will be used by the user.

--- ADuCM3029/Basics/SPI/spi.h
+++ ADuCM3029/Basics/SPI/spi.h
@@ -33,6 +33,46 @@ typedef enum
 
 
 /**
+  * @brief   SPI MODES.
+  *
+  *             NOTES:
+  *                 CPOL: Clock Polarity.
+  *                 CPHA: Clock Phase.
+  */
+typedef enum
+{
+    SPI_MODE_0  =   0x00,           /*!<  SPI MODE 0: CPOL 0 | CPHA 0          */
+    SPI_MODE_1  =   0x01,           /*!<  SPI MODE 1: CPOL 0 | CPHA 1          */
+    SPI_MODE_2  =   0x02,           /*!<  SPI MODE 2: CPOL 1 | CPHA 0          */
+    SPI_MODE_3  =   0x03            /*!<  SPI MODE 3: CPOL 1 | CPHA 1          */
+} spi_modes_t;
+
+
+/**
+  * @brief   SPI ENABLE LINE MODE.
+  *
+  *             NOTES:
+  *                 How the Chip select is active: High or Low
+  */
+typedef enum
+{
+    SPI_ENABLE_LINE_HIGH  =   0x00,         /*!<  Chip Select is Active High                */
+    SPI_ENABLE_LINE_LOW   =   0x01          /*!<  Chip Select is Active Low ( Standard )    */
+} spi_cs_enable_line_t;
+
+
+/**
+  * @brief   SPI BYTE ORDER.
+  *
+  */
+typedef enum
+{
+    SPI_ORDER_MSB_FIRST  =   0x00,          /*!<  MSB is sent first                         */
+    SPI_ORDER_LSB_FIRST  =   0x01           /*!<  LSB is sent first                         */
+} spi_byte_order_t;
+
+
+/**
   * @brief   INTERNAL CONSTANTS
   */
 typedef enum
@@ -63,12 +103,20 @@ typedef struct{
     /* SPI frequency   */
     uint32_t freq;
 
-
     /* SPI instance  */
     ADI_SPI_TypeDef* SPIInstance;
 
     /* Core clock that drives the SPI peripheral	 */
     uint32_t pclkFrequency;
+
+    /* SPI Mode */
+    spi_modes_t SPImode;
+
+    /* SPI Chip Select enable line mode */
+    spi_cs_enable_line_t SPIenable_line_mode;
+
+    /* SPI Byte order */
+    spi_byte_order_t SPIbyte_order;
 } spi_parameters_t;
 
 
