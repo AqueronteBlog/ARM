666881d57ef6135ff3b781ee384792e2f80c92f7
Manuel Caballero <aqueronteblog@gmail.com>
2020-11-14 22:13:06 +0000

    ADuCM3029 Basics. SPI driver. Spi init function is completed, SPI clock configuration was added, this function has the capability to inspect if the SPI frequency is wrong according to the main PCLK clock

--- ADuCM3029/Basics/SPI/spi.c
+++ ADuCM3029/Basics/SPI/spi.c
@@ -30,7 +30,8 @@
  *
  * @author      Manuel Caballero
  * @date        11/October/2020
- * @version     11/November/2020    SPI configuration was completed.
+ * @version     14/November/2020    SPI frequency calculator was added ( errors functionality included ).
+ * 				11/November/2020    SPI configuration was completed.
  * 				08/November/2020    GPIO configuration was added.
  * 				11/October/2020     The ORIGIN
  * @pre         N/A.
@@ -40,6 +41,8 @@
  */
 spi_status_t spi_init ( spi_parameters_t mySPIparameters )
 {
+	uint32_t spi_frequency_div	 =	 0UL;
+	uint32_t spi_frequency_mul	 =	 0UL;
 
 	/* Make sure the SPI is not in use	 */ // [todo] add a counter for sefty
 	do{
@@ -49,76 +52,104 @@ spi_status_t spi_init ( spi_parameters_t mySPIparameters )
 	/* Disable the SPI peripheral	 */
 	mySPIparameters.SPIInstance->CTL	&=	~( 1U << BITP_SPI_CTL_SPIEN );
 
-	/* Multiplexed function 1 for SPI peripheral ( MOSI, MISO, SCLK and CS )	 */
-	mySPIparameters.mosiPort->CFG	&=	~( 0b11 << ( mySPIparameters.mosi << 1U ) );
-	mySPIparameters.mosiPort->CFG	|=	 ( 0b01 << ( mySPIparameters.mosi << 1U ) );
-
-	mySPIparameters.misoPort->CFG	&=	~( 0b11 << ( mySPIparameters.miso << 1U ) );
-	mySPIparameters.misoPort->CFG	|=	 ( 0b01 << ( mySPIparameters.miso << 1U ) );
-
-	mySPIparameters.sclkPort->CFG	&=	~( 0b11 << ( mySPIparameters.sclk << 1U ) );
-	mySPIparameters.sclkPort->CFG	|=	 ( 0b01 << ( mySPIparameters.sclk << 1U ) );
-
-	mySPIparameters.csPort->CFG		&=	~( 0b11 << ( mySPIparameters.cs << 1U ) );
-	mySPIparameters.csPort->CFG		|=	 ( 0b01 << ( mySPIparameters.cs << 1U ) );
-
-	/* SPI: Chip select active mode	 */
-	mySPIparameters.SPIInstance->CS_OVERRIDE	&=	~( 0b11 << BITP_SPI_CS_OVERRIDE_CTL );
-	if ( mySPIparameters.SPIenable_line_mode == SPI_ENABLE_LINE_HIGH )
+	/* SPI: Configure the SPI frequency	 */
+	if ( mySPIparameters.freq > 13000000UL )
 	{
-		/* CS is forced to drive 'LOW'	 */
-		mySPIparameters.SPIInstance->CS_OVERRIDE	|=	 ( 0b10 << BITP_SPI_CS_OVERRIDE_CTL );
+		/* SPI frequency cannot be greater than 13MHz	 */
+		return SPI_ERROR_FREQUENCY_TOO_HIGH;
 	}
-
-	/* SPI: MSB or LSB transmitted first	 */
-	mySPIparameters.SPIInstance->CTL	&=	~( 1U << BITP_SPI_CTL_LSB );
-	if ( mySPIparameters.SPIbyte_order == SPI_ORDER_LSB_FIRST )
+	else
 	{
-		mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_LSB );
+		spi_frequency_mul	 =	 ( mySPIparameters.freq << 2UL );	// SPI_freq * 2
+		if ( spi_frequency_mul > mySPIparameters.pclkFrequency )
+		{
+			return SPI_ERROR_FREQUENCY_TOO_HIGH;
+		}
+		else
+		{
+			spi_frequency_div	 =	 (uint32_t)( ( mySPIparameters.pclkFrequency - spi_frequency_mul )/spi_frequency_mul );
+			if ( spi_frequency_div > 63UL )
+			{
+				return SPI_ERROR_FREQUENCY_TOO_LOW;
+			}
+			else
+			{
+				mySPIparameters.SPIInstance->DIV	&=	~0x003F;
+				mySPIparameters.SPIInstance->DIV	|=	 (uint16_t)( spi_frequency_div << BITP_SPI_DIV_VALUE );
+
+				/* Multiplexed function 1 for SPI peripheral ( MOSI, MISO, SCLK and CS )	 */
+				mySPIparameters.mosiPort->CFG	&=	~( 0b11 << ( mySPIparameters.mosi << 1U ) );
+				mySPIparameters.mosiPort->CFG	|=	 ( 0b01 << ( mySPIparameters.mosi << 1U ) );
+
+				mySPIparameters.misoPort->CFG	&=	~( 0b11 << ( mySPIparameters.miso << 1U ) );
+				mySPIparameters.misoPort->CFG	|=	 ( 0b01 << ( mySPIparameters.miso << 1U ) );
+
+				mySPIparameters.sclkPort->CFG	&=	~( 0b11 << ( mySPIparameters.sclk << 1U ) );
+				mySPIparameters.sclkPort->CFG	|=	 ( 0b01 << ( mySPIparameters.sclk << 1U ) );
+
+				mySPIparameters.csPort->CFG		&=	~( 0b11 << ( mySPIparameters.cs << 1U ) );
+				mySPIparameters.csPort->CFG		|=	 ( 0b01 << ( mySPIparameters.cs << 1U ) );
+
+				/* SPI: Chip select active mode	 */
+				mySPIparameters.SPIInstance->CS_OVERRIDE	&=	~( 0b11 << BITP_SPI_CS_OVERRIDE_CTL );
+				if ( mySPIparameters.SPIenable_line_mode == SPI_ENABLE_LINE_HIGH )
+				{
+					/* CS is forced to drive 'LOW'	 */
+					mySPIparameters.SPIInstance->CS_OVERRIDE	|=	 ( 0b10 << BITP_SPI_CS_OVERRIDE_CTL );
+				}
+
+				/* SPI: MSB or LSB transmitted first	 */
+				mySPIparameters.SPIInstance->CTL	&=	~( 1U << BITP_SPI_CTL_LSB );
+				if ( mySPIparameters.SPIbyte_order == SPI_ORDER_LSB_FIRST )
+				{
+					mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_LSB );
+				}
+
+				/* SPI: Set CS0 is used always	 */
+				mySPIparameters.SPIInstance->CS_CTL	&=	~( 0b1111 << BITP_SPI_CS_CTL_SEL );
+
+				/* SPI: Set working mode according to CPHA and CPOL	 */
+				mySPIparameters.SPIInstance->CTL	&=	~( ( 1U << BITP_SPI_CTL_CPOL ) | ( 1U << BITP_SPI_CTL_CPHA ) );
+				switch ( mySPIparameters.SPImode )
+				{
+				default:
+				case SPI_MODE_0:
+					/* SPI MODE 0: CPOL 0 | CPHA 0	 */
+					break;
+
+				case SPI_MODE_1:
+					/* SPI MODE 0: CPOL 0 | CPHA 1	 */
+					mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_CPHA );
+					break;
+
+				case SPI_MODE_2:
+					/* SPI MODE 0: CPOL 1 | CPHA 0	 */
+					mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_CPOL );
+					break;
+
+				case SPI_MODE_3:
+					/* SPI MODE 0: CPOL 1 | CPHA 1	 */
+					mySPIparameters.SPIInstance->CTL	|=	 ( ( 1U << BITP_SPI_CTL_CPOL ) | ( 1U << BITP_SPI_CTL_CPHA ) );
+					break;
+				}
+
+				/* SPI: 4-pin interface	 */
+				mySPIparameters.SPIInstance->RD_CTL	&=	~( 1U << BITP_SPI_RD_CTL_THREEPIN );
+
+				/* SPI: Master mode enabled	 */
+				mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_MASEN );
+
+				/* SPI: No DMA for transfer data	 */
+				mySPIparameters.SPIInstance->DMA	&=	~( 1U << BITP_SPI_DMA_EN );
+
+				/* SPI: All interrupts are disabled	 */
+				mySPIparameters.SPIInstance->IEN	&=	~( ( 1U << BITP_SPI_IEN_TXEMPTY ) | ( 1U << BITP_SPI_IEN_XFRDONE ) | ( 1U << BITP_SPI_IEN_TXDONE ) | ( 1U << BITP_SPI_IEN_RDY ) | ( 1U << BITP_SPI_IEN_RXOVR ) | ( 1U << BITP_SPI_IEN_TXUNDR ) | ( 1U << BITP_SPI_IEN_CS ) );
+
+				/* SPI: Reset all the STATUS flags	 */
+				mySPIparameters.SPIInstance->STAT	|=	 ( ( 1U << BITP_SPI_STAT_RDY ) | ( 1U << BITP_SPI_STAT_CSFALL ) | ( 1U << BITP_SPI_STAT_CSRISE ) | ( 1U << BITP_SPI_STAT_CSERR ) | ( 1U << BITP_SPI_STAT_RXOVR ) | ( 1U << BITP_SPI_STAT_RXIRQ ) | ( 1U << BITP_SPI_STAT_TXIRQ ) | ( 1U << BITP_SPI_STAT_TXUNDR ) | ( 1U << BITP_SPI_STAT_TXDONE ) | ( 1U << BITP_SPI_STAT_TXEMPTY ) );
+			}
+		}
 	}
-
-	/* SPI: Set CS0 is used always	 */
-	mySPIparameters.SPIInstance->CS_CTL	&=	~( 0b1111 << BITP_SPI_CS_CTL_SEL );
-
-	/* SPI: Set working mode according to CPHA and CPOL	 */
-	mySPIparameters.SPIInstance->CTL	&=	~( ( 1U << BITP_SPI_CTL_CPOL ) | ( 1U << BITP_SPI_CTL_CPHA ) );
-	switch ( mySPIparameters.SPImode )
-	{
-	default:
-	case SPI_MODE_0:
-		/* SPI MODE 0: CPOL 0 | CPHA 0	 */
-		break;
-
-	case SPI_MODE_1:
-		/* SPI MODE 0: CPOL 0 | CPHA 1	 */
-		mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_CPHA );
-		break;
-
-	case SPI_MODE_2:
-		/* SPI MODE 0: CPOL 1 | CPHA 0	 */
-		mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_CPOL );
-		break;
-
-	case SPI_MODE_3:
-		/* SPI MODE 0: CPOL 1 | CPHA 1	 */
-		mySPIparameters.SPIInstance->CTL	|=	 ( ( 1U << BITP_SPI_CTL_CPOL ) | ( 1U << BITP_SPI_CTL_CPHA ) );
-		break;
-	}
-
-	/* SPI: 4-pin interface	 */
-	mySPIparameters.SPIInstance->RD_CTL	&=	~( 1U << BITP_SPI_RD_CTL_THREEPIN );
-
-	/* SPI: Master mode enabled	 */
-	mySPIparameters.SPIInstance->CTL	|=	 ( 1U << BITP_SPI_CTL_MASEN );
-
-	/* SPI: No DMA for transfer data	 */
-	mySPIparameters.SPIInstance->DMA	&=	~( 1U << BITP_SPI_DMA_EN );
-
-	/* SPI: All interrupts are disabled	 */
-	mySPIparameters.SPIInstance->IEN	&=	~( ( 1U << BITP_SPI_IEN_TXEMPTY ) | ( 1U << BITP_SPI_IEN_XFRDONE ) | ( 1U << BITP_SPI_IEN_TXDONE ) | ( 1U << BITP_SPI_IEN_RDY ) | ( 1U << BITP_SPI_IEN_RXOVR ) | ( 1U << BITP_SPI_IEN_TXUNDR ) | ( 1U << BITP_SPI_IEN_CS ) );
-
-	/* SPI: Reset all the STATUS flags	 */
-	mySPIparameters.SPIInstance->STAT	|=	 ( ( 1U << BITP_SPI_STAT_RDY ) | ( 1U << BITP_SPI_STAT_CSFALL ) | ( 1U << BITP_SPI_STAT_CSRISE ) | ( 1U << BITP_SPI_STAT_CSERR ) | ( 1U << BITP_SPI_STAT_RXOVR ) | ( 1U << BITP_SPI_STAT_RXIRQ ) | ( 1U << BITP_SPI_STAT_TXIRQ ) | ( 1U << BITP_SPI_STAT_TXUNDR ) | ( 1U << BITP_SPI_STAT_TXDONE ) | ( 1U << BITP_SPI_STAT_TXEMPTY ) );
 
 
 	/* Peripheral configured successfully	 */
