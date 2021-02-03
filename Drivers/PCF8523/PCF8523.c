2b2649395080e98bc891d418c07c10dcf4e01994
Manuel.Caballero <aqueronteblog@gmail.com>
2021-02-03 12:20:16 +0000

    Drivers. PCF8523. Minor changes, no useful code was removed and some comments were improved.

--- Drivers/PCF8523/PCF8523.c
+++ Drivers/PCF8523/PCF8523.c
@@ -1,17 +1,17 @@
 /**
  * @brief       PCF8523.h
- * @details     Remote 8-bit I/O expander for I2C-bus with interrupt.
+ * @details     Real-Time Clock (RTC) and calendar.
  *              Functions file.
  *
  *
- * @return      NA
+ * @return      N/A
  *
  * @author      Manuel Caballero
- * @date        11/October/2017
- * @version     11/October/2017    The ORIGIN
- * @pre         NaN.
- * @warning     NaN
- * @pre         This code belongs to AqueronteBlog ( http://unbarquero.blogspot.com ).
+ * @date        03/February/2021
+ * @version     03/February/2021    The ORIGIN
+ * @pre         N/A.
+ * @warning     N/A
+ * @pre         This code belongs to Nimbus Centre ( https://www.nimbus.cit.ie ).
  */
 
 #include "PCF8523.h"
@@ -24,106 +24,35 @@
  *
  * @param[in]    myI2Cparameters:       I2C parameters.
  *
- * @param[out]   NaN.
+ * @param[out]   N/A.
  *
  *
  * @return       Status of PCF8523_Init.
  *
  *
  * @author      Manuel Caballero
- * @date        18/October/2017
- * @version     18/October/2017   The ORIGIN
- * @pre         NaN
- * @warning     NaN.
+ * @date        03/February/2021
+ * @version     03/February/2021   The ORIGIN
+ * @pre         N/A
+ * @warning     N/A.
  */
 PCF8523_status_t  PCF8523_Init ( I2C_parameters_t myI2Cparameters )
 {
-    i2c_status_t aux;
+  i2c_status_t aux;
 
-    aux  =   i2c_init ( myI2Cparameters );
+  aux  =   i2c_init ( myI2Cparameters );
 
 
 
-    if ( aux == I2C_SUCCESS )
-        return   PCF8523_SUCCESS;
-    else
-        return   PCF8523_FAILURE;
+  if ( aux == I2C_SUCCESS )
+  {
+    return   PCF8523_SUCCESS;
+  }
+  else
+  {
+    return   PCF8523_FAILURE;
+  }
 }
 
 
 
-/**
- * @brief       PCF8523_SetPins ( I2C_parameters_t , PCF8523_vector_data_t )
- *
- * @details     It configures the port of the device.
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- * @param[in]    myConfDATA:        Data to set up the device.
- *
- * @param[out]   NaN.
- *
- *
- * @return       Status of PCF8523_SetPins.
- *
- *
- * @author      Manuel Caballero
- * @date        11/October/2017
- * @version     18/October/2017     Adapted to the new I2C driver.
- *              11/October/2017     The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-PCF8523_status_t  PCF8523_SetPins   ( I2C_parameters_t myI2Cparameters, PCF8523_vector_data_t  myConfDATA )
-{
-    uint32_t     aux     =    0;
-
-
-    // Configure the ports of the device
-    aux = i2c_write ( myI2Cparameters, &myConfDATA.data, 1, I2C_STOP_BIT );
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   PCF8523_SUCCESS;
-    else
-        return   PCF8523_FAILURE;
-}
-
-
-
-/**
- * @brief       PCF8523_ReadPins ( I2C_parameters_t , PCF8523_vector_data_t*  )
- *
- * @details     It gets the data from the device ( port status ).
- *
- * @param[in]    myI2Cparameters:   I2C parameters.
- *
- * @param[out]   myReadDATA:        ADC result into the chosen channel.
- *
- *
- * @return       Status of PCF8523_ReadPins.
- *
- *
- * @author      Manuel Caballero
- * @date        11/October/2017
- * @version     18/October/2017     Adapted to the new I2C driver.
- *              11/October/2017     The ORIGIN
- * @pre         NaN
- * @warning     NaN.
- */
-PCF8523_status_t  PCF8523_ReadPins ( I2C_parameters_t myI2Cparameters, PCF8523_vector_data_t* myReadDATA )
-{
-    uint32_t     aux  =    0;
-
-
-    // Read the port
-    aux = i2c_read ( myI2Cparameters, &myReadDATA->data, 1 );
-
-
-
-
-    if ( aux == I2C_SUCCESS )
-        return   PCF8523_SUCCESS;
-    else
-        return   PCF8523_FAILURE;
-}
